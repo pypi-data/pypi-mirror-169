6faf6140e93fbea12b85e2d358648e60ef5cda75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:59 +0000

    Add package imbrium/0.1.8/imbrium-0.1.8.tar.gz

---
+++ packages/imbrium/imbrium-0.1.8.tar.gz/imbrium-0.1.8/CHANGELOG.md
@@ -0,0 +1,44 @@
+# Imbrium
+
+## Released
+
+### 0.1.0
+
+- base library
+
+### 0.1.2
+
+- encoder-decoder predictor support for multistep multivariate time series
+
+### 0.1.3
+
+- added model loss variable type
+- architecture layers can now be adjusted by defining number of neurons and
+activation function type
+- improved docstrings
+
+### 0.1.4
+
+- added keras native call-back feature to fit_model method
+- minor docstring changes
+- minor default value issues in fit_model method solved
+
+### 0.1.5
+
+- added getter methods for optimizer and model id
+- added more unit-tests
+- save_model method allows now to specify a customized path
+
+### 0.1.6
+
+- solved python version conflict by only allowing python 3.7.*
+
+### 0.1.7
+
+- improved source code format
+- improved scaling selector mechanism
+
+### 0.1.8
+
+- major refactoring of cod base
+- minor name change from Imbrium to imbrium
---
+++ packages/imbrium/imbrium-0.1.8.tar.gz/imbrium-0.1.8/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+with open('README.md', encoding="utf8") as file:
+    long_description = file.read()
+
+setup(
+    author="Maximilian Mekiska",
+    author_email="maxmekiska@gmail.com",
+    url="https://github.com/maxmekiska/Imbrium",
+    description="Standard and Hybrid Deep Learning Multivariate-Multi-Step & Univariate-Multi-Step Time Series Forecasting.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    name="imbrium",
+    version="0.1.8",
+    packages = find_packages(include=["imbrium", "imbrium.*"]),
+    install_requires=[
+        "tensorflow==2.9.1",
+        "scikit-learn==0.21.3",
+        "matplotlib==3.5.2",
+        "numpy==1.21.6",
+        "pandas==0.25.1",
+    ],
+    classifiers = [
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+    ],
+    keywords = ["machineleaning", "keras", "deeplearning", "timeseries", "forecasting"],
+    python_rquieres="== 3.7.*"
+)
