7c0d62fe8ed5e48bb340e477c08780301630ba9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:25 +0000

    Add package eon-logger/0.0.8/eon_logger-0.0.8-py3-none-any.whl

---
+++ packages/eon-logger/eon_logger-0.0.8-py3-none-any.whl/eon_logger/logging.py
@@ -0,0 +1,34 @@
+import sys
+import logging
+
+
+class Logger():
+    def __init__(self, log_name, log_level):
+        self.log_name = log_name
+        self.log_level = log_level
+        self.create_logger()
+
+    def create_logger(self):
+        logger = logging.getLogger(self.log_name)
+
+        if self.log_level == 'info':
+            logger.setLevel(logging.INFO)  
+
+        if self.log_level == 'warning':
+            logger.setLevel(logging.WARNING)  
+
+        if self.log_level == 'debug':
+            logger.setLevel(logging.DEBUG)  
+            
+        formatter = logging.Formatter(  
+            '%(levelname)s:%(module)s:%(message)s')
+        
+        handler = logging.StreamHandler(sys.stderr)
+        handler.setLevel(logging.INFO)
+        
+        handler.setFormatter(formatter)
+        logger.addHandler(handler) 
+        _logger = logging.LoggerAdapter(logging.getLogger(self.log_name)) 
+        return _logger
+
+
