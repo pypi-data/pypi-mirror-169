bc17f6a39e71e7767cfc6028ff8b2ad06fda74ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:19 +0000

    Add package ViduDongGoiPython/1.0.0/ViduDongGoiPython-1.0.0-py3-none-any.whl

---
+++ packages/ViduDongGoiPython/ViduDongGoiPython-1.0.0-py3-none-any.whl/vd_DongGoi/app.py
@@ -0,0 +1,16 @@
+def sum(a, b):
+    return a + b
+
+def difference(a, b):
+    return a - b;
+    
+def hello():
+    print("Hi everyone of group 19NH11")
+
+def member_team():
+    print("Include 5 members: ")
+    print("\tNguyen Thi Hai Yen")
+    print("\tBui Thi Ha Tien")
+    print("\tDuong Truong Vu")
+    print("\tTran Ngoc Tin")
+    print("\tTran Anh Vu")
