0d0408d41ced6a74a86d0313a4a1240ba16fe966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:27 +0000

    Add package ssrspeed/1.4.8/ssrspeed-1.4.8.tar.gz

---
+++ packages/ssrspeed/ssrspeed-1.4.8.tar.gz/ssrspeed-1.4.8/ssrspeed/type/node/__init__.py
@@ -0,0 +1,6 @@
+from .nss import NodeShadowsocks
+from .nssr import NodeShadowsocksR
+from .ntrojan import NodeTrojan
+from .nv2ray import NodeV2Ray
+
+__all__ = ["NodeShadowsocks", "NodeShadowsocksR", "NodeTrojan", "NodeV2Ray"]
