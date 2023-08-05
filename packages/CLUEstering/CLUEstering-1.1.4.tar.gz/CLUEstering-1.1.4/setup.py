726694752bb6dd42cf35d973ff0e680ddaa48a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:04 +0000

    Add package CLUEstering/1.1.4/CLUEstering-1.1.4.tar.gz

---
+++ packages/CLUEstering/CLUEstering-1.1.4.tar.gz/CLUEstering-1.1.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+__version__ = "1.1.4"
+
+setup(
+    name="CLUEstering",
+    version=__version__,
+    author="Simone Balducci",
+    author_email="simone.balducci00@gmail.com",
+    description="A library that generalizes the original 2-dimensional CLUE algorithm made at CERN.",
+	 install_requires=['sklearn','numpy','matplotlib','pandas']
+)
