8a0f803bf47368fd9cd1f29d251aa9bd66d241ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:50 +0000

    Add package tid-classifier-model/0.0.4/tid_classifier_model-0.0.4-py3-none-any.whl

---
+++ packages/tid-classifier-model/tid_classifier_model-0.0.4-py3-none-any.whl/classifier_model/processing/validation.py
@@ -0,0 +1,62 @@
+from turtle import title
+from typing import Any, List, Optional, Tuple, Union
+
+import numpy as np
+import pandas as pd
+from pydantic import BaseModel, ValidationError
+
+from classifier_model.config.core import config
+from classifier_model.processing.data_manager import pre_pipeline_preparation
+
+
+class TitanicDataInputSchema(BaseModel):
+    pclass: Optional[int]
+    name: Optional[str]
+    sex: Optional[str]
+    age: Optional[int]
+    sibsp: Optional[int]
+    parch: Optional[int]
+    ticket: Optional[int]
+    fare: Optional[float]
+    cabin: Optional[str]
+    embarked: Optional[str]
+    boat: Optional[Union[str, int]]
+    body: Optional[int]
+    title: Optional[str]
+
+
+class MultipleTitanicDataInputs(BaseModel):
+    inputs: List[TitanicDataInputSchema]
+
+
+def drop_na_inputs(*, input_data: pd.DataFrame) -> pd.DataFrame:
+    """Check model inputs for na values and filter."""
+    validated_data = input_data.copy()
+    new_vars_with_na = [
+        var
+        for var in config.model_config.features
+        if validated_data[var].isnull().sum() > 0
+    ]
+    validated_data.dropna(subset=new_vars_with_na, inplace=True)
+
+    return validated_data
+
+
+def validate_inputs(*, input_data: pd.DataFrame) -> Tuple[pd.DataFrame, Optional[dict]]:
+    """Check model inputs for unprocessable values."""
+
+    pre_processed = pre_pipeline_preparation(dataframe=input_data)
+    validated_data = pre_processed[config.model_config.features].copy()    
+    errors = None
+
+    try:
+        # replace numpy nans so that pydantic can validate
+        MultipleTitanicDataInputs(
+            inputs=validated_data.replace({np.nan: None}).to_dict(orient="records")
+        )
+    except ValidationError as error:
+        errors = error.json()
+
+    return validated_data, errors
+
+
