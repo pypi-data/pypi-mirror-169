43a1223f2f5dd7ca412d8200de8902b623ce78c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:03 +0000

    Add package django-apiview/1.0.19/django_apiview-1.0.19-py3-none-any.whl

---
+++ packages/django-apiview/django_apiview-1.0.19-py3-none-any.whl/django_apiview/apps.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+from __future__ import absolute_import, division, generators, nested_scopes, print_function, unicode_literals, with_statement
+
+import logging
+
+from django.apps import AppConfig
+
+logger = logging.getLogger(__name__)
+
+class DjangoApiviewConfig(AppConfig):
+    name = 'django_apiview'
+
+    def ready(self):
+        pass
