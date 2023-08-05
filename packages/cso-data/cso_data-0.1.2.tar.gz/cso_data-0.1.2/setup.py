61fb9967f1e33a009ca48bb27d667faa6d56995d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:27 +0000

    Add package cso-data/0.1.2/cso_data-0.1.2.tar.gz

---
+++ packages/cso-data/cso_data-0.1.2.tar.gz/cso_data-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(name='cso_data',
+version='0.1.2',
+description='A handy tool to get cso data in python',
+url='https://github.com/Aidzillafont/cso_app',
+author='Aidzillafont',
+author_email='aheffernan0@gmail.com',
+license='MIT',
+packages=['cso_data'],
+long_description=open('README.md').read(),
+long_description_content_type='text/markdown',
+zip_safe=False)
+
+
