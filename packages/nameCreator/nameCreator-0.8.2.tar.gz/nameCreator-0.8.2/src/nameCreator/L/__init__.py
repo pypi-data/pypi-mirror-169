6a7c85684cac49dedbd103f6d2fd4e491984e5b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:28 +0000

    Add package nameCreator/0.8.2/nameCreator-0.8.2.tar.gz

---
+++ packages/nameCreator/nameCreator-0.8.2.tar.gz/nameCreator-0.8.2/src/nameCreator/L/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding=utf-8
+"""
+Generate a last name
+
+"""
+
+__version__ = "0.8.2"
+__author__ = 'Justin Huddleston'
---
+++ packages/nameCreator/nameCreator-0.8.2.tar.gz/nameCreator-0.8.2/src/nameCreator/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding=utf-8
+"""
+Generates male or female names, from a specified country. Languages are "american", "russian", "spanish", "italian", "german","french","swedish".
+
+"""
+
+__version__ = "0.8.2"
+__author__ = 'Justin Huddleston'
+
+from nameCreator.F import firstF
+from nameCreator.M import firstM
+from nameCreator.L import last
