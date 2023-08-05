285b704b71292345760cad2fc7ac471beea3f56e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:33 +0000

    Add package retracto/1.0.1/retracto-1.0.1.tar.gz

---
+++ packages/retracto/retracto-1.0.1.tar.gz/retracto-1.0.1/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['retracto']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cohere>=2.4.2,<3.0.0',
+ 'google-api-python-client>=2.62.0,<3.0.0',
+ 'google-auth-oauthlib>=0.5.3,<0.6.0',
+ 'python-dotenv>=0.21.0,<0.22.0',
+ 'requests>=2.28.1,<3.0.0',
+ 'typer[all]>=0.6.1,<0.7.0']
+
+entry_points = \
+{'console_scripts': ['retracto = retracto.app:app']}
+
+setup_kwargs = {
+    'name': 'retracto',
+    'version': '1.0.1',
+    'description': 'This app helps you as a youtube contnt creator to delete abusive and spam comments on a video in one shot.',
+    'long_description': 'None',
+    'author': 'Amit Krishna A',
+    'author_email': 'amit.ananthkumar@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
