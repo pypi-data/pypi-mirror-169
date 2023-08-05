7bd32334cf56396f87370928781f27954bda72d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:35 +0000

    Add package pytorch-zero-lit/0.2.1/pytorch-zero-lit-0.2.1.tar.gz

---
+++ packages/pytorch-zero-lit/pytorch-zero-lit-0.2.1.tar.gz/pytorch-zero-lit-0.2.1/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['lit']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['onnx2torch>=1.5.3,<2.0.0',
+ 'onnxruntime>=1.12.1,<2.0.0',
+ 'opencv-python>=4.4.0,<5.0.0,!=4.6.0.66',
+ 'pytorch-lantern>=0.12.1,<0.13.0',
+ 'transformers>=4.22.1,<5.0.0',
+ 'wget>=3.2,<4.0']
+
+setup_kwargs = {
+    'name': 'pytorch-zero-lit',
+    'version': '0.2.1',
+    'description': 'LiT: Zero-Shot Transfer with Locked-image text Tuning',
+    'long_description': None,
+    'author': 'Richard Löwenström',
+    'author_email': 'samedii@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
