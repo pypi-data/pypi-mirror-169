09a980d970ca592a96465fb8e4b728085a4aed0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:53 +0000

    Add package recruiter-app/0.0.23/recruiter_app-0.0.23-py3-none-any.whl

---
+++ packages/recruiter-app/recruiter_app-0.0.23-py3-none-any.whl/recruiter_app/elt_app/service/__init__.py
@@ -0,0 +1,5 @@
+from recruiter_app.elt_app.service.rating_recipes import *
+from recruiter_app.elt_app.service.mongo import *
+from recruiter_app.elt_app.service.rating_prep import *
+from recruiter_app.elt_app.service.merge_data import *
+from recruiter_app.elt_app.service.exec_recipe import *
