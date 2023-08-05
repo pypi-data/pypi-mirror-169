c97a28c74c7dcb297e348fe4fa9c39f2237acc0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:19 +0000

    Add package ViduDongGoiPython/1.0.0/ViduDongGoiPython-1.0.0.tar.gz

---
+++ packages/ViduDongGoiPython/ViduDongGoiPython-1.0.0.tar.gz/ViduDongGoiPython-1.0.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="ViduDongGoiPython",
+    version="1.0.0",
+    author="TranNgocTin",
+    author_email="tnt123@gmail.com",
+    description="Python for Math",
+    long_description="Full description",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
