89d46da6ea8789012e5ae8511eee185e7a98fe1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:09 +0000

    Add package badi-utils/0.1.2/badi_utils-0.1.2.tar.gz

---
+++ packages/badi-utils/badi_utils-0.1.2.tar.gz/badi_utils-0.1.2/badi_utils/i18n.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+
+from src.badi_utils.errors import BadiErrorCodes
+
+default_lang = getattr(settings, "BADI_I18N", "fa")
+i18n_values = {
+    'fa': {
+        BadiErrorCodes.permission_denied: 'شما دسترسی به این بخش ندارید!',
+        "custom": "سفارشی",
+        "login": "ورود",
+        "logout": "خروج",
+
+    },
+    'en': {
+        BadiErrorCodes.permission_denied: 'You dont have permission to this section!',
+
+    },
+}
+
+
+class BadiI18n:
+    lang = default_lang
+
+    def __init__(self, lang=default_lang) -> None:
+        self.lang = lang
+
+    @classmethod
+    def t(cls, value):
+        return i18n_values[cls.lang].get(value, value)
