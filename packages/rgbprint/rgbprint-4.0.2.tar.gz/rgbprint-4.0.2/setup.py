50517c27d3dd9b35a4ab239c63fac75e7e804c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:49 +0000

    Add package rgbprint/4.0.2/rgbprint-4.0.2.tar.gz

---
+++ packages/rgbprint/rgbprint-4.0.2.tar.gz/rgbprint-4.0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    LONG_DESCRIPTION = f.read()
+
+setuptools.setup(
+    name="rgbprint",
+    version="4.0.2",
+    license="GNU",
+    author="ddjerqq",
+    author_email="ddjerqq@gmail.com",
+    url="https://github.com/ddjerqq/rgbprint",
+    keywords="rgb print gradient color colorama",
+    description="Print colors and gradients in your terminal. Official github: https://github.com/ddjerqq/rgbprint",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=["rgbprint"],
+)
