de2812bf80616c407bb185f863b055039e42e3f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:08 +0000

    Add package metaNNvis/0.0.3/metannvis-0.0.3-py3-none-any.whl

---
+++ packages/metaNNvis/metannvis-0.0.3-py3-none-any.whl/metannvis/methods/method_keys.py
@@ -0,0 +1,38 @@
+INTEGRATED_GRADIENTS = 'integrated_gradients'
+LAYER_INTEGRATED_GRADIENTS = 'layer_integrated_gradients'
+NEURON_INTEGRATED_GRADIENTS = 'neuron_integrated_gradients'
+
+SALIENCY = 'saliency'
+
+DEEP_LIFT = 'deep_lift'
+LAYER_DEEP_LIFT = 'layer_deep_lift'
+NEURON_DEEP_LIFT = 'neuron_deep_lift'
+
+INPUT_X_GRADIENT = 'input_x_gradient'
+LAYER_GRADIENT_X_ACTIVATION = 'layer_gradient_x_activation'
+
+FEATURE_ABLATION = 'feature_ablation'
+LAYER_FEATURE_ABLATION = 'layer_feature_ablation'
+NEURON_FEATURE_ABLATION = 'neuron_feature_ablation'
+
+FEATURE_PERMUTATION = 'feature_permutation'
+
+GRAD_CAM = 'grad_cam'
+GRAD_CAM_PLUS_PLUS = 'grad_cam++'
+SCORE_CAM = 'score_cam'
+LAYER_CAM = 'layer_cam'
+
+ACTIVATION_MAXIMIZATION = 'activation_maximization'
+
+GRADIENT_SHAP = 'gradient_shap'
+LAYER_GRADIENT_SHAP = 'layer_gradient_shap'
+NEURON_GRADIENT_SHAP = 'neuron_gradient_shap'
+
+DECONVOLUTION = 'deconvolution'
+NEURON_DECONVOLUTION = 'neuron_deconvolution'
+
+LAYER_CONDUCTANCE = 'layer_conductance'
+NEURON_CONDUCTANCE = 'neuron_conductance'
+
+LAYER_ACTIVATION = 'layer_activation'
+NEURON_GRADIENT = 'neuron_gradient'
