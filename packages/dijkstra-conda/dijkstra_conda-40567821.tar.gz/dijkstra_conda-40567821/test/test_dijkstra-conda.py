7fdcf4f68fef045b960827f06e4b45f64475425b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:59 +0000

    Add package dijkstra-conda/40567821/dijkstra_conda-40567821.tar.gz

---
+++ packages/dijkstra-conda/dijkstra_conda-40567821.tar.gz/dijkstra_conda-40567821/test/test_dijkstra-conda.py
@@ -0,0 +1,19 @@
+"""
+Tests for `dijkstra_conda` module.
+"""
+import pytest
+import dijkstra_conda
+
+
+class TestDijkstra_conda(object):
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    def test_something(self):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
