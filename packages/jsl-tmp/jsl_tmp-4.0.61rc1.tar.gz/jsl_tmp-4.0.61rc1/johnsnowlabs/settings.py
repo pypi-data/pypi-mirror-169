3bf1c76867b4aee4069de7bca57e4b4617992ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:37 +0000

    Add package jsl-tmp/4.0.61rc1/jsl_tmp-4.0.61rc1.tar.gz

---
+++ packages/jsl-tmp/jsl_tmp-4.0.61rc1.tar.gz/jsl_tmp-4.0.61rc1/johnsnowlabs/settings.py
@@ -0,0 +1,47 @@
+from os.path import expanduser
+
+# libs, these versions are used for auto-installs and version  checks
+raw_version_jsl_lib = '4.0.61rc1'
+raw_version_nlp = '4.1.0'
+raw_version_medical = '4.1.0'
+raw_version_ocr = '4.0.1'
+raw_version_nlu = '4.0.1rc3'
+raw_version_pyspark = '3.1.2'
+raw_version_nlp_display = '4.1'
+
+pypi_page = 'https://pypi.org/project/jsl-tmp'
+
+json_indent = 4
+enforce_secret_on_version = False
+# Local paths for jsl home
+
+## Directories
+root_dir = f'{expanduser("~")}/.johnsnowlabs'
+license_dir = f'{root_dir}/licenses'
+java_dir = f'{root_dir}/java_installs'
+py_dir = f'{root_dir}/py_installs'
+
+# Info Files
+root_info_file = f'{root_dir}/info.json'
+java_info_file = f'{java_dir}/info.json'
+py_info_file = f'{py_dir}/info.json'
+creds_info_file = f'{license_dir}/info.json'
+
+# databricks paths
+dbfs_home_dir = 'dbfs:/johnsnowlabs'
+dbfs_java_dir = f'{dbfs_home_dir}/java_installs'
+dbfs_py_dir = f'{dbfs_home_dir}/py_installs'
+db_py_jobs_dir = f'{dbfs_home_dir}/py_jobs'
+db_py_notebook_dir = f'{dbfs_home_dir}/py_notebook_jobs'
+db_jar_jobs_dir = f'{dbfs_home_dir}/jar_jobs'
+
+db_cluster_name = 'John-Snow-Labs-Databricks-Auto-Cluster🚀'
+db_job_name = 'John-Snow-Labs-Job {job} 🚀'
+db_run_name = 'John-Snow-Labs-Run 🚀'
+
+# Local Spark mode
+spark_session_name = 'John-Snow-Labs-Spark-Session 🚀'
+
+
+#### Testing
+success_worker_print = '$$JSL_TESTING_WORKER_SUC$$'
---
+++ packages/jsl-tmp/jsl_tmp-4.0.61rc1.tar.gz/jsl_tmp-4.0.61rc1/johnsnowlabs/utils/health_checks/hc.py
@@ -0,0 +1,24 @@
+# from johnsnowlabs import *
+# spark = jsl.start()
+from pyspark.ml import Pipeline
+from sparknlp import DocumentAssembler
+from sparknlp.annotator import ContextSpellCheckerModel, Tokenizer
+
+documentAssembler = DocumentAssembler() \
+    .setInputCol("text") \
+    .setOutputCol("document")
+
+tokenizer = Tokenizer() \
+    .setInputCols(["document"]) \
+    .setOutputCol("token")
+
+spellModel = ContextSpellCheckerModel \
+    .pretrained('spellcheck_clinical', 'en', 'clinical/models') \
+    .setInputCols("token") \
+    .setOutputCol("checked")
+
+pipeline = Pipeline(stages = [documentAssembler, tokenizer, spellModel])
+
+empty = spark.createDataFrame([[""]]).toDF("text")
+example = spark.createDataFrame([["Witth the hell of phisical terapy.",]]).toDF("text")
+pipeline.fit(empty).transform(example).show()
