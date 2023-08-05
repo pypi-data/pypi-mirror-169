7f4a168f697e5cc7809c45bdd54f1b83376bd260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:23 +0000

    Add package d4rl/1.1/d4rl-1.1.tar.gz

---
+++ packages/d4rl/d4rl-1.1.tar.gz/d4rl-1.1/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='d4rl',
+    version='1.1',
+    install_requires=['gym<0.25.0',
+                      'numpy',
+                      'mujoco_py',
+                      'pybullet',
+                      'h5py',
+                      'termcolor',  # adept_envs dependency
+                      'click',  # adept_envs dependency
+                      'dm_control>=1.0.3',
+                      # 'mjrl @ git+https://github.com/aravindr93/mjrl@master#egg=mjrl'
+                      ],
+    packages=find_packages(),
+    package_data={'d4rl': ['locomotion/assets/*',
+                           'hand_manipulation_suite/assets/*',
+                           'hand_manipulation_suite/Adroit/*',
+                           'hand_manipulation_suite/Adroit/gallery/*',
+                           'hand_manipulation_suite/Adroit/resources/*',
+                           'hand_manipulation_suite/Adroit/resources/meshes/*',
+                           'hand_manipulation_suite/Adroit/resources/textures/*',
+                           ]},
+    include_package_data=True,
+)
