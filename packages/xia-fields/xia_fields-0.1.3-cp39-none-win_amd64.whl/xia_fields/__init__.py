980e4697eed7fbe626327da6104676346c816f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:36 +0000

    Add package xia-fields/0.1.3/xia_fields-0.1.3-cp39-none-win_amd64.whl

---
+++ packages/xia-fields/xia_fields-0.1.3-cp39-none-win_amd64.whl/xia_fields/__init__.py
@@ -0,0 +1,12 @@
+from xia_fields.base import BaseField
+from xia_fields.fields import StringField, IntField, FloatField, BooleanField, DateTimeField, ByteField
+from xia_fields.fields import DictField, CompressedStringField
+
+
+__all__ = [
+    "BaseField",
+    "StringField", "IntField", "FloatField", "BooleanField", "DateTimeField", "ByteField",
+    "DictField", "CompressedStringField"
+]
+
+__version__ = "0.1.3"
