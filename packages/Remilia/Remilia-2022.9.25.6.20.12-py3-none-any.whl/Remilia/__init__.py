a908a78a6bbb744b684c1099def2fe55ea69e590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:26 +0000

    Add package Remilia/2022.9.25.6.20.12/Remilia-2022.9.25.6.20.12-py3-none-any.whl

---
+++ packages/Remilia/Remilia-2022.9.25.6.20.12-py3-none-any.whl/Remilia/__init__.py
@@ -0,0 +1,17 @@
+from . import LiteLog,LiteMixin,LiteUtils,LitePGL,LiteTasks,LiteEvent,LiteConfig,LiteResource,LiteI18n,LiteThread,DecoratorUtils,base
+__all__=[
+    LiteLog,
+    LiteMixin,
+    LiteUtils,
+    LiteThread,
+    LitePGL,
+    LiteResource,
+    LiteTasks,
+    LiteEvent,
+    LiteConfig,
+    LiteI18n,
+    DecoratorUtils,
+    base,
+    ]
+
+version="2022.09.25.06.20.12"
