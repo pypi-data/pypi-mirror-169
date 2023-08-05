dbeae765bb9187adb5150016e5129c0f81861009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:36 +0000

    Add package transiter/0.5.6.dev90/transiter-0.5.6.dev90-py3-none-any.whl

---
+++ packages/transiter/transiter-0.5.6.dev90-py3-none-any.whl/transiter/__metadata__.py
@@ -0,0 +1,15 @@
+# When updating the version, also update it in the docs.
+__version__ = "0.5.6"
+
+# The following are set by the CI process at build time.
+__build_number__ = None
+__build_timestamp__ = None
+__build_href__ = None
+__git_commit_hash__ = None
+
+# NOTE: the following were set by the CI process
+__version__ = "0.5.6.dev90"
+__build_number__ = 90
+__build_timestamp__ = 1664155282
+__build_href__ = "https://github.com/jamespfennell/transiter/actions/runs/3124437652"
+__git_commit_hash__ = "26d68db41003b8d88a0d722d989bbb9c33dffe1a"
