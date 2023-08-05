e85e6de6b8d12576076ddf5c1c564d90772c6ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:13 +0000

    Add package slicemap/1.0.13/slicemap-1.0.13.tar.gz

---
+++ packages/slicemap/slicemap-1.0.13.tar.gz/slicemap-1.0.13/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "slicemap" / "README.md").read_text()
+
+packages = find_packages()
+print("Packages found:", packages)
+
+setup(
+    name='slicemap',
+    version='1.0.13',
+    url='https://github.com/gahaalt/slicemap.git',
+    author='Szymon Mikler',
+    license="MIT License",
+    author_email='sjmikler@gmail.com',
+    description='A tiny package containing a dict-like data structure with numeric'
+                ' slices as keys.',
+    packages=packages,
+    install_requires=['sortedcontainers>=2.4.0'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
