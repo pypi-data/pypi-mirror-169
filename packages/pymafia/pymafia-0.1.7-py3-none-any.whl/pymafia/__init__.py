5830164b155926b0ca7bab6963260dd1013fe040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:59 +0000

    Add package pymafia/0.1.7/pymafia-0.1.7-py3-none-any.whl

---
+++ packages/pymafia/pymafia-0.1.7-py3-none-any.whl/pymafia/__init__.py
@@ -0,0 +1,9 @@
+from pymafia import ash
+from pymafia.combat import Macro
+from pymafia.datatypes import *
+from pymafia.iotms import *
+from pymafia.kolmafia import MafiaError, km
+from pymafia.player import *
+from pymafia.preference import *
+from pymafia.utils import *
+from pymafia.wanderer import *
