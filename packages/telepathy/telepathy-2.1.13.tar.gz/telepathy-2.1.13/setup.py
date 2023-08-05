a2835868e87618dc7f8432de1cac8687bfdfd690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:02 +0000

    Add package telepathy/2.1.13/telepathy-2.1.13.tar.gz

---
+++ packages/telepathy/telepathy-2.1.13.tar.gz/telepathy-2.1.13/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name="telepathy",
+    version='2.1.13',
+    author='Jordan Wildon',
+    author_email='j.wildon@pm.me',
+    packages=['telepathy'],
+    url='https://pypi.python.org/pypi/telepathy/',
+    license='LICENSE.txt',
+    description='An OSINT toolkit for investigating Telegram chats.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    py_modules=['telepathy'],
+    install_requires=[
+        'click == 7.1.2',
+        'telethon == 1.24.0',
+        'pandas == 1.4.2',
+        'colorama == 0.4.3',
+        'alive_progress == 2.4.1',
+        'beautifulsoup4 == 4.9.3',
+        'requests == 2.28.1',
+        'googletrans == 3.1.0',
+        'pprintpp == 0.4.0'
+    ],
+    entry_points='''
+        [console_scripts]
+        telepathy=telepathy.telepathy:cli
+    ''',
+)
