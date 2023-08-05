ffaa4d1dbd90f73e5529164f76674112a289ccf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:01 +0000

    Add package st-card-component-0/0.0.1/st_card_component_0-0.0.1-py3-none-any.whl

---
+++ packages/st-card-component-0/st_card_component_0-0.0.1-py3-none-any.whl/st_card_component_0/frontend/build/precache-manifest.c38bd9a9dce45c73bc96de3a83aee1f7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c545a22725c0ee77ffdebaa1d45d16c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "24d0364cafccebfad24c",
+    "url": "./static/css/2.b2d9eb83.chunk.css"
+  },
+  {
+    "revision": "24d0364cafccebfad24c",
+    "url": "./static/js/2.9007bbcc.chunk.js"
+  },
+  {
+    "revision": "156e5292f4455e5106f7ca2436008665",
+    "url": "./static/js/2.9007bbcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a8466f54bb8428d996e",
+    "url": "./static/js/main.81b26cbe.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  },
+  {
+    "revision": "62b936e168110e58e89e70ec82e22755",
+    "url": "./static/media/roboto-all-400-normal.62b936e1.woff"
+  },
+  {
+    "revision": "d9ac47c7e500fb7083b8d595eaf6fe12",
+    "url": "./static/media/roboto-cyrillic-400-normal.d9ac47c7.woff2"
+  },
+  {
+    "revision": "c00467dc3792a8ab586955a3faefcac9",
+    "url": "./static/media/roboto-cyrillic-ext-400-normal.c00467dc.woff2"
+  },
+  {
+    "revision": "28668857bef1b85c5748a482cf9b74af",
+    "url": "./static/media/roboto-greek-400-normal.28668857.woff2"
+  },
+  {
+    "revision": "35de3738b76d249ed060dd3d0f9286be",
+    "url": "./static/media/roboto-greek-ext-400-normal.35de3738.woff2"
+  },
+  {
+    "revision": "15d9f621c3bd1599f0169dcf0bd5e63e",
+    "url": "./static/media/roboto-latin-400-normal.15d9f621.woff2"
+  },
+  {
+    "revision": "87ace20058325aa069320aa4af875dff",
+    "url": "./static/media/roboto-latin-ext-400-normal.87ace200.woff2"
+  },
+  {
+    "revision": "ca3b09b62fda648a4511700413313fd0",
+    "url": "./static/media/roboto-vietnamese-400-normal.ca3b09b6.woff2"
+  }
+]);
