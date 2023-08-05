5f554df59730639a38a1dca12a43d9d9ed52cc82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:42 +0000

    Add package ruth-python/0.0.6/ruth_python-0.0.6-py3-none-any.whl

---
+++ packages/ruth-python/ruth_python-0.0.6-py3-none-any.whl/ruth/shared/constants.py
@@ -0,0 +1,15 @@
+DEFAULT_OBJECT_DIRECTORY = "models"
+KEY_NAME = "name"
+ELEMENT_INDEX = "index"
+TOKENS = "tokens"
+INTENT_NAME_KEY = "name"
+PREDICTED_CONFIDENCE_KEY = "confidence"
+ENTITIES = "entities"
+INPUT_IDS = "input_ids"
+ATTENTION_MASKS = "attention_masks"
+# PIPELINE
+
+KEY_PIPELINE = "pipeline"
+KEY_LANGUAGE = "language"
+
+DEFAULT_ENCODING = "utf-8"
