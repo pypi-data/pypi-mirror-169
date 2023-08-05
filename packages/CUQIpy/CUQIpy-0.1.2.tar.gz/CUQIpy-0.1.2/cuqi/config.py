36c63510a39c5c6a7ff689546cb7ea374c217df4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:17 +0000

    Add package CUQIpy/0.1.2/CUQIpy-0.1.2.tar.gz

---
+++ packages/CUQIpy/CUQIpy-0.1.2.tar.gz/CUQIpy-0.1.2/cuqi/config.py
@@ -0,0 +1,11 @@
+""" This module controls global modifiable configuration settings. """
+
+
+MAX_DIM_INV = 2000
+""" Max total dimension for expensive matrix computations (inversion, eigenvalues etc.) """
+
+DEFAULT_SEED = 0
+""" Default seed for random number generators. """
+
+MAX_STACK_SEARCH_DEPTH = 1000
+""" Maximum depth to search the Python variable stack for the name of a density if not set."""
