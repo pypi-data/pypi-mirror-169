01644423467325809b6eaa832f3bdee352bc33ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:27:42 +0000

    Add package tbs2022-distributions/0.1/tbs2022_distributions-0.1.tar.gz

---
+++ packages/tbs2022-distributions/tbs2022_distributions-0.1.tar.gz/tbs2022_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='tbs2022_distributions',
+      version='0.1',
+      description='TBS Gaussian and Binomial distributions',
+      packages=['tbs2022_distributions'],
+      zip_safe=False)
