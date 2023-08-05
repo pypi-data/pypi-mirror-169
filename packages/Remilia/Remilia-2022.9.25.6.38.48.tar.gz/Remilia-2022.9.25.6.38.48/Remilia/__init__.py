d1f8194e26fb3b0310d0174c4457f7418e10b3bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:56 +0000

    Add package Remilia/2022.9.25.6.38.48/Remilia-2022.9.25.6.38.48.tar.gz

---
+++ packages/Remilia/Remilia-2022.9.25.6.38.48.tar.gz/Remilia-2022.9.25.6.38.48/Remilia/__init__.py
@@ -0,0 +1,17 @@
+from . import LiteLog,LiteMixin,LiteUtils,LitePGL,LiteTasks,LiteEvent,LiteConfig,LiteResource,LiteI18n,LiteThread,DecoratorUtils,base
+__all__=[
+    LiteLog,
+    LiteMixin,
+    LiteUtils,
+    LiteThread,
+    LitePGL,
+    LiteResource,
+    LiteTasks,
+    LiteEvent,
+    LiteConfig,
+    LiteI18n,
+    DecoratorUtils,
+    base,
+    ]
+
+__version__="2022.09.25.06.38.48"
