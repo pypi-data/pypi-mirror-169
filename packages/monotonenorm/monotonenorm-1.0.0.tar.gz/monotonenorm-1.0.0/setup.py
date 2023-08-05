cb8d410750581514a48981b21a8f9dd8ec4c916e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:09 +0000

    Add package monotonenorm/1.0.0/monotonenorm-1.0.0.tar.gz

---
+++ packages/monotonenorm/monotonenorm-1.0.0.tar.gz/monotonenorm-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='monotonenorm',
+    version='1.0.0',
+    packages=['monotonenorm'],
+    url='',
+    license='MIT',
+    author='Ouail Kitouni, Niklas Nolte',
+    author_email='kitouni@mit.edu, nnolte@mit.edu',
+    description='Pytorch Implementation of constrained weight operator norms for robustness and monotonicity'
+)
