d95bd470faf580a9eaee3fe0ea5631427600c101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:24 +0000

    Add package vcspull/1.13.0a13/vcspull-1.13.0a13-py3-none-any.whl

---
+++ packages/vcspull/vcspull-1.13.0a13-py3-none-any.whl/vcspull/cli/__init__.py
@@ -0,0 +1,42 @@
+"""CLI utilities for vcspull.
+
+vcspull.cli
+~~~~~~~~~~~
+
+"""
+import logging
+
+import click
+
+from libvcs.__about__ import __version__ as libvcs_version
+
+from ..__about__ import __version__
+from ..log import setup_logger
+from .sync import sync
+
+log = logging.getLogger(__name__)
+
+
+@click.group(
+    context_settings={
+        "help_option_names": ["-h", "--help"],
+        "allow_interspersed_args": True,
+    }
+)
+@click.option(
+    "--log-level",
+    default="INFO",
+    help="Log level (DEBUG, INFO, WARNING, ERROR, CRITICAL)",
+)
+@click.version_option(
+    __version__,
+    "-V",
+    "--version",
+    message=f"%(prog)s %(version)s, libvcs {libvcs_version}",
+)
+def cli(log_level):
+    setup_logger(log=log, level=log_level.upper())
+
+
+# Register sub-commands here
+cli.add_command(sync)
