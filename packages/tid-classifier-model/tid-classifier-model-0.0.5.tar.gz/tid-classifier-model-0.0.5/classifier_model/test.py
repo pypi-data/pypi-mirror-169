046011de40a65308ec6c0fab66293f6369d646c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:00 +0000

    Add package tid-classifier-model/0.0.5/tid-classifier-model-0.0.5.tar.gz

---
+++ packages/tid-classifier-model/tid-classifier-model-0.0.5.tar.gz/tid-classifier-model-0.0.5/classifier_model/test.py
@@ -0,0 +1,16 @@
+
+import pandas as pd
+
+def add():
+    pass
+def function(a, b):
+    """_summary_
+
+    Args:
+        a (_type_): _description_
+        b (_type_): _description_
+    """
+
+    # add
+    pass
+
