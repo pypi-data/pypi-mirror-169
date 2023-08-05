99c0145472852e41ca3ab1a6f642e90b6496f1f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:16 +0000

    Add package pytorch-functional/0.2.0.dev2/pytorch-functional-0.2.0.dev2.tar.gz

---
+++ packages/pytorch-functional/pytorch-functional-0.2.0.dev2.tar.gz/pytorch-functional-0.2.0.dev2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "pytorch-functional" / "README.md").read_text()
+
+packages = find_packages()
+print("Packages found:", packages)
+
+setup(
+    name="pytorch-functional",
+    version="0.2.0.dev2",
+    url="https://github.com/gahaalt/pytorch-functional.git",
+    author="Szymon Mikler",
+    author_email="sjmikler@gmail.com",
+    description="Provides functional API for model creation in PyTorch.",
+    packages=packages,
+    python_requires=">=3.7",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    install_requires=["torch>=1.9.0"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
