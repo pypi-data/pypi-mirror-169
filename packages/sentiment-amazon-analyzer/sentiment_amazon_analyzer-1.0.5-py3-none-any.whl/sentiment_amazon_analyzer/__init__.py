7bfd5d505c52defeb53900e11b2acce1707e5af7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:15 +0000

    Add package sentiment-amazon-analyzer/1.0.5/sentiment_amazon_analyzer-1.0.5-py3-none-any.whl

---
+++ packages/sentiment-amazon-analyzer/sentiment_amazon_analyzer-1.0.5-py3-none-any.whl/sentiment_amazon_analyzer/__init__.py
@@ -0,0 +1,5 @@
+from sentiment_amazon_analyzer.import_data import DataImporter
+from sentiment_amazon_analyzer.pickle_service import PickleService
+from sentiment_amazon_analyzer.sentiment_pipeline import SentimentPipeline
+from sentiment_amazon_analyzer.text_normalizer_service import SentimentTextNormalizerService
+from sentiment_amazon_analyzer.words_tokenizer_service import SentimentWordsTokenizerService
---
+++ packages/sentiment-amazon-analyzer/sentiment_amazon_analyzer-1.0.5-py3-none-any.whl/sentiment_amazon_analyzer/pickle_service.py
@@ -0,0 +1,16 @@
+import pickle
+
+class PickleService:
+    def __init__(self):
+        pass
+
+    def save_pickle(self, data, path):
+        file = open(path, 'wb')
+        pickle.dump(data, file)
+        file.close()
+
+    def load_pickle(self, path):
+        file = open(path, 'rb')
+        data = pickle.load(file)
+        file.close()
+        return data
