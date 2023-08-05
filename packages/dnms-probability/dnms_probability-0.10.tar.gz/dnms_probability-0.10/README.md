2876ef64e10c4c1b45a69d6abfad8b602ec500da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:15 +0000

    Add package dnms-probability/0.10/dnms_probability-0.10.tar.gz

---
+++ packages/dnms-probability/dnms_probability-0.10.tar.gz/dnms_probability-0.10/README.md
@@ -0,0 +1,9 @@
+dnms-probability package
+
+Summary of the package
+
+# Files
+
+Explanation of the files in the package
+
+# Installation
---
+++ packages/dnms-probability/dnms_probability-0.10.tar.gz/dnms_probability-0.10/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dnms_probability',
+      version='0.10',
+      description='Gaussian and Binomial distributions',
+      packages=['dnms_probability'],
+      author='James Oyebade',
+      author_email='jamesthepink@gmail.com',
+      zip_safe=False)
