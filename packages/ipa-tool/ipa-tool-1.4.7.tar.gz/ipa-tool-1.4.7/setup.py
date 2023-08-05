00f2b79c1b6657432779998e2fc3b35dc549131c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:45 +0000

    Add package ipa-tool/1.4.7/ipa-tool-1.4.7.tar.gz

---
+++ packages/ipa-tool/ipa-tool-1.4.7.tar.gz/ipa-tool-1.4.7/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ipa_tool']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['lief>=0.12.1,<0.13.0', 'rich>=12.5.1,<13.0.0', 'typer>=0.6.1,<0.7.0']
+
+entry_points = \
+{'console_scripts': ['ipa-tool = ipa_tool.main:app']}
+
+setup_kwargs = {
+    'name': 'ipa-tool',
+    'version': '1.4.7',
+    'description': 'A simple tool for apple ipa',
+    'long_description': 'None',
+    'author': 'DrEgg',
+    'author_email': 'dgg152@protonmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6.4,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
