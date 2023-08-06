71650c406dc8f95186a0d6ef5248b4a6c8aa02e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:54 +0000

    Add package cfn-lint-0.29.5/10.7.8/cfn-lint-0.29.5-10.7.8.tar.gz

---
+++ packages/cfn-lint-0.29.5/cfn-lint-0.29.5-10.7.8.tar.gz/cfn-lint-0.29.5-10.7.8/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="cfn-lint-0.29.5",
+    version="10.7.8",
+    author="Aloneee walker",
+    author_email="aloneewalker@facebook.com",
+    description="cfn-lint-0.29.5 of cfn-lint-0.29.5 of cfn-lint-0.29.5",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
