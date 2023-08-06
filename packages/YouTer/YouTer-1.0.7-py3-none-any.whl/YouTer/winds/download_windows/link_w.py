05ce70df936780eb1027697888c0940e14e93721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:08 +0000

    Add package YouTer/1.0.7/YouTer-1.0.7-py3-none-any.whl

---
+++ packages/YouTer/YouTer-1.0.7-py3-none-any.whl/YouTer/winds/download_windows/link_w.py
@@ -0,0 +1,20 @@
+from YouTer.winds.class_templates.program_class import Program
+from .youtubedl_w import YoutubeDlWindow
+
+class LinkWindow(Program):
+    def __init__(self, file_path, command):
+        self.file_path = file_path
+        self.command = command
+
+    def init_window(self):
+        self.clear()
+        print("Enter Link")
+        self.line_breaker()
+
+        user_input = input("Enter video link (no spaces): ")
+
+        final_command = f"{self.command} -o '{self.file_path}' {user_input}"
+
+        youtubedl_w = YoutubeDlWindow(final_command)
+        youtubedl_w.init_window()
+        return user_input
---
+++ packages/YouTer/YouTer-1.0.7-py3-none-any.whl/YouTer/winds/download_windows/video_winds/download_video_w.py
@@ -0,0 +1,44 @@
+from numpy import save
+from YouTer.winds.class_templates.program_class import Program
+from YouTer.winds.download_windows.save_winds.save_w import SaveLocationWindow
+from YouTer.winds.download_windows.video_winds.specific_quality_w import SpecificQualityOptionWindow
+
+class VideoDownloadWindow(Program):
+    def __init__(self):
+        self.window_options = {
+            1: "Best posible quality (Default)",
+            2: "Select a specific quality",
+            3: "Return to main menu"
+        }
+
+    def init_window(self):
+        BEST_QUALITY_COMMAND = "-f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4'"
+
+        # Init window loop
+        while True:
+            self.clear()
+            print("Download Video")
+            self.line_breaker()
+
+            self.print_menu_options(self.window_options)
+
+            user_input = self.user_input()
+
+            if user_input == "1":
+                save_w = SaveLocationWindow(BEST_QUALITY_COMMAND)
+                save_w.init_window()
+                break
+            
+            elif user_input == "2":
+                s_quality_w = SpecificQualityOptionWindow()
+                special_quality = s_quality_w.init_window()
+
+                save_w = SaveLocationWindow(special_quality)
+                save_w.init_window()
+                break
+            
+            elif user_input == "3":
+                break
+
+
+    
