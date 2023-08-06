cd1c61f7bb0d6536ea8b2c107742e6eeceb7b735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:18:34 +0000

    Add package checkov/2.1.228/checkov-2.1.228-py3-none-any.whl

---
+++ packages/checkov/checkov-2.1.228-py3-none-any.whl/checkov/github_actions/checks/job/ReverseShellNetcat.py
@@ -0,0 +1,29 @@
+from typing import Any, Dict, Tuple
+
+from checkov.common.models.enums import CheckResult
+from checkov.github_actions.checks.base_github_action_check import BaseGithubActionsCheck
+from checkov.yaml_doc.enums import BlockType
+import re
+
+
+class ReverseShellNetcat(BaseGithubActionsCheck):
+    def __init__(self):
+        name = "Suspicious use of netcat with IP address"
+        id = "CKV_GHA_4"
+        super().__init__(
+            name=name,
+            id=id,
+            block_type=BlockType.ARRAY,
+            supported_entities=('jobs', 'jobs.*.steps[]')
+        )
+
+    def scan_entity_conf(self, conf: Dict[str, Any]) -> Tuple[CheckResult, Dict[str, Any]]:
+        if not isinstance(conf, dict):
+            return CheckResult.UNKNOWN, conf
+        run = conf.get("run", "")
+        if re.search(r'(nc|netcat) (\d{1,3}).(\d{1,3}).(\d{1,3}).(\d{1,3})', run):
+            return CheckResult.FAILED, conf
+        return CheckResult.PASSED, conf
+
+
+check = ReverseShellNetcat()
