f821bcbb0e8d3470d05383a163ffb991bbb0b005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:14 +0000

    Add package diffusionkit/0.10/diffusionkit-0.10.tar.gz

---
+++ packages/diffusionkit/diffusionkit-0.10.tar.gz/diffusionkit-0.10/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	name='diffusionkit',
+	description='This is a placeholder package. The real package will be released next week.',
+	version='0.10',
+	packages=[
+		'diffusionkit'
+	]
+)
