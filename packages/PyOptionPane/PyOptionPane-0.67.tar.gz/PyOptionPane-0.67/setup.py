35af12fc0bdb671e7f42e0c4300dda2962951234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:35 +0000

    Add package PyOptionPane/0.67/PyOptionPane-0.67.tar.gz

---
+++ packages/PyOptionPane/PyOptionPane-0.67.tar.gz/PyOptionPane-0.67/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1.45'
+DESCRIPTION = 'Python GUIS made easy'
+LONG_DESCRIPTION = 'Python GUIS made easy.'
+
+setup(
+    name="PyOptionPane",
+    version="0.67",
+    author="yavda1",
+    description="Python GUIs made easy",
+    packages=find_packages(),
+    keywords=['python', 'tkinter', 'gui'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
