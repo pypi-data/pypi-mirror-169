bd0679db86c728f8e37d8c24f599b5deefa69aec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:09 +0000

    Add package MeineUtils/0.0.36/MeineUtils-0.0.36-py3-none-any.whl

---
+++ packages/MeineUtils/MeineUtils-0.0.36-py3-none-any.whl/MeineUtils/MachineLearning/__init__.py
@@ -0,0 +1,19 @@
+from .linear_regression import LinearRegression
+from .logistic_regression import LogisticRegression
+
+# from .normalization import (
+#     MinMaxScaler,
+#     StandardScaler
+# )
+
+from .bounding_box import (
+    BoundingBoxConverter
+)
+
+from .augmentation import (
+    RandomRotate
+)
+
+from .NaturalLanguageProcessing import (
+    preprocessing
+)
