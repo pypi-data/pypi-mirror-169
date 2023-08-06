5e95173c943f97d4e5627a8e9f11b16bb0ead1c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:23 +0000

    Add package reportportal-client/5.2.5/reportportal-client-5.2.5.tar.gz

---
+++ packages/reportportal-client/reportportal-client-5.2.5.tar.gz/reportportal-client-5.2.5/setup.py
@@ -0,0 +1,30 @@
+"""Config for setup package client Python."""
+
+from setuptools import setup, find_packages
+
+__version__ = '5.2.5'
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='reportportal-client',
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    version=__version__,
+    description='Python client for Report Portal v5.',
+    author_email='SupportEPMC-TSTReportPortal@epam.com',
+    url='https://github.com/reportportal/client-Python',
+    download_url=('https://github.com/reportportal/client-Python/'
+                  'tarball/%s' % __version__),
+    license='Apache 2.0.',
+    keywords=['testing', 'reporting', 'reportportal'],
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
+    ],
+    install_requires=requirements
+)
