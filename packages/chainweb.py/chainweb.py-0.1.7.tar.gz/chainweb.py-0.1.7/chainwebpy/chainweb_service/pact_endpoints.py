0a1726ad669661773e0778c3db03cc246a56d092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:46 +0000

    Add package chainweb.py/0.1.7/chainweb.py-0.1.7.tar.gz

---
+++ packages/chainweb.py/chainweb.py-0.1.7.tar.gz/chainweb.py-0.1.7/chainwebpy/chainweb_service/pact_endpoints.py
@@ -0,0 +1,31 @@
+from typing import Union
+from chainwebpy.url import (
+    GenericNodeAPIEndpoint,
+    P2PBootstrapAPIEndpoint,
+    ServiceAPIEndpoint,
+)
+
+
+class PactEndpoints:
+    """Will be implemented soon."""
+
+    def __init__(
+        self,
+        api: Union[
+            GenericNodeAPIEndpoint, P2PBootstrapAPIEndpoint, ServiceAPIEndpoint
+        ],
+    ):
+        self.node = api
+
+    def set_node_endpoint(
+        self,
+        api: Union[
+            GenericNodeAPIEndpoint, P2PBootstrapAPIEndpoint, ServiceAPIEndpoint
+        ],
+    ):
+        """Set the node url that serves endpoints.
+
+        Args:
+            `api` (Union[GenericNodeAPIEndpoint, P2PBootstrapAPIEndpoint, ServiceAPIEndpoint]): The node url that serves endpoints.
+        """
+        self.node = api
