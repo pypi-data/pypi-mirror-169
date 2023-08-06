824310302d99878e791785a27f24993f2bb663ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:57 +0000

    Add package oiffile/2022.9.29/oiffile-2022.9.29.tar.gz

---
+++ packages/oiffile/oiffile-2022.9.29.tar.gz/oiffile-2022.9.29/setup.py
@@ -0,0 +1,81 @@
+# oiffile/setup.py
+
+"""Oiffile package setuptools script."""
+
+import sys
+import re
+
+from setuptools import setup
+
+
+def search(pattern, code, flags=0):
+    # return first match for pattern in code
+    match = re.search(pattern, code, flags)
+    if match is None:
+        raise ValueError(f'{pattern!r} not found')
+    return match.groups()[0]
+
+
+with open('oiffile/oiffile.py') as fh:
+    code = fh.read()
+
+version = search(r"__version__ = '(.*?)'", code)
+
+description = search(r'"""(.*)\.(?:\r\n|\r|\n)', code)
+
+readme = search(
+    r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}[__version__|from]',
+    code,
+    re.MULTILINE | re.DOTALL,
+)
+
+readme = '\n'.join(
+    [description, '=' * len(description)] + readme.splitlines()[1:]
+)
+
+license = search(
+    r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""',
+    code,
+    re.MULTILINE | re.DOTALL,
+)
+
+license = license.replace('# ', '').replace('#', '')
+
+if 'sdist' in sys.argv:
+    with open('LICENSE', 'w') as fh:
+        fh.write('BSD 3-Clause License\n\n')
+        fh.write(license)
+    with open('README.rst', 'w') as fh:
+        fh.write(readme)
+
+setup(
+    name='oiffile',
+    version=version,
+    license='BSD',
+    description=description,
+    long_description=readme,
+    author='Christoph Gohlke',
+    author_email='cgohlke@cgohlke.com',
+    url='https://www.cgohlke.com',
+    project_urls={
+        'Bug Tracker': 'https://github.com/cgohlke/oiffile/issues',
+        'Source Code': 'https://github.com/cgohlke/oiffile',
+        # 'Documentation': 'https://',
+    },
+    packages=['oiffile'],
+    python_requires='>=3.8',
+    install_requires=['numpy>=1.19.2', 'tifffile>=2021.11.2'],
+    platforms=['any'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
