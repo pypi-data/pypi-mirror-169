396bc8b88b07437af6422cdb372981040e1aa68d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:25 +0000

    Add package pycov-convert-relative-filenames/0.1.2/pycov-convert-relative-filenames-0.1.2.tar.gz

---
+++ packages/pycov-convert-relative-filenames/pycov-convert-relative-filenames-0.1.2.tar.gz/pycov-convert-relative-filenames-0.1.2/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+
+from setuptools import setup
+
+
+setup(
+    entry_points={
+        "console_scripts": [
+            "pycov-convert-relative-filenames = pycov_convert_relative_filenames.__main__:main",
+        ],
+    },
+)
