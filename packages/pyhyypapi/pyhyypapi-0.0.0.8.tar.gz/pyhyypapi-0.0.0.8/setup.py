1c64c112982b9c25085f3b1a92d58213fbc8769d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:13 +0000

    Add package pyhyypapi/0.0.0.8/pyhyypapi-0.0.0.8.tar.gz

---
+++ packages/pyhyypapi/pyhyypapi-0.0.0.8.tar.gz/pyhyypapi-0.0.0.8/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pyhyypapi',
+    version="0.0.0.8",
+    license='Apache Software License 2.0',
+    author='Renier Moorcroft',
+    author_email='renierm26@users.github.com',
+    description='IDS Hyyp/ADT Secure Home API',
+    long_description="API for accessing IDS Hyyp. This is used by ADT Home Connect and possibly others. Please view readme on github",
+    url='https://github.com/RenierM26/pyHyypApi/',
+    packages=setuptools.find_packages(),
+    setup_requires=[
+        'requests',
+        'setuptools'
+    ],
+    install_requires=[
+        'requests',
+        'pandas',
+        'oscrypto',
+        'protobuf',
+        'http-ece',
+        'appdirs'
+    ],
+    python_requires = '>=3.6'
+)
