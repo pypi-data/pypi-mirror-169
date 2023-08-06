adcc108e440c0f8fb43347cb63f255cc5276a7e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:14 +0000

    Add package pontos/22.9.4/pontos-22.9.4-py3-none-any.whl

---
+++ packages/pontos/pontos-22.9.4-py3-none-any.whl/pontos/github/cmds.py
@@ -0,0 +1,183 @@
+# Copyright (C) 2022 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import json
+import sys
+from argparse import Namespace
+from pathlib import Path
+
+import httpx
+
+from pontos.github.api import GitHubRESTApi
+from pontos.terminal import Terminal
+
+
+def pull_request(terminal: Terminal, args: Namespace):
+    args.pr_func(terminal, args)
+
+
+def create_pull_request(terminal: Terminal, args: Namespace):
+    git = GitHubRESTApi(token=args.token)
+
+    try:
+        # check if branches exist
+        if not git.branch_exists(repo=args.repo, branch=args.head):
+            terminal.error(
+                f"Head branch {args.head} is not existing "
+                "or authorisation failed."
+            )
+            sys.exit(1)
+
+        terminal.ok(f"Head branch {args.head} is existing.")
+
+        if not git.branch_exists(repo=args.repo, branch=args.target):
+            terminal.error(
+                f"Target branch {args.target} is not existing or "
+                "authorisation failed."
+            )
+            sys.exit(1)
+
+        terminal.ok(f"Target branch {args.target} exists.")
+
+        git.create_pull_request(
+            repo=args.repo,
+            head_branch=args.head,
+            base_branch=args.target,
+            title=args.title,
+            body=args.body,
+        )
+    except httpx.HTTPError as e:
+        terminal.error(str(e))
+        sys.exit(1)
+
+    terminal.ok("Pull Request created.")
+
+
+def update_pull_request(terminal: Terminal, args: Namespace):
+    git = GitHubRESTApi(token=args.token)
+
+    try:
+        if args.target:
+            # check if branches exist
+            if not git.branch_exists(repo=args.repo, branch=args.target):
+                terminal.error(
+                    f"Target branch {args.target} is not existing or "
+                    "authorisation failed."
+                )
+                sys.exit(1)
+
+            terminal.ok(f"Target branch {args.target} exists.")
+        git.update_pull_request(
+            repo=args.repo,
+            pull_request=args.pull_request,
+            base_branch=args.target,
+            title=args.title,
+            body=args.body,
+        )
+    except httpx.HTTPError as e:
+        terminal.error(str(e))
+        sys.exit(1)
+
+    terminal.ok("Pull Request updated.")
+
+
+def file_status(terminal: Terminal, args: Namespace):
+    git = GitHubRESTApi(token=args.token)
+
+    try:
+        # check if PR is existing
+        if not git.pull_request_exists(
+            repo=args.repo, pull_request=args.pull_request
+        ):
+            terminal.error(
+                f"PR {args.pull_request} is not existing "
+                "or authorisation failed."
+            )
+            sys.exit(1)
+        terminal.ok(f"PR {args.pull_request} exists.")
+
+        file_dict = git.pull_request_files(
+            repo=args.repo,
+            pull_request=args.pull_request,
+            status_list=args.status,
+        )
+        for status in args.status:
+            terminal.info(f"{status.value}:")
+            files = [str(f.resolve()) for f in file_dict[status]]
+
+            for file_string in files:
+                terminal.print(file_string)
+
+            if args.output:
+                args.output.write("\n".join(files) + "\n")
+
+    except httpx.HTTPError as e:
+        terminal.error(str(e))
+        sys.exit(1)
+
+
+def labels(terminal: Terminal, args: Namespace):
+    git = GitHubRESTApi(token=args.token)
+
+    try:
+        # check if PR is existing
+        if not git.pull_request_exists(repo=args.repo, pull_request=args.issue):
+            terminal.error(
+                f"PR {args.issue} is not existing or authorisation failed."
+            )
+            sys.exit(1)
+
+        terminal.ok(f"PR {args.issue} exists.")
+
+        issue_labels = git.get_labels(
+            repo=args.repo,
+            issue=args.issue,
+        )
+
+        issue_labels.extend(args.labels)
+
+        git.set_labels(repo=args.repo, issue=args.issue, labels=issue_labels)
+
+    except httpx.HTTPError as e:
+        terminal.error(str(e))
+        sys.exit(1)
+
+
+def repos(terminal: Terminal, args: Namespace):
+    git = GitHubRESTApi(token=args.token)
+
+    try:
+        # check if Orga is existing
+        if not git.organisation_exists(orga=args.orga):
+            terminal.error(
+                f"PR {args.orga} is not existing or authorisation failed."
+            )
+            sys.exit(1)
+        terminal.ok(f"Organization {args.orga} exists.")
+        orga_json = git.get_repositories(
+            orga=args.orga, repository_type=args.type
+        )
+        if args.path:
+            repo_info = Path(args.path)
+            with open(repo_info, encoding="utf-8", mode="w") as fp:
+                json.dump(orga_json, fp, indent=2)
+        else:
+            terminal.print(orga_json)
+
+    except httpx.HTTPError as e:
+        terminal.error(str(e))
+        sys.exit(1)
---
+++ packages/pontos/pontos-22.9.4-py3-none-any.whl/pontos/helper.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2021-2022 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import subprocess
+from contextlib import contextmanager
+from pathlib import Path
+from typing import Any, Dict, Generator, Iterable, Iterator, Optional, Union
+
+import httpx
+
+DEFAULT_TIMEOUT = 1000
+DEFAULT_CHUNK_SIZE = 4096
+
+
+class DownloadProgressIterable:
+    def __init__(
+        self,
+        *,
+        content_iterator: Iterator,
+        url: str,
+        destination: Path,
+        length: int,
+    ):
+        self._content_iterator = content_iterator
+        self._url = url
+        self._destination = destination
+        self._length = None if length is None else int(length)
+
+    @property
+    def length(self) -> Optional[int]:
+        """
+        Size in bytes of the to be downloaded file or None if the size is not
+        available
+        """
+        return self._length
+
+    @property
+    def destination(self) -> Path:
+        """
+        Destination path of the to be downloaded file
+        """
+        return self._destination
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    def _download(self) -> Iterator[Optional[float]]:
+        dl = 0
+        with self._destination.open("wb") as f:
+            for content in self._content_iterator:
+                dl += len(content)
+                f.write(content)
+                yield dl / self._length if self._length else None
+
+    def __iter__(self) -> Iterator[Optional[float]]:
+        return self._download()
+
+    def run(self):
+        """
+        Just run the download without caring about the progress
+        """
+        try:
+            it = iter(self)
+            while True:
+                next(it)
+        except StopIteration:
+            pass
+
+
+@contextmanager
+def download(
+    url: str,
+    destination: Optional[Union[Path, str]] = None,
+    *,
+    headers: Dict[str, Any] = None,
+    params: Dict[str, Any] = None,
+    chunk_size: int = DEFAULT_CHUNK_SIZE,
+    timeout: int = DEFAULT_TIMEOUT,
+) -> Generator[DownloadProgressIterable, None, None]:
+    """Download file in url to filename
+
+    Arguments:
+        url: The url of the file we want to download
+        destination: Path of the file to store the download in. If set it will
+                     be derived from the passed URL.
+        headers: HTTP headers to use for the download
+        params: HTTP request parameters to use for the download
+        chunk_size: Download file in chunks of this size
+        timeout: Connection timeout
+
+    Raises:
+        HTTPError if the request was invalid
+
+    Returns:
+        A DownloadProgressIterator that yields the progress of the download in
+        percent for each downloaded chunk or None for each chunk if the progress
+        is unknown.
+
+    Example:
+        .. code-block:: python
+
+        with download("https://example.com/some/file")) as progress_it:
+            for progress in progress_it:
+                print(progress)
+    """
+    destination = (
+        Path(url.split("/")[-1]) if not destination else Path(destination)
+    )
+
+    with httpx.stream(
+        "GET",
+        url,
+        timeout=timeout,
+        follow_redirects=True,
+        headers=headers,
+        params=params,
+    ) as response:
+        response.raise_for_status()
+
+        total_length = response.headers.get("content-length")
+
+        yield DownloadProgressIterable(
+            url=url,
+            content_iterator=response.iter_bytes(chunk_size=chunk_size),
+            destination=destination,
+            length=total_length,
+        )
+
+
+def shell_cmd_runner(args: Iterable[str]) -> subprocess.CompletedProcess:
+    return subprocess.run(
+        args,
+        shell=True,
+        check=True,
+        errors="utf-8",
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
