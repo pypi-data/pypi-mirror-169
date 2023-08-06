4558296bd0f832a2c517faf6fc862ba487419411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:48 +0000

    Add package pywifi-gtyc/1.1.12/pywifi-gtyc-1.1.12.zip

---
+++ packages/pywifi-gtyc/pywifi-gtyc-1.1.12.zip/pywifi-gtyc-1.1.12/setup.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+"""
+Created on 2022年9月29日
+@author: GTYC
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pywifi-gtyc',
+    version='1.1.12',
+    keywords='pywifi',
+    description='A LIB',
+    license='GTYC License',
+    url='',
+    author='GTYC',
+    author_email='',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any'
+)
