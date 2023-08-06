d7cfd5533e5d1e40ba8b06de6902e019c753a4b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:51 +0000

    Add package taipy-config/2.0.0/taipy_config-2.0.0-py3-none-any.whl

---
+++ packages/taipy-config/taipy_config-2.0.0-py3-none-any.whl/taipy/config/__init__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Avaiga Private Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+from .checker.issue import Issue
+from .checker.issue_collector import IssueCollector
+from .section import Section
+from .unique_section import UniqueSection
+from .config import Config
+from .global_app.global_app_config import GlobalAppConfig
+from .common.scope import Scope
+from .common.frequency import Frequency
