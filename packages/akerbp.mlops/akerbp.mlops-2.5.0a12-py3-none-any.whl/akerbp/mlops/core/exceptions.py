4ea1fd99b63840f9819469e64f472765bc2480cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:27 +0000

    Add package akerbp.mlops/2.5.0a12/akerbp.mlops-2.5.0a12-py3-none-any.whl

---
+++ packages/akerbp.mlops/akerbp.mlops-2.5.0a12-py3-none-any.whl/akerbp/mlops/core/exceptions.py
@@ -0,0 +1,18 @@
+class MissingFieldError(Exception):
+    """Raised when fields are missing from mlops_settings.yaml"""
+
+
+class MissingMetadataError(Exception):
+    """Raised when fields are missing from the metadata (from mlops_settings.yaml)"""
+
+
+class TestError(Exception):
+    """Raised when tests are failing"""
+
+
+class DeploymentError(Exception):
+    """Raised when deployment fails"""
+
+
+class FunctionNameError(Exception):
+    """Raised when function name (in CDF) is invalid"""
