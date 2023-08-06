0bce4cf09c432d5129e6fdb3c7a29ab96b781627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:47 +0000

    Add package learning-path-package/0.0/learning_path_package-0.0.tar.gz

---
+++ packages/learning-path-package/learning_path_package-0.0.tar.gz/learning_path_package-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='learning_path_package',
+version='0.0',
+description='functions to draw beautiful plots from dataframes, and functions to calculate ROC curves',
+packages=['learning_path_package'],
+author_email='hernangarciamayoral@gmail.com',
+zip_safe=False)
