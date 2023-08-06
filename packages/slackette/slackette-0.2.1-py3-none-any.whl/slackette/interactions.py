bc4025fc0fad7e507dbb86b6de427a7ec63036bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:14 +0000

    Add package slackette/0.2.1/slackette-0.2.1-py3-none-any.whl

---
+++ packages/slackette/slackette-0.2.1-py3-none-any.whl/slackette/interactions.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from pydantic import BaseModel
+
+
+class BlockAction(BaseModel):
+    type: str
+    block_id: str
+    action_id: str
+    value: str
+    action_ts: str
+
+
+class BlockInteraction(BaseModel):
+    type: str
+    actions: List[BlockAction]
+    response_url: str
+
+
+class InteractionResponse(BaseModel):
+    replace_original: bool = True
+    text: str
