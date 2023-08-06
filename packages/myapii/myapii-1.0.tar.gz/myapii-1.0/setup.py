0a446cdaac0f332d247ccc855b79d89e9343382c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:31 +0000

    Add package myapii/1.0/myapii-1.0.tar.gz

---
+++ packages/myapii/myapii-1.0.tar.gz/myapii-1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+# with open('DESCRIPTION.txt') as file:
+#     long_description = file.read()
+
+# with open('requirements.txt') as f:
+#     required = f.read().splitlines()
+
+setup(name='myapii',
+      version='1.0',
+      description='A small api that uses fastapi-users',
+      url = "https://github.com/ladejavu/python-dev/tree/docker",
+      author='sundus_ladejavu',
+      author_email='sundus.khalid@ladejavu.com',
+      license='MIT',
+      packages=['venv'],
+      keywords='api'
+      )
+
