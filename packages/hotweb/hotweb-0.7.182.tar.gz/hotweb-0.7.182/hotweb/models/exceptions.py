d866f5e5a8914402aa35ea1357c7c85510f4709b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:57 +0000

    Add package hotweb/0.7.182/hotweb-0.7.182.tar.gz

---
+++ packages/hotweb/hotweb-0.7.182.tar.gz/hotweb-0.7.182/hotweb/models/exceptions.py
@@ -0,0 +1,9 @@
+"""
+compare model key value pairs
+"""
+class InvalidModelEntries(Exception):
+    pass
+class ErrorCreatingTable(Exception):
+    pass
+class ErrorDropingTable(Exception):
+    pass
