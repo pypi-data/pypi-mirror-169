0f1a1aade4185d3c0663eac0da34f00fba0f1b73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:07 +0000

    Add package apachecn-sec-zh-pt2/2022.9.27.0/apachecn-sec-zh-pt2-2022.9.27.0.tar.gz

---
+++ packages/apachecn-sec-zh-pt2/apachecn-sec-zh-pt2-2022.9.27.0.tar.gz/apachecn-sec-zh-pt2-2022.9.27.0/ApachecnSecZhPt2/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""apachecn-sec-zh-pt2
+https://github.com/apachecn/apachecn-sec-zh-pt2"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.9.27.0"
