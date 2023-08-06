93d700799de7038c421a1bf516a89a096b414bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:31 +0000

    Add package GLAM/0.2.22.9.28.9.30/GLAM-0.2.22.9.28.9.30.tar.gz

---
+++ packages/GLAM/GLAM-0.2.22.9.28.9.30.tar.gz/GLAM-0.2.22.9.28.9.30/glam/matching/embedded/_predict.py
@@ -0,0 +1,38 @@
+from distutils.archive_util import make_archive
+import os
+from scipy.spatial import cKDTree
+import pandas as pd
+import numpy as np
+
+from glam.matching.embedded._model import make_embeddings
+
+def lookup_addresses(addresses, linz, trees):
+
+    # embed addresses
+    parsed_addresses = pd.DataFrame([x if x is not None else {} for x in addresses]).fillna('None')
+
+    parsed_addresses['embedding'] = parsed_addresses.apply(make_embeddings, axis = 1)
+    parsed_addresses['embedding_dim'] = parsed_addresses['embedding'].apply(len)
+
+    # group addresses
+    matches = [zip(embeddings.index,query_tree(embeddings['embedding'], trees[embedding_dim])) for embedding_dim, embeddings in parsed_addresses.groupby(['embedding_dim'])]
+    matches = (
+        pd.DataFrame(
+            [(id, match, dist) for query in matches for id,(dist,match) in query], 
+            columns = ['id', 'match', 'embedding_distance']
+            )
+            .sort_values('id')
+            .set_index('id', drop=True)
+    )
+
+    return_cols = ['unit_value','address_number','address_number_suffix','address_number_high','full_road_name_ascii','suburb_locality_ascii','town_city_ascii','shape_X','shape_Y','embedding_distance']
+    df = matches.merge(linz, left_on='match', how='left', right_index=True)[return_cols].to_dict('records')
+    return df
+
+
+def query_tree(embeddings, tree):
+    dist, idx = tree.query(embeddings.to_list())
+    return zip(dist,idx)
+
+
+
---
+++ packages/GLAM/GLAM-0.2.22.9.28.9.30.tar.gz/GLAM-0.2.22.9.28.9.30/setup.py
@@ -0,0 +1,69 @@
+from setuptools import find_packages, setup
+from datetime import datetime
+import os
+
+
+package_list=[
+    'glam', 
+    'glam.interface',
+    'glam.preprocessing',
+    'glam.parsing',
+    'glam.matching',
+    'glam.utils',
+]
+
+version_base = '0.2.'
+now = datetime.now()
+stamp = datetime(
+    now.year,
+    now.month,
+    now.day,
+    now.hour,
+    (now.minute//10)*10
+)
+
+version_sub = stamp.strftime(r'%y.%m.%d.%H.%M')
+
+setup(
+    name='GLAM',
+    version=version_base+version_sub,
+    packages=package_list,
+    author='Liam Morris',
+    author_email='liam.morris04@gmail.com',
+    description='Geocoding via LINZ address matching',
+    install_requires=[
+        'tensorflow==2.8.*',
+        'rapidfuzz==1.7.1',
+        'numpy',
+        'pandas',
+        'tqdm',
+    ],
+    package_data= {
+        'glam.parsing' : [
+            'rnn',
+            'rnn/*',
+            'rnn/*/*',
+            'rnn/*/*/*',
+            'hmm',
+            'hmm/*',
+            'hmm/*/*',
+            'hmm/*/*/*',
+        ],
+        'glam.matching' : [
+            'siamese',
+            'siamese/*',
+            'siamese/*/*',
+            'siamese/*/*/*',
+            'fuzzy',
+            'fuzzy/*',
+            'fuzzy/*/*',
+            'fuzzy/*/*/*',
+            'embedded',
+            'embedded/*',
+            'embedded/*/*',
+            'embedded/*/*/*',
+        ],
+    },
+    include_package_data=True
+)
+
