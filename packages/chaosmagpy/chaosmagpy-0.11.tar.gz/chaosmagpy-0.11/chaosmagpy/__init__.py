f6924555f038d0799f6353d34bc4c5dd47585cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:22 +0000

    Add package chaosmagpy/0.11/chaosmagpy-0.11.tar.gz

---
+++ packages/chaosmagpy/chaosmagpy-0.11.tar.gz/chaosmagpy-0.11/chaosmagpy/__init__.py
@@ -0,0 +1,21 @@
+__all__ = [
+    "CHAOS", "load_CHAOS_matfile", "load_CHAOS_shcfile",
+    "load_CovObs_txtfile", "load_gufm1_txtfile", "load_CALS7K_txtfile",
+    "basicConfig", "synth_values", "mjd2000", "timestamp", "dyear_to_mjd",
+    "mjd_to_dyear"
+]
+
+__version__ = "0.11"
+
+from .chaos import (
+    CHAOS,
+    load_CHAOS_matfile,
+    load_CHAOS_shcfile,
+    load_CovObs_txtfile,
+    load_gufm1_txtfile,
+    load_CALS7K_txtfile
+)
+
+from .config_utils import basicConfig
+from .model_utils import synth_values
+from .data_utils import mjd2000, timestamp, dyear_to_mjd, mjd_to_dyear
