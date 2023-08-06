329fc1f2bbdb42722175c793d88524ac3164bc5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:35 +0000

    Add package Geode-ModuleTemplate/1.1.0/Geode_ModuleTemplate-1.1.0-cp310-cp310-win_amd64.whl

---
+++ packages/Geode-ModuleTemplate/Geode_ModuleTemplate-1.1.0-cp310-cp310-win_amd64.whl/geode_moduletemplate/mylib.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2019 - 2022 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+from geode_common import core
+
+from .geode_moduletemplate_py_mylib import *
+MyModuleMyLib.initialize()
