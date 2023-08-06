9f5aaac81c18ab8f93bbeef20cc62a5e92874470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:30 +0000

    Add package dicom-mask/0.0.29/dicom_mask-0.0.29.tar.gz

---
+++ packages/dicom-mask/dicom_mask-0.0.29.tar.gz/dicom_mask-0.0.29/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+from pathlib import Path
+import io
+
+current_dir = Path(__file__).parent
+long_description = io.open(current_dir / "README.md", mode="r", encoding="utf-8").read()
+
+setup(
+  name = 'dicom_mask',
+  packages = ['dicom_mask'],
+  version = '0.0.29',
+  license = 'BSD', 
+  description = 'Export structure from dicom to numpy binary mask',
+  long_description_content_type='text/markdown',
+  long_description=long_description,
+  author = 'Abraham George Smith',
+  author_email = 'abe@abesmith.co.uk',
+  url = 'https://github.com/Abe404/dicom_mask',
+  download_url = 'https://github.com/Abe404/dicom_mask/archive/refs/tags/0.0.20.tar.gz',
+  keywords = ['DICOM', 'NUMPY', 'RTSRUCT', 'MASK'],
+  scripts = ['bin/dicom_mask'],
+  entry_points={
+    'console_scripts': [
+      'hello-world = dicom_mask:hello_world',
+    ]
+  },
+  install_requires=[
+      "dicompyler-core ==0.5.5",
+      "numpy ==1.21.0",
+      "pydicom ==2.2.0",
+      "scikit-image ==0.18.2"
+  ],
+  classifiers=[
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent'
+  ]
+)
