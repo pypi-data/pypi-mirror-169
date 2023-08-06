0fc06bce1e92cfa3b9d320f0d0f11f33bf0f2a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:22 +0000

    Add package vban-cmd/1.5.0/vban_cmd-1.5.0-py3-none-any.whl

---
+++ packages/vban-cmd/vban_cmd-1.5.0-py3-none-any.whl/vban_cmd/event.py
@@ -0,0 +1,38 @@
+import logging
+
+
+class Event:
+    logger = logging.getLogger("event.event")
+
+    def __init__(self, subs: dict):
+        self.subs = subs
+
+    def info(self, msg):
+        info = (f"{msg} events",)
+        if self.any():
+            info += (f"now listening for {', '.join(self.get())} events",)
+        else:
+            info += (f"not listening for any events",)
+        self.logger.info(", ".join(info))
+
+    @property
+    def pdirty(self):
+        return self.subs["pdirty"]
+
+    @property
+    def ldirty(self):
+        return self.subs["ldirty"]
+
+    def get(self) -> list:
+        return [k for k, v in self.subs.items() if v]
+
+    def any(self) -> bool:
+        return any(self.subs.values())
+
+    def add(self, event):
+        self.subs[event] = True
+        self.info(f"{event} added to")
+
+    def remove(self, event):
+        self.subs[event] = False
+        self.info(f"{event} removed from")
