cf41c1926dc84a94a71f67b30a1b28326d74f36f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:22 +0000

    Add package karton-archive-extractor/1.3.1/karton_archive_extractor-1.3.1-py3-none-any.whl

---
+++ packages/karton-archive-extractor/karton_archive_extractor-1.3.1-py3-none-any.whl/karton/archive_extractor/archive_extractor.py
@@ -0,0 +1,124 @@
+import tempfile
+
+from karton.core import Karton, Resource, Task
+from sflock import unpack  # type: ignore
+
+from .__version__ import __version__
+
+
+class ArchiveExtractor(Karton):
+    """
+    Extracts files from known archives and e-mail attachments.
+    Produces "raw" artifacts for further classification.
+    """
+
+    # Maximum levels of nested extraction
+    max_depth = 5
+    # Maximum unpacked child filesize, larger files are not reported
+    max_size = 25 * 1024 * 1024
+    # Maximum number of childs for further analysis
+    max_children = 1000
+
+    identity = "karton.archive-extractor"
+    version = __version__
+    persistent = True
+    filters = [
+        {"type": "sample", "stage": "recognized", "kind": "archive"},
+    ]
+
+    def process(self, task: Task) -> None:
+        sample = task.get_resource("sample")
+        task_password = task.get_payload("password", default=None)
+
+        attributes = task.get_payload("attributes", default={})
+        if not task_password and attributes.get("password"):
+            self.log.info("Accepting password from attributes")
+            task_password = attributes.get("password")[0]
+
+        fname = "archive"
+        try:
+            if sample.name:
+                fname = sample.name
+
+            classifier_extension = task.headers.get("extension")
+            if classifier_extension:
+                classifier_extension = "." + classifier_extension
+                if not fname.endswith(classifier_extension):
+                    fname += classifier_extension
+        except Exception as e:
+            self.log.warning("Exception during extraction: %r", e)
+
+        extraction_level = task.get_payload("extraction_level", 0)
+
+        if extraction_level > self.max_depth:
+            self.log.warning(
+                "Maximum extraction depth exceeded. Can't extract this archive."
+            )
+            return
+
+        with tempfile.TemporaryDirectory() as dir_name:
+            filepath = f"{dir_name}/{fname}"
+            with open(filepath, "wb") as f:
+                f.write(sample.content)
+
+            archive_password = None
+            if task_password is not None:
+                archive_password = task_password
+
+            try:
+                unpacked = unpack(
+                    filename=fname.encode("utf-8"),
+                    filepath=filepath.encode("utf-8"),
+                    password=archive_password,
+                )
+            except Exception as e:
+                # we can't really do anything about corrupted archives :(
+                self.log.warning("Error while unpacking archive: %s", e)
+                return
+
+        try:
+            fname = (
+                unpacked.filename and unpacked.filename.decode("utf8")
+            ) or unpacked.sha256
+        except Exception as e:
+            self.log.warning("Exception during extraction: %r", e)
+            fname = "(unknown)"
+
+        self.log.info("Got archive {}".format(fname))
+
+        if not unpacked.children:
+            self.log.warning("Don't know how to unpack this archive")
+            return
+
+        if len(unpacked.children) > self.max_children:
+            self.log.warning("Too many children for further processing")
+            return
+
+        for child in unpacked.children:
+            fname = (child.filename and child.filename.decode("utf8")) or child.sha256
+
+            self.log.info("Unpacked child {}".format(fname))
+
+            if not child.contents:
+                self.log.warning(
+                    "Child has no contents or protected by unknown password"
+                )
+                continue
+
+            if len(child.contents) > self.max_size:
+                self.log.warning("Child is too big for further processing")
+                continue
+
+            task = Task(
+                headers={
+                    "type": "sample",
+                    "kind": "raw",
+                    "quality": task.headers.get("quality", "high"),
+                },
+                payload={
+                    "sample": Resource(fname, child.contents),
+                    "parent": sample,
+                    "extraction_level": extraction_level + 1,
+                },
+            )
+            self.send_task(task)
