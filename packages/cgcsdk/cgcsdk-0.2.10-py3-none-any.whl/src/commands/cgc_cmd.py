4d7e43d6558779ba1f0a358a2635e29c6bc698eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:47 +0000

    Add package cgcsdk/0.2.10/cgcsdk-0.2.10-py3-none-any.whl

---
+++ packages/cgcsdk/cgcsdk-0.2.10-py3-none-any.whl/src/commands/cgc_cmd.py
@@ -0,0 +1,16 @@
+import click
+
+from src.commands.compute.compute_cmd import compute_delete
+from src.utils.click_group import CustomCommand
+
+
+@click.command("rm", cls=CustomCommand)
+@click.argument("name", type=click.STRING)
+def cgc_rm(name: str):
+    """
+    Delete an app in user namespace
+    \f
+    :param name: name of the app to delete
+    :type name: str
+    """
+    compute_delete(name)
