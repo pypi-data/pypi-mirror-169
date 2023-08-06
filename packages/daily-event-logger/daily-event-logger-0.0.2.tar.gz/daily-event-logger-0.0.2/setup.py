b9161d48836af4fed04a0aa418c73c5b17f3629b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:31 +0000

    Add package daily-event-logger/0.0.2/daily-event-logger-0.0.2.tar.gz

---
+++ packages/daily-event-logger/daily-event-logger-0.0.2.tar.gz/daily-event-logger-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="daily-event-logger",
+    version="0.0.2",
+    license="GPL-3.0",
+    author="Jeffrey Serio",
+    author_email="hyperreal@fedoraproject.org",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url="https://github.com/hyperreal64/daily-event-logger",
+    keywords="daily-event-logger",
+    install_requires=[
+        "jsonschema",
+        "rich",
+    ],
+)
