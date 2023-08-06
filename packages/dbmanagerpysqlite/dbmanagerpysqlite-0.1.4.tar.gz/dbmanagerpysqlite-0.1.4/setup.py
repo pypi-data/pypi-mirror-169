7fc04cc3d7ccd10c1a273e015538750e39cfcded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:40 +0000

    Add package dbmanagerpysqlite/0.1.4/dbmanagerpysqlite-0.1.4.tar.gz

---
+++ packages/dbmanagerpysqlite/dbmanagerpysqlite-0.1.4.tar.gz/dbmanagerpysqlite-0.1.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name='dbmanagerpysqlite',
+    version='0.1.4',
+    license='MIT',
+    author="Mohammad Amin Asadi",
+    author_email='mohammadasadiamin@example.com',
+    packages=find_packages('dbmanagerpysqlite'),
+    package_dir={'': 'dbmanagerpysqlite'},
+    url='https://github.com/prankshtain/db_manager',
+)
