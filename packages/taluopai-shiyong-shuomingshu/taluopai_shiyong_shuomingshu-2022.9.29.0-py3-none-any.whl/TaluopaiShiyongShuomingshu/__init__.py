e4fb7eebeeb44e912316d441435426858b68a7ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:49 +0000

    Add package taluopai-shiyong-shuomingshu/2022.9.29.0/taluopai_shiyong_shuomingshu-2022.9.29.0-py3-none-any.whl

---
+++ packages/taluopai-shiyong-shuomingshu/taluopai_shiyong_shuomingshu-2022.9.29.0-py3-none-any.whl/TaluopaiShiyongShuomingshu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""taluopai-shiyong-shuomingshu
+https://github.com/apachecn/taluopai-shiyong-shuomingshu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.9.29.0"
