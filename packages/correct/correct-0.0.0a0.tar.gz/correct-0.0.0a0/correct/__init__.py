c1dc9a0914e6c2e6522e386ab2343c67cc77b6ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:02 +0000

    Add package correct/0.0.0a0/correct-0.0.0a0.tar.gz

---
+++ packages/correct/correct-0.0.0a0.tar.gz/correct-0.0.0a0/correct/__init__.py
@@ -0,0 +1,3 @@
+"""Type validation."""
+
+__version__ = '0.0.0-alpha'
---
+++ packages/correct/correct-0.0.0a0.tar.gz/correct-0.0.0a0/setup.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+from setuptools import (find_packages,
+                        setup)
+
+import correct
+
+project_base_url = 'https://github.com/lycantropos/correct/'
+
+
+def read_file(path_string: str) -> str:
+    return Path(path_string).read_text(encoding='utf-8')
+
+
+setup(name=correct.__name__,
+      packages=find_packages(exclude=('tests', 'tests.*')),
+      version=correct.__version__,
+      description=correct.__doc__,
+      long_description=read_file('README.md'),
+      long_description_content_type='text/markdown',
+      author='Azat Ibrakov',
+      author_email='azatibrakov@gmail.com',
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
+      ],
+      license='MIT License',
+      url=project_base_url,
+      download_url=project_base_url + 'archive/master.zip',
+      python_requires='>=3.6',
+      install_requires=read_file('requirements.txt'))
