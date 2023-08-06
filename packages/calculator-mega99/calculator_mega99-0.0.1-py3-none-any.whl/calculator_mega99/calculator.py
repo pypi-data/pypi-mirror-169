552276ed43d1038036a89777ee61027b7bd471b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:05 +0000

    Add package calculator-mega99/0.0.1/calculator_mega99-0.0.1-py3-none-any.whl

---
+++ packages/calculator-mega99/calculator_mega99-0.0.1-py3-none-any.whl/calculator_mega99/calculator.py
@@ -0,0 +1,28 @@
+import math
+from random import randint
+def add(a, b):
+    return a+b
+def sub(a, b):
+    return a-b
+def mul(a, b):
+    return a*b
+def div(a, b):
+    return a/b
+def add_one(a):
+    return a+1
+def sq(a):
+    return a**a
+def sqrt(a):
+    return math.sqrt(a)
+def ex(a, b):
+    return a**b
+def cbrt(a):
+    return a ** (1. / 3)
+def randnum(a, b):
+    return randint(a, b)
+def sin(a):
+    return math.sin(a)
+def cos(a):
+    return math.cos(a)
+def tan(a):
+    return math.tan(a)
