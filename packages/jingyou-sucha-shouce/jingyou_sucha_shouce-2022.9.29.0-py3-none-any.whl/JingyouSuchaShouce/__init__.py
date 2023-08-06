ecae09907d7e6f73c3d0243cfddeba3d58da5d99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:23 +0000

    Add package jingyou-sucha-shouce/2022.9.29.0/jingyou_sucha_shouce-2022.9.29.0-py3-none-any.whl

---
+++ packages/jingyou-sucha-shouce/jingyou_sucha_shouce-2022.9.29.0-py3-none-any.whl/JingyouSuchaShouce/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jingyou-sucha-shouce
+https://github.com/apachecn/jingyou-sucha-shouce"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.9.29.0"
