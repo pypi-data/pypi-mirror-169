1dd885320c019fd2ebc83deb58cca472742fe0fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:15 +0000

    Add package ssrspeed/1.4.9/ssrspeed-1.4.9-py3-none-any.whl

---
+++ packages/ssrspeed/ssrspeed-1.4.9-py3-none-any.whl/ssrspeed/parser/v2ray/__init__.py
@@ -0,0 +1,5 @@
+from .quantumult import ParserV2RayQuantumult
+from .v2rayn import ParserV2RayN
+from .vclash import ParserV2RayClash
+
+__all__ = ["ParserV2RayClash", "ParserV2RayQuantumult", "ParserV2RayN"]
