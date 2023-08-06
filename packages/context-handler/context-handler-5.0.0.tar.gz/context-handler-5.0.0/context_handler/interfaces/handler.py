276cea9e33c0f36f726a99047f5c88d6d7e8d998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:36 +0000

    Add package context-handler/5.0.0/context-handler-5.0.0.tar.gz

---
+++ packages/context-handler/context-handler-5.0.0.tar.gz/context-handler-5.0.0/context_handler/interfaces/handler.py
@@ -0,0 +1,55 @@
+import typing
+
+from context_handler.interfaces.adapter import Adapter
+from context_handler.interfaces.adapter import AsyncAdapter
+from context_handler.typedef import AsyncT
+from context_handler.typedef import T
+from context_handler.utils import lazy
+
+
+class Handler(typing.Protocol[T]):
+    def __init__(self, adapter: Adapter[T]) -> None:
+        ...
+
+    def is_active(self) -> bool:
+        """Returns if current handler has an active context"""
+        ...
+
+    client: lazy.LazyPropertyDescriptor['Handler[T]', T]
+
+    @property
+    def adapter(self) -> Adapter[T]:
+        ...
+
+    def open(self) -> typing.ContextManager[None]:
+        """Initializes an internal client"""
+        ...
+
+    def begin(self) -> typing.ContextManager[T]:
+        """Returns initialized internal client
+        or new client if none is found"""
+        ...
+
+
+class AsyncHandler(typing.Protocol[AsyncT]):
+    adapter: AsyncAdapter[AsyncT]
+
+    def __init__(self, adapter: AsyncAdapter[AsyncT]) -> None:
+        ...
+
+    def is_active(self) -> bool:
+        """Returns if current handler has an active context"""
+        ...
+
+    async def client(self) -> AsyncT:
+        """Returns internal client or creates a new one"""
+        ...
+
+    def open(self) -> typing.AsyncContextManager[None]:
+        """Initializes an internal client"""
+        ...
+
+    def begin(self) -> typing.AsyncContextManager[AsyncT]:
+        """Returns initialized internal client
+        or new client if none is found"""
+        ...
