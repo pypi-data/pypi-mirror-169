b62601ffd3c022aff35d182d57cb3a0188b7ce72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:01 +0000

    Add package AutoPostTelegram/0.0.9/AutoPostTelegram-0.0.9.tar.gz

---
+++ packages/AutoPostTelegram/AutoPostTelegram-0.0.9.tar.gz/AutoPostTelegram-0.0.9/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+with open("README.md", "r") as txt:
+    long_description = txt.read()
+
+setuptools.setup(
+    name='AutoPostTelegram',
+    version='0.0.9',
+    description='An Telegram Auto Post Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+    author='Aasfcyberking',
+    author_email='aasfvl@gmail.com',
+    url='https://telegram.me/Aasf_CyberKing',
+    packages=setuptools.find_packages(),
+    install_requires= ['requests'],
+    python_requires='>=3.6'
+)
