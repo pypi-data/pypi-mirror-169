cfad027ef35096b380464a051ad5051513235b3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:57 +0000

    Add package Flask-SQLAlchemy/3.0.0a2/Flask-SQLAlchemy-3.0.0a2.tar.gz

---
+++ packages/Flask-SQLAlchemy/Flask-SQLAlchemy-3.0.0a2.tar.gz/Flask-SQLAlchemy-3.0.0a2/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(name="Flask-SQLAlchemy", install_requires=["Flask>=2.0", "SQLAlchemy>=1.4"])
