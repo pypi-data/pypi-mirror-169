1ec409b4da9df5b850fb93ed0ed5ea149d354ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:41 +0000

    Add package acdh-arche-assets/3.5.0/acdh_arche_assets-3.5.0-py3-none-any.whl

---
+++ packages/acdh-arche-assets/acdh_arche_assets-3.5.0-py3-none-any.whl/AcdhArcheAssets/uri_norm_rules.py
@@ -0,0 +1,26 @@
+import json
+import pkg_resources
+import re
+
+
+def get_rules():
+    """ returns a list of regex pattern matches
+
+    :return: a list of regex patterns to normalize Authority IDs
+    :rtype: list
+
+    """
+    return json.loads(pkg_resources.resource_string(__name__, 'uriNormRules.json'))
+
+
+def get_normalized_uri(uri):
+    """ takes a normdata uri and returns a normlalized version
+    :param uri: A normdata uri
+    :param type: str
+
+    :return: The normalized URI
+    :rtype: str
+    """
+    for x in get_rules():
+        uri = re.sub(x['match'], x['replace'], uri)
+    return uri
