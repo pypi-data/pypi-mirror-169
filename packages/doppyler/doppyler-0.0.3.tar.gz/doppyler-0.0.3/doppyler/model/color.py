4f43cb61b0dc94af2e50eed8032fad2734a9da5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:28 +0000

    Add package doppyler/0.0.3/doppyler-0.0.3.tar.gz

---
+++ packages/doppyler/doppyler-0.0.3.tar.gz/doppyler-0.0.3/doppyler/model/color.py
@@ -0,0 +1,42 @@
+"""Models for color."""
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TypedDict
+
+
+class ColorDict(TypedDict):
+    """Representation of a color dictionary."""
+
+    red: int
+    green: int
+    blue: int
+
+
+@dataclass
+class Color:
+    """Color class."""
+
+    red: int
+    green: int
+    blue: int
+
+    def to_list(self) -> list[int]:
+        """Convert Color to a list of ints."""
+        return [self.red, self.green, self.blue]
+
+    def to_dict(self) -> ColorDict:
+        """Convert Color to a dictionary."""
+        return ColorDict(red=self.red, green=self.green, blue=self.blue)
+
+    @staticmethod
+    def from_list(color_list: list[int]) -> "Color":
+        """Convert a list to a Color."""
+        if len(color_list) != 3 or any(not isinstance(x, int) for x in color_list):
+            raise ValueError("Input must be a three integer list")
+        return Color(*color_list)
+
+    @staticmethod
+    def from_dict(color_dict: ColorDict) -> "Color":
+        """Convert a dictionary to a Color."""
+        return Color(color_dict["red"], color_dict["green"], color_dict["blue"])
