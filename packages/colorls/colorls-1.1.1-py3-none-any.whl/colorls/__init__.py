ff4cabf11e8a4c92a1286e383f448116e7aaa304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:59 +0000

    Add package colorls/1.1.1/colorls-1.1.1-py3-none-any.whl

---
+++ packages/colorls/colorls-1.1.1-py3-none-any.whl/colorls/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for c*ls: ls wrapper."""
+
+__author__ = """Alastair Irvine"""
+__email__ = 'alastair@plug.org.au'
+__version__ = '1.1.1'
+
+
+
+# *** DEFINITIONS ***
+size_col_width = 12
+ownership_col_width = 11
