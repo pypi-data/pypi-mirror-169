80f93fe80a2b7d18bd5a5c9527252dcd9646787e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:47 +0000

    Add package napalm-aruba505/0.0.142/napalm_aruba505-0.0.142-py3-none-any.whl

---
+++ packages/napalm-aruba505/napalm_aruba505-0.0.142-py3-none-any.whl/napalm_aruba505/__init__.py
@@ -0,0 +1,6 @@
+"""load napalm_aruba505.aruba505 class."""
+# load ArubaDriver class.
+
+# from napalm_aruba505.aruba505 import Aruba505Driver
+from napalm_aruba505.aruba import ArubaDriver
+__all__ = ('ArubaDriver',)
