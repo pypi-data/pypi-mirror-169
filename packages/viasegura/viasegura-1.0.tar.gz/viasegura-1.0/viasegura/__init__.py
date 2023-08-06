de0b70c09b102271b002995256c86c502878db60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:53 +0000

    Add package viasegura/1.0/viasegura-1.0.tar.gz

---
+++ packages/viasegura/viasegura-1.0.tar.gz/viasegura-1.0/viasegura/__init__.py
@@ -0,0 +1,9 @@
+from viasegura.labelers import Preprocess, ModelLabeler, LanesLabeler
+from viasegura.downloader import Downloader
+from viasegura.processing import processors, workflows
+import os 
+import sys
+
+dl = Downloader()
+download_models = dl.download
+
