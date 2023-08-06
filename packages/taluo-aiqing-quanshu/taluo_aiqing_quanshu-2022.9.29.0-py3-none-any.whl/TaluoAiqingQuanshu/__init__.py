d9254906ec6028c95b0325493c1613b077168e2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:56 +0000

    Add package taluo-aiqing-quanshu/2022.9.29.0/taluo_aiqing_quanshu-2022.9.29.0-py3-none-any.whl

---
+++ packages/taluo-aiqing-quanshu/taluo_aiqing_quanshu-2022.9.29.0-py3-none-any.whl/TaluoAiqingQuanshu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""taluo-aiqing-quanshu
+https://github.com/apachecn/taluo-aiqing-quanshu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.9.29.0"
