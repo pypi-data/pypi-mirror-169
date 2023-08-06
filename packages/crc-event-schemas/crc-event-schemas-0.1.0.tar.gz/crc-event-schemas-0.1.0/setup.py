0f105cb4ef84918fe41cf5b8374411713f4ac7de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:14 +0000

    Add package crc-event-schemas/0.1.0/crc-event-schemas-0.1.0.tar.gz

---
+++ packages/crc-event-schemas/crc-event-schemas-0.1.0.tar.gz/crc-event-schemas-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='crc-event-schemas',
+    version='0.1.0',
+    url='https://github.com/RedHatInsights/event-schemas-python',
+    description='CloudEvents type bindings for console.redhat.com',
+    packages=find_packages(),
+)
