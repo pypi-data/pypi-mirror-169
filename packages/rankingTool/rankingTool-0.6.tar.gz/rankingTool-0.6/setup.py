e217cb0542449a7aa762c67e9d8b04cb18ec70f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:10 +0000

    Add package rankingTool/0.6/rankingTool-0.6.tar.gz

---
+++ packages/rankingTool/rankingTool-0.6.tar.gz/rankingTool-0.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rankingTool',
+    version='0.6',
+    license='MIT',
+    author="Haoqiang Kang",
+    author_email='haoqik@cs.washington.edu',
+    packages=find_packages(),
+    url='https://github.com/mk322/Rankings-UI',
+    description="This is the first version of the package of Rankings UI, which helps visualize the rankings and ratings for comparison.",
+    keywords=['GUI'],
+    install_requires=[
+          'toml',
+          'pandas',
+          'numpy']
+    )
