e4d739ee24fdcbc01df47b2bdb08f6b817c0b544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:58 +0000

    Add package devcloudcli/1.4.4/devcloudcli-1.4.4-py3-none-any.whl

---
+++ packages/devcloudcli/devcloudcli-1.4.4-py3-none-any.whl/src/scripts/dl-model-services/openvino-samples-and-demos/openvino-samples/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Copyright (C) 2018-2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+
+# Creating a symbolic link 
+
+ln -s  /opt/intel/openvino_2021/inference_engine/samples/ .
+
+echo -e "\e[1;31mOpenVino Samples Symbolic link created\e[0m"
+
+echo -e "\e[1;32m\nFollow the README.md in the respective folders for usage\e[0m"
