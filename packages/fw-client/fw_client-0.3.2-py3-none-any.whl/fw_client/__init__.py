add609b1618e688a49bf2b2d9c560eb9088e8845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:34 +0000

    Add package fw-client/0.3.2/fw_client-0.3.2-py3-none-any.whl

---
+++ packages/fw-client/fw_client-0.3.2-py3-none-any.whl/fw_client/__init__.py
@@ -0,0 +1,20 @@
+"""Flywheel HTTP API Client."""
+try:
+    from importlib.metadata import version
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version  # type: ignore
+
+from .client import FWClient
+from .config import FWClientConfig
+from .errors import ConnectionError  # pylint: disable=redefined-builtin
+from .errors import ClientError, NotFound, ServerError
+
+__version__ = version(__name__)
+__all__ = [
+    "FWClient",
+    "FWClientConfig",
+    "ConnectionError",
+    "ClientError",
+    "NotFound",
+    "ServerError",
+]
