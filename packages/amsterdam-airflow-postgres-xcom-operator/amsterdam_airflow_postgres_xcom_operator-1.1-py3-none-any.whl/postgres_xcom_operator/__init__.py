08507fb5648d913df1ffcf6a1ffc3327a980142f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:44 +0000

    Add package amsterdam-airflow-postgres-xcom-operator/1.1/amsterdam_airflow_postgres_xcom_operator-1.1-py3-none-any.whl

---
+++ packages/amsterdam-airflow-postgres-xcom-operator/amsterdam_airflow_postgres_xcom_operator-1.1-py3-none-any.whl/postgres_xcom_operator/__init__.py
@@ -0,0 +1,1 @@
+from .postgres_xcom_operator import *
---
+++ packages/amsterdam-airflow-postgres-xcom-operator/amsterdam_airflow_postgres_xcom_operator-1.1-py3-none-any.whl/postgres_xcom_operator/postgres_xcom_operator.py
@@ -0,0 +1,31 @@
+from typing import Optional
+from db import PostgresOnAzureHook
+from db import PostgresOnAzureOperator
+
+
+class PostgresXcomOperator(PostgresOnAzureOperator):
+    """Regular PostgresOperator does not return a value,
+    so cannot do Xcom
+    """
+    def __init__(self, dataset_name:Optional[str] = None, *args, **kwargs):
+        """Initialize.
+
+        args:
+            dataset_name: Name of the dataset as known in the Amsterdam schema.
+                Since the DAG name can be different from the dataset name, the latter
+                can be explicity given. Only applicable for Azure referentie db connection.
+                Defaults to None. If None, it will use the execution context to get the
+                DAG id as surrogate. Assuming that the DAG id equals the dataset name
+                as defined in Amsterdam schema.
+
+        """
+        self.dataset_name = dataset_name
+        self.args = args
+        self.kwargs = kwargs
+        super().__init__(dataset_name=self.dataset_name, *self.args, **self.kwargs)
+
+    def execute(self, context):
+        self.log.info("Executing: %s", self.sql)
+        self.hook = PostgresOnAzureHook(dataset_name=self.dataset_name, context=context, postgres_conn_id=self.postgres_conn_id, schema=self.database)
+        return self.hook.get_first(self.sql, parameters=self.parameters)
+
