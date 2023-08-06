fd198981a2000a6b9ce2e4d380ece9bfaeaaaf67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:27 +0000

    Add package red-team-language/0.1.1/red_team_language-0.1.1.tar.gz

---
+++ packages/red-team-language/red_team_language-0.1.1.tar.gz/red_team_language-0.1.1/rtl/parser/_with.py
@@ -0,0 +1,19 @@
+from rtl.parser.protocol.shell import Shell
+
+
+class With:
+    LINE_TRIGGER = "with"
+    PROTOCOLS = {
+        "shell": Shell.parse,
+    }
+
+    @classmethod
+    def parse(cls, tokens, fh):
+
+        protocol = tokens[1]
+        options = tokens[1:]
+
+        if protocol not in cls.PROTOCOLS.keys():
+            return False
+
+        return cls.PROTOCOLS[protocol](options, fh)
---
+++ packages/red-team-language/red_team_language-0.1.1.tar.gz/red_team_language-0.1.1/rtl/parser/helpers.py
@@ -0,0 +1,56 @@
+import llvmlite.ir as ir
+
+import random
+import string
+
+GENERATE_ID_SET = string.ascii_lowercase + string.ascii_uppercase
+
+
+def helper_create_global_char_array(module, symbol_name, data):
+    """
+    helper_create_global_variable
+        module - the llvmlite module
+        symbol_name - what to call the thing
+        data - the value to set the thing to
+
+    create or locate and then return the symbol defined as 'symbol_name'
+    """
+    try:
+        gv = module.get_global(symbol_name)
+    except KeyError:
+        data_len = len(data)
+        gv = ir.GlobalVariable(
+            module,
+            ir.ArrayType(ir.IntType(8), data_len),
+            symbol_name,
+        )
+        gv.global_constant = False
+        gv.linkage = "private"
+        gv.align = 1
+        gv.initializer = ir.Constant(
+            ir.ArrayType(ir.IntType(8), data_len),
+            [i for i in data],
+        )
+
+    return gv
+
+
+def helper_create_global_int(module, symbol_name, value):
+    gv = ir.GlobalVariable(
+        module,
+        ir.IntType(32),
+        symbol_name,
+    )
+    gv.global_constant = False
+    gv.linkage = "private"
+    gv.align = 1
+    gv.initializer = ir.Constant(
+        ir.IntType(32),
+        value,
+    )
+
+    return gv
+
+
+def helper_generate_id():
+    return "".join(random.sample(GENERATE_ID_SET, 8))
