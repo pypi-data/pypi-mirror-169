528bcd7b2d99a0e016b719f6aa073b6614fbb74c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:09 +0000

    Add package replit-bot/1.1.6/replit_bot-1.1.6-py3-none-any.whl

---
+++ packages/replit-bot/replit_bot-1.1.6-py3-none-any.whl/replit_bot/colors.py
@@ -0,0 +1,9 @@
+# https://gist.github.com/JBlond/2fea43a3049b38287e5e9cefc87b2124
+
+green = "\033[0;32m"
+blue = "\033[0;34m"
+purple = "\033[0;35m"
+red = "\033[0;31m"
+end = "\033[0m"
+bold_green = "\033[1;92m"
+bold_blue = "\033[1;94m"
