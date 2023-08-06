8dbf0a506970924c55d68df8d6da10e4370c99fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:29 +0000

    Add package agileupisa/0.0.14/agileupisa-0.0.14-py3-none-any.whl

---
+++ packages/agileupisa/agileupisa-0.0.14-py3-none-any.whl/agileupisa/cli.py
@@ -0,0 +1,53 @@
+import click
+import pkg_resources
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+@click.group(help='CLI to manage Informatica Secure Agents.')
+def cli() -> int:
+    pass
+
+
+@cli.command(help='Display the current version.')
+def version() -> None:
+    click.echo(f"AgileUp ISA Version: {pkg_resources.get_distribution('agileupisa').version}")
+
+
+@cli.command(help='Install software.')
+def install() -> None:
+    click.secho('Install software', fg='green')
+
+
+@cli.command(help='Configure software.')
+def configure() -> None:
+    click.secho('Configure software', fg='green')
+
+
+@cli.command(help='Smoke test application.')
+def smoke() -> None:
+    click.secho('Smoke test application', fg='green')
+
+
+@cli.command(help='Start application.')
+def start() -> None:
+    click.secho('Start application', fg='green')
+
+
+@cli.command(help='Stop application.')
+def stop() -> None:
+    click.secho('Stop application', fg='green')
+
+
+@cli.command(help='Backup application.')
+def backup() -> None:
+    click.secho('Backup application', fg='green')
+
+
+@cli.command(help='Restore application.')
+def restore() -> None:
+    click.secho('Restore application', fg='green')
+
+
+if __name__ == '__main__':
+    exit(cli())
