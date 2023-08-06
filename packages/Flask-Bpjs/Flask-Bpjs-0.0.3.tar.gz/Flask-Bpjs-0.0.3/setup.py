e2f1ecb1be8763022de5cebbe26f4c9820108e74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:44 +0000

    Add package Flask-Bpjs/0.0.3/Flask-Bpjs-0.0.3.tar.gz

---
+++ packages/Flask-Bpjs/Flask-Bpjs-0.0.3.tar.gz/Flask-Bpjs-0.0.3/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"""
+Flask-Telebot
+-------------
+This is the description for that library
+"""
+from setuptools import setup
+
+setup(
+    name='Flask-Bpjs',
+    version='0.0.3',
+    url='https://github.com/GatLab/Flask-Bpjs.git',
+    license='BSD',
+    author='Gatlab',
+    author_email='dev@gatlab.co',
+    description='Bpjs on flask',
+    long_description=__doc__,
+    py_modules=['flask_bpjs'],
+    # if you would be using a package instead use packages instead
+    # of py_modules:
+    # packages=['flask_telebot'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'lzstring',
+        'pycryptodome',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
