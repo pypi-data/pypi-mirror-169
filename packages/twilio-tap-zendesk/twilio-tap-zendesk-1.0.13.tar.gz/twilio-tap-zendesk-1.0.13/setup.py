f5067dd151932853840c08bac7896d002621d883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:15 +0000

    Add package twilio-tap-zendesk/1.0.13/twilio-tap-zendesk-1.0.13.tar.gz

---
+++ packages/twilio-tap-zendesk/twilio-tap-zendesk-1.0.13.tar.gz/twilio-tap-zendesk-1.0.13/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+      name='twilio-tap-zendesk',
+      version='1.0.13',
+      description='Singer.io tap for extracting data from the Zendesk API',
+      author='Twilio',
+      url='https://github.com/twilio-labs/twilio-tap-zendesk',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_zendesk'],
+      install_requires=[
+          'pipelinewise-singer-python==1.2.0',
+          'zenpy==2.0.25',
+      ],
+      extras_require={
+          'dev': [
+              'ipdb',
+              'pylint',
+              'nose',
+              'nose-watch',
+          ]
+      },
+      entry_points='''
+          [console_scripts]
+          tap-zendesk=tap_zendesk:main
+      ''',
+      packages=['tap_zendesk'],
+      include_package_data=True,
+)
