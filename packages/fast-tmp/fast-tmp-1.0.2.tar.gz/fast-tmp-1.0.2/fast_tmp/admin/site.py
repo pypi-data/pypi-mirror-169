2c93c52dcf5e1b303d90da2f778e1b78e72d52dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:59 +0000

    Add package fast-tmp/1.0.2/fast-tmp-1.0.2.tar.gz

---
+++ packages/fast-tmp/fast-tmp-1.0.2.tar.gz/fast-tmp-1.0.2/fast_tmp/admin/site.py
@@ -0,0 +1,42 @@
+from fast_tmp.models import Group, Permission, User
+from fast_tmp.site import ModelAdmin
+from fast_tmp.site.field import Password
+
+
+class UserAdmin(ModelAdmin):
+    model = User
+    list_display = ("name", "username", "is_active", "is_superuser", "is_staff")
+    inline = ("is_active", "is_superuser", "is_staff")
+    create_fields = (
+        "username",
+        "password",
+        "name",
+        "groups",
+        "is_active",
+        "is_superuser",
+        "is_staff",
+    )
+    update_fields = (
+        "username",
+        "password",
+        "name",
+        "groups",
+        "is_active",
+        "is_superuser",
+        "is_staff",
+    )
+    fields = {"password": Password}  # type: ignore
+
+
+class GroupAdmin(ModelAdmin):
+    model = Group
+    list_display = ("name", "users", "permissions")
+    create_fields = ("name", "users", "permissions")
+    update_fields = ("name", "users", "permissions")
+
+
+class PermissionAdmin(ModelAdmin):
+    model = Permission
+    list_display = ("label", "codename", "groups")
+    create_fields = ("label", "codename", "groups")
+    update_fields = ("label", "codename", "groups")
