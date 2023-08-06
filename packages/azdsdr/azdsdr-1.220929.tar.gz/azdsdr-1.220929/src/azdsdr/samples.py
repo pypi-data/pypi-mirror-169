865d814c477ababd34414ad500edaf05e12e8ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:07 +0000

    Add package azdsdr/1.220929/azdsdr-1.220929.tar.gz

---
+++ packages/azdsdr/azdsdr-1.220929.tar.gz/azdsdr-1.220929/src/azdsdr/samples.py
@@ -0,0 +1,16 @@
+#%% 
+from azdsdr.azdsdr import DremioReader
+username    = "anzhu@microsoft.com"
+token       = "token string"
+dr          = DremioReader(username=username,token=token)
+
+#%%
+sql = '''
+select 
+    * 
+from 
+    BizApps.PROD."vw_customer_powerapps_portalusage"
+limit 10
+'''
+r = dr.run_sql(sql)
+display(r)
