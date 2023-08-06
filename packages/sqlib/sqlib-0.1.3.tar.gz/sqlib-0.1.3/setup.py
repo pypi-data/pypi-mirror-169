015f3cb1d581acf459bfbcc8d0b9bd4acf0e4c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:33 +0000

    Add package sqlib/0.1.3/sqlib-0.1.3.tar.gz

---
+++ packages/sqlib/sqlib-0.1.3.tar.gz/sqlib-0.1.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, Extension
+
+version = '0.1.3'
+
+long_description = '''
+It provides a simple interface for quick and convenient work with MariaDB.
+'''
+
+setup(
+    name='sqlib',
+
+    version=version,
+
+    packages=['sqlib'],
+    install_requires=['mariadb'],
+
+    url='https://github.com/meyiapir/sqlib',
+
+    license='Apache License, Version 2.0, see LICENSE file',
+
+    author='meyap',
+
+    author_email='mmeyiapir@gmail.com',
+
+    description='Sqlib is a small ORM for MariaDB.',
+
+    description_content_type='text/markdown',
+
+    long_description=open('README.md').read,
+    long_description_content_type='text/markdown',
+)
