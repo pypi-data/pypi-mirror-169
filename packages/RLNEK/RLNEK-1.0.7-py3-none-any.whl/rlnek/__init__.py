b8879b7831de8b19e820773e7779ae97686af3cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:27 +0000

    Add package RLNEK/1.0.7/RLNEK-1.0.7-py3-none-any.whl

---
+++ packages/RLNEK/RLNEK-1.0.7-py3-none-any.whl/rlnek/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 15 11:42:22 2022
+
+@author: Allison
+"""
+
---
+++ packages/RLNEK/RLNEK-1.0.7-py3-none-any.whl/rlnek/__main__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 27 17:00:13 2022
+
+@author: Allison
+"""
+
+import os
+
+os.system('NTOptimization.py')
