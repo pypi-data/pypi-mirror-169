ea5a0b42ea56fe06161d9b2a311c826a4eb5420c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:50 +0000

    Add package aflowey/0.1.3/aflowey-0.1.3.tar.gz

---
+++ packages/aflowey/aflowey-0.1.3.tar.gz/aflowey-0.1.3/src/aflowey/async_flow.py
@@ -0,0 +1,109 @@
+from copy import copy
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Union
+
+from aflowey.functions import ensure_callable
+from aflowey.f import F
+from aflowey.functions import ensure_f
+from aflowey.functions import named
+from aflowey.functions import side_effect
+from aflowey.types import Function
+
+
+class AsyncFlow:
+    """
+    Describe an async flow chaining function
+
+    >>>flow = (AsyncFlow() >> gen1 >> gen2 >> gen3)
+
+    >>>await flow.run()
+    """
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        # first arguments input
+        self.args = args
+        self.kwargs = kwargs
+
+        # function to be executed in the flow
+        self.aws: List[F] = []
+
+        # intermediary store results
+        self.steps: Dict[str, Any] = {}
+
+    @staticmethod
+    def ensure_f_function(func: Union[Function, F]) -> F:
+        new_func: Function = ensure_callable(func)
+        return ensure_f(new_func)
+
+    def __rshift__(
+        self, aws: Union[List[Union[F, Function]], Union[F, Function]]
+    ) -> "AsyncFlow":
+        """add a new step to the flow
+
+        Args:
+            aws: list of callable or callable
+        """
+        if isinstance(aws, list):
+            self.aws += [self.ensure_f_function(a) for a in aws]
+        else:
+            self.aws.append(self.ensure_f_function(aws))
+
+        return self
+
+    def __copy__(self) -> "AsyncFlow":
+        """make a shallow copy"""
+        aws = self.aws[:]
+        args = self.args[:]
+        kwargs = self.kwargs.copy()
+        new_flow = self.__class__(*args, **kwargs)
+        new_flow.aws = aws
+        return new_flow
+
+    async def run(self) -> Any:
+        """run the flow
+
+        Returns:
+            coroutine
+        """
+        from aflowey.single_executor import SingleFlowExecutor
+
+        return await SingleFlowExecutor(self).execute_flow(is_root=True)
+
+    @staticmethod
+    def from_args(*args: Any, **kwargs: Any) -> "AsyncFlow":
+        """create a flow with given arguments as first input"""
+        return AsyncFlow(*args, **kwargs)
+
+    @staticmethod
+    def empty() -> "AsyncFlow":
+        """create an empty flow"""
+        return AsyncFlow()
+
+    @staticmethod
+    def from_flow(flow: "AsyncFlow") -> "AsyncFlow":
+        """create a new flow from given flow, copying it
+        (args, kwargs, and aws functions)
+        """
+        assert isinstance(flow, AsyncFlow)
+        return copy(flow)
+
+
+# aliases
+aflow = async_flow = AsyncFlow
+
+
+def step(
+    *func: Union[F, Function], name: Optional[str] = None, impure: bool = False
+) -> Union[Function, List[F], F]:
+    if len(func) > 1 and impure is False:
+        raise ValueError(
+            "Can not have several functions for impure method"
+        )  # pragma: no cover
+    if impure:
+        new_func = side_effect(*func)
+        return new_func
+    named_func = func[0]
+    return named(named_func, name) if name is not None else named_func
---
+++ packages/aflowey/aflowey-0.1.3.tar.gz/aflowey-0.1.3/src/aflowey/executor.py
@@ -0,0 +1,93 @@
+import asyncio
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+from typing import Any
+from typing import Awaitable
+from typing import List
+from typing import Union
+from typing import cast
+
+from aflowey import AsyncFlow
+from aflowey import aflow
+from aflowey.runner import AsyncRunner, ExecutorType
+from aflowey.single_executor import SingleFlowExecutor
+from aflowey.types import AnyCallable
+
+FlowOrListFlow = Union[List[AsyncFlow], AsyncFlow]
+
+
+class AsyncFlowExecutor:
+    """
+    Execute several flows concurrently
+
+    >>> await (aexec().from_flows(flows) | flow).run()
+
+    """
+
+    def __init__(self) -> None:
+        """
+        Creates a new async flow executor
+        """
+        self.flows: List[Union[AsyncFlow, List[AsyncFlow]]] = []
+
+    def __or__(self, flow: Any) -> "AsyncFlowExecutor":
+        """add a flow to execute in parallel"""
+        return self.from_flows(flow)
+
+    @staticmethod
+    async def _execute_one_flow(flow: AsyncFlow) -> Any:
+        """Run"""
+        return await SingleFlowExecutor(flow).execute_flow(is_root=True)
+
+    def _execute_or_gather(self, flow: FlowOrListFlow) -> Awaitable[Any]:
+        if isinstance(flow, list):
+            flows_task = [self._execute_one_flow(flow) for flow in flow]
+            return asyncio.gather(*flows_task)
+        return self._execute_one_flow(flow)
+
+    def run(self, **kwargs: Any) -> Any:
+        """main function to run stuff in parallel"""
+        # if other flow run in parallel
+        flows = [self._execute_or_gather(flow) for flow in self.flows]
+        return asyncio.gather(*flows, **kwargs)
+
+    def thread_runner(self, **kwargs):
+        return AsyncRunner(self.run, ExecutorType.THREAD_POOL, **kwargs)
+
+    def process_runner(self, **kwargs):
+        return AsyncRunner(self.run, ExecutorType.PROCESS_POOL, **kwargs)
+
+    def with_thread_runner(self, thread_pool: ThreadPoolExecutor, **kwargs):
+        return AsyncRunner(self.run, thread_pool, **kwargs)
+
+    def with_process_runner(self, process_pool: ProcessPoolExecutor, **kwargs):
+        return AsyncRunner(self.run, process_pool, **kwargs)
+
+    @staticmethod
+    def ensure_flow(fn: Any, arg: Any = None) -> AsyncFlow:
+        if isinstance(fn, AsyncFlow):
+            return fn
+        flow = aflow.empty() if arg is None else aflow.from_args(arg)
+        return cast(AsyncFlow, flow >> fn)
+
+    def from_flows(self, flows: Any) -> "AsyncFlowExecutor":
+        """create a new executor from one flow or array of flows"""
+        if not isinstance(flows, list):
+            flows = AsyncFlowExecutor.ensure_flow(flows)
+            self.flows.append(flows)
+        else:
+            new_flows = [AsyncFlowExecutor.ensure_flow(value) for value in flows]
+            self.flows.append(new_flows)
+        return self
+
+    @staticmethod
+    def starmap(*flows: Any) -> AnyCallable:
+        async def wrapper(arg: Any) -> Any:
+            new_flows = [AsyncFlowExecutor.ensure_flow(fn, arg) for fn in flows]
+            result = await AsyncFlowExecutor().from_flows(new_flows).run()
+            return result[0]
+
+        return wrapper
+
+
+async_exec = aexec = AsyncFlowExecutor
+flows_from_arg = spawn_flows = run_flows = astarmap = AsyncFlowExecutor.starmap
