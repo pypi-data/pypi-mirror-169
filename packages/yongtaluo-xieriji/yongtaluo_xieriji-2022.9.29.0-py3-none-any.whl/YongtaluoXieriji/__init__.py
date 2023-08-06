7aa59fc168166cc27f5b4f56082a8c0ca1c254e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:40 +0000

    Add package yongtaluo-xieriji/2022.9.29.0/yongtaluo_xieriji-2022.9.29.0-py3-none-any.whl

---
+++ packages/yongtaluo-xieriji/yongtaluo_xieriji-2022.9.29.0-py3-none-any.whl/YongtaluoXieriji/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yongtaluo-xieriji
+https://github.com/apachecn/yongtaluo-xieriji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.9.29.0"
