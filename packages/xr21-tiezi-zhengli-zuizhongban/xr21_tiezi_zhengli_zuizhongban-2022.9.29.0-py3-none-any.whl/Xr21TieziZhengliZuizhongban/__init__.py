529a78d7944f1b40150d1a8acd5f15c35f029481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:35 +0000

    Add package xr21-tiezi-zhengli-zuizhongban/2022.9.29.0/xr21_tiezi_zhengli_zuizhongban-2022.9.29.0-py3-none-any.whl

---
+++ packages/xr21-tiezi-zhengli-zuizhongban/xr21_tiezi_zhengli_zuizhongban-2022.9.29.0-py3-none-any.whl/Xr21TieziZhengliZuizhongban/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xr21-tiezi-zhengli-zuizhongban
+https://github.com/apachecn/xr21-tiezi-zhengli-zuizhongban"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.9.29.0"
