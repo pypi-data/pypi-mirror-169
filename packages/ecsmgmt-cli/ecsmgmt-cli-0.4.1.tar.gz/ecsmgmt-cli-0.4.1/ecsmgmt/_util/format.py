958bb25ccb5e5f716702ccd02111d0bcd8bc68a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:55 +0000

    Add package ecsmgmt-cli/0.4.1/ecsmgmt-cli-0.4.1.tar.gz

---
+++ packages/ecsmgmt-cli/ecsmgmt-cli-0.4.1.tar.gz/ecsmgmt-cli-0.4.1/ecsmgmt/_util/format.py
@@ -0,0 +1,21 @@
+from tabulate import tabulate
+import yaml
+
+
+class IndentedListDumper(yaml.SafeDumper):
+    def increase_indent(self, flow=False, indentless=False):
+        return super(IndentedListDumper, self).increase_indent(flow, False)
+
+
+def pretty_table(data: list, headers: list = None, tablefmt: str = 'github') -> str:
+    """Tabulate wrapper function with predefined format"""
+    if headers is None:
+        table = tabulate(data, tablefmt=tablefmt)
+    else:
+        table = tabulate(data, headers=headers, tablefmt=tablefmt)
+    return table
+
+
+def pretty_info(data: dict) -> str:
+    """Print data in a nice form"""
+    return yaml.safe_dump(data)
