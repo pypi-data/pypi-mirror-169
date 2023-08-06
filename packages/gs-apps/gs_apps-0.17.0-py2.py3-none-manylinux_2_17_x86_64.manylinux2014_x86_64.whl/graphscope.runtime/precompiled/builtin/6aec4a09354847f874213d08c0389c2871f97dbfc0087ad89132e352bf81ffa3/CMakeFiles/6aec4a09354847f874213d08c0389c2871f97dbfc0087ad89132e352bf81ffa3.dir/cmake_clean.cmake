b5f12a8f7c7133ea8ee2230258987107f81948eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:41 +0000

    Add package gs-apps/0.17.0/gs_apps-0.17.0-py2.py3-none-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/gs-apps/gs_apps-0.17.0-py2.py3-none-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/graphscope.runtime/precompiled/builtin/6aec4a09354847f874213d08c0389c2871f97dbfc0087ad89132e352bf81ffa3/CMakeFiles/6aec4a09354847f874213d08c0389c2871f97dbfc0087ad89132e352bf81ffa3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/6aec4a09354847f874213d08c0389c2871f97dbfc0087ad89132e352bf81ffa3.dir/opt/graphscope/include/graphscope/frame/app_frame.cc.o"
+  "CMakeFiles/6aec4a09354847f874213d08c0389c2871f97dbfc0087ad89132e352bf81ffa3.dir/opt/graphscope/include/graphscope/frame/app_frame.cc.o.d"
+  "lib6aec4a09354847f874213d08c0389c2871f97dbfc0087ad89132e352bf81ffa3.pdb"
+  "lib6aec4a09354847f874213d08c0389c2871f97dbfc0087ad89132e352bf81ffa3.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/6aec4a09354847f874213d08c0389c2871f97dbfc0087ad89132e352bf81ffa3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ packages/gs-apps/gs_apps-0.17.0-py2.py3-none-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/graphscope.runtime/precompiled/builtin/fb87f09763a37caaa06855c2a74a6a15b426578e8e75f538a4d0d9c606b794a6/CMakeFiles/fb87f09763a37caaa06855c2a74a6a15b426578e8e75f538a4d0d9c606b794a6.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fb87f09763a37caaa06855c2a74a6a15b426578e8e75f538a4d0d9c606b794a6.dir/opt/graphscope/include/graphscope/frame/app_frame.cc.o"
+  "CMakeFiles/fb87f09763a37caaa06855c2a74a6a15b426578e8e75f538a4d0d9c606b794a6.dir/opt/graphscope/include/graphscope/frame/app_frame.cc.o.d"
+  "libfb87f09763a37caaa06855c2a74a6a15b426578e8e75f538a4d0d9c606b794a6.pdb"
+  "libfb87f09763a37caaa06855c2a74a6a15b426578e8e75f538a4d0d9c606b794a6.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fb87f09763a37caaa06855c2a74a6a15b426578e8e75f538a4d0d9c606b794a6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
