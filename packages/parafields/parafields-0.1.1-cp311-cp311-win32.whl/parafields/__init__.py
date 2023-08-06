3f316086a8c46d9200ddb13d6d52b7b87b50a5ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:53 +0000

    Add package parafields/0.1.1/parafields-0.1.1-cp311-cp311-win32.whl

---
+++ packages/parafields/parafields-0.1.1-cp311-cp311-win32.whl/parafields/__init__.py
@@ -0,0 +1,9 @@
+# Export the version given in project metadata
+from importlib import metadata
+
+__version__ = metadata.version(__package__)
+del metadata
+
+from parafields.field import generate_field
+from parafields.interactive import interactive_generate_field
+from parafields.mpi import MPI
