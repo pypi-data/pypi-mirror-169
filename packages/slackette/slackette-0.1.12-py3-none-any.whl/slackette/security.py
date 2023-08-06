52a528c7f6cdf3593576bf6ab6b56ad152573e19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:20 +0000

    Add package slackette/0.1.12/slackette-0.1.12-py3-none-any.whl

---
+++ packages/slackette/slackette-0.1.12-py3-none-any.whl/slackette/security.py
@@ -0,0 +1,74 @@
+from functools import wraps
+from hashlib import sha256
+from hmac import new as hmac
+from time import time
+from typing import Any, Callable, Union, cast
+
+from .types import Endpoint, RequestProtocol, StringProvider
+
+
+class SlackHeaders(object):
+    X_SLACK_SIGNATURE = "X-Slack-Signature"
+    X_SLACK_REQUEST_TIMESTAMP = "X-Slack-Request-Timestamp"
+
+
+class InvalidSignatureError(ValueError):
+    pass
+
+
+class ExpiredTimestampError(ValueError):
+    pass
+
+
+def compute_slack_signature(
+    request: RequestProtocol,
+    signing_secret: str,
+    version: str,
+) -> str:
+    """Compute a Slack signature from the given request."""
+    timestamp = request.headers.get(SlackHeaders.X_SLACK_REQUEST_TIMESTAMP)
+    if abs(time() - float(cast(int, timestamp))) > 60 * 5:
+        raise ExpiredTimestampError()
+    body = request.get_data()
+    message = f"{version}:{timestamp}:{body}"
+    signature = hmac(
+        signing_secret.encode("utf-8"),
+        msg=message.encode("utf-8"),
+        digestmod=sha256,
+    )
+    return f"{version}={signature.hexdigest()}"
+
+
+def SignedSlackRoute(
+    signing_secret: Union[str, StringProvider],
+    version: str = "v0",
+) -> Callable[[Endpoint], Endpoint]:
+    """
+    Decorator that acts as middleware to ensure that an
+    incoming HTTP request is a valid issued Slack request.
+
+    See https://api.slack.com/authentication/verifying-requests-from-slack
+    """
+
+    def wrapper(endpoint: Endpoint) -> Endpoint:
+        @wraps(endpoint)
+        def middleware(request: RequestProtocol) -> Any:
+            if isinstance(signing_secret, str):
+                secret = signing_secret
+            else:
+                secret = signing_secret()
+            expected = request.headers.get(SlackHeaders.X_SLACK_SIGNATURE)
+            actual = compute_slack_signature(
+                request,
+                secret,
+                version,
+            )
+            print(f"slackette.security: Received signature {expected}")
+            print(f"slackette.security: Generated signature {actual}")
+            if actual != expected:
+                raise InvalidSignatureError()
+            return endpoint(request)
+
+        return middleware
+
+    return wrapper
