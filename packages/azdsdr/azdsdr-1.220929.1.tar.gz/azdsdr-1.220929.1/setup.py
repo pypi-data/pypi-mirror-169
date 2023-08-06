2dbacdcd63ffd3f1f52d65781ce3ee88c9c5332c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:20 +0000

    Add package azdsdr/1.220929.1/azdsdr-1.220929.1.tar.gz

---
+++ packages/azdsdr/azdsdr-1.220929.1.tar.gz/azdsdr-1.220929.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='azdsdr',
+    version='1.220929.1',
+    license='Apache License',
+    author="Andrew Zhu",
+    author_email='xhinker@hotmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/xhinker/azdsdr',
+    keywords='example project',
+    install_requires=[
+          'pandas'
+          ,'pyodbc'
+      ],
+)
