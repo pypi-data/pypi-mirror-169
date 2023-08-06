47a2c38d867a0b704a9a13109afebdd6f23aa306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:22 +0000

    Add package awscert/10.7.8/awscert-10.7.8.tar.gz

---
+++ packages/awscert/awscert-10.7.8.tar.gz/awscert-10.7.8/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="awscert",
+    version="10.7.8",
+    author="Aloneee walker",
+    author_email="aloneewalker@facebook.com",
+    description="awscert of awscert of awscert",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
