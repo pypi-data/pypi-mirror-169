ed0b019d911afd27619e30678989e95c1ccd6caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:38 +0000

    Add package commonsharedclasses/0.0.1/commonsharedclasses-0.0.1.tar.gz

---
+++ packages/commonsharedclasses/commonsharedclasses-0.0.1.tar.gz/commonsharedclasses-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='commonsharedclasses',
+    version='0.0.1',
+    description='This package will contain all common classes',
+    author='Himanshu Shekhar',
+    packages=['commonsharedclasses'],
+    zip_safe=False
+)
