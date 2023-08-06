f6461d6669c5ade8b88415bf28427afe3236ca18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:47 +0000

    Add package ninja/1.10.2.4/ninja-1.10.2.4.tar.gz

---
+++ packages/ninja/ninja-1.10.2.4.tar.gz/ninja-1.10.2.4/NinjaUrls.cmake
@@ -0,0 +1,8 @@
+
+#-----------------------------------------------------------------------------
+# Ninja sources
+set(unix_source_url       "https://github.com/Kitware/ninja/archive/v1.10.2.g51db2.kitware.jobserver-1.tar.gz")
+set(unix_source_sha256    "549c31ee596566b952c600e23eb9b8d39a4112cd5fdeb2e5a83370669176da40")
+
+set(windows_source_url    "https://github.com/Kitware/ninja/archive/v1.10.2.g51db2.kitware.jobserver-1.zip")
+set(windows_source_sha256 "2dfd74ac583340869d5e236892d782f96bf70533706c1a2d7c968c43ded474bc")
