8495f3e03f7c99fdaf35feca4c6fd643a900b637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:17 +0000

    Add package ciyu-linghun-zhiliao-shijian-shouce/2022.9.29.0/ciyu_linghun_zhiliao_shijian_shouce-2022.9.29.0-py3-none-any.whl

---
+++ packages/ciyu-linghun-zhiliao-shijian-shouce/ciyu_linghun_zhiliao_shijian_shouce-2022.9.29.0-py3-none-any.whl/CiyuLinghunZhiliaoShijianShouce/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ciyu-linghun-zhiliao-shijian-shouce
+https://github.com/apachecn/ciyu-linghun-zhiliao-shijian-shouce"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.9.29.0"
