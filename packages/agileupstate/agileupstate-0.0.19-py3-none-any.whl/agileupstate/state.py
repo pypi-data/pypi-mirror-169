de21e574d3fe7c2f0304eb7c18ecc98ada453f7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:28 +0000

    Add package agileupstate/0.0.19/agileupstate-0.0.19-py3-none-any.whl

---
+++ packages/agileupstate/agileupstate-0.0.19-py3-none-any.whl/agileupstate/state.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+from typing import Any
+
+import click
+import yaml
+
+from agileupstate.terminal import print_cross_message
+
+
+class State:
+
+    def __init__(self, client, state_file='siab-state.yml'):
+        self.client = client
+        self.state_file = state_file
+        self.format_vault = 'siab/' \
+                            + self.client.id + '-' \
+                            + self.client.cloud + '-' \
+                            + self.client.location + '-' \
+                            + self.client.context
+
+        self.format_client = {'client-id': self.client.id,
+                              'client-cloud': self.client.cloud,
+                              'client-location': self.client.location,
+                              'client-context': self.client.context,
+                              }
+
+    def file(self):
+        return self.state_file
+
+    def write(self) -> None:
+        file = Path(self.state_file)
+        with open(file, 'w') as f:
+            yaml.dump(self.format_client, f, sort_keys=False, default_flow_style=False)
+
+    def read(self) -> dict:
+        file = Path(self.state_file)
+        if file.is_file():
+            click.echo(f'- Reading {file}')
+            with open(file, 'r') as f:
+                return yaml.safe_load(f)
+        else:
+            print_cross_message(f'Could not read from state file {file}!', leave=True)
