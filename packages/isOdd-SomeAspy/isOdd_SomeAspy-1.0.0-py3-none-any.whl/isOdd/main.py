62af31e434907ec967066f745b93f61df49a85c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:27 +0000

    Add package isOdd-SomeAspy/1.0.0/isOdd_SomeAspy-1.0.0-py3-none-any.whl

---
+++ packages/isOdd-SomeAspy/isOdd_SomeAspy-1.0.0-py3-none-any.whl/isOdd/main.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022 Aiden Baker
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+def checkOdd(input):
+    if (input % 2 !=0):
+        return True
+    return False
