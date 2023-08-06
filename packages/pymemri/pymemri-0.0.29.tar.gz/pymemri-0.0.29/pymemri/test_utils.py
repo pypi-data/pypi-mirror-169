201c2cd8a32cbb285c0383668748ba179548cbca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:46 +0000

    Add package pymemri/0.0.29/pymemri-0.0.29.tar.gz

---
+++ packages/pymemri/pymemri-0.0.29.tar.gz/pymemri-0.0.29/pymemri/test_utils.py
@@ -0,0 +1,18 @@
+# hide
+import os
+from pathlib import Path
+
+
+def get_ci_variables(*varnames):
+    """
+    Gets ci variables from environment, can be set on gitlab in settings -> ci/cd -> variables
+    """
+    return {varname: os.environ.get(varname) for varname in varnames}
+
+
+# hide
+from pathlib import Path
+
+
+def get_project_root():
+    return Path(__file__).parent.parent
