5e9095f67c2830d26de932e7ffc0aa8c1e3108e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:24 +0000

    Add package robotframework-debug/3.0.0/robotframework_debug-3.0.0-py3-none-any.whl

---
+++ packages/robotframework-debug/robotframework_debug-3.0.0-py3-none-any.whl/DebugLibrary/keywords.py
@@ -0,0 +1,39 @@
+import sys
+
+from robot.libraries.BuiltIn import run_keyword_variant
+
+from .debugcmd import DebugCmd
+from .steplistener import RobotLibraryStepListenerMixin, is_step_mode
+from .styles import print_output
+
+
+class DebugKeywords(RobotLibraryStepListenerMixin):
+    """Debug Keywords for RobotFramework."""
+
+    def debug(self):
+        """Open a interactive shell, run any RobotFramework keywords.
+
+        Keywords separated by two space or one tab, and Ctrl-D to exit.
+        """
+        # re-wire stdout so that we can use the cmd module and have readline
+        # support
+        old_stdout = sys.stdout
+        sys.stdout = sys.__stdout__
+        try:
+
+            show_intro = not is_step_mode()
+            if show_intro:
+                print_output("\n>>>>>", "Enter interactive shell")
+
+            self.debug_cmd = DebugCmd()
+            if show_intro:
+                self.debug_cmd.cmdloop()
+            else:
+                self.debug_cmd.cmdloop(intro="")
+
+            show_intro = not is_step_mode()
+            if show_intro:
+                print_output("\n>>>>>", "Exit shell.")
+        finally:
+            # put stdout back where it was
+            sys.stdout = old_stdout
