04ac7be60c58260dfb283dbf8005863ea7b245a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:47 +0000

    Add package starlite/1.23.0/starlite-1.23.0-py3-none-any.whl

---
+++ packages/starlite/starlite-1.23.0-py3-none-any.whl/starlite/exceptions/__init__.py
@@ -0,0 +1,34 @@
+# flake8: noqa
+from .exceptions import (
+    HTTPException,
+    ImproperlyConfiguredException,
+    InternalServerException,
+    MethodNotAllowedException,
+    MissingDependencyException,
+    NotAuthorizedException,
+    NotFoundException,
+    PermissionDeniedException,
+    ServiceUnavailableException,
+    StarLiteException,
+    TemplateNotFoundException,
+    TooManyRequestsException,
+    ValidationException,
+    WebSocketException,
+)
+
+__all__ = [
+    "HTTPException",
+    "ImproperlyConfiguredException",
+    "InternalServerException",
+    "MethodNotAllowedException",
+    "MissingDependencyException",
+    "NotAuthorizedException",
+    "NotFoundException",
+    "PermissionDeniedException",
+    "ServiceUnavailableException",
+    "StarLiteException",
+    "TemplateNotFoundException",
+    "TooManyRequestsException",
+    "ValidationException",
+    "WebSocketException",
+]
