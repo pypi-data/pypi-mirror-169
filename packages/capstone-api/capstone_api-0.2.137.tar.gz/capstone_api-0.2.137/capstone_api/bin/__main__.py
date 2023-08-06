0c9e070d4bd4bd398a2f20ec293f27c944bc29dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:50 +0000

    Add package capstone-api/0.2.137/capstone_api-0.2.137.tar.gz

---
+++ packages/capstone-api/capstone_api-0.2.137.tar.gz/capstone_api-0.2.137/capstone_api/bin/__main__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from capstone_api.discord_api import DiscordAPI
+from capstone_api.canvas_api import CanvasAPI
+import argparse
+import sys
+
+
+def printTeams():
+    discord = DiscordAPI()
+    for team in discord.getTeamCategories():
+        print(team.get('name'))
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-t', '--teams', default=False, action='store_true', help="print team names (scrape from discord)")
+    args = ap.parse_args()
+
+    if args.teams:
+        printTeams()
+
+if __name__ == '__main__':
+    sys.exit(main())
