f135d71d48186eb9c4346d46fc0cd44e0afcbf08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:29 +0000

    Add package kgdata/3.0.4/kgdata-3.0.4.tar.gz

---
+++ packages/kgdata/kgdata-3.0.4.tar.gz/kgdata-3.0.4/kgdata/wikidata/models/__init__.py
@@ -0,0 +1,21 @@
+from .wdclass import WDClass
+from .wdproperty import WDProperty, WDPropertyDomains, WDPropertyRanges
+from .propertystats import WDQuantityPropertyStats, QuantityStats
+from .wdentity import WDEntity
+from .wdvalue import (
+    WDValue,
+    WDValueKind,
+    WDValueType,
+    WDValueQuantity,
+    WDValueMonolingualText,
+    WDValueEntityId,
+    WDValueGlobeCoordinate,
+    WDValueString,
+    WDValueTime,
+    ValueGlobeCoordinate,
+    ValueMonolingualText,
+    ValueQuantity,
+    ValueTime,
+    ValueWikibaseEntityId,
+)
+from .wdentitylabel import WDEntityLabel
