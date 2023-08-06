e10693e1583a5193e7910552bcd1433dd68ef47f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:27 +0000

    Add package CompNeuroPy/0.0.8/CompNeuroPy-0.0.8.tar.gz

---
+++ packages/CompNeuroPy/CompNeuroPy-0.0.8.tar.gz/CompNeuroPy-0.0.8/setup.py
@@ -0,0 +1,31 @@
+# setuptools
+try:
+    import setuptools
+except:
+    print('Error : Python package "setuptools" is required.')
+    assert False
+
+dependencies = [
+    "numpy",
+    "scipy",
+    "matplotlib",
+    "cython",
+    "sympy",
+    "hyperopt",
+    "ANNarchy",
+    "pandas",
+    "sbi",
+    "torch",
+]
+
+setuptools.setup(
+    name="CompNeuroPy",
+    version="0.0.8",
+    license="MIT",
+    author="Oliver Maith",
+    author_email="oli_maith@gmx.de",
+    description="General package for computational neuroscience with ANNarchy.",
+    url="https://github.com/Olimaol/compneuropy",
+    packages=setuptools.find_packages(),
+    install_requires=dependencies,
+)
