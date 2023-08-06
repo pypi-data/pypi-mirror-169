0d6d74fbdb3957be72fbfb4a14b094606c772928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:37 +0000

    Add package servalcat/0.2.122/servalcat-0.2.122.tar.gz

---
+++ packages/servalcat/servalcat-0.2.122.tar.gz/servalcat-0.2.122/setup.py
@@ -0,0 +1,19 @@
+from __future__ import division, absolute_import, print_function
+import setuptools
+from numpy.distutils.core import setup, Extension
+import servalcat
+
+setup(name='servalcat',
+    version=servalcat.__version__,
+    author='Keitaro Yamashita and Garib N. Murshudov',
+    url='https://github.com/keitaroyam/servalcat',
+    description= 'Structure refinement and validation for crystallography and single particle analysis',
+    license='MPL-2.0',
+    packages=setuptools.find_packages(),
+    install_requires=['numpy>=1.15','scipy','pandas>=0.24.2', 'gemmi==0.5.7'],
+    entry_points={
+      'console_scripts': [
+          'servalcat = servalcat.command_line:main',
+                          ],
+      },
+    zip_safe= False)
