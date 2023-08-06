be55ea82a43489d9acc9df9c3ba42f99d6c89c4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:16 +0000

    Add package chessengine/0.1.2/chessengine-0.1.2-py3-none-any.whl

---
+++ packages/chessengine/chessengine-0.1.2-py3-none-any.whl/chessengine/play.py
@@ -0,0 +1,15 @@
+from .bitboard import Board
+
+
+if __name__ == "__main__":
+    player_side = input("Do you want to play white or black (w/b)? - ")
+    while not player_side.lower().strip().startswith(("b", "w")):
+        print(
+            f"You entered an invalid side - {player_side}. Enter b for black and w for white"
+        )
+        player_side = input("Do you want to play white or black (w/b)? - ")
+    if player_side.lower().strip().startswith("w"):
+        b = Board("black")
+    else:
+        b = Board("white")
+    b.play()
