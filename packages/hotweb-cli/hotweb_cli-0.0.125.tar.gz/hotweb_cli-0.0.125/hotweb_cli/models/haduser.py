624a4496c4b32df39f816b88d5e731a39c2897ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:55 +0000

    Add package hotweb-cli/0.0.125/hotweb_cli-0.0.125.tar.gz

---
+++ packages/hotweb-cli/hotweb_cli-0.0.125.tar.gz/hotweb_cli-0.0.125/hotweb_cli/models/haduser.py
@@ -0,0 +1,7 @@
+
+from hotweb.models.models import Models
+model = Models()
+model.init({'jfjfj': 'jdjj'})
+def haduser():
+    {'jfjfj': 'jdjj'}
+    
---
+++ packages/hotweb-cli/hotweb_cli-0.0.125.tar.gz/hotweb_cli-0.0.125/hotweb_cli/models/nan.py
@@ -0,0 +1,7 @@
+
+from hotweb.models.models import Models
+model = Models()
+model.init({'jdjjd': 'djdj'})
+def nan():
+    {'jdjjd': 'djdj'}
+    
