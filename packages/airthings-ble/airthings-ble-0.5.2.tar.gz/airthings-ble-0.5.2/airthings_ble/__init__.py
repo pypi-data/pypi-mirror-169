89ef161753a15f374b1cd273c80cca1775a3a711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:09 +0000

    Add package airthings-ble/0.5.2/airthings-ble-0.5.2.tar.gz

---
+++ packages/airthings-ble/airthings-ble-0.5.2.tar.gz/airthings-ble-0.5.2/airthings_ble/__init__.py
@@ -0,0 +1,8 @@
+"""Parser for Airthings BLE advertisements."""
+from __future__ import annotations
+
+from .parser import AirthingsBluetoothDeviceData, AirthingsDevice
+
+__version__ = "0.5.2"
+
+__all__ = ["AirthingsBluetoothDeviceData", "AirthingsDevice"]
