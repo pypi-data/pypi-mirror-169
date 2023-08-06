a4c3e0645e7b8efcc2921de621f1ae1efe93c6db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:13 +0000

    Add package aac-datasets/0.3.0/aac_datasets-0.3.0.tar.gz

---
+++ packages/aac-datasets/aac_datasets-0.3.0.tar.gz/aac_datasets-0.3.0/src/aac_datasets/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Audio Captioning Datasets package.
+"""
+
+from .datasets.audiocaps import AudioCaps
+from .datasets.clotho import Clotho
+from .datasets.macs import MACS
+
+
+__author__ = "Etienne Labbé (Labbeti)"
+__maintainer__ = "Etienne Labbé (Labbeti)"
+__name__ = "aac-datasets"
+__status__ = "Development"
+__version__ = "0.3.0"
