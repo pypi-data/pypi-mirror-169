d11082950db0209fe9c0670d8cd2939ccad321b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:20:34 +0000

    Add package OpenTimelineIO/0.15.0/OpenTimelineIO-0.15.0-cp38-cp38-win_amd64.whl

---
+++ packages/OpenTimelineIO/OpenTimelineIO-0.15.0-cp38-cp38-win_amd64.whl/opentimelineio/schema/serializable_collection.py
@@ -0,0 +1,67 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright Contributors to the OpenTimelineIO project
+
+from .. core._core_utils import add_method
+from .. import _otio
+
+
+@add_method(_otio.SerializableCollection)
+def __str__(self):
+    return "SerializableCollection({}, {}, {})".format(
+        str(self.name),
+        str(list(self)),
+        str(self.metadata)
+    )
+
+
+@add_method(_otio.SerializableCollection)
+def __repr__(self):
+    return (
+        "otio.{}("
+        "name={}, "
+        "children={}, "
+        "metadata={}"
+        ")".format(
+            "schema.SerializableCollection",
+            repr(self.name),
+            repr(list(self)),
+            repr(self.metadata)
+        )
+    )
+
+
+@add_method(_otio.SerializableCollection)
+def each_child(self, search_range=None, descended_from_type=_otio.Composable,
+               shallow_search=False):
+    """ Generator that returns each child contained in the serializable
+    collection in the order in which it is found.
+
+    .. deprecated:: 0.14.0
+        Use :meth:`children_if` instead.
+
+    :param TimeRange search_range: if specified, only children whose range overlaps
+                                   with the search range will be yielded.
+    :param type descended_from_type: if specified, only children who are a descendent
+                                     of the descended_from_type will be yielded.
+    :param bool shallow_search: if True, will only search children of self and not
+                                recurse into children of children.
+    """
+    for child in self.children_if(descended_from_type, search_range, shallow_search):
+        yield child
+
+
+@add_method(_otio.SerializableCollection)
+def each_clip(self, search_range=None, shallow_search=False):
+    """ Generator that returns each clip contained in the serializable
+    collection in the order in which it is found.
+
+    .. deprecated:: 0.14.0
+        Use :meth:`each_clip` instead.
+
+    :param TimeRange search_range: if specified, only children whose range overlaps
+                                   with the search range will be yielded.
+    :param bool shallow_search: if True, will only search children of self and not
+                                recurse into children of children.
+    """
+    for child in self.clip_if(search_range, shallow_search):
+        yield child
