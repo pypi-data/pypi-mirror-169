bb0987d7d87a4e63a30a6b925bf3c095b7c80773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:18:35 +0000

    Add package cltoolbox/1.3.0/cltoolbox-1.3.0.tar.gz

---
+++ packages/cltoolbox/cltoolbox-1.3.0.tar.gz/cltoolbox-1.3.0/docs/command.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from cltoolbox import command, main
+
+
+@command
+def cmd(foo, bar):
+    """Here stands the help.
+
+    And here the description of this useless command.
+
+    :param foo: Well, the first arg.
+    :param bar: Obviously the second arg. Nonsense.
+    """
+    print(foo, bar)
+
+
+if __name__ == "__main__":
+    main()
