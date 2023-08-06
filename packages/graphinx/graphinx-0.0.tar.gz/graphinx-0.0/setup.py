9ffd5911b1be183338c48f51b248f3d1a13202ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:15 +0000

    Add package graphinx/0.0/graphinx-0.0.tar.gz

---
+++ packages/graphinx/graphinx-0.0.tar.gz/graphinx-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='graphinx',
+version='0.0',
+description='Functions to draw beautigul plots from dataframes',
+packages=['graphinx'],
+author_email='hernangarciamayoral@gmail.com',
+zip_safe=False)
