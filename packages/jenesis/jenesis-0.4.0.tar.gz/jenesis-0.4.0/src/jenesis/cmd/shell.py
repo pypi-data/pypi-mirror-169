7627741d98e894c03ac9afc6dc4378299387a0dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:08 +0000

    Add package jenesis/0.4.0/jenesis-0.4.0.tar.gz

---
+++ packages/jenesis/jenesis-0.4.0.tar.gz/jenesis-0.4.0/src/jenesis/cmd/shell.py
@@ -0,0 +1,118 @@
+import argparse
+import os
+import sys
+
+from cosmpy.aerial.client import LedgerClient
+from cosmpy.aerial.faucet import FaucetApi
+from cosmpy.aerial.wallet import LocalWallet
+from cosmpy.crypto.keypairs import PrivateKey
+from ptpython import embed
+from jenesis.config import Config
+from jenesis.contracts.detect import detect_contracts
+from jenesis.contracts.monkey import MonkeyContract
+from jenesis.contracts.observer import DeploymentUpdater
+from jenesis.keyring import query_keychain_items, query_keychain_item
+from jenesis.network import run_local_node
+
+
+def load_config(args: argparse.Namespace) -> dict:
+    project_path = os.getcwd()
+
+    # check that we are actually running the command from the project root
+    if not os.path.exists(os.path.join(project_path, "jenesis.toml")):
+        raise RuntimeError("Please run command from project root or create project first")
+
+    cfg = Config.load(project_path)
+    contracts = {contract.name: contract for contract in detect_contracts(project_path)}
+
+    shell_globals = {}
+    contract_instances = {}
+
+    if args.profile is not None and args.profile not in cfg.profiles:
+        print(f'Invalid profile name. Expected one of {",".join(cfg.profiles.keys())}')
+        sys.exit(1)
+
+    profile_name = args.profile or cfg.get_default_profile()
+
+    selected_profile = cfg.profiles.get(profile_name)
+
+    deployments = selected_profile.deployments
+
+    if selected_profile is not None:
+        if selected_profile.network.is_local:
+            run_local_node(selected_profile.network)
+
+        # build the ledger client
+        client = LedgerClient(selected_profile.network)
+
+        print(f'Network: {selected_profile.network.name}')
+
+        print('Detecting contracts...')
+
+        for (deployment_name, deployment) in deployments.items():
+            contract = contracts[deployment.contract]
+            print("C", deployment_name)
+
+            # skip contracts that we have not compiled
+            if contract.digest() is None:
+                continue
+
+            monkey = MonkeyContract(
+                contract,
+                client,
+                address=deployment.address,
+                code_id=deployment.code_id,
+                observer=DeploymentUpdater(
+                    cfg,
+                    project_path,
+                    profile_name,
+                    deployment_name,
+                ),
+                init_args=deployment.init,
+            )
+
+            contract_instances[deployment_name] = monkey
+
+        print('Detecting contracts...complete')
+
+        shell_globals["ledger"] = LedgerClient(selected_profile.network)
+        if selected_profile.network.faucet_url is not None:
+            shell_globals["faucet"] = FaucetApi(selected_profile.network)
+
+    wallets = {}
+    print("Detecting local wallet keys...")
+    try:
+        for key in query_keychain_items():
+            try:
+                info = query_keychain_item(key)
+                wallets[key] = LocalWallet(PrivateKey(info.private_key))
+                print(f"Importing wallets['{key}']")
+            except Exception:
+                print(f"Failed to import local key '{key}'")
+        print("Detecting local wallet keys...complete")
+    except Exception:
+        print("No local keychain found")
+
+    shell_globals["cfg"] = cfg
+    shell_globals["project_path"] = project_path
+    shell_globals["profile"] = profile_name
+    shell_globals["contracts"] = contracts
+    shell_globals["wallets"] = wallets
+    for (name, instance) in contract_instances.items():
+        shell_globals[name] = instance
+
+    return shell_globals
+
+
+def run(args: argparse.Namespace):
+    shell_globals = load_config(args)
+    shell_globals.update(globals())
+    embed(shell_globals, vi_mode=False, history_filename=".shell_history")
+
+
+def add_shell_command(parser):
+    shell_cmd = parser.add_parser("shell")
+    shell_cmd.add_argument(
+        "-p", "--profile", default=None, help="The profile to use"
+    )
+    shell_cmd.set_defaults(handler=run)
