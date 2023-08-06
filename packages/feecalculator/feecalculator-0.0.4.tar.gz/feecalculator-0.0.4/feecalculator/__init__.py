6d3d071891787e2ef3709330c492789dd4527346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:00 +0000

    Add package feecalculator/0.0.4/feecalculator-0.0.4.tar.gz

---
+++ packages/feecalculator/feecalculator-0.0.4.tar.gz/feecalculator-0.0.4/feecalculator/__init__.py
@@ -0,0 +1,11 @@
+#This is a program for calculation that removes the fees 
+# import lib for rounding up function
+def calculate(fee, amount):
+
+    calculatedfee = 10 - (fee / 10)
+    
+    return amount * 10 / calculatedfee
+
+
+
+
