274a35d908c1fde14d3463fca129908ce73c3f70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:29 +0000

    Add package dsdl/0.1.9/dsdl-0.1.9-py3-none-any.whl

---
+++ packages/dsdl/dsdl-0.1.9-py3-none-any.whl/dsdl/exception.py
@@ -0,0 +1,19 @@
+class ValidationError(RuntimeError):
+    pass
+
+
+class ClassHasDefinedError(RuntimeError):
+    pass
+
+
+class ClassNotFoundError(RuntimeError):
+    pass
+
+
+class DefineSyntaxError(SyntaxError):
+    pass
+
+
+class DefineTypeError(TypeError):
+    pass
+
---
+++ packages/dsdl/dsdl-0.1.9-py3-none-any.whl/dsdl/types/__init__.py
@@ -0,0 +1,38 @@
+from .struct import Struct
+from .generic import StrField, IntField, BoolField, NumField, ListField
+from .special import (
+    LabelField,
+    CoordField,
+    Coord3DField,
+    IntervalField,
+    BBoxField,
+    PolygonField,
+    DateField,
+    TimeField,
+)
+from .unstructure import ImageField
+from .registry import STRUCT, CLASSDOMAIN, LABEL
+from .class_domain import ClassDomain, Label
+
+__all__ = [
+    "Struct",
+    "StrField",
+    "IntField",
+    "BoolField",
+    "NumField",
+    "ImageField",
+    "LabelField",
+    "ListField",
+    "CoordField",
+    "Coord3DField",
+    "IntervalField",
+    "BBoxField",
+    "PolygonField",
+    "DateField",
+    "TimeField",
+    "STRUCT",
+    "CLASSDOMAIN",
+    "ClassDomain",
+    "LABEL",
+    "Label",
+]
