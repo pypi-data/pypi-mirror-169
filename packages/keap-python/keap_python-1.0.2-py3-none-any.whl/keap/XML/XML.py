c014ba3230b8f2e21af08c9f4b53b634c094499b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:09 +0000

    Add package keap-python/1.0.2/keap_python-1.0.2-py3-none-any.whl

---
+++ packages/keap-python/keap_python-1.0.2-py3-none-any.whl/keap/XML/XML.py
@@ -0,0 +1,22 @@
+from keap.XML import DataService, ContactService, AffiliateService, AffiliateProgramService, DiscountService, \
+    EmailService, FileService, FunnelService, InvoiceService, OrderService, ProductService, SearchService, \
+    ShippingService, WebFormService
+
+
+class XML:
+    def __init__(self, keap):
+        self.keap = keap
+        self.AffiliateProgramService = AffiliateProgramService(keap)
+        self.AffiliateService = AffiliateService(keap)
+        self.ContactService = ContactService(keap)
+        self.DataService = DataService(keap)
+        self.DiscountService = DiscountService(keap)
+        self.EmailService = EmailService(keap)
+        self.FileService = FileService(keap)
+        self.FunnelService = FunnelService(keap)
+        self.InvoiceService = InvoiceService(keap)
+        self.OrderService = OrderService(keap)
+        self.ProductService = ProductService(keap)
+        self.SearchService = SearchService(keap)
+        self.ShippingService = ShippingService(keap)
+        self.WebFormService = WebFormService(keap)
