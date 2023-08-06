1dcbcd1467fc0866f22078f1568ae9a0ef37bb05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:08 +0000

    Add package mgtwr/2.0.1/mgtwr-2.0.1.tar.gz

---
+++ packages/mgtwr/mgtwr-2.0.1.tar.gz/mgtwr-2.0.1/mgtwr/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="mgtwr",
+    version="2.0.1",
+    long_description="To fit geographically weighted model, "
+                     "multiscale geographically weighted regression model, "
+                     "geographically and temporally weighted regression model and "
+                     "multiscale geographically and temporally weighted regression model.",
+    author="Kun Sun",
+    author_email="849024477@qq.com",
+    packages=['mgtwr'],
+    url="https://github.com/sunkun1997/mgtwr",
+)
