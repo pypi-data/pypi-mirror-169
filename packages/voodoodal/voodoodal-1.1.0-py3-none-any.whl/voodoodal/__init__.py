35abade3d530877ccf75e76c6b4bb87b3c5b6e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:20 +0000

    Add package voodoodal/1.1.0/voodoodal-1.1.0-py3-none-any.whl

---
+++ packages/voodoodal/voodoodal-1.1.0-py3-none-any.whl/voodoodal/__init__.py
@@ -0,0 +1,15 @@
+from .voodoodal import ModelBuilder, DB, Table, Field, Index, DBValidatorWrapper
+
+__all__ = (
+    'ModelBuilder',
+    'DB',
+    'Table',
+    'Field',
+    'Index',
+    'DBValidatorWrapper',
+)
+
+__author__ = "Valery Kucherov <valq7711@gmail.com>"
+__copyright__ = "Copyright (C) 2022 Valery Kucherov"
+__license__ = "MIT"
+__version__ = "1.1.0"
