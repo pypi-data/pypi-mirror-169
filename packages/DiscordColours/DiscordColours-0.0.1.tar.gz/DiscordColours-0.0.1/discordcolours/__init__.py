f73e74d5ee5420e0cb90a02187b72002b0eff9f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:32 +0000

    Add package DiscordColours/0.0.1/DiscordColours-0.0.1.tar.gz

---
+++ packages/DiscordColours/DiscordColours-0.0.1.tar.gz/DiscordColours-0.0.1/discordcolours/__init__.py
@@ -0,0 +1,1 @@
+from discordcolours.discordcolours import colourcoder
---
+++ packages/DiscordColours/DiscordColours-0.0.1.tar.gz/DiscordColours-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Discord Embed Colours.'
+LONG_DESCRIPTION = 'A package that allows You To Reference Discord Colors By String Name.'
+
+# Setting up
+setup(
+    name="DiscordColours",
+    version=VERSION,
+    author="DustinFoes & CringleySDays",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
