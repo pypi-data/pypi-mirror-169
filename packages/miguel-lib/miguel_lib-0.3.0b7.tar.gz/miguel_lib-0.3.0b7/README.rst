6f74818ec657290d30ecd38c69230523d179f1fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:52 +0000

    Add package miguel-lib/0.3.0b7/miguel_lib-0.3.0b7.tar.gz

---
+++ packages/miguel-lib/miguel_lib-0.3.0b7.tar.gz/miguel_lib-0.3.0b7/README.rst
@@ -0,0 +1,63 @@
+.. badges-start
+
+|CI| |pypi| |pyversion| |implementation| |license| |downloads| |black|
+
+.. |CI| image:: https://github.com/MiguelX413/miguel_lib/actions/workflows/CI.yml/badge.svg
+   :alt: GitHub Actions Build Status
+   :target: https://github.com/MiguelX413/miguel_lib/actions/workflows/CI.yml
+
+.. |pypi| image:: https://img.shields.io/pypi/v/miguel_lib.svg
+   :alt: miguel_lib PyPI Project Page
+   :target: https://pypi.org/project/miguel_lib/
+
+.. |license| image:: https://img.shields.io/github/license/MiguelX413/miguel_lib.svg
+   :alt: LGPL-2.1 License
+   :target: https://github.com/MiguelX413/miguel_lib/blob/master/LICENSE
+
+.. |pyversion| image:: https://img.shields.io/pypi/pyversions/miguel_lib.svg
+   :alt: Supported Python Versions
+
+.. |implementation| image:: https://img.shields.io/pypi/implementation/miguel_lib.svg
+   :alt: PyPI - Implementation
+
+.. |downloads| image:: https://pepy.tech/badge/miguel_lib/month
+   :alt: PyPI Download Count
+   :target: https://pepy.tech/project/miguel_lib
+
+.. |black| image:: https://img.shields.io/badge/code%20style-black-000000.svg
+   :alt: Code Style Black
+   :target: https://github.com/psf/black
+
+.. badges-end
+
+Random crap I like to use
+
+.. code-block:: console
+
+    $ pip install miguel_lib
+
+**miguel_lib**
+
+- ChunksIter:
+    An Iterator which takes an interator and outputs its output in groups the size of a given chunk size
+
+- match_indices(string: str, substring: str) -> List[int]:
+    Returns a list of the UTF-8 indices of disjoint matches, from start to end.
+
+- match_utf16_indices(string: str, substring: str) -> List[int]:
+    Returns a list of the UTF-16 indices of disjoint matches, from start to end.
+
+- match_byte_indices(string: str, substring: str) -> List[int]:
+    Returns a list of the byte indices of disjoint matches, from start to end.
+
+- rmatch_indices(string: str, substring: str) -> List[int]:
+    Returns a list of the UTF-8 indices of disjoint matches, from end to start.
+
+- rmatch_utf16_indices(string: str, substring: str) -> List[int]:
+    Returns a list of the UTF-16 indices of disjoint matches, from end to start.
+
+- rmatch_byte_indices(string: str, substring: str) -> List[int]:
+    Returns a list of the byte indices of disjoint matches, from end to start.
+
+- utf16len(string: str) -> int:
+    A function that returns the UTF-16 length of a string.
---
+++ packages/miguel-lib/miguel_lib-0.3.0b7.tar.gz/miguel_lib-0.3.0b7/src/lib.rs
@@ -0,0 +1,174 @@
+use pyo3::exceptions::{PyStopIteration, PyValueError};
+
+use pyo3::prelude::*;
+
+/// An Iterator which takes an interator and outputs its output in groups the size of a given chunk size
+#[pyclass]
+pub struct ChunksIter {
+    chunk_size: usize,
+    iter: Py<PyAny>,
+    complete: bool,
+}
+
+#[pymethods]
+impl ChunksIter {
+    #[new]
+    fn py_new(py: Python, iter: Py<PyAny>, chunk_size: usize) -> PyResult<Self> {
+        if chunk_size < 1 {
+            return Err(PyValueError::new_err("chunk_size cannot be 0 or lower"));
+        }
+        Ok(Self {
+            chunk_size,
+            iter: iter.call_method0(py, "__iter__")?,
+            complete: false,
+        })
+    }
+    fn __iter__(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
+        slf
+    }
+    fn __next__(mut slf: PyRefMut<'_, Self>, py: Python) -> PyResult<Option<Vec<PyObject>>> {
+        if slf.complete {
+            return Ok(None);
+        }
+
+        let output = (0..slf.chunk_size)
+            .into_iter()
+            .filter_map(|_| {
+                if slf.complete {
+                    return None;
+                }
+                match slf.iter.call_method0(py, "__next__") {
+                    Ok(ok) => {
+                        if ok.is_none(py) {
+                            slf.complete = true;
+                            return None;
+                        }
+                        Some(Ok(ok))
+                    }
+                    Err(err) => {
+                        if err.is_instance_of::<PyStopIteration>(py) {
+                            slf.complete = true;
+                            return None;
+                        }
+                        Some(Err(err))
+                    }
+                }
+            })
+            .collect::<PyResult<Vec<PyObject>>>()?;
+
+        if output.is_empty() {
+            Ok(None)
+        } else {
+            Ok(Some(output))
+        }
+    }
+}
+
+/// Returns a list of the UTF-8 indices of disjoint matches, from start to end.
+#[pyfunction]
+fn match_indices(string: &str, substring: &str) -> Vec<usize> {
+    let mut byte_index = 0;
+    let mut len = 0;
+    string
+        .match_indices(substring)
+        .map(|f| {
+            len += string[byte_index..f.0].chars().count();
+            byte_index = f.0;
+            len
+        })
+        .collect::<Vec<usize>>()
+}
+
+/// Returns a list of the UTF-16 indices of disjoint matches, from start to end.
+#[pyfunction]
+fn match_utf16_indices(string: &str, substring: &str) -> Vec<usize> {
+    let mut byte_index = 0;
+    let mut len = 0;
+    string
+        .match_indices(substring)
+        .map(|f| {
+            len += utf16len(&string[byte_index..f.0]);
+            byte_index = f.0;
+            len
+        })
+        .collect::<Vec<usize>>()
+}
+
+/// Returns a list of the byte indices of disjoint matches, from start to end.
+#[pyfunction]
+fn match_byte_indices(string: &str, substring: &str) -> Vec<usize> {
+    string
+        .match_indices(substring)
+        .map(|f| f.0)
+        .collect::<Vec<usize>>()
+}
+
+/// Returns a list of the UTF-8 indices of disjoint matches, from end to start.
+#[pyfunction]
+fn rmatch_indices(string: &str, substring: &str) -> Vec<usize> {
+    let mut byte_index = 0;
+    let mut len = 0;
+    let mut output = string
+        .rmatch_indices(substring)
+        .collect::<Vec<(usize, &str)>>()
+        .iter()
+        .rev()
+        .map(|f| {
+            len += string[byte_index..f.0].chars().count();
+            byte_index = f.0;
+            len
+        })
+        .collect::<Vec<usize>>();
+    output.reverse();
+    output
+}
+
+/// Returns a list of the UTF-16 indices of disjoint matches, from end to start.
+#[pyfunction]
+fn rmatch_utf16_indices(string: &str, substring: &str) -> Vec<usize> {
+    let mut byte_index = 0;
+    let mut len = 0;
+    let mut output = string
+        .rmatch_indices(substring)
+        .collect::<Vec<(usize, &str)>>()
+        .iter()
+        .rev()
+        .map(|f| {
+            len += utf16len(&string[byte_index..f.0]);
+            byte_index = f.0;
+            len
+        })
+        .collect::<Vec<usize>>();
+    output.reverse();
+    output
+}
+
+/// Returns a list of the byte indices of disjoint matches, from end to start.
+#[pyfunction]
+fn rmatch_byte_indices(string: &str, substring: &str) -> Vec<usize> {
+    string
+        .rmatch_indices(substring)
+        .map(|f| f.0)
+        .collect::<Vec<usize>>()
+}
+
+/// A function that returns the UTF-16 length of a string.
+#[pyfunction]
+fn utf16len(string: &str) -> usize {
+    string.chars().map(|char| char.len_utf16()).sum()
+}
+
+/// Random crap I like to use.
+#[pymodule]
+fn miguel_lib(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(match_indices, m)?)?;
+    m.add_function(wrap_pyfunction!(match_utf16_indices, m)?)?;
+    m.add_function(wrap_pyfunction!(match_byte_indices, m)?)?;
+    m.add_function(wrap_pyfunction!(rmatch_indices, m)?)?;
+    m.add_function(wrap_pyfunction!(rmatch_utf16_indices, m)?)?;
+    m.add_function(wrap_pyfunction!(rmatch_byte_indices, m)?)?;
+    m.add_function(wrap_pyfunction!(utf16len, m)?)?;
+    m.add_class::<ChunksIter>()?;
+    m.add("__version__", env!("CARGO_PKG_VERSION"))?;
+    Ok(())
+}
