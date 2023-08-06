606bf296cd15af0b143677805afaf24eadc0f4a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:43 +0000

    Add package taipy-gui/2.0.0/taipy_gui-2.0.0-py3-none-any.whl

---
+++ packages/taipy-gui/taipy_gui-2.0.0-py3-none-any.whl/taipy/gui/gui_actions.py
@@ -0,0 +1,197 @@
+# Copyright 2022 Avaiga Private Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+import typing as t
+import warnings
+
+from .gui import Gui
+from .state import State
+
+
+def download(state: State, content: t.Any, name: t.Optional[str] = "", on_action: t.Optional[str] = ""):
+    """Download content to the client.
+
+    Arguments:
+        state (State^): The current user state as received in any callback.
+        content: File path or file content.
+        name: File name for the content on the client browser (default to content name).
+        on_action: Function called when the download starts.
+    """
+    if state and isinstance(state._gui, Gui):
+        state._gui._download(content, name, on_action)
+    else:
+        warnings.warn("'download()' must be called in the context of a callback")
+
+
+def notify(
+    state: State,
+    notification_type: str = "I",
+    message: str = "",
+    system_notification: t.Optional[bool] = None,
+    duration: t.Optional[int] = None,
+):
+    """Send a notification to the user interface.
+
+    Arguments:
+        state (State^): The current user state as received in any callback.
+        notification_type: The notification type. This can be one of _"success"_, _"info"_,
+            _"warning"_, or _"error"_.<br/>
+            To remove the last notification, set this parameter to the empty string.
+        message: The text message to display.
+        system_notification: If True, the system will also show the notification.
+            If not specified or set to None, this parameter will use the value of
+            _configuration[system_notification]_.
+        duration: The time, in milliseconds, during which the notification is shown.
+            If not specified or set to None, this parameter will use the value of
+            _configuration[notification_duration]_.
+
+    Note that you can also call this function with _notification_type_ set to the first letter
+    or the alert type (ie setting _notification_type_ to "i" is equivalent to setting it to
+    "info").
+
+    If _system_notification_ is set to True, then the browser requests the system
+    to display a notification as well. They usually appear in small windows that
+    fly out of the system tray.<br/>
+    The first time your browser is requested to show such a system notification for
+    Taipy applications, you may be prompted to authorize the browser to do so. Please
+    refer to your browser documentation for details on how to allow or prevent this
+    feature.
+    """
+    if state and isinstance(state._gui, Gui):
+        state._gui._notify(notification_type, message, system_notification, duration)
+    else:
+        warnings.warn("'notify()' must be called in the context of a callback")
+
+
+def hold_control(
+    state: State,
+    callback: t.Optional[t.Union[str, t.Callable]] = None,
+    message: t.Optional[str] = "Work in Progress...",
+):
+    """Hold the User Interface actions.
+
+    When the User Interface is held, users cannot interact with visual elements.<br/>
+    The application must call `resume_control()^` so that users can interact again
+    with the visual elements.
+
+    Arguments:
+        state (State^): The current user state as received in any callback.
+        callback (Optional[Union[str, Callable]]): the function to be called if the user
+            chooses to cancel.<br/>
+            If empty or None, no cancel action is provided to the user.
+        message: the message to show.
+    """
+    if state and isinstance(state._gui, Gui):
+        state._gui._hold_actions(callback, message)
+    else:
+        warnings.warn("'hold_actions()' must be called in the context of a callback")
+
+
+def resume_control(state: State):
+    """Resume the User Interface actions.
+
+    This function must be called after `hold_control()^` was invoked, when interaction
+    must be allowed again for the user.
+
+    Arguments:
+        state (State^): The current user state as received in any callback.
+    """
+    if state and isinstance(state._gui, Gui):
+        state._gui._resume_actions()
+    else:
+        warnings.warn("'resume_actions()' must be called in the context of a callback")
+
+
+def navigate(state: State, to: t.Optional[str] = ""):
+    """Navigate to a page.
+
+    Arguments:
+        state (State^): The current user state as received in any callback.
+        to: The name of the page to navigate to. This must be a valid page identifier.
+            If ommitted, the application navigates to the root page.
+    """
+    if state and isinstance(state._gui, Gui):
+        state._gui._navigate(to)
+    else:
+        warnings.warn("'navigate()' must be called in the context of a callback")
+
+
+def get_state_id(state: State) -> t.Optional[str]:
+    """Get the identifier of a state.
+
+    The state identifier is a string generated by Taipy GUI for a given `State^` that is used
+    to serialize callbacks.
+    See the [User Manual section on Long Running Callbacks](../../gui/callbacks/#long-running-callbacks)
+    for details on when and how this function can be used.
+
+    Arguments:
+        state (State^): The current user state as received in any callback.
+
+    Returns:
+        A string that uniquely identifies the state.<br/>
+        If None, then **state** was not handled by a `Gui^` instance.
+    """
+    if state and isinstance(state._gui, Gui):
+        return state._gui._get_client_id()
+    return None
+
+
+def get_context_id(state: State) -> t.Any:
+    warnings.warn("'get_context_id()' was deprecated in Taipy GUI 2.0. Use 'get_state_id()' instead.")
+    return get_state_id(state)
+
+
+def get_module_name_from_state(state: State) -> t.Optional[str]:
+    """Get the module name that triggered a callback.
+
+    Pages can be defined in different modules yet share callbacks declared elsewhere (typically, the
+    application's main module).
+
+    This function returns the name of the module where the page that holds the control that
+    triggered the callback was declared.  This lets applications implement different behaviors
+    depending on what page is involved.
+
+    This function must be called only in the body of a callback function.
+
+    Arguments:
+        state (State^): The `State^` instance, which is an argument of the callback function.
+
+    Returns:
+        The name of the module that holds the definition of the page containing the control
+            that triggered the callback that was provided the *state* object.
+    """
+    if state and isinstance(state._gui, Gui):
+        return state._gui._get_locals_context()
+    return None
+
+
+def invoke_callback(gui: Gui, state_id: str, callback: t.Callable, args: t.Union[t.Tuple, t.List]) -> t.Any:
+    """Invoke a user callback in a given state.
+
+    See the [User Manual section on Long Running Callbacks](../../gui/callbacks/#long-running-callbacks)
+    for details on when and how this function can be used.
+
+    Arguments:
+        gui (Gui^): The current gui instance.
+        state_id: The identifier of the state to use, as returned by `get_state_id()^`.
+        callback (Callable[[State^, ...], None]): The user-defined function that is invoked.<br/>
+            The first parameter of this function **must** be a `State^`.
+        args (Union[Tuple, List]): The remaining arguments, as a List or a Tuple.
+    """
+    if isinstance(gui, Gui):
+        return gui._call_user_callback(state_id, callback, list(args))
+    else:
+        warnings.warn("'invoke_callback()' must be called with a valid Gui instance")
+
+
+def invoke_state_callback(gui: Gui, state_id: str, callback: t.Callable, args: t.Union[t.Tuple, t.List]) -> t.Any:
+    warnings.warn("'invoke_state_callback()' was deprecated in Taipy GUI 2.0. Use 'invoke_callback()' instead.")
+    return invoke_callback(gui, state_id, callback, args)
