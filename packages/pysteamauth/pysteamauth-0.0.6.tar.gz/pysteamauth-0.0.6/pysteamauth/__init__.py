519060e4bf731f02621be7f49986d7a21fe04452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:32 +0000

    Add package pysteamauth/0.0.6/pysteamauth-0.0.6.tar.gz

---
+++ packages/pysteamauth/pysteamauth-0.0.6.tar.gz/pysteamauth-0.0.6/pysteamauth/__init__.py
@@ -0,0 +1,17 @@
+from . import (
+    abstract,
+    auth,
+    base,
+    errors,
+)
+
+
+__all__ = [
+    'abstract',
+    'base',
+    'errors',
+    'auth',
+]
+
+__version__ = '0.0.6'
+__author__ = 'Mike M'
