7e45a791524b7f2edbd317c1976cf6e4aca7dfcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:08 +0000

    Add package distris-probability/0.2/distris_probability-0.2.tar.gz

---
+++ packages/distris-probability/distris_probability-0.2.tar.gz/distris_probability-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distris_probability',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distris_probability'],
+      author= 'Azeez Liadi',
+      author_email= 'Liadiazeez3@gmail.com',
+      zip_safe=False)
