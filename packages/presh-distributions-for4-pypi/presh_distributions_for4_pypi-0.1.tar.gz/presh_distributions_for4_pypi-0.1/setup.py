55def667dba3378f8db94443da6e126f8d8d8897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:14 +0000

    Add package presh-distributions-for4-pypi/0.1/presh_distributions_for4_pypi-0.1.tar.gz

---
+++ packages/presh-distributions-for4-pypi/presh_distributions_for4_pypi-0.1.tar.gz/presh_distributions_for4_pypi-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='presh_distributions_for4_pypi',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['presh_prob_distributions'],
+      zip_safe=False)
