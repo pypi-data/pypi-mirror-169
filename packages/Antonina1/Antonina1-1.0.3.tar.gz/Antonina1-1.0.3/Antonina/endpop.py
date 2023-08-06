dabade9e0667f836e80f64b0cd40a8721976ba00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:46 +0000

    Add package Antonina1/1.0.3/Antonina1-1.0.3.tar.gz

---
+++ packages/Antonina1/Antonina1-1.0.3.tar.gz/Antonina1-1.0.3/Antonina/endpop.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 29 10:41:01 2022
+
+@author: Jinqk
+"""
+
+import PySimpleGUI as sg
+def endpop():
+    title = '安冬妮娜小助手'
+    sg.popup_ok('已经算完了哦，可以下班了吧',title =title,keep_on_top = True,grab_anywhere = True,image = '.\\figures\\Antonina2.png')
+    
+# if __name__ == '__main__':
+#     main()
