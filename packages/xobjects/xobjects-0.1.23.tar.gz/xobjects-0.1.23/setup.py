216fe44a5837dff067008d342dd3192cb9d4fe3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:05 +0000

    Add package xobjects/0.1.23/xobjects-0.1.23.tar.gz

---
+++ packages/xobjects/xobjects-0.1.23.tar.gz/xobjects-0.1.23/setup.py
@@ -0,0 +1,27 @@
+# copyright ################################# #
+# This file is part of the Xobjects Package.  #
+# Copyright (c) CERN, 2021.                   #
+# ########################################### #
+
+from setuptools import setup
+
+setup(
+    name="xobjects",
+    version="0.1.23",
+    description="In-memory serialization and code generator for CPU and GPU",
+    long_description="In-memory serialization and code generator for CPU and GPU",
+    author="Riccardo De Maria",
+    author_email="riccardo.de.maria@cern.ch",
+    url='https://xsuite.readthedocs.io/',
+    python_requires=">=3.7",
+    setup_requires=[],
+    install_requires=["numpy", "cffi"],
+    packages=["xobjects"],
+    license='Apache 2.0',
+    download_url="https://pypi.python.org/pypi/xobjects",
+    project_urls={
+            "Bug Tracker": "https://github.com/xsuite/xsuite/issues",
+            "Documentation": 'https://xsuite.readthedocs.io/',
+            "Source Code": "https://github.com/xsuite/xobjects",
+        },
+)
