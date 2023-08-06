20e276b14a70c122597d14c7e2ea8df8dd9e6564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:34 +0000

    Add package edc-offstudy/0.3.27/edc-offstudy-0.3.27.tar.gz

---
+++ packages/edc-offstudy/edc-offstudy-0.3.27.tar.gz/edc-offstudy-0.3.27/edc_offstudy/modelform_mixins/prn/offstudy_non_crf_modelform_mixin.py
@@ -0,0 +1,23 @@
+from django import forms
+
+from ...exceptions import OffstudyError
+from ...utils import raise_if_offstudy
+
+
+class OffstudyNonCrfModelFormMixin:
+    """ModelForm mixin for non-CRF modelforms / PRNs."""
+
+    def clean(self):
+        cleaned_data = super().clean()
+        self.raise_if_offstudy_by_report_datetime()
+        return cleaned_data
+
+    def raise_if_offstudy_by_report_datetime(self) -> None:
+        try:
+            raise_if_offstudy(
+                source_obj=self.instance,
+                subject_identifier=self.subject_identifier,
+                report_datetime=self.report_datetime,
+            )
+        except OffstudyError as e:
+            raise forms.ValidationError(e)
