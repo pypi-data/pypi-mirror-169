067ee809339f3f5b33feb446997e4fbf7c15f1a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:23 +0000

    Add package feetech-servo-sdk/0.2.0/feetech-servo-sdk-0.2.0.tar.gz

---
+++ packages/feetech-servo-sdk/feetech-servo-sdk-0.2.0.tar.gz/feetech-servo-sdk-0.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='feetech-servo-sdk',
+    version='0.2.0',
+    packages=['scservo_sdk'],
+    url='https://github.com/Adam-Software/FEETECH-Servo-Python-SDK',
+    license='UNLICENSE',
+    author='vertigra',
+    author_email='a@nesterof.com',
+    description='This is source code from official feetech repository',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: The Unlicense (Unlicense)',
+        'Operating System :: POSIX :: Other',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Hardware :: Universal Serial Bus (USB) :: Communications Device Class (CDC)'
+    ]
+)
