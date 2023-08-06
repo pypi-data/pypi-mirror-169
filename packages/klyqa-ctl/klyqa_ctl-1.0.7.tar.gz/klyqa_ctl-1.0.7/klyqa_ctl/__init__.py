66a9fcb506841682b3487925ea890e80145455d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:46 +0000

    Add package klyqa-ctl/1.0.7/klyqa_ctl-1.0.7.tar.gz

---
+++ packages/klyqa-ctl/klyqa_ctl-1.0.7.tar.gz/klyqa_ctl-1.0.7/klyqa_ctl/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+###################################################################
+#
+# Interactive Klyqa Control commandline client package
+#
+# Company: QConnex GmbH / Klyqa
+# Author: Frederick Stallmeyer
+#
+# E-Mail: frederick.stallmeyer@gmx.de
+#
+###################################################################
+__version__ = "1.0.7"
+from .klyqa_ctl import Klyqa_account
+
---
+++ packages/klyqa-ctl/klyqa_ctl-1.0.7.tar.gz/klyqa_ctl-1.0.7/setup.py
@@ -0,0 +1,16 @@
+# import setuptools
+from setuptools import setup
+
+# this grabs the requirements from requirements.txt
+# REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+if __name__ == '__main__':
+    setup()
+# setuptools.setup(
+#     name="klyqa_ctl",
+#     # version="1.0.5",
+#     # author="Frederick Stallmeyer",
+#     # description="Control the Klyqa Smart Devices.",
+#     # packages=["klyqa_ctl"],
+#     # requires=[""],
+#     # install_requires=REQUIREMENTS
+# )
