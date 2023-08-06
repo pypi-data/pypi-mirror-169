e394bfcd8eed4de32b0fca8118bb817050968e74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:13 +0000

    Add package checkov/2.1.235/checkov-2.1.235.tar.gz

---
+++ packages/checkov/checkov-2.1.235.tar.gz/checkov-2.1.235/checkov/github/schemas/org_security.py
@@ -0,0 +1,70 @@
+from checkov.common.vcs.vcs_schema import VCSSchema
+
+
+class OrgSecuritySchema(VCSSchema):
+    def __init__(self) -> None:
+        schema = {
+            "type": "object",
+            "properties": {
+                "data": {
+                    "type": "object",
+                    "properties": {
+                        "organization": {
+                            "type": "object",
+                            "properties": {
+                                "name": {
+                                    "type": "string"
+                                },
+                                "login": {
+                                    "type": "string"
+                                },
+                                "description": {
+                                    "type": "string"
+                                },
+                                "ipAllowListEnabledSetting": {
+                                    "type": "string"
+                                },
+                                "ipAllowListForInstalledAppsEnabledSetting": {
+                                    "type": "string"
+                                },
+                                "requiresTwoFactorAuthentication": {
+                                    "type": "boolean"
+                                },
+                                "samlIdentityProvider": {
+                                    "anyOf": [
+                                        {
+                                            "type": "object",
+                                            "properties": {
+                                                "ssoUrl": {
+                                                    "type": "string"
+                                                }
+                                            }
+                                        },
+                                        {
+                                            "type": "null"
+                                        }
+                                    ]
+                                }
+                            },
+                            "required": [
+                                "name",
+                                "login",
+                                "description",
+                                "ipAllowListEnabledSetting",
+                                "ipAllowListForInstalledAppsEnabledSetting",
+                                "requiresTwoFactorAuthentication"]
+                        }
+                    },
+                    "required": [
+                        "organization"
+                    ]
+                }
+            },
+            "required": [
+                "data"
+            ]
+        }
+        super().__init__(schema=schema)
+
+
+schema = OrgSecuritySchema()
---
+++ packages/checkov/checkov-2.1.235.tar.gz/checkov-2.1.235/checkov/github_actions/checks/job/ShellInjection.py
@@ -0,0 +1,35 @@
+import re
+from typing import Any, Tuple, Dict
+
+from checkov.common.models.enums import CheckResult
+from checkov.github_actions.checks.base_github_action_check import BaseGithubActionsCheck
+from checkov.github_actions.common.shell_injection_list import terms as bad_inputs
+from checkov.yaml_doc.enums import BlockType
+
+
+class DontAllowShellInjection(BaseGithubActionsCheck):
+    def __init__(self):
+        name = "Ensure run commands are not vulnerable to shell injection"
+        id = "CKV_GHA_2"
+        super().__init__(
+            name=name,
+            id=id,
+            block_type=BlockType.ARRAY,
+            supported_entities=('jobs', 'jobs.*.steps[]')
+        )
+
+    def scan_entity_conf(self, conf: Dict[str, Any]) -> Tuple[CheckResult, Dict[str, Any]]:
+        if not isinstance(conf, dict):
+            return CheckResult.UNKNOWN, conf
+
+        if "run" not in conf:
+            return CheckResult.PASSED, conf
+        run = conf.get("run", "")
+        for term in bad_inputs:
+            if re.search(term, run):
+                return CheckResult.FAILED, conf
+
+        return CheckResult.PASSED, conf
+
+
+check = DontAllowShellInjection()
