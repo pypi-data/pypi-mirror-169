898146bcbbd300296287803c23a0a58627cf9d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:16 +0000

    Add package tuote-taluo-xuexi-shouce/2022.9.29.0/tuote_taluo_xuexi_shouce-2022.9.29.0-py3-none-any.whl

---
+++ packages/tuote-taluo-xuexi-shouce/tuote_taluo_xuexi_shouce-2022.9.29.0-py3-none-any.whl/TuoteTaluoXuexiShouce/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tuote-taluo-xuexi-shouce
+https://github.com/apachecn/tuote-taluo-xuexi-shouce"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.9.29.0"
