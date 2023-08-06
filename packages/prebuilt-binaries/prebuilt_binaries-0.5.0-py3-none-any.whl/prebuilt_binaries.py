30bd83a41463d1896ce97eaa4318fad9824e2317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:25 +0000

    Add package prebuilt-binaries/0.5.0/prebuilt_binaries-0.5.0-py3-none-any.whl

---
+++ packages/prebuilt-binaries/prebuilt_binaries-0.5.0-py3-none-any.whl/prebuilt_binaries.py
@@ -0,0 +1,36 @@
+import os
+import pathlib
+
+from distutils.file_util import copy_file
+from setuptools.command import build_ext
+from setuptools import Extension
+
+
+class PrebuiltExtension(Extension):
+    def __init__(self, input_file, package=None):
+        name = pathlib.Path(input_file).stem
+        if package is not None:
+            name = f'{package}.{name}'
+        if not os.path.exists(input_file):
+            raise ValueError(f'Prebuilt extension file not found\n{input_file}')
+        self.input_file = input_file
+        super().__init__(name, ['no-source-needed.c'])
+
+
+class prebuilt_binary(build_ext.build_ext):
+
+    def run(self):
+        for ext in self.extensions:
+            fullname = self.get_ext_fullname(ext.name)
+            filename = self.get_ext_filename(fullname)
+            dest_filename = os.path.join(self.build_lib, filename)
+            dest_folder = os.path.dirname(dest_filename)
+            if not os.path.exists(dest_folder):
+                os.makedirs(dest_folder)
+
+            copy_file(
+                ext.input_file, dest_filename, verbose=self.verbose,
+                dry_run=self.dry_run
+            )
+        if self.inplace:
+            self.copy_extensions_to_source()
