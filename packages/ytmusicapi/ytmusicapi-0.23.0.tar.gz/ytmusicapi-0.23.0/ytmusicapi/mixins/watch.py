0508fa1e8ccbe3edb5969962881df1fd198a86a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:39 +0000

    Add package ytmusicapi/0.23.0/ytmusicapi-0.23.0.tar.gz

---
+++ packages/ytmusicapi/ytmusicapi-0.23.0.tar.gz/ytmusicapi-0.23.0/ytmusicapi/mixins/watch.py
@@ -0,0 +1,171 @@
+from typing import List, Dict, Union
+
+from ytmusicapi.continuations import get_continuations
+from ytmusicapi.parsers.playlists import validate_playlist_id
+from ytmusicapi.parsers.watch import *
+
+
+class WatchMixin:
+    def get_watch_playlist(self,
+                           videoId: str = None,
+                           playlistId: str = None,
+                           limit=25,
+                           params: str = None) -> Dict[str, Union[List[Dict]]]:
+        """
+        Get a watch list of tracks. This watch playlist appears when you press
+        play on a track in YouTube Music.
+
+        Please note that the `INDIFFERENT` likeStatus of tracks returned by this
+        endpoint may be either `INDIFFERENT` or `DISLIKE`, due to ambiguous data
+        returned by YouTube Music.
+
+        :param videoId: videoId of the played video
+        :param playlistId: playlistId of the played playlist or album
+        :param limit: minimum number of watch playlist items to return
+        :param params: only used internally by :py:func:`get_watch_playlist_shuffle`
+        :return: List of watch playlist items. The counterpart key is optional and only
+            appears if a song has a corresponding video counterpart (UI song/video
+            switcher).
+
+        Example::
+
+            {
+                "tracks": [
+                    {
+                      "videoId": "9mWr4c_ig54",
+                      "title": "Foolish Of Me (feat. Jonathan Mendelsohn)",
+                      "length": "3:07",
+                      "thumbnail": [
+                        {
+                          "url": "https://lh3.googleusercontent.com/ulK2YaLtOW0PzcN7ufltG6e4ae3WZ9Bvg8CCwhe6LOccu1lCKxJy2r5AsYrsHeMBSLrGJCNpJqXgwczk=w60-h60-l90-rj",
+                          "width": 60,
+                          "height": 60
+                        }...
+                      ],
+                      "feedbackTokens": {
+                        "add": "AB9zfpIGg9XN4u2iJ...",
+                        "remove": "AB9zfpJdzWLcdZtC..."
+                      },
+                      "likeStatus": "INDIFFERENT",
+                      "videoType": "MUSIC_VIDEO_TYPE_ATV",
+                      "artists": [
+                        {
+                          "name": "Seven Lions",
+                          "id": "UCYd2yzYRx7b9FYnBSlbnknA"
+                        },
+                        {
+                          "name": "Jason Ross",
+                          "id": "UCVCD9Iwnqn2ipN9JIF6B-nA"
+                        },
+                        {
+                          "name": "Crystal Skies",
+                          "id": "UCTJZESxeZ0J_M7JXyFUVmvA"
+                        }
+                      ],
+                      "album": {
+                        "name": "Foolish Of Me",
+                        "id": "MPREb_C8aRK1qmsDJ"
+                      },
+                      "year": "2020",
+                      "counterpart": {
+                        "videoId": "E0S4W34zFMA",
+                        "title": "Foolish Of Me [ABGT404] (feat. Jonathan Mendelsohn)",
+                        "length": "3:07",
+                        "thumbnail": [...],
+                        "feedbackTokens": null,
+                        "likeStatus": "LIKE",
+                        "artists": [
+                          {
+                            "name": "Jason Ross",
+                            "id": null
+                          },
+                          {
+                            "name": "Seven Lions",
+                            "id": null
+                          },
+                          {
+                            "name": "Crystal Skies",
+                            "id": null
+                          }
+                        ],
+                        "views": "6.6K"
+                      }
+                    },...
+                ],
+                "playlistId": "RDAMVM4y33h81phKU",
+                "lyrics": "MPLYt_HNNclO0Ddoc-17"
+            }
+
+        """
+        body = {
+            'enablePersistentPlaylistPanel': True,
+            'isAudioOnly': True,
+            'tunerSettingValue': 'AUTOMIX_SETTING_NORMAL'
+        }
+        if not videoId and not playlistId:
+            raise Exception("You must provide either a video id, a playlist id, or both")
+        if videoId:
+            body['videoId'] = videoId
+            if not playlistId:
+                playlistId = "RDAMVM" + videoId
+            if not params:
+                body['watchEndpointMusicSupportedConfigs'] = {
+                    'watchEndpointMusicConfig': {
+                        'hasPersistentPlaylistPanel': True,
+                        'musicVideoType': "MUSIC_VIDEO_TYPE_ATV",
+                    }
+                }
+        body['playlistId'] = validate_playlist_id(playlistId)
+        is_playlist = body['playlistId'].startswith('PL') or \
+                      body['playlistId'].startswith('OLA')
+        if params:
+            body['params'] = params
+        endpoint = 'next'
+        response = self._send_request(endpoint, body)
+        watchNextRenderer = nav(response, [
+            'contents', 'singleColumnMusicWatchNextResultsRenderer', 'tabbedRenderer',
+            'watchNextTabbedResultsRenderer'
+        ])
+
+        lyrics_browse_id = get_tab_browse_id(watchNextRenderer, 1)
+        related_browse_id = get_tab_browse_id(watchNextRenderer, 2)
+
+        results = nav(watchNextRenderer,
+                      TAB_CONTENT + ['musicQueueRenderer', 'content', 'playlistPanelRenderer'])
+        playlist = next(
+            filter(
+                bool,
+                map(
+                    lambda x: nav(x, ['playlistPanelVideoRenderer'] + NAVIGATION_PLAYLIST_ID, True
+                                  ), results['contents'])), None)
+        tracks = parse_watch_playlist(results['contents'])
+
+        if 'continuations' in results:
+            request_func = lambda additionalParams: self._send_request(
+                endpoint, body, additionalParams)
+            parse_func = lambda contents: parse_watch_playlist(contents)
+            tracks.extend(
+                get_continuations(results, 'playlistPanelContinuation', limit - len(tracks),
+                                  request_func, parse_func, '' if is_playlist else 'Radio'))
+
+        return dict(tracks=tracks,
+                    playlistId=playlist,
+                    lyrics=lyrics_browse_id,
+                    related=related_browse_id)
+
+    def get_watch_playlist_shuffle(self,
+                                   videoId: str = None,
+                                   playlistId: str = None,
+                                   limit=50) -> Dict[str, Union[List[Dict]]]:
+        """
+        Shuffle any playlist
+
+        :param videoId: Optional video id of the first video in the shuffled playlist
+        :param playlistId: Playlist id
+        :param limit: The number of watch playlist items to return
+        :return: A list of watch playlist items (see :py:func:`get_watch_playlist`)
+        """
+        return self.get_watch_playlist(videoId=videoId,
+                                       playlistId=playlistId,
+                                       limit=limit,
+                                       params='wAEB8gECKAE%3D')
---
+++ packages/ytmusicapi/ytmusicapi-0.23.0.tar.gz/ytmusicapi-0.23.0/ytmusicapi/navigation.py
@@ -0,0 +1,93 @@
+# commonly used navigation paths
+RUN_TEXT = ['runs', 0, 'text']
+TAB_CONTENT = ['tabs', 0, 'tabRenderer', 'content']
+SINGLE_COLUMN_TAB = ['contents', 'singleColumnBrowseResultsRenderer'] + TAB_CONTENT
+SECTION_LIST = ['sectionListRenderer', 'contents']
+SECTION_LIST_ITEM = ['sectionListRenderer', 'contents', 0]
+ITEM_SECTION = ['itemSectionRenderer', 'contents', 0]
+MUSIC_SHELF = ['musicShelfRenderer']
+GRID = ['gridRenderer']
+GRID_ITEMS = GRID + ['items']
+MENU = ['menu', 'menuRenderer']
+MENU_ITEMS = MENU + ['items']
+MENU_LIKE_STATUS = MENU + ['topLevelButtons', 0, 'likeButtonRenderer', 'likeStatus']
+MENU_SERVICE = ['menuServiceItemRenderer', 'serviceEndpoint']
+TOGGLE_MENU = 'toggleMenuServiceItemRenderer'
+PLAY_BUTTON = [
+    'overlay', 'musicItemThumbnailOverlayRenderer', 'content', 'musicPlayButtonRenderer'
+]
+NAVIGATION_BROWSE = ['navigationEndpoint', 'browseEndpoint']
+NAVIGATION_BROWSE_ID = NAVIGATION_BROWSE + ['browseId']
+PAGE_TYPE = [
+    'browseEndpointContextSupportedConfigs', 'browseEndpointContextMusicConfig', 'pageType'
+]
+NAVIGATION_VIDEO_ID = ['navigationEndpoint', 'watchEndpoint', 'videoId']
+NAVIGATION_PLAYLIST_ID = ['navigationEndpoint', 'watchEndpoint', 'playlistId']
+NAVIGATION_WATCH_PLAYLIST_ID = ['navigationEndpoint', 'watchPlaylistEndpoint', 'playlistId']
+NAVIGATION_VIDEO_TYPE = [
+    'watchEndpoint', 'watchEndpointMusicSupportedConfigs', 'watchEndpointMusicConfig',
+    'musicVideoType'
+]
+HEADER_DETAIL = ['header', 'musicDetailHeaderRenderer']
+DESCRIPTION_SHELF = ['musicDescriptionShelfRenderer']
+DESCRIPTION = ['description'] + RUN_TEXT
+CAROUSEL = ['musicCarouselShelfRenderer']
+IMMERSIVE_CAROUSEL = ['musicImmersiveCarouselShelfRenderer']
+CAROUSEL_CONTENTS = CAROUSEL + ['contents']
+CAROUSEL_TITLE = ['header', 'musicCarouselShelfBasicHeaderRenderer', 'title', 'runs', 0]
+FRAMEWORK_MUTATIONS = ['frameworkUpdates', 'entityBatchUpdate', 'mutations']
+TITLE = ['title', 'runs', 0]
+TITLE_TEXT = ['title'] + RUN_TEXT
+TEXT_RUNS = ['text', 'runs']
+TEXT_RUN = TEXT_RUNS + [0]
+TEXT_RUN_TEXT = TEXT_RUN + ['text']
+SUBTITLE = ['subtitle'] + RUN_TEXT
+SUBTITLE2 = ['subtitle', 'runs', 2, 'text']
+SUBTITLE3 = ['subtitle', 'runs', 4, 'text']
+THUMBNAIL = ['thumbnail', 'thumbnails']
+THUMBNAILS = ['thumbnail', 'musicThumbnailRenderer'] + THUMBNAIL
+THUMBNAIL_RENDERER = ['thumbnailRenderer', 'musicThumbnailRenderer'] + THUMBNAIL
+THUMBNAIL_CROPPED = ['thumbnail', 'croppedSquareThumbnailRenderer'] + THUMBNAIL
+FEEDBACK_TOKEN = ['feedbackEndpoint', 'feedbackToken']
+BADGE_LABEL = [
+    'badges', 0, 'musicInlineBadgeRenderer', 'accessibilityData', 'accessibilityData', 'label'
+]
+RELOAD_CONTINUATION = ['continuations', 0, 'reloadContinuationData', 'continuation']
+CATEGORY_TITLE = ['musicNavigationButtonRenderer', 'buttonText'] + RUN_TEXT
+CATEGORY_PARAMS = ['musicNavigationButtonRenderer', 'clickCommand', 'browseEndpoint', 'params']
+MRLIR = 'musicResponsiveListItemRenderer'
+MTRIR = 'musicTwoRowItemRenderer'
+TASTE_PROFILE_ITEMS = ["contents", "tastebuilderRenderer", "contents"]
+TASTE_PROFILE_ARTIST = ["title", "runs"]
+
+
+def nav(root, items, none_if_absent=False):
+    """Access a nested object in root by item sequence."""
+    try:
+        for k in items:
+            root = root[k]
+        return root
+    except Exception as err:
+        if none_if_absent:
+            return None
+        else:
+            raise err
+
+
+def find_object_by_key(object_list, key, nested=None, is_key=False):
+    for item in object_list:
+        if nested:
+            item = item[nested]
+        if key in item:
+            return item[key] if is_key else item
+    return None
+
+
+def find_objects_by_key(object_list, key, nested=None):
+    objects = []
+    for item in object_list:
+        if nested:
+            item = item[nested]
+        if key in item:
+            objects.append(item)
+    return objects
