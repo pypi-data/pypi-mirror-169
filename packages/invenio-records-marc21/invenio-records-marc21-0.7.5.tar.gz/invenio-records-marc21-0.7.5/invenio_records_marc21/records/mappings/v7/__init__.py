156b6fd15e1c4981179c1ba4a845d41a5f5d8884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:45 +0000

    Add package invenio-records-marc21/0.7.5/invenio-records-marc21-0.7.5.tar.gz

---
+++ packages/invenio-records-marc21/invenio-records-marc21-0.7.5.tar.gz/invenio-records-marc21-0.7.5/invenio_records_marc21/records/mappings/v7/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+#
+# Copyright (C) 2021 Graz University of Technology.
+#
+# Invenio-Records-Marc21 is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Mappings for Elasticsearch 7.x."""
+
+from __future__ import absolute_import, print_function
