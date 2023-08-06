175e089485d81ba7b45d870705d2dae50ea02af7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:30 +0000

    Add package helloex1/0.0.1/helloex1-0.0.1.tar.gz

---
+++ packages/helloex1/helloex1-0.0.1.tar.gz/helloex1-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'helloworld example'
+
+
+# Setting up
+setup(
+    name="helloex1",
+    version=VERSION,
+    author="Omar Sherif",
+    author_email="<osherif1@ford.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
