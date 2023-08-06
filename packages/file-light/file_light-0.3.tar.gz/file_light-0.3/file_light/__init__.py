c0baa0729d794a3a1779381514848360c1b23cb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:01 +0000

    Add package file-light/0.3/file_light-0.3.tar.gz

---
+++ packages/file-light/file_light-0.3.tar.gz/file_light-0.3/file_light/__init__.py
@@ -0,0 +1,7 @@
+from .file_light import Renaming
+from .file_light import Path
+from .file_light import Copy_File
+from .file_light import New_Path
+from .file_light import Read_Split_File
+from .file_light import Delete_File
+from .file_light import Delete_Folder
