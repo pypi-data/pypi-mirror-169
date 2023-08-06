d58f283b6e0940179af00b3188a97ce2d4e9f8a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:47 +0000

    Add package pytiers/2.1.9/pytiers-2.1.9.tar.gz

---
+++ packages/pytiers/pytiers-2.1.9.tar.gz/pytiers-2.1.9/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='pytiers',
+    version='2.1.9',
+    author='Huang Bo-Xuan',
+    author_email='r09142003@gmail.com',
+    install_requires=[
+        'seaborn',
+        'matplotlib',
+        'pandas',
+        'plotly',
+        'nbformat>=4.2.0',
+        'numpy'
+    ]
+)
