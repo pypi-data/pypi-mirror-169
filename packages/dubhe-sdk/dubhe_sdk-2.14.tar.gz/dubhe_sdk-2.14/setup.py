0d9975deb4656f27f30f6a2489cb3ab2559b1811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:06:57 +0000

    Add package dubhe-sdk/2.14/dubhe_sdk-2.14.tar.gz

---
+++ packages/dubhe-sdk/dubhe_sdk-2.14.tar.gz/dubhe_sdk-2.14/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='dubhe_sdk',
+      version="2.14",
+      description='TCL AI platform, algorithm software development kit',
+      long_description='TCL AI platform, algorithm software development kit',
+      classifiers=[],
+      keywords='dubhe',
+      author='huanghui',
+      author_email='',
+      url='https://github.com/hathawayhh/dubhe-sdk',
+      license='MIT',
+      packages=find_packages(),
+      data_files=[],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+        "Flask==1.1.1",
+        "kafka-python==2.0.1"
+      ])
