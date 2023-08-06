aa34ffc8209d2f73cb8ea2f035d0894a16710c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:02 +0000

    Add package CLUEstering/1.1.14/CLUEstering-1.1.14.tar.gz

---
+++ packages/CLUEstering/CLUEstering-1.1.14.tar.gz/CLUEstering-1.1.14/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from pybind11.setup_helpers import Pybind11Extension
+import codecs
+import os
+
+__version__ = "1.1.14"
+
+ext_modules = [
+	Pybind11Extension(
+		"CLUEsteringCPP",
+		['CLUEstering/test.cc'],
+	),
+]
+
+setup(
+    name="CLUEstering",
+    version=__version__,
+    author="Simone Balducci",
+    author_email="simone.balducci00@gmail.com",
+    description="A library that generalizes the original 2-dimensional CLUE algorithm made at CERN.",
+	 packages=find_packages(),
+	 install_requires=['sklearn','numpy','matplotlib','pandas'],
+	 ext_modules=ext_modules,
+	 keywords=['Python','Clustering','Binding'],
+	 classifiers=[
+		'Intended Audience :: Developers',
+		'Programming Language :: Python :: 3',
+		'Operating System :: Unix',
+		'Operating System :: MacOS :: MacOS X',
+		'Operating System :: Microsoft :: Windows',
+	 ]
+)
