7f12afe31dc877bc66090da536979512392f01ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:58 +0000

    Add package mongo-redis-postgres-read-only-user/0.1.1/mongo-redis-postgres-read-only-user-0.1.1.tar.gz

---
+++ packages/mongo-redis-postgres-read-only-user/mongo-redis-postgres-read-only-user-0.1.1.tar.gz/mongo-redis-postgres-read-only-user-0.1.1/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mongo_redis_postgres_read_only_user']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['psycopg2==2.9.3', 'pymongo==4.2.0', 'redis==4.3.4', 'typer==0.6.1']
+
+entry_points = \
+{'console_scripts': ['create-read-user = '
+                     'mongo_redis_postgres_read_only_user.main:app']}
+
+setup_kwargs = {
+    'name': 'mongo-redis-postgres-read-only-user',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': '# Script for creating read only user for MongoDB, PostgreSQL, Redis\n## PostgresQL\n- `create-read-user postgres --uri Postgres_URI -u USERNAME -p PASSWORD `\n## Redis\n- `create-read-user redis --uri Redis_URI -u USERNAME -p PASSWORD `\n## MongoDB\n- `create-read-user mongodb --uri Mongo_URI -u USERNAME -p PASSWORD `',
+    'author': 'ARJUN',
+    'author_email': '74967141+arjun2038@users.noreply.github.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
