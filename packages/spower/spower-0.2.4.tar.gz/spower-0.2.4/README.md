74a84de05e7e268e87734388c97fbe4fd843ae4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:28 +0000

    Add package spower/0.2.4/spower-0.2.4.tar.gz

---
+++ packages/spower/spower-0.2.4.tar.gz/spower-0.2.4/README.md
@@ -0,0 +1,34 @@
+## Usage
+
+**easy search using this library**
+
+```
+
+#Search with Google
+>>> from spower import *
+>>> search.google("mishakorzik")
+['https://github.com/mishakorzik', 'https://github.com/mishakorzik/AllHackingTools', ...
+>>> 
+
+#Search with DuckDuckGo
+>>> from spower import *
+>>> search.duckduckgo("mishakorzik")
+'["https://github.com/mishakorzik/IpHack", "https://github.com/mishakorzik/virus.bat", ...
+>>> 
+
+#Search with Shodan
+>>> from spower import *
+>>> search.shodan("github")
+[+] IP        : 192.30.25*.***
+[+] Port      : 80
+[+] Org       : GitHub, Inc.
+[+] Country   : United States
+[+] longitude : -77.0****
+[+] latitude  : 38.8****
+[+] City      : Washington
+[+] Layer     : tcp
+[+] Domains   : ['github.com']
+[+] Hostnames : ['lb-192-30-25*-***-iad.github.com']
+>>> 
+
+```
---
+++ packages/spower/spower-0.2.4.tar.gz/spower-0.2.4/setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+# Reads the content of your README.md into a variable to be used in the setup below
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='spower',  # should match the package folder
+    packages=['spower'],  # should match the package folder
+    version='0.2.4',  # important for updates
+    python_requires=">=3.4",
+    license='Apache 2.0',  # should match your chosen license
+    description='Free searcher with python, no API keys are required',
+    long_description=long_description,  # loads your README.md
+    long_description_content_type="text/markdown",  # README.md is of type 'markdown'
+    author='MishaKorzhik_He1Zen',
+    author_email='developer.mishakorzhik@gmail.com',
+    project_urls={  # Optional
+        "Sponsor": "https://www.buymeacoffee.com/mishakorzik"
+    },
+    install_requires=['googlesearcher' 'shodan', 'urllib3'],
+    keywords=["search", "google", "duck", "duckduckgo", "searcher", "shodan", "api", "easy", "pip", "pypi", "dork", "dorks", "dorking"],  # descriptive meta-data
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries",
+    ],
+
+)
