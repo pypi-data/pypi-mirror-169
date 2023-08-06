b33974b014b4e97bb4734240811f31e3bbe9b6ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:24 +0000

    Add package lantools/0.0.47/lantools-0.0.47-py3-none-any.whl

---
+++ packages/lantools/lantools-0.0.47-py3-none-any.whl/lantools/schema/logger.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from loguru import logger
+
+class Logger(BaseModel):
+    level: str = 'INFO'
+    retention: str = '7 days'
+    compression: str = 'zip'
+    rotation: str = '00:00'
+    log_path: str = "/var/log"
+
+    def initial(self, filename):
+        filename = '{}/{}'.format(self.log_path, filename)
+        logger.add(filename, rotation=self.rotation, retention=self.retention, compression=self.compression, level=self.level)
+
+        return logger
+
