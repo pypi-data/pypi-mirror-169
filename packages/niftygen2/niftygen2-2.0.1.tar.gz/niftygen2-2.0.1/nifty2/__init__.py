fcc6db439667e1cba44d6716679744cafc0099bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:57 +0000

    Add package niftygen2/2.0.1/niftygen2-2.0.1.tar.gz

---
+++ packages/niftygen2/niftygen2-2.0.1.tar.gz/niftygen2-2.0.1/nifty2/__init__.py
@@ -0,0 +1,3 @@
+__version__ = "2.0.1"
+__author__ = 'Vaedz#9210'
+__name__ = "NiFTy"
---
+++ packages/niftygen2/niftygen2-2.0.1.tar.gz/niftygen2-2.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='niftygen2',
+    version='2.0.1',    
+    description='Python based random image generation',
+    url='https://github.com/Vaedz7/NiFTy.git',
+    author='Vaedz#9210',
+    author_email='dhruv.vaed@gmail.com',
+    license='MIT Licence',
+    packages=['nifty2'],
+    install_requires=['pillow'],
+
+    classifiers=[
+      'Development Status :: 5 - Production/Stable',
+      'Programming Language :: Python :: 2',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.4',
+      'Programming Language :: Python :: 3.5',
+      'Natural Language :: English',
+    ],
+)
