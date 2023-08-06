1557cc967e77e782288d6145df22a5681bddba60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:24 +0000

    Add package poiebot/1.0.3/poiebot-1.0.3.tar.gz

---
+++ packages/poiebot/poiebot-1.0.3.tar.gz/poiebot-1.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name ='poiebot',
+    version = '1.0.3',
+    description = 'poiebot',
+    author = 'pb',
+    author_email = 'poiega@poie.ga',
+    url = None,
+    py_modules = ['poiebot'] 
+)
