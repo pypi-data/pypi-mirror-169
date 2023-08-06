d45df3418f233f5112dedbc8bd92a4fc3eb9de38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:58 +0000

    Add package simphile/0.1.9/simphile-0.1.9.tar.gz

---
+++ packages/simphile/simphile-0.1.9.tar.gz/simphile-0.1.9/simphile/__init__.py
@@ -0,0 +1,5 @@
+from . import sets
+from .naive_bayes import NaiveBayes
+from .textsimilarity.compression_similarity import compression_similarity, CompressionSimilarity
+from .textsimilarity.euclidian_similarity import euclidian_similarity, EuclidianSimilarity
+from .textsimilarity.jaccard_similarity import jaccard_similarity, JaccardSimilarity
