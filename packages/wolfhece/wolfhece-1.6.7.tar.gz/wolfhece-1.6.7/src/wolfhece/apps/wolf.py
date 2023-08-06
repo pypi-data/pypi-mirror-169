c11e7100547b3a7375167015c0e32aa01ef31c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:36 +0000

    Add package wolfhece/1.6.7/wolfhece-1.6.7.tar.gz

---
+++ packages/wolfhece/wolfhece-1.6.7.tar.gz/wolfhece-1.6.7/src/wolfhece/apps/wolf.py
@@ -0,0 +1,16 @@
+import os
+import wx
+try:
+    from ..PyTranslate import _
+    from ..PyGui import MapManager
+except:
+    from wolfhece.PyTranslate import _
+    from wolfhece.PyGui import MapManager
+
+def main():
+    ex = wx.App()
+    mywolf=MapManager()
+    ex.MainLoop()
+
+if __name__=='__main__':
+    main()
