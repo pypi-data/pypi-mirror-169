7a5b851caf054a0f606a72fb49111005d8f78268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:06 +0000

    Add package cjutools/0.0.1/cjutools-0.0.1.tar.gz

---
+++ packages/cjutools/cjutools-0.0.1.tar.gz/cjutools-0.0.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: cjylzs(691086891@qq.com)
+# Description: utils and cmd framework
+
+from setuptools import setup, find_packages
+
+setup(
+    name='cjutools',
+    version='0.0.1',
+    keywords='cjutools',
+    description='tools depend cjutils',
+    license='MIT License',
+    url='https://github.com/CJYLZS/cjtools.git',
+    author='cjylzs',
+    author_email='691086891@qq.com',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=['cjutils']
+)
