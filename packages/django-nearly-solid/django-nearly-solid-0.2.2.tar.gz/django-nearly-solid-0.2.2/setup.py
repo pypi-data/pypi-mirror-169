3207e31c3b61f1018eca5b52faaa713a63cb00d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:41 +0000

    Add package django-nearly-solid/0.2.2/django-nearly-solid-0.2.2.tar.gz

---
+++ packages/django-nearly-solid/django-nearly-solid-0.2.2.tar.gz/django-nearly-solid-0.2.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+version = '0.2.2'
+
+packages = find_packages()
+
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='django-nearly-solid',
+    version=version,
+    author="Artem Romanukov",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    include_package_data=True,
+    zip_safe=False,
+    packages=packages,
+    requires=[
+        'django',
+        'pydantic',
+    ],
+    url="https://github.com/romanukov/django-nearly-solid",
+)
