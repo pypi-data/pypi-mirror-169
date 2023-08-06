c7d862c437b798acf953398036fbffb2bc1837e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:39 +0000

    Add package hmd-dbt-pbt/1.2/hmd_dbt_pbt-1.2.tar.gz

---
+++ packages/hmd-dbt-pbt/hmd_dbt_pbt-1.2.tar.gz/hmd_dbt_pbt-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='hmd_dbt_pbt',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['hmd_dbt_pbt'],
+      author='Ahmed Limem',
+      author_email='ahmed.limem@etudiant-enit.utm.tn',
+      zip_safe=False)
