c63d4ff9c599d60719e962323c7f7bc771ffdd6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:03 +0000

    Add package xj-dictionary/1.0.4/xj_dictionary-1.0.4.tar.gz

---
+++ packages/xj-dictionary/xj_dictionary-1.0.4.tar.gz/xj_dictionary-1.0.4/xj_dictionary/urls.py
@@ -0,0 +1,26 @@
+# encoding: utf-8
+"""
+@project: djangoModel->url
+@author: 孙楷炎
+@Email: sky4834@163.com
+@synopsis:
+@created_time: 2022/6/6 14:25
+"""
+
+from django.urls import re_path
+
+from .apis.dictionary_configure import DictionaryConfigure, DictionaryConfigurePage, BatchDictionaryConfigure
+
+# from apps.dictionary.apis.dictionary_configure import DictionaryConfigure
+
+# 应用名称
+app_name = 'xj_dictionary'
+
+urlpatterns = [
+    re_path("^configure/?$", DictionaryConfigure.as_view()),
+    re_path("^list/?$", DictionaryConfigurePage.as_view()),  # 分页展示配置
+    re_path("^groups/?$", BatchDictionaryConfigure.as_view()),  # 分组配置获取
+
+    # path("_configure/<str:group>", DictionaryConfigure.as_view()),
+    # path("_configure/<str:group>/<str:key>", DictionaryConfigure.as_view()),
+]
