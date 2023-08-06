f547e9763fb7dfc55fb273bb04b7cc98e07d450a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:14 +0000

    Add package MeineUtils/0.0.21/MeineUtils-0.0.21.tar.gz

---
+++ packages/MeineUtils/MeineUtils-0.0.21.tar.gz/MeineUtils-0.0.21/MeineUtils/MachineLearning/__init__.py
@@ -0,0 +1,16 @@
+from .linear_regression import (
+    LinearRegression
+)
+
+# from .normalization import (
+#     MinMaxScaler,
+#     StandardScaler
+# )
+
+from .bounding_box import (
+    BoundingBoxConverter
+)
+
+from .augmentation import (
+    RandomRotate
+)
