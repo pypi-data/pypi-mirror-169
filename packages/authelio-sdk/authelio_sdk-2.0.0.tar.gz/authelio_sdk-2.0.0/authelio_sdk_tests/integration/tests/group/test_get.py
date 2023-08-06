5cb26693af0d9e209d204c29476f1ee93d033522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:25 +0000

    Add package authelio-sdk/2.0.0/authelio_sdk-2.0.0.tar.gz

---
+++ packages/authelio-sdk/authelio_sdk-2.0.0.tar.gz/authelio_sdk-2.0.0/authelio_sdk_tests/integration/tests/group/test_get.py
@@ -0,0 +1,18 @@
+from authelio_sdk.client import Client
+from authelio_sdk.models.group import Group
+
+
+def test_FUNC_client_group_get_WITH_valid_configuration_EXPECT_group_retrieved(sdk_client: Client, group: Group) -> None:
+    """
+    Check whether with good configuration a group can be retrieved.
+
+    :param sdk_client: Authelio SDK client.
+    :param group: Group fixture.
+
+    :return: No return.
+    """
+    # Ensure group exists.
+    groups_data = sdk_client.group.get(group_ids=[group.group_id])
+    retrieved_group = groups_data[group.group_id]
+
+    assert retrieved_group.group_id == group.group_id
