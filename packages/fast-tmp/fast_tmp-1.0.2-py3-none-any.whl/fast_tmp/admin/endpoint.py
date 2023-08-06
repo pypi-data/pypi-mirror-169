9fc4b7770972a9669abd7db2f9148dffb412f1cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:01 +0000

    Add package fast-tmp/1.0.2/fast_tmp-1.0.2-py3-none-any.whl

---
+++ packages/fast-tmp/fast_tmp-1.0.2-py3-none-any.whl/fast_tmp/admin/endpoint.py
@@ -0,0 +1,121 @@
+from typing import Optional
+
+from fastapi import APIRouter, Depends
+from starlette.requests import Request
+
+from fast_tmp.admin.depends import get_staff
+from fast_tmp.responses import BaseRes, ListDataWithPage
+from fast_tmp.site import ModelAdmin, get_model_site
+
+router = APIRouter()
+
+
+@router.get("/{resource}/list", dependencies=[Depends(get_staff)])
+async def list_view(
+    request: Request,
+    page_model: ModelAdmin = Depends(get_model_site),
+    perPage: int = 10,
+    page: int = 1,
+):
+    datas = await page_model.list(request, perPage, page)
+    return BaseRes(
+        data=ListDataWithPage(
+            total=datas["total"],
+            items=datas["items"],
+        )
+    )
+
+
+@router.get("/{resource}/prefetch/{field_name}", dependencies=[Depends(get_staff)])
+async def prefetch_view(
+    request: Request,
+    field_name: str,
+    pk: Optional[str] = None,
+    perPage: Optional[int] = None,
+    page: Optional[int] = None,
+    page_model: ModelAdmin = Depends(get_model_site),
+):
+    """
+    对多对多外键进行额外的加载
+    """
+    datas = await page_model.select_options(request, field_name, pk, perPage, page)
+    return BaseRes(data=datas)
+
+
+@router.get("/{resource}/select/{field_name}", dependencies=[Depends(get_staff)])
+async def select_view(
+    request: Request,
+    field_name: str,
+    pk: Optional[str] = None,
+    perPage: Optional[int] = None,
+    page: Optional[int] = None,
+    page_model: ModelAdmin = Depends(get_model_site),
+):
+    """
+    枚举字段的额外加载，主要用于外键
+    """
+    datas = await page_model.select_options(request, field_name, pk, perPage, page)
+    return BaseRes(data=datas)
+
+
+@router.post("/{resource}/patch/{pk}", dependencies=[Depends(get_staff)])
+async def patch_data(
+    request: Request,
+    pk: str,
+    page_model: ModelAdmin = Depends(get_model_site),
+):
+    """
+    内联模式快速修改需要的接口
+    """
+    data = await request.json()
+    await page_model.patch(request, pk, data)
+    return BaseRes().dict()
+
+
+@router.put("/{resource}/update/{pk}", dependencies=[Depends(get_staff)])
+async def update_data(
+    request: Request,
+    pk: str,
+    page_model: ModelAdmin = Depends(get_model_site),
+):
+    data = await request.json()
+    data = await page_model.put(request, pk, data)
+    return BaseRes(data=data)
+
+
+@router.get("/{resource}/update/{pk}", dependencies=[Depends(get_staff)])
+async def update_view(
+    request: Request,
+    pk: str,
+    page_model: ModelAdmin = Depends(get_model_site),
+):
+    data = await page_model.put_get(request, pk)
+    return BaseRes(data=data)
+
+
+@router.post("/{resource}/create", dependencies=[Depends(get_staff)])
+async def create(
+    request: Request,
+    page_model: ModelAdmin = Depends(get_model_site),
+):
+    data = await request.json()
+    await page_model.create(request, data)
+    return BaseRes(data=data)
+
+
+@router.delete("/{resource}/delete/{pk}", dependencies=[Depends(get_staff)])
+async def delete_func(
+    request: Request,
+    pk: str,
+    page_model: ModelAdmin = Depends(get_model_site),
+):
+    await page_model.delete(request, pk)
+    return BaseRes()
+
+
+@router.get("/{resource}/schema", dependencies=[Depends(get_staff)])
+async def get_schema(
+    request: Request,
+    page: ModelAdmin = Depends(get_model_site),
+):
+    return BaseRes(data=await page.get_app_page(request))
