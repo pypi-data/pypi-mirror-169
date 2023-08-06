5121330788a1dc84851625faca73ef68c508a16c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:22 +0000

    Add package feincms3/4.0a1/feincms3-4.0a1.tar.gz

---
+++ packages/feincms3/feincms3-4.0a1.tar.gz/feincms3-4.0a1/feincms3/plugins/__init__.py
@@ -0,0 +1,23 @@
+# flake8: noqa
+
+from . import html, richtext, snippet
+
+
+try:
+    import requests
+except ImportError:  # pragma: no cover
+    pass
+else:
+    from . import external
+try:
+    import imagefield
+except ImportError:  # pragma: no cover
+    pass
+else:
+    from . import image
+try:
+    import ckeditor
+except ImportError:  # pragma: no cover
+    pass
+else:
+    from . import old_richtext
