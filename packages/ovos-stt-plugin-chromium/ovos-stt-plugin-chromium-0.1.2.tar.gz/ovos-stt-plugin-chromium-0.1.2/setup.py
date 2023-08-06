5e37da111c3112d626f84f507ef29d503dc17ea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:00 +0000

    Add package ovos-stt-plugin-chromium/0.1.2/ovos-stt-plugin-chromium-0.1.2.tar.gz

---
+++ packages/ovos-stt-plugin-chromium/ovos-stt-plugin-chromium-0.1.2.tar.gz/ovos-stt-plugin-chromium-0.1.2/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+PLUGIN_ENTRY_POINT = 'ovos-stt-plugin-chromium = ovos_stt_plugin_chromium:ChromiumSTT'
+CONFIG_ENTRY_POINT = 'ovos-stt-plugin-chromium.config = ovos_stt_plugin_chromium:ChromiumSTTConfig'
+
+setup(
+    name='ovos-stt-plugin-chromium',
+    version='0.1.2',
+    description='A stt plugin for mycroft using the google chrome browser api',
+    url='https://github.com/OpenVoiceOS/ovos-stt-plugin-chromium',
+    author='JarbasAi',
+    author_email='jarbasai@mailfence.com',
+    license='Apache-2.0',
+    packages=['ovos_stt_plugin_chromium'],
+    install_requires=["requests",
+                      "ovos_utils>=0.0.12a1",
+                      "ovos-plugin-manager>=0.0.1a7"],
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Text Processing :: Linguistic',
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='mycroft ovos plugin stt',
+    entry_points={'mycroft.plugin.stt': PLUGIN_ENTRY_POINT,
+                  'mycroft.plugin.stt.config': CONFIG_ENTRY_POINT}
+)
