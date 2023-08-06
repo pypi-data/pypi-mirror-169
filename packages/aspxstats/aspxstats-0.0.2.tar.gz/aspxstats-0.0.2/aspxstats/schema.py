a905550ce63ebed31c8d3f71f3f237ecde3da47a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:20 +0000

    Add package aspxstats/0.0.2/aspxstats-0.0.2.tar.gz

---
+++ packages/aspxstats/aspxstats-0.0.2.tar.gz/aspxstats-0.0.2/aspxstats/schema.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from typing import Optional, Dict, Union, TypeVar
+
+
+@dataclass
+class AttributeSchema:
+    type: type
+    is_numeric: bool = False
+    is_floaty: bool = False
+    is_ratio: bool = False
+    children: Optional[Dict[str, Union[dict, 'AttributeSchema']]] = None
+
+
+DictSchema = TypeVar('DictSchema', bound=Dict[str, Union[dict, AttributeSchema]])
