096fdef2464d3ba843d156822ac089d6cf9dd0cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:08 +0000

    Add package mwpdfify/1.1.2/mwpdfify-1.1.2.tar.gz

---
+++ packages/mwpdfify/mwpdfify-1.1.2.tar.gz/mwpdfify-1.1.2/README.md
@@ -0,0 +1,56 @@
+# mwpdfify
+Batch download multiple pages from MediaWiki sites (All pages or pages of a category) to printable PDFs. 
+
+## Install / Run
+`pip install mwpdfify`
+
+...or clone repo and `pip install .`
+
+...or directly download and run `src/mwpdfify.py`
+
+There are two PDF rendering backends to choose from: `pdfkit` (installed as a dependency by default) or `weasyprint`. Use `pip install -r requirements.txt` to install both or choose one yourself. **If using the former remember to also install `wkhtmltopdf` on your system.**
+
+## Usage
+1. Get the address of the **root** of your wiki, where its `api.php` and `index.php` resides. Typically it's identical to the site's root (`/`). For Wikipedia it's at `/w/`; tell me if there are other exceptions ;)
+2. (optional) If you want only a specific category, get its title (in the form of `Category:XXX`)
+3. Run the script. eg.:
+   - `mwpdfify https://lycoris-recoil.fandom.com` - Download all pages (as in Special:AllPages) from [Lycoris Recoil Fandom Wiki](https://lycoris-recoil.fandom.com/) as PDF
+   - `mwpdfify wiki.archlinux.org -c Category:Installation_process` - Download all pages under Category:Installation_process from [ArchWiki](https://wiki.archlinux.org/) as PDF
+   - `mwpdfify https://en.wikipedia.org/w/ -c Category:Guangzhou_Metro_stations -l 10 -t 4` - Download all pages under Category:Guangzhou_Metro_stations (except subcategories) from [Wikipedia](https://en.wikipedia.org), with 4 download threads and an one-time query limit of 10
+
+The downloaded PDFs should be avaliable in a folder marked with the site's domain name in the current directory. 
+
+See below for other parameters:
+```
+usage: mwpdfify [-h] [-c CATEGORY] [-p] [-t THREADS] [-l LIMIT] [-w] url
+
+positional arguments:
+  url                   site root of destination site
+
+options:
+  -h, --help            show this help message and exit
+  -c CATEGORY, --category CATEGORY
+                        Download only a specified category
+  -p, --no-printable    Force normal instead of printable version of pages
+  -t THREADS, --threads THREADS
+                        Number of download threads, defaults to 8
+  -l LIMIT, --limit LIMIT
+                        Limit of JSON info returned at once, defaults to maximum
+                        (0)
+  -w, --use-weasyprint  Use weasyprint as PDF rendering backend
+```
+## Known issues
+- `&printable=yes` is deprecated in recent versions of MediaWiki (while no substitute API solutions are provided) so there might be layout issues when used with certain wikis; *especially* Fandom wikis as they also contain ads. 
+- Recursively download pages from subcategories of a category is currently not supported. 
+
+## Changelog
+- v1.1.2 (2022/09/30):
+  - Set `pdfkit` as required dependency
+- v1.1 (2022/09/04):
+  - Changed address handling logic
+  - Bug fixes
+- v1.0 (2022/09/03): 
+  - Initial release
+
+## License
+LGPLv3
---
+++ packages/mwpdfify/mwpdfify-1.1.2.tar.gz/mwpdfify-1.1.2/src/mwpdfify.py
@@ -0,0 +1,133 @@
+import re
+import os
+import json
+import argparse
+import urllib.request
+from concurrent.futures import ThreadPoolExecutor, as_completed
+try:
+    import pdfkit
+except ImportError:
+    pdfkit = None
+try:
+    import weasyprint
+except ImportError:
+    weasyprint = None
+
+def init_dir(name):
+    dir_path = os.path.join(os.getcwd(), name)
+    os.makedirs(name, exist_ok=True)
+    os.chdir(dir_path)
+
+def site_url(url, is_short):
+    regex = r'^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/?\n]+)'
+    if is_short:
+        return re.findall(regex, url)[0]
+    res = ""
+    if ("http://" not in url) and ("https://" not in url):
+        res = "http://"
+    return res + url
+
+def get_pages(api_addr, limit, is_category, title, resume_pos):
+    address = f"{api_addr}/api.php?action=query&format=json"
+    limit = 'max' if not limit else limit
+    if is_category:
+        if resume_pos:
+            address = address + f"&cmcontinue={resume_pos}"
+        address = address + f"&list=categorymembers&cmtitle={title}&cmlimit={limit}"
+    else:
+        if resume_pos:
+            address = address + f"&apcontinue={resume_pos}"
+        address = address + f"&list=allpages&aplimit={limit}"
+    response = urllib.request.urlopen(address)
+    string = response.read().decode('utf-8')
+    data = json.loads(string)
+    return data
+
+def weasyprint_write(url, name):
+    pdf = weasyprint.HTML(url).write_pdf()
+    with open(name, 'wb') as f:
+        f.write(pdf)
+
+def download(api, backend, threads=8, limit=None, title=None, noprintable=False, recursive=False):
+    is_category = bool(title)
+    with ThreadPoolExecutor(max_workers=threads) as pool:
+        resume_pos = None
+        futures = {}
+        cnt = 0
+        print(f"Using backend '{backend}'")
+        notifier = f"Retrieving pages info of {site_url(api, True)}"
+        if is_category:
+            notifier = notifier + f" ({title})"
+        print(notifier + "...")
+        while True:
+            data = get_pages(api, limit, is_category, title, resume_pos)
+            pages = data["query"][("categorymembers" if is_category else "allpages")]
+            cnt = cnt + len(pages)
+            for page in pages:
+                name = page['title']
+                if 'Category:' in name:
+                    if recursive:
+                        pass # todo
+                    else:
+                        cnt = cnt - 1
+                        continue
+                url = f"{api}/index.php?title={name}"
+                if not noprintable:
+                    url = url + "&printable=yes"
+                name = re.sub(r'/', '_', name)
+                if backend == "pdfkit":
+                    #options={'disable-javascript': None}
+                    futures.update({pool.submit(pdfkit.from_url, url, output_path=f'{name}.pdf'): name})
+                else:
+                    futures.update({pool.submit(weasyprint_write, url, f'{name}.pdf'): name})
+            if 'continue' not in data:
+                break
+            resume_pos = data['continue'][('cmcontinue' if is_category else 'apcontinue')]
+        output(futures,cnt)
+
+def output(futures, cnt):
+    print(f"Found {cnt} pages to download")
+    errcnt = 0
+    curpos = 1
+    for future in as_completed(futures):
+        name = futures[future]
+        try:
+            _d = future.result()
+        except Exception as e:
+            print(f"({curpos}/{cnt}) ERROR on '{name}': {e}")
+            errcnt = errcnt + 1
+        else:
+            print(f"({curpos}/{cnt}) Completed '{name}'")
+        curpos = curpos + 1
+    print(f"Done. {curpos-1} pages processed, {errcnt} errors")
+
+def main():
+    parser = argparse.ArgumentParser(description="Utility for batch downloading (certain) pages from MediaWiki sites as printable PDFs")
+    parser.add_argument('url', help='site root of destination site')
+    parser.add_argument('-c', '--category', help='Download only a specified category', type=str)
+#    parser.add_argument('-r', '--recursive', help='Download through subcategories recursively, only to be used with -c', action='store_true')
+    parser.add_argument('-p', '--no-printable', help='Force normal instead of printable version of pages', action='store_true')
+    parser.add_argument('-t', '--threads', help='Number of download threads, defaults to %(default)s', type=int, default=8)
+    parser.add_argument('-l', '--limit', help='Limit of JSON info returned at once, defaults to maximum (%(default)s)', type=int, default=0)
+    parser.add_argument('-w', '--use-weasyprint', help='Use weasyprint as PDF rendering backend', action='store_true')
+
+    args = parser.parse_args()
+    folder_name = site_url(args.url, True)
+    root_address = site_url(args.url, False)
+
+    args.recursive = None
+#    if args.recursive and not args.category:
+#        print("ERROR: -r/--recursive option is meant to only be used with -c/--category")
+#        exit(1)
+    if ((not args.use_weasyprint) and not pdfkit) or (args.use_weasyprint and not weasyprint):
+        print(f"ERROR: Backend '{args.backend}' unavailable; please install it first or switch to another")
+        exit(1)
+    if urllib.request.urlopen(root_address).code != 200:
+        print("ERROR: Given address not valid")
+        exit(1)
+    if args.category:
+        folder_name = folder_name + f" ({re.sub(r':', '=', args.category)})"
+    init_dir(folder_name)
+    download(root_address, ('weasyprint' if args.use_weasyprint else 'pdfkit'), args.threads, args.limit, args.category, args.no_printable, args.recursive)
+if __name__ == '__main__':
+    main()
