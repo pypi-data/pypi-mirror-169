094cc1187b8c1b561ea3ae39380f051ecfeb8975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:40 +0000

    Add package megadata/0.0.5/megadata-0.0.5-py3-none-any.whl

---
+++ packages/megadata/megadata-0.0.5-py3-none-any.whl/megadata/clt_ipc.py
@@ -0,0 +1,48 @@
+from .mypy import *
+
+def on_quit(*a):
+  print('on_quit',*a)
+  os._exit(0)
+hook_quit(on_quit)
+
+def clt_ipc(argv):
+  argc = len(argv)
+  address = build_address(argv[1], argv[2] if argc>2 else None)
+  print('address=',address)
+
+  out={}
+  server= lambda v: ipc(address, v, out=out)
+
+  import pickle
+  for line in sys.stdin:
+
+    if line.startswith('lambda'): # TMP TEST RPC
+      fc = eval(line)
+      #print('debug fc',fc,fc.__code__)
+      print(server(dumps_func(fc)))
+
+    # e.g. /"Adm","ping"
+    # have fund like /"Adm","ping",*args,**kwargs
+    #  or /api("Adm","ping")
+    elif line.startswith('/') and not line.startswith('/api'):
+        print(server(f'/api({line[1:]})'))
+
+    # super local lines
+    # wanjo: play:
+    # ;type(ipc(r'\\.\pipe\ipctest','algodata.dao,20220704'))
+    # ;server('algodata.dao,20220704')
+    # ;server('(api)')('Adm','ping')
+    elif line.startswith(';'):
+      print( tryx(lambda:eval(line[1:])) )
+      #print(out.keys())
+
+    # e.g.
+    # (api("Adm","ping"))
+    # /api("Adm","ping")
+    # what-so-ever
+    else:
+      rt = tryx(lambda:server(line))
+      print(type(rt),'=>',rt)
+
+if __name__ == '__main__':
+  clt_ipc(argv)
