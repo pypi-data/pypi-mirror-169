535cc7313634ff835c9f3c13078578b3bc1e597f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:53 +0000

    Add package trajectory-analysis-tools/1.1.0/trajectory_analysis_tools-1.1.0.tar.gz

---
+++ packages/trajectory-analysis-tools/trajectory_analysis_tools-1.1.0.tar.gz/trajectory_analysis_tools-1.1.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+INSTALL_REQUIRES = ["numpy", "scipy", "networkx", "replay_trajectory_classification"]
+TESTS_REQUIRE = ["pytest >= 2.7.1"]
+
+setup(
+    name="trajectory_analysis_tools",
+    version="1.1.0",
+    license="MIT",
+    description=(""),
+    author="Eric Denovellis",
+    author_email="eric.denovellis@ucsf.edu",
+    url="",
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+)
---
+++ packages/trajectory-analysis-tools/trajectory_analysis_tools-1.1.0.tar.gz/trajectory_analysis_tools-1.1.0/trajectory_analysis_tools/__init__.py
@@ -0,0 +1,22 @@
+# flake8: noqa
+from trajectory_analysis_tools.distance1D import (
+    get_ahead_behind_distance,
+    get_map_speed,
+    get_trajectory_data,
+)
+from trajectory_analysis_tools.distance2D import (
+    get_2D_distance,
+    get_ahead_behind_distance2D,
+    get_speed,
+    get_velocity,
+    head_direction_simliarity,
+    make_2D_track_graph_from_environment,
+)
+from trajectory_analysis_tools.highest_posterior_density import (
+    get_highest_posterior_threshold,
+    get_HPD_spatial_coverage,
+)
+from trajectory_analysis_tools.posterior import (
+    maximum_a_posteriori_estimate,
+    sample_posterior,
+)
