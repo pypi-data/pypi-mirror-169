f0beffda690b537de298e62bb907b6d45041f7b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:33 +0000

    Add package txdpy/4.0.6/txdpy-4.0.6.tar.gz

---
+++ packages/txdpy/txdpy-4.0.6.tar.gz/txdpy-4.0.6/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import  setup
+
+packages = ['txdpy']
+
+setup(name='txdpy',
+    version='4.0.6',
+    author='唐旭东',
+    install_requires=['mmh3','pymysql','loguru','redis','lxml'],
+    packages=packages,
+    package_dir={'requests': 'requests'})
