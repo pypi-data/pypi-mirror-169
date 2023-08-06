393f3333bab57df6e700152264b510c78a78263f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:47 +0000

    Add package hotweb-cli/0.0.130/hotweb_cli-0.0.130.tar.gz

---
+++ packages/hotweb-cli/hotweb_cli-0.0.130.tar.gz/hotweb_cli-0.0.130/hotweb_cli/models/__init__.py
@@ -0,0 +1,13 @@
+
+from bafana import Bafana
+from cash import Cash
+from hadson import Hadson
+from haduser import Haduser
+from models import Models
+from model_skeleton import Model_skeleton
+from nan import Nan
+from testd import Testd
+from users import Users
+from __init__ import __init__
+
+    
