f5e96a42270fc90ed5f5371567b72e67a4205f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:28 +0000

    Add package trainsegmentation/0.1.7/trainsegmentation-0.1.7.tar.gz

---
+++ packages/trainsegmentation/trainsegmentation-0.1.7.tar.gz/trainsegmentation-0.1.7/README.md
@@ -0,0 +1,14 @@
+# Trainable Segmentation
+Segmentation Tools that apply scikit-learn machine learning and various filters to build image feature sets and classify image pixels.
+
+# Contact information:
+
+Heather S. Deter, PhD
+Postdoctoral Research Assistant
+University of Illinois Urbana Champaign
+Email: hdeter2013@gmail.com
+
+# Acknowldegments
+
+H.S.D. was supported by an appointment to the Intelligence Community Postdoctoral Research Fellowship Program at the University of Illinois Urbana-Champaign, administered by Oak Ridge Institute for Science and Education through an interagency agreement between the U.S. Department of Energy and the Office of the Director of National Intelligence.
+
---
+++ packages/trainsegmentation/trainsegmentation-0.1.7.tar.gz/trainsegmentation-0.1.7/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  8 14:35:47 2022
+
+@author: hdeter
+
+# Copyright (C) 2022 Heather S. Deter <hdeter2013@gmail.com>
+# License: 3-clause BSD
+"""
+
+
+from setuptools import setup, find_packages
+
+VERSION = '0.1.7' 
+DESCRIPTION = 'Trainable Image Segmentation'
+LONG_DESCRIPTION = 'Functions written to streamline generating image feature sets and training scikit-learn classifiers'
+
+# Setting up
+setup(
+        name="trainsegmentation", 
+        version=VERSION,
+        author="Heather S. Deter",
+        author_email="<hdeter2013@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        url = 'https://github.com/hdeter/trainsegmentation',
+        packages=find_packages(),
+        install_requires=['numpy','scipy','scikit-image','scikit-learn'], 
+        keywords=['python', 'sci-kit learn','image segmentation','image classification'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "License :: OSI Approved :: BSD License",
+            "Programming Language :: Python :: 3",
+            "Topic :: Scientific/Engineering"
+        ]
+)
