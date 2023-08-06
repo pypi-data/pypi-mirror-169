213362c9c75631ead26cdee83ed1843883b303d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:26 +0000

    Add package barbell2/2.2.0/barbell2-2.2.0-py2.py3-none-any.whl

---
+++ packages/barbell2/barbell2-2.2.0-py2.py3-none-any.whl/barbell2/bodycomp/decompressdicom.py
@@ -0,0 +1,31 @@
+import os
+import pydicom
+import pydicom.errors
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_dir', help='Input directory', default='.')
+    parser.add_argument('--output_dir', help='Output directory', default='output')
+    args = parser.parse_args()
+    os.makedirs(args.output_dir)
+    for f in os.listdir(args.input_dir):
+        f_path = os.path.join(args.input_dir, f)
+        try:
+            p = pydicom.dcmread(f_path)
+            f_path_new = os.path.join(args.output_dir, f)
+            p.decompress()
+            p.save_as(f_path_new)
+            print(f_path_new)
+        except pydicom.errors.InvalidDicomError:
+            pass
+
+
+if __name__ == '__main__':
+    from barbell2 import common
+    common.setup_args_env([
+        '--input_dir=/Users/Ralph/data/surfdrive/projects/hpb/bodycomposition/data/l3tag_cohorts/SURG-PANC',
+        '--output_dir=/Users/Ralph/Desktop/output',
+    ])
+    main()
