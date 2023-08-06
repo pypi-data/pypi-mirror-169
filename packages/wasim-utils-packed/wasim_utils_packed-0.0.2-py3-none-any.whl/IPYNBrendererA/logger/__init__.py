403f0076e66016e1df21237b88903c34a15076cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:43 +0000

    Add package wasim-utils-packed/0.0.2/wasim_utils_packed-0.0.2-py3-none-any.whl

---
+++ packages/wasim-utils-packed/wasim_utils_packed-0.0.2-py3-none-any.whl/IPYNBrendererA/logger/__init__.py
@@ -0,0 +1,25 @@
+import logging
+from datetime import datetime
+import os
+import sys
+
+LOG_DIR = "./logs"
+
+CURRENT_TIME_STAMP = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+
+LOG_FILE_NAME = f"log_{CURRENT_TIME_STAMP}.log"
+
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE_NAME)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s] %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler(LOG_FILE_PATH),
+        logging.StreamHandler(sys.stdout),
+    ],
+)
+
+logger = logging.getLogger("IPYNBrenderer")
