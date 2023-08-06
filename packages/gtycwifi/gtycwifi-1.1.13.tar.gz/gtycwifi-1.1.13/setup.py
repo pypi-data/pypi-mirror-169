81187260160282467f14e1359f41e98025918263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:19 +0000

    Add package gtycwifi/1.1.13/gtycwifi-1.1.13.tar.gz

---
+++ packages/gtycwifi/gtycwifi-1.1.13.tar.gz/gtycwifi-1.1.13/setup.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+"""
+Created on 2022年9月29日
+@author: GTYC
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='gtycwifi',
+    version='1.1.13',
+    keywords='gtycwifi',
+    description='A LIB',
+    license='GTYC License',
+    url='',
+    author='GTYC',
+    author_email='',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any'
+)
