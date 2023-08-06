fe02c5e30176709585fe7615c1bde8a0298aa1e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:25 +0000

    Add package tons/0.0.26/tons-0.0.26.tar.gz

---
+++ packages/tons/tons-0.0.26.tar.gz/tons-0.0.26/tons/ui/gui/__init__.py
@@ -0,0 +1,7 @@
+from .._utils import init_shared_object, setup_app
+
+
+def main():
+    # config = init_shared_object(specific_config_path)
+    # setup_app(config)
+    raise SystemExit("Gui version hasn't been implemented yet")
