70cc3f674fda113421cab5f18cc91eb3b9f273d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:07 +0000

    Add package djsuperadmin/0.11.3/djsuperadmin-0.11.3-py2.py3-none-any.whl

---
+++ packages/djsuperadmin/djsuperadmin-0.11.3-py2.py3-none-any.whl/djsuperadmin/mixins.py
@@ -0,0 +1,8 @@
+class DjSuperAdminMixin:
+    @property
+    def superadmin_get_url(self):
+        raise NotImplementedError("You must define superadmin_get_url!")
+
+    @property
+    def superadmin_patch_url(self):
+        raise NotImplementedError("You must define superadmin_patch_url!")
