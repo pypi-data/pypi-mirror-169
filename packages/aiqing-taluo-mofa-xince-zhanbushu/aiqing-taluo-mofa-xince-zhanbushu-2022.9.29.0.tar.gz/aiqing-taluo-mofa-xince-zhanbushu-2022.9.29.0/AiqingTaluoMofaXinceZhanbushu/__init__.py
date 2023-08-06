740c32b862d323a3686ed88eae0b122945095ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:18 +0000

    Add package aiqing-taluo-mofa-xince-zhanbushu/2022.9.29.0/aiqing-taluo-mofa-xince-zhanbushu-2022.9.29.0.tar.gz

---
+++ packages/aiqing-taluo-mofa-xince-zhanbushu/aiqing-taluo-mofa-xince-zhanbushu-2022.9.29.0.tar.gz/aiqing-taluo-mofa-xince-zhanbushu-2022.9.29.0/AiqingTaluoMofaXinceZhanbushu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""aiqing-taluo-mofa-xince-zhanbushu
+https://github.com/apachecn/aiqing-taluo-mofa-xince-zhanbushu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.9.29.0"
