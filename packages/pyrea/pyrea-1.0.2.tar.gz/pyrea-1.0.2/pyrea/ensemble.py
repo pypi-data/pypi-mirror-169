43580967f97313dab2e987b47b8d2785445d92e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:03 +0000

    Add package pyrea/1.0.2/pyrea-1.0.2.tar.gz

---
+++ packages/pyrea/pyrea-1.0.2.tar.gz/pyrea-1.0.2/pyrea/ensemble.py
@@ -0,0 +1,8 @@
+# Pyrea: Multi-view hierarchical clustering with flexible ensemble structures
+# Copyright (C) 2022 Marcus D. Bloice, Bastian Pfeifer
+#
+# Licenced under the terms of the MIT license.
+#
+# ensemble.py
+# Contains classes required to create ensebles, such as the main Ensemble
+# class and the View class.
