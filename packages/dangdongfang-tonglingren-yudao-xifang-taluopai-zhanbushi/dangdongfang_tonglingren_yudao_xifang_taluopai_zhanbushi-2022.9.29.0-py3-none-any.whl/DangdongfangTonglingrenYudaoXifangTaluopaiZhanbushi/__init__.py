a474c8a8d82394c45b1d3d62b053c79bcb7ac00f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:22 +0000

    Add package dangdongfang-tonglingren-yudao-xifang-taluopai-zhanbushi/2022.9.29.0/dangdongfang_tonglingren_yudao_xifang_taluopai_zhanbushi-2022.9.29.0-py3-none-any.whl

---
+++ packages/dangdongfang-tonglingren-yudao-xifang-taluopai-zhanbushi/dangdongfang_tonglingren_yudao_xifang_taluopai_zhanbushi-2022.9.29.0-py3-none-any.whl/DangdongfangTonglingrenYudaoXifangTaluopaiZhanbushi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""dangdongfang-tonglingren-yudao-xifang-taluopai-zhanbushi
+https://github.com/apachecn/dangdongfang-tonglingren-yudao-xifang-taluopai-zhanbushi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.9.29.0"
