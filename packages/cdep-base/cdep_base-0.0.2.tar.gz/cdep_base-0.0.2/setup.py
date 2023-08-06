15419a6d0c7c8b6ca87a79fb180bef26d100bd02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:18 +0000

    Add package cdep-base/0.0.2/cdep_base-0.0.2.tar.gz

---
+++ packages/cdep-base/cdep_base-0.0.2.tar.gz/cdep_base-0.0.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+from pathlib import Path
+
+PACKAGE_NAME = "cdep_base"
+DESCRIPTION = "Base package for the installation of the rest of icdep's dependencies"
+URL = ""
+AUTHOR = "John Doe"
+AUTHOR_EMAIL = "jmartinpe@sia.es"
+LICENSE = "UNLICENSED"
+REQUIREMENTS = []
+
+setuptools.setup(
+    name=PACKAGE_NAME,
+    version="0.0.2",
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
+    license=LICENSE,
+    url=URL,
+    extras_require={
+        "package_a": ["cdep_package_a"],
+        "package_b": ["cdep_package_b"],
+    },
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    install_requires=REQUIREMENTS,
+    classifiers=["Programming Language :: Python :: 3"],
+)
