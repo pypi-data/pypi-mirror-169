c83b64fc17484096e4bb63a5cbc919a17f0cabdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:59 +0000

    Add package retrieval/0.0.1.dev1/retrieval-0.0.1.dev1.tar.gz

---
+++ packages/retrieval/retrieval-0.0.1.dev1.tar.gz/retrieval-0.0.1.dev1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='retrieval',
+    version='0.0.1dev1',
+    author="Xing Han Lu",
+    author_email="pypi@xinghanlu.com",
+    url='https://github.com/xhluca/retrieval',
+    description='Toolkit for dense neural retrieval.',
+    long_description='This project is currently under development, so please email the author for any question or access to the code repository, which will be open-sourced when released.',
+    packages=find_packages(
+        where='src',
+        include=["retrieval"],
+        exclude=[],
+    ),
+    install_requires=[
+        # dependencies here
+    ],
+    extras_require={
+        # For special installation, e.g. pip install retrieval[dev]
+        'dev': ['black', 'twine']
+    }
+)
