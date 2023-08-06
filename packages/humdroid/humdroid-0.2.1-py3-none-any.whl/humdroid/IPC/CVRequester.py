ba1362284729a8a73896dac1d2f72ec56a58ea12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:50 +0000

    Add package humdroid/0.2.1/humdroid-0.2.1-py3-none-any.whl

---
+++ packages/humdroid/humdroid-0.2.1-py3-none-any.whl/humdroid/IPC/CVRequester.py
@@ -0,0 +1,106 @@
+from .IPCSocket import IPCSocket
+import json
+import os
+import os.path
+import subprocess
+import time
+
+class CVRequester:
+    def __init__(self):
+        self.outputSock = IPCSocket(6070)
+        self.inputSock = IPCSocket(6069)
+
+    def GetIDHash(self, fullPath):
+        """
+            Given any string, return an ID based on the hash of that string.
+        """
+
+        return abs(hash(fullPath)) % (10 ** 8)
+
+    def LoadImages(self, path, group : int):
+        """ 
+        Given a path to a directory, load all PNGs and JPG's in the root
+        directory to OpenCV.
+        """
+
+        if os.path.isdir(path):
+            for (dirpath, dirnames, filenames) in os.walk(path):
+                for file in filenames:
+                    if ".png" in file or ".jpg" in file or ".jpeg" in file:
+                        file = os.path.join(path, file)
+                        self.LoadImage(file, group)
+
+                break # Remove if you want recursive
+        else:
+            raise Exception(path + " is not a path to a folder.")
+
+    def LoadImage(self, path, group : int):
+        """
+        Loads any image as long as its is a JPG or PNG. `path` has to be the
+        absolute, full path to the image. ID is calculated off of absolute
+        path.
+        """
+
+        data = {
+            "loadTemplate" : { }
+        }
+
+        if os.path.isabs(path):
+            file = os.path.basename(path)
+            if ".png" in file or ".jpg" in file or ".jpeg" in file:
+                data["loadTemplate"]["path"] = path
+                data["loadTemplate"]["id"] = self.GetIDHash(path)
+                data["loadTemplate"]["group"] = group
+
+                jsonData = json.dumps(data).encode("UTF-8")
+
+                # "$" is delimeter for multiple messages should they arrive
+                # so fast they buffer
+                self.inputSock.send(bytearray(jsonData) + b"$")
+            else:
+                raise Exception(path + " is not an image.")
+        else:
+            raise Exception(path + " is not an absolute path.")
+
+
+
+    def CompareID(self, photo, ID, minConfidence=0.95):
+        """
+            Send a CompareID request message. ID can be calculated from the 
+            full path of the template using self.GetIDHash(). Stalls until
+            result is received.
+        """
+
+        compareData = {
+            "compareID" : {
+                "photo" : photo,
+                "minConfidence" : minConfidence,
+                "id" : ID
+            }
+        }
+        compareJSON = json.dumps(compareData).encode("UTF-8")
+        self.inputSock.send(bytearray(compareJSON) + b"$")
+
+        return json.loads(self.outputSock.receive())
+
+
+    def CompareGroup(self, photo, group : int, minConfidence=0.95):
+
+        compareData = {
+            "compareGroup" : {
+                "photo" : photo,
+                "minConfidence" : minConfidence,
+                "group" : group
+            }
+        }
+
+        compareJSON = json.dumps(compareData).encode("UTF-8")
+        self.inputSock.send(bytearray(compareJSON) + b"$")
+
+        return json.loads(self.outputSock.receive())
+
+    def Close(self):
+        self.inputSock.close()
+        self.outputSock.close()
+
+
---
+++ packages/humdroid/humdroid-0.2.1-py3-none-any.whl/humdroid/wrappers/ScrcpyWrapper.py
@@ -0,0 +1,41 @@
+from adbutils import adb
+from PIL import Image
+import scrcpy
+import time
+
+class ScrcpyWrapper:
+    def __init__(self, max_width : int = 0, bitrate : int = 8000000):
+        adb.connect("127.0.0.1:5555")
+        self.client = scrcpy.Client(device=adb.device_list()[0].serial, max_width=max_width, bitrate=bitrate)
+        self.client.start(threaded=True)
+
+        self.frameReady = False
+        self.client.add_listener(scrcpy.EVENT_FRAME, self._FrameReady)
+
+    def Touch(self, x, y, duration=-1.0):
+        self.client.control.touch(x, y, scrcpy.ACTION_DOWN);
+        if duration > 0:
+            time.sleep(duration)
+        self.client.control.touch(x, y, scrcpy.ACTION_UP);
+
+    def _FrameReady(self, frame):
+        if frame is not None:
+            self.frameReady = True
+            #self.client.remove_listener(scrcpy.EVENT_FRAME, self._FrameReady)
+
+    def LastFrame(self):
+        self.frameReady = False
+        while not self.frameReady:
+            time.sleep(0.01)
+
+        rgb = self.client.last_frame[...,::-1].copy() # bgr to rgb
+        return Image.fromarray(rgb)
+
+    def Close(self):
+        self.client.stop()
+
+    def Swipe(self, start_x: int, start_y: int, end_x: int, end_y: int, move_step_length: int = 5, move_steps_delay: float = 0.005):
+        self.client.control.swipe(start_x, start_y, end_x, end_y, move_step_length, move_steps_delay)
+
+    def GetResolution(self) -> (int, int):
+        return self.client.resolution
