ca3f6a46a20600d35dd09798b7b087719407b1a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:23 +0000

    Add package pocketsphinx/5.0.0rc5/pocketsphinx-5.0.0rc5.tar.gz

---
+++ packages/pocketsphinx/pocketsphinx-5.0.0rc5.tar.gz/pocketsphinx-5.0.0rc5/test/regression/test-main.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+: ${CMAKE_BINARY_DIR:=$(pwd)}
+. ${CMAKE_BINARY_DIR}/test/testfuncs.sh
+
+bn=`basename $0 .sh`
+
+echo "Test: $bn"
+sox $data/librivox/*.wav $(run_program pocketsphinx soxflags) | \
+    run_program pocketsphinx \
+                -loglevel INFO \
+                -hmm $model/en-us/en-us \
+                -lm $model/en-us/en-us.lm.bin \
+                -dict $model/en-us/cmudict-en-us.dict - \
+                2>$bn.log >$bn.json
+
+# Test whether it actually completed
+if [ $? = 0 ]; then
+    pass "run"
+else
+    fail "run"
+fi
+
+if grep -q 'define FIXED' "${CMAKE_BINARY_DIR}/config.h"; then
+   ref="$data/librivox/$bn.fixed.json"
+else
+   ref="$data/librivox/$bn.json"
+fi
+
+# Check the decoding results
+compare_table "match" $ref $bn.json 1000000
