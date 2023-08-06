9dbcae2def796a27b2b7ae8b8a2167b6ee7115c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:43 +0000

    Add package speech-analytics/0.1.3/speech-analytics-0.1.3.tar.gz

---
+++ packages/speech-analytics/speech-analytics-0.1.3.tar.gz/speech-analytics-0.1.3/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['speech_analytics']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['spacy>=3.4.1,<4.0.0']
+
+entry_points = \
+{'console_scripts': ['analysis = speech_analytics._models:Analysis',
+                     'tester = speech_analytics._models:test_function']}
+
+setup_kwargs = {
+    'name': 'speech-analytics',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': '',
+    'author': 'Ashleigh Richardson',
+    'author_email': 'ashleigh.richardson@uqconnect.edu.au',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
