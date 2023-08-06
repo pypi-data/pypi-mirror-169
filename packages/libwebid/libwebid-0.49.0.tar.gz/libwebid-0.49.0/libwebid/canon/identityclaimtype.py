2d226239235e9864f2243835b1d0128304e742be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:13 +0000

    Add package libwebid/0.49.0/libwebid-0.49.0.tar.gz

---
+++ packages/libwebid/libwebid-0.49.0.tar.gz/libwebid-0.49.0/libwebid/canon/identityclaimtype.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2022 Cochise Ruhulessin
+#
+# All rights reserved. No warranty, explicit or implicit, provided. In
+# no event shall the author(s) be liable for any claim or damages.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+from typing import TypeAlias
+
+from .birthplaceidentityclaim import BirthplaceIdentityClaim
+from .dateidentityclaim import DateIdentityClaim
+from .genderidentityclaim import GenderIdentityClaim
+from .stringidentityclaim import StringIdentityClaim
+from .officialgivennamesclaim import OfficialGivenNameIdentityClaim
+from .nameidentityclaim import NameIdentityClaim
+from .ncnidentityclaim import NCNIdentityClaim
+
+
+IdentityClaimType: TypeAlias = (
+    GenderIdentityClaim | BirthplaceIdentityClaim | NCNIdentityClaim |
+    OfficialGivenNameIdentityClaim | NameIdentityClaim |
+    DateIdentityClaim | StringIdentityClaim
+)
