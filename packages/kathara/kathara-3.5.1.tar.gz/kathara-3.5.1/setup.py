8a1774e5ce2c3f086cb8ec8f8dd682ab7fa11a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:13 +0000

    Add package kathara/3.5.1/kathara-3.5.1.tar.gz

---
+++ packages/kathara/kathara-3.5.1.tar.gz/kathara-3.5.1/setup.py
@@ -0,0 +1,43 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='kathara',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    py_modules=['kathara'],
+    version='3.5.1',
+    license='gpl-3.0',
+    description='A lightweight container based emulation system.',
+    author='Kathara Framework',
+    author_email='contact@kathara.org',
+    url='https://www.kathara.org',
+    download_url='https://github.com/KatharaFramework/Kathara/archive/refs/tags/3.5.1.tar.gz',
+    keywords=['NETWORK-EMULATION', 'CONTAINERS', 'NFV'],
+    install_requires=[
+        "binaryornot>=0.4.4",
+        "docker>=4.4.4",
+        "kubernetes>=23.3.0",
+        "requests>=2.22.0",
+        "coloredlogs>=10.0",
+        "terminaltables>=3.1.0",
+        "slug>=2.0",
+        "deepdiff>=4.0.9",
+        "pyroute2>=0.5.19",
+        "progressbar2>=1.14.0",
+        "libtmux>=0.8.2; platform_system == 'darwin' or platform_system == 'linux'",
+        "pyuv @ git+https://github.com/saghul/pyuv@master#egg=pyuv",
+        "appscript>=1.1.0; platform_system == 'darwin'",
+        "pypiwin32>=223; platform_system == 'win32'",
+        "windows-curses>=2.1.0; platform_system == 'win32'"
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
