ebdd59844ea431a9c19c8e60e751983778d9e059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:08 +0000

    Add package themispy/0.2.4/themispy-0.2.4-py3-none-any.whl

---
+++ packages/themispy/themispy-0.2.4-py3-none-any.whl/themispy/scrapy/__init__.py
@@ -0,0 +1,13 @@
+from themispy.scrapy.items import FileDownloader
+from themispy.scrapy.pipelines import (AzureBlobUploadPipeline,
+                                       AzureFileDownloaderPipeline)
+from themispy.scrapy.readers import read_jsonlines_blob
+from themispy.scrapy.spiders import run_spider
+
+__all__ = [
+    "FileDownloader",
+    "AzureBlobUploadPipeline",
+    "AzureFileDownloaderPipeline",
+    "read_jsonlines_blob",
+    "run_spider"
+]
