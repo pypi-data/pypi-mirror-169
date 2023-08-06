f548423e84c89f9df522e0f457c59fbe4186ae3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:28 +0000

    Add package sbp/4.8.0/sbp-4.8.0.tar.gz

---
+++ packages/sbp/sbp-4.8.0.tar.gz/sbp-4.8.0/sbp/_version.py
@@ -0,0 +1,7 @@
+# THIS FILE IS GENERATED FROM TRAITS SETUP.PY
+version = '4.8.0'
+full_version = '4.8.0'
+git_revision = 'ff94c72c'
+is_released = True
+if not is_released:
+    version = full_version
