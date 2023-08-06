62870c6bba8fbeb10e68ae9c664cba08bd9b5038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:48 +0000

    Add package desk-probability/0.1/desk_probability-0.1.tar.gz

---
+++ packages/desk-probability/desk_probability-0.1.tar.gz/desk_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='desk_probability',
+      version='0.1',
+      description='Gaussian (a.k.a., Normal) and Binomial probability distributions',
+      packages=['desk_probability'],
+      author='Sankar Ray',
+      zip_safe=False)
