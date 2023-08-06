99191b6f33700d4b7e47442163b198e602b6f657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:35 +0000

    Add package replit-bot/1.1.8/replit-bot-1.1.8.tar.gz

---
+++ packages/replit-bot/replit-bot-1.1.8.tar.gz/replit-bot-1.1.8/replit_bot/html_default_templates.py
@@ -0,0 +1,15 @@
+original_html = """<center><h1>Commands are as followed</h1></center>
+<pre><code>@{} {}command-here param1:here param2:here</code></pre>
+<hr><center><h1>Commands</h1></center>{}<hr>"""
+
+html_list = """<li>{}</li>
+<ul>
+    <li>Description: <em style="color: red; background: black">{}</em></li>
+    <li>required = {}</li>
+    <li>default = {}</li>
+    <li>type = {}</li>
+</ul>"""
+
+param_bio = """<center><h2>{}{}</h2></center>{}"""
+
+blockquote = """<blockquote>{}</blockquote>"""
