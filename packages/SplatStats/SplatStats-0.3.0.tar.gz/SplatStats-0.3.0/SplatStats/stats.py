b635dd0b0bdc2989927531c3101bdde235f38228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:08 +0000

    Add package SplatStats/0.3.0/SplatStats-0.3.0.tar.gz

---
+++ packages/SplatStats/SplatStats-0.3.0.tar.gz/SplatStats-0.3.0/SplatStats/stats.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+def calcBattleHistoryStats(bHist):
+    """Calculates the basic player stats for a battle history dataframe.
+
+    Args:
+        bHist (dataframe): Battle history dataframe for a player.
+
+    Returns:
+        dict: Stats (kills, paint, assists, deaths, specials) in normal, average, and per minute forms.
+    """    
+    # Getting constants ---------------------------------------------------
+    matchNum = bHist.shape[0]
+    matchDuration = (bHist['duration']/60)
+    # Getting counts ------------------------------------------------------
+    cats = ('kill', 'death', 'assist', 'special', 'paint')
+    (kCnt, dCnt, aCnt, sCnt, pCnt) = [bHist[cat] for cat in cats]
+    # Kill/Death/Assist/Paint stats ---------------------------------------
+    (kTot, dTot, aTot, sTot, pTot) = [
+        sum(i) for i in (kCnt, dCnt, aCnt, sCnt, pCnt)
+    ]
+    (kAvg, dAvg, aAvg, sAvg, pAvg) = [
+        i/matchNum for i in (kTot, dTot, aTot, sTot, pTot)
+    ]
+    # Win/lose stats (NA are loss) ----------------------------------------
+    win  = [True if i=='W' else False for i in bHist['win']]
+    wins = sum(win)
+    winR = wins/len(win)
+    loss = len(win)-wins
+    # Ratios --------------------------------------------------------------
+    killRatio = kTot/dTot
+    (kpm, dpm, apm, spm, ppm) = [
+        np.mean(bHist[i]/matchDuration) for i in cats
+    ]
+    # Stats dictionary ----------------------------------------------------
+    pStats = {
+        # W/L stats
+        'general': {
+            'total matches': matchNum,
+            'win': wins, 'loss': loss, 
+            'win ratio': winR, 'kill ratio': killRatio
+        },
+        # KPADS stats
+        'kpads': {
+            'kills': kTot, 'deaths': dTot, 'assists': aTot, 
+            'special': sTot, 'paint': pTot 
+        },
+        'kpads avg': {
+            'kills': kAvg,  'deaths': dAvg, 'assist': aAvg,
+            'special': sAvg, 'paint': pAvg,
+        },
+        'kpads per min': {
+            'kills': kpm, 'deaths': dpm, 'assist': apm,
+            'special': spm, 'paint': ppm
+        }
+    }
+    return pStats
