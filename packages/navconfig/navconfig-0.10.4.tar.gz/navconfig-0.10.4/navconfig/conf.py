7e844795189d0c7f4f09769dafe71a8e09025fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:22 +0000

    Add package navconfig/0.10.4/navconfig-0.10.4.tar.gz

---
+++ packages/navconfig/navconfig-0.10.4.tar.gz/navconfig-0.10.4/navconfig/conf.py
@@ -0,0 +1,24 @@
+import os
+import logging
+from navconfig import (
+    BASE_DIR
+)
+os.chdir(str(BASE_DIR))
+
+### Global-Settings.
+try:
+    from settings.settings import * # pylint: disable=W0401,W0614
+except (ImportError) as err:
+    from settings import settings
+    logging.error(f'Cannot loaded a settings Module {err}, module: {settings}')
+    print(
+        'Settings.py module is missing.'
+        'Hint: Its recommended to use a settings/settings.py module to customize '
+        ' NAV Configuration.'
+    )
+
+### User Local Settings
+try:
+    from settings.local_settings import * # pylint: disable=W0401,W0614
+except (ImportError) as err:
+    pass
