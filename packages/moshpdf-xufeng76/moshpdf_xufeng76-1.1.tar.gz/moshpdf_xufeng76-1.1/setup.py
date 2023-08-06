19f0417e5f0bc617098a316c56c0fb20cccac132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:00 +0000

    Add package moshpdf-xufeng76/1.1/moshpdf_xufeng76-1.1.tar.gz

---
+++ packages/moshpdf-xufeng76/moshpdf_xufeng76-1.1.tar.gz/moshpdf_xufeng76-1.1/setup.py
@@ -0,0 +1,10 @@
+
+from pathlib import Path
+import setuptools
+
+setuptools.setup(
+    nanme="xufengpdf",
+    version=1.1,
+    long_description=Path("README.md").read_text(),
+    packages=setuptools.find_packages(exclude=["tests", "data"])
+)
