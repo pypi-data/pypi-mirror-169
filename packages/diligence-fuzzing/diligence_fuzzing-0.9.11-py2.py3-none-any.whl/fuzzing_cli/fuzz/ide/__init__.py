9181567464e02fc262bc1cb0ea0fcc13a59ce734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:15 +0000

    Add package diligence-fuzzing/0.9.11/diligence_fuzzing-0.9.11-py2.py3-none-any.whl

---
+++ packages/diligence-fuzzing/diligence_fuzzing-0.9.11-py2.py3-none-any.whl/fuzzing_cli/fuzz/ide/__init__.py
@@ -0,0 +1,17 @@
+from .brownie import BrownieArtifacts
+from .dapptools import DapptoolsArtifacts
+from .generic import IDEArtifacts
+from .hardhat import HardhatArtifacts
+from .repository import IDERepository
+from .truffle import TruffleArtifacts
+
+# Initializing of IDERepository singleton
+# NOTE: Each new IDE should register respective class here
+repo = IDERepository()
+
+repo.register_ide(TruffleArtifacts)
+repo.register_ide(HardhatArtifacts)
+repo.register_ide(BrownieArtifacts)
+repo.register_ide(DapptoolsArtifacts)
+
+IDERepository.set_instance(repo)
