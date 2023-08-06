efb677ff6ebe90581a3cf40d47e33c54f300ac48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:52 +0000

    Add package barbell2/1.54.0/barbell2-1.54.0-py2.py3-none-any.whl

---
+++ packages/barbell2/barbell2-1.54.0-py2.py3-none-any.whl/barbell2/castor/buildimport.py
@@ -0,0 +1,22 @@
+import argparse
+from barbell2.castor import loaders
+from barbell2.castor import builders
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('castor-export', help='Castor export Excel file')
+    parser.add_argument('dica-export', help='DHBA or DPCA export Excel data file')
+    parser.add_argument('dica-dd', help='DHBA or DPCA data dictionary')
+    parser.add_argument('output-dir', help='Output directory to store generated CSV files')
+    args = parser.parse_args()
+    cast_loader = loaders.CastorExportFileLoader(args.castor_export)
+    cast_data = cast_loader.load()
+    dica_loader = loaders.DicaExportFileLoader(args.dica_export, args.dica_dd)
+    dica_data = dica_loader.load()
+    builder = builders.CastorRecordBuilder(cast_data, dica_data)
+    builder.execute()
+    builder.save(args.output_dir)
+
+
+main()
