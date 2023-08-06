94e1cb28f574e559e2613e59b8615ea66f1a7f61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:33 +0000

    Add package AutoTransform/1.0.5a1/AutoTransform-1.0.5a1.tar.gz

---
+++ packages/AutoTransform/AutoTransform-1.0.5a1.tar.gz/AutoTransform-1.0.5a1/src/python/autotransform/step/condition/state.py
@@ -0,0 +1,46 @@
+# AutoTransform
+# Large scale, component based code modification library
+#
+# Licensed under the MIT License <http://opensource.org/licenses/MIT>
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2022-present Nathan Rockenbach <http://github.com/nathro>
+
+# @black_format
+
+"""The implementation for the ChangeStateCondition."""
+
+from __future__ import annotations
+
+from typing import ClassVar, List
+
+from autotransform.change.base import Change, ChangeState
+from autotransform.step.condition.base import ComparisonCondition, ConditionName
+from autotransform.step.condition.comparison import ComparisonType
+
+
+class ChangeStateCondition(ComparisonCondition[ChangeState]):
+    """A condition which checks the ChangeState against the state supplied using the supplied
+    comparison. Note: only equals and not equals are valid, all others will result in an error.
+
+    Attributes:
+        comparison (ComparisonType): The type of comparison to perform.
+        value (ChangeState | List[ChangeState]): The state(s) to compare against.
+        name (ClassVar[ConditionName]): The name of the Component.
+    """
+
+    comparison: ComparisonType
+    value: ChangeState | List[ChangeState]
+
+    name: ClassVar[ConditionName] = ConditionName.CHANGE_STATE
+
+    def get_val_from_change(self, change: Change) -> ChangeState:
+        """Gets the state from the Change.
+
+        Args:
+            change (Change): The Change the Condition is checking.
+
+        Returns:
+            ChangeState: The state of the Change.
+        """
+
+        return change.get_state()
