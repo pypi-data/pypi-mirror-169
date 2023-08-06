3349ced8d7dd82f2d5e2da8e3d502b95935574ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:19:13 +0000

    Add package nanoStringPy/0.0.1/nanoStringPy-0.0.1.tar.gz

---
+++ packages/nanoStringPy/nanoStringPy-0.0.1.tar.gz/nanoStringPy-0.0.1/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+	name ="nanoStringPy",
+	version="0.0.1",
+	author="Rudramani Pokhrel",
+	description="Python package to analyze nanostring mRNA data",
+	packages=["nanoStringPy"]
+	)
