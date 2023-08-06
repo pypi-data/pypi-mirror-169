bae2fe707153ac6d9ef0997fbd8afb70592e6737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:22 +0000

    Add package py-protocol/0.0.3a0/py-protocol-0.0.3a0.tar.gz

---
+++ packages/py-protocol/py-protocol-0.0.3a0.tar.gz/py-protocol-0.0.3a0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='py-protocol',
+    version='0.0.3a0',
+    description= 'Support for network protocol senders',
+    long_description='Hot fix',
+    author='Phuc Bao',
+    packages=['protocol'],
+)
