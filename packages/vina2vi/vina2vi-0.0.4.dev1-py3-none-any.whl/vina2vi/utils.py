5d412831708621bd8ac207f4197b3317eeab0c36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:09 +0000

    Add package vina2vi/0.0.4.dev1/vina2vi-0.0.4.dev1-py3-none-any.whl

---
+++ packages/vina2vi/vina2vi-0.0.4.dev1-py3-none-any.whl/vina2vi/utils.py
@@ -0,0 +1,38 @@
+import string
+import unicodedata
+from dataclasses import dataclass, field
+
+
+@dataclass(frozen=True)
+class Vietnamese:
+    ambiguous_chars = {
+        "a": "aáảãàạâấẩẫầậăắẳẵằặ",
+        "d": "dđ",
+        "e": "eéẻẽèẹêếểễềệ",
+        "i": "iíỉĩìị",
+        "o": "oóỏõòôọôốổỗồộơớởỡờợ",
+        "u": "uúủũùụưứửữừự",
+        "y": "yýỷỹỳỵ",
+    }
+    for k, v in ambiguous_chars.items():
+        ambiguous_chars[k] = unicodedata.normalize("NFC", v)
+    lowers = "".join(ambiguous_chars.values()) + string.ascii_lowercase
+    uppers = lowers.upper()
+    lowers = set(lowers)
+    uppers = set(uppers)
+    chars = lowers.union(uppers)
+    puncs = ",.!?_()[]-'\": \t\n"
+    puncs = unicodedata.normalize("NFC", puncs)
+    puncs = set(puncs)
+
+    @staticmethod
+    def is_foreign(string):
+        string = string.strip()
+        if string == "":
+            print("empty string!!!")
+            return False
+
+        string = unicodedata.normalize("NFC", string)
+        char_set = set(string)
+        intersection = char_set.intersection(Vietnamese.chars | Vietnamese.puncs)
+        return len(intersection) < len(char_set)
