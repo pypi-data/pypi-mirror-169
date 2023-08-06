e944fd02788f129d2b13f2000e8cf38ca19cfe9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:57 +0000

    Add package fastgen/0.2.3/fastgen-0.2.3.tar.gz

---
+++ packages/fastgen/fastgen-0.2.3.tar.gz/fastgen-0.2.3/README.md
@@ -0,0 +1,77 @@
+# ⚡ _**FastGen**_
+
+Start FastAPI Projects in Lightning Speed
+
+Built With **Typer** To Help With <span style="color:green">**FastAPI**</span>.
+
+## 👀 **Take A Look**
+
+this is a glanc of the project structure you will have once you use **FastGen**
+
+![dirs_images](./docs/dir.png)
+
+## ✨ **Installation**
+
+```console
+$ python -m pip install fastgen
+```
+
+**Usage**:
+
+```console
+$ fastgen [OPTIONS] COMMAND [ARGS]...
+```
+
+**Options**:
+
+- `--install-completion`: Install completion for the current shell.
+- `--show-completion`: Show completion for the current shell, to copy it or customize the installation.
+- `--help`: Show this message and exit.
+
+**Commands**:
+
+- `info`
+- `new`
+
+## `fastgen info`
+
+**Usage**:
+
+```console
+$ fastgen info [OPTIONS]
+```
+
+**Options**:
+
+- `--help`: Show this message and exit.
+
+## `fastgen new`
+
+**Usage**:
+
+```console
+$ fastgen new [OPTIONS] ⭐ Project Name
+```
+
+**Arguments**:
+
+- `⭐ Project Name`: <span style="color:pink">**required**
+
+**Options**:
+
+- `--dir 📁 Directory Path`
+- `--package-manager 📦 Package Manager`: [default: pip]
+  ( Options are pip , poetry )
+- `--migrations / --no-migrations`: [default: False]
+- `--docker / --no-docker`: [default: False]
+- `--testing / --no-testing`: [default: False]
+- `--database 📅 Database`: [default: postgresql] ( Options are postgresql,mysql,sqlite )
+- `--help`: Show this message and exit.
+
+## 🪲 **Encountered A Problem !**
+
+feel free to open an issue discussing the problem you faced
+
+## 🤝🏻 **Contributing**
+
+please refer to [Contribution Guide](./CONTRIBUTING.md)
---
+++ packages/fastgen/fastgen-0.2.3.tar.gz/fastgen-0.2.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fastgen', 'fastgen.managers']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click==8.1.3', 'rich>=12.5.1,<13.0.0', 'typer[all]>=0.6.1,<0.7.0']
+
+entry_points = \
+{'console_scripts': ['fastgen = fastgen.main:app']}
+
+setup_kwargs = {
+    'name': 'fastgen',
+    'version': '0.2.3',
+    'description': 'FastGen, Start FastAPI Projects In Lightning Speed',
+    'long_description': '# ⚡ _**FastGen**_\n\nStart FastAPI Projects in Lightning Speed\n\nBuilt With **Typer** To Help With <span style="color:green">**FastAPI**</span>.\n\n## 👀 **Take A Look**\n\nthis is a glanc of the project structure you will have once you use **FastGen**\n\n![dirs_images](./docs/dir.png)\n\n## ✨ **Installation**\n\n```console\n$ python -m pip install fastgen\n```\n\n**Usage**:\n\n```console\n$ fastgen [OPTIONS] COMMAND [ARGS]...\n```\n\n**Options**:\n\n- `--install-completion`: Install completion for the current shell.\n- `--show-completion`: Show completion for the current shell, to copy it or customize the installation.\n- `--help`: Show this message and exit.\n\n**Commands**:\n\n- `info`\n- `new`\n\n## `fastgen info`\n\n**Usage**:\n\n```console\n$ fastgen info [OPTIONS]\n```\n\n**Options**:\n\n- `--help`: Show this message and exit.\n\n## `fastgen new`\n\n**Usage**:\n\n```console\n$ fastgen new [OPTIONS] ⭐ Project Name\n```\n\n**Arguments**:\n\n- `⭐ Project Name`: <span style="color:pink">**required**\n\n**Options**:\n\n- `--dir 📁 Directory Path`\n- `--package-manager 📦 Package Manager`: [default: pip]\n  ( Options are pip , poetry )\n- `--migrations / --no-migrations`: [default: False]\n- `--docker / --no-docker`: [default: False]\n- `--testing / --no-testing`: [default: False]\n- `--database 📅 Database`: [default: postgresql] ( Options are postgresql,mysql,sqlite )\n- `--help`: Show this message and exit.\n\n## 🪲 **Encountered A Problem !**\n\nfeel free to open an issue discussing the problem you faced\n\n## 🤝🏻 **Contributing**\n\nplease refer to [Contribution Guide](./CONTRIBUTING.md)\n',
+    'author': 'kareem',
+    'author_email': 'kareemmahlees@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/kareemmahlees/fastgen',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
