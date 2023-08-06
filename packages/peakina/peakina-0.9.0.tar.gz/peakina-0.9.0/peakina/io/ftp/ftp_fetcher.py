34c4ef8e485d107c2c7a94ccb287abd621b3ab22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:44 +0000

    Add package peakina/0.9.0/peakina-0.9.0.tar.gz

---
+++ packages/peakina/peakina-0.9.0.tar.gz/peakina-0.9.0/peakina/io/ftp/ftp_fetcher.py
@@ -0,0 +1,36 @@
+import os
+from typing import IO, Any
+
+from ..fetcher import Fetcher, register
+from .ftp_utils import FTP_SCHEMES, dir_mtimes, ftp_mtime, ftp_open
+
+
+@register(schemes=FTP_SCHEMES)
+class FTPFetcher(Fetcher):
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self._mtimes_cache: dict[str, dict[str, int | None]] = {}
+
+    def get_dir_mtimes(self, dirpath: str) -> dict[str, int | None]:
+        if dirpath not in self._mtimes_cache:
+            self._mtimes_cache[dirpath] = dir_mtimes(dirpath)
+        return self._mtimes_cache[dirpath]
+
+    def open(self, filepath: str) -> IO[bytes]:
+        return ftp_open(filepath)
+
+    def listdir(self, dirpath: str) -> list[str]:
+        """
+        Make use of listdir to get all mtimes of remote files at once and
+        put this information in cache.
+        This will save us time by avoiding to create a new FTP connection
+        for each file when we'll want to get its mtime.
+        """
+        return list(self.get_dir_mtimes(dirpath).keys())
+
+    def mtime(self, filepath: str) -> int | None:
+        dirpath, filename = os.path.split(filepath)
+        if dirpath in self._mtimes_cache:
+            return self._mtimes_cache[dirpath][filename]
+        else:
+            return ftp_mtime(filepath)
