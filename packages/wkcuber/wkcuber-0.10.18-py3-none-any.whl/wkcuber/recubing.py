19de1eb891a5b3c0df68b3705fc346203f584b67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:29 +0000

    Add package wkcuber/0.10.18/wkcuber-0.10.18-py3-none-any.whl

---
+++ packages/wkcuber/wkcuber-0.10.18-py3-none-any.whl/wkcuber/recubing.py
@@ -0,0 +1,56 @@
+from argparse import ArgumentParser
+
+from webknossos import Dataset
+
+from ._internal.utils import (
+    add_data_format_flags,
+    add_distribution_flags,
+    add_verbose_flag,
+    parse_path,
+    setup_logging,
+    setup_warnings,
+)
+
+
+def create_parser() -> ArgumentParser:
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "source_path",
+        help="Directory containing the datasource properties.",
+        type=parse_path,
+    )
+
+    parser.add_argument(
+        "target_path",
+        help="Output directory for the generated dataset.",
+        type=parse_path,
+    )
+
+    parser.add_argument(
+        "--no_compression",
+        help="Use compression, default false",
+        type=bool,
+        default=False,
+    )
+
+    add_verbose_flag(parser)
+    add_distribution_flags(parser)
+    add_data_format_flags(parser)
+
+    return parser
+
+
+if __name__ == "__main__":
+    setup_warnings()
+    args = create_parser().parse_args()
+    setup_logging(args)
+
+    Dataset.open(args.source_path).copy_dataset(
+        args.target_path,
+        data_format=args.data_format,
+        chunk_shape=args.chunk_shape,
+        chunks_per_shard=args.chunks_per_shard,
+        compress=not args.no_compression,
+        args=args,
+    )
