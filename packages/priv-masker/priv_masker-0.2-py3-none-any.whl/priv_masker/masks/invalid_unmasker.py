d17e32ad0326f2ac9f6c3e25f1101809e6afc6f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:45 +0000

    Add package priv-masker/0.2/priv_masker-0.2-py3-none-any.whl

---
+++ packages/priv-masker/priv_masker-0.2-py3-none-any.whl/priv_masker/masks/invalid_unmasker.py
@@ -0,0 +1,40 @@
+from spacy.language import Language
+from spacy.matcher import Matcher
+
+from .components.base import Masker
+
+
+@Language.factory("invalid_unmask")
+class InvalidUnmasker(Masker):
+    def __init__(self, nlp, name):
+        super().__init__(nlp, name)
+        self.matcher = make_numbers_to_unmask_matcher(nlp)
+
+    def __call__(self, doc):
+        tokens_to_unmask = list()
+        matches = self.matcher(doc)
+        for _, start, end in matches:
+            for token in doc[start:end]:
+                tokens_to_unmask.append(token)
+
+        for token in tokens_to_unmask:
+            token._.set('mask', None)
+
+        return doc
+
+
+def make_numbers_to_unmask_matcher(nlp):
+    matcher = Matcher(nlp.vocab)
+    matcher.add("Rule", [
+        [{'LOWER': {'IN': ['art', 'ust', 'str', 'poz', 'pkt', 'zał', '§', '$']}},
+         {'TEXT': '.', 'OP': "?"},
+         {'LOWER': 'nr', 'OP': "?"},
+         {'TEXT': '.', 'OP': "?"},
+         {'_': {'priv_number': True}}],
+        [{'lemma': {'IN': ['artykuł', 'ustawa', 'strona', 'pozycja', 'punkt', 'załącznik', 'rozdział']}},
+         {'LOWER': 'nr', 'OP': "?"},
+         {'LOWER': 'numer', 'OP': "?"},
+         {'TEXT': '.', 'OP': "?"},
+         {'_': {'priv_number': True}}],
+    ])
+    return matcher
---
+++ packages/priv-masker/priv_masker-0.2-py3-none-any.whl/priv_masker/masks/orgname_masker.py
@@ -0,0 +1,20 @@
+from spacy.language import Language
+
+from .components.base import Masker
+from .components.base import mask_decorator
+
+
+@Language.factory("orgname_mask")
+class OrgnameMasker(Masker):
+    def __init__(self, nlp, name):
+        super().__init__(nlp, name)
+
+    @mask_decorator
+    def __call__(self, doc):
+        masked_tokens = list()
+
+        for token in doc:
+            if token.ent_type_ == 'ORGNAME':
+                masked_tokens.append(token)
+
+        return masked_tokens
