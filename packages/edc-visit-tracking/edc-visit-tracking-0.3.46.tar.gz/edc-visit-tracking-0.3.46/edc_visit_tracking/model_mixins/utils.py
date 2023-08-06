f20bdec6465738874f0363cf1d87e77b676e071e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:11 +0000

    Add package edc-visit-tracking/0.3.46/edc-visit-tracking-0.3.46.tar.gz

---
+++ packages/edc-visit-tracking/edc-visit-tracking-0.3.46.tar.gz/edc-visit-tracking-0.3.46/edc_visit_tracking/model_mixins/utils.py
@@ -0,0 +1,23 @@
+from ..exceptions import RelatedVisitFieldError
+from .visit_model_mixin import VisitModelMixin
+
+
+def get_related_visit_model_attr(model_cls) -> str:
+    attrs = []
+    for fld_cls in model_cls._meta.get_fields():
+        if fld_cls.related_model is not None and issubclass(
+            fld_cls.related_model, (VisitModelMixin,)
+        ):
+            attrs.append(fld_cls.name)
+            break
+    if len(attrs) > 1:
+        raise RelatedVisitFieldError(
+            f"More than one field is related to a visit model. Got {attrs}."
+        )
+    elif len(attrs) == 0:
+        raise RelatedVisitFieldError(
+            f"{model_cls} has no related visit model. "
+            f"Expected the related visit model to be an instance "
+            "of `VisitModelMixin`."
+        )
+    return attrs[0]
