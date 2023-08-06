3adf9b8411723a6e93a256fef289d133bea6780a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:49 +0000

    Add package theseus-ai/0.1.1/theseus_ai-0.1.1-cp39-cp39-manylinux_2_17_x86_64.whl

---
+++ packages/theseus-ai/theseus_ai-0.1.1-cp39-cp39-manylinux_2_17_x86_64.whl/theseus/__init__.py
@@ -0,0 +1,94 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+__version__ = "0.1.1"
+
+from .core import (
+    CostFunction,
+    CostWeight,
+    DiagonalCostWeight,
+    Objective,
+    ScaleCostWeight,
+    Variable,
+    Vectorize,
+    RobustLoss,
+    AutoDiffCostFunction,
+    AutogradMode,
+    RobustCostFunction,
+    HuberLoss,
+    WelschLoss,
+    as_variable,
+)
+from .geometry import (
+    SE2,
+    SE3,
+    SO2,
+    SO3,
+    LieGroup,
+    Manifold,
+    Point2,
+    Point3,
+    Vector,
+    adjoint,
+    between,
+    local,
+    retract,
+    compose,
+    inverse,
+    log_map,
+    exp_map,
+    LieGroupTensor,
+    set_lie_tangent_enabled,
+    enable_lie_tangent,
+    no_lie_tangent,
+    rand_vector,
+    rand_point2,
+    rand_point3,
+    rand_so2,
+    rand_so3,
+    rand_se2,
+    rand_se3,
+    randn_vector,
+    randn_point2,
+    randn_point3,
+    randn_so2,
+    randn_so3,
+    randn_se2,
+    randn_se3,
+)
+from .optimizer import (
+    DenseLinearization,
+    OptimizerInfo,
+    SparseLinearization,
+    VariableOrdering,
+    ManifoldGaussian,
+    local_gaussian,
+    retract_gaussian,
+)
+from .optimizer.linear import (
+    CholeskyDenseSolver,
+    CholmodSparseSolver,
+    DenseSolver,
+    LinearOptimizer,
+    LUCudaSparseSolver,
+    LUDenseSolver,
+)
+from .optimizer.nonlinear import (
+    GaussNewton,
+    LevenbergMarquardt,
+    NonlinearLeastSquares,
+    NonlinearOptimizerParams,
+    NonlinearOptimizerStatus,
+    NonlinearOptimizerInfo,
+    BackwardMode,
+)
+from .theseus_layer import TheseusLayer
+
+import theseus.embodied as eb
+
+# Aliases for some standard cost functions
+Difference = eb.Local
+Between = eb.Between
+Local = eb.Local
---
+++ packages/theseus-ai/theseus_ai-0.1.1-cp39-cp39-manylinux_2_17_x86_64.whl/theseus/optimizer/autograd/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .lu_cuda_sparse_autograd import LUCudaSolveFunction
+from .sparse_autograd import CholmodSolveFunction
+
+__all__ = [
+    "CholmodSolveFunction",
+    "LUCudaSolveFunction",
+]
