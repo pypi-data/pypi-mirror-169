c4be9856e083b85a385256ac0bb0d922233a3400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:54 +0000

    Add package lupascua-wild/0.1/lupascua_wild-0.1.tar.gz

---
+++ packages/lupascua-wild/lupascua_wild-0.1.tar.gz/lupascua_wild-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='lupascua_wild',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['lupascua_wild'],
+      author = 'Andreea Lupascu',
+      author_email = 'anamaria-andreea.lupascu@boehringer-ingelheim.com',
+      zip_safe=False)
