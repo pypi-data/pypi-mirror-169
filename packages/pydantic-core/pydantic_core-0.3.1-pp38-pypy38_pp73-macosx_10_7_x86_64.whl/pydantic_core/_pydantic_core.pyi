2731b6e2763b542df1b66a41c1706d749ea338f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:09 +0000

    Add package pydantic-core/0.3.1/pydantic_core-0.3.1-pp38-pypy38_pp73-macosx_10_7_x86_64.whl

---
+++ packages/pydantic-core/pydantic_core-0.3.1-pp38-pypy38_pp73-macosx_10_7_x86_64.whl/pydantic_core/_pydantic_core.pyi
@@ -0,0 +1,52 @@
+import sys
+from typing import Any, TypedDict
+
+from pydantic_core.core_schema import CoreConfig, CoreSchema
+
+if sys.version_info < (3, 11):
+    from typing_extensions import NotRequired
+else:
+    from typing import NotRequired
+
+__all__ = '__version__', 'SchemaValidator', 'SchemaError', 'ValidationError', 'PydanticValueError'
+__version__: str
+build_profile: str
+
+class SchemaValidator:
+    title: str
+    def __init__(self, schema: CoreSchema, config: 'CoreConfig | None' = None) -> None: ...
+    def validate_python(self, input: Any, strict: 'bool | None' = None, context: Any = None) -> Any: ...
+    def isinstance_python(self, input: Any, strict: 'bool | None' = None, context: Any = None) -> bool: ...
+    def validate_json(
+        self, input: 'str | bytes | bytearray', strict: 'bool | None' = None, context: Any = None
+    ) -> Any: ...
+    def isinstance_json(
+        self, input: 'str | bytes | bytearray', strict: 'bool | None' = None, context: Any = None
+    ) -> bool: ...
+    def validate_assignment(
+        self, field: str, input: Any, data: 'dict[str, Any]', strict: 'bool | None' = None, context: Any = None
+    ) -> 'dict[str, Any]': ...
+
+class SchemaError(Exception):
+    pass
+
+class ErrorDetails(TypedDict):
+    kind: str
+    loc: 'list[int | str]'
+    message: str
+    input_value: Any
+    context: NotRequired['dict[str, Any]']
+
+class ValidationError(ValueError):
+    title: str
+
+    def error_count(self) -> int: ...
+    def errors(self) -> 'list[ErrorDetails]': ...
+
+class PydanticValueError(ValueError):
+    kind: str
+    message_template: str
+    context: 'dict[str, str | int] | None'
+
+    def __init__(self, kind: str, message_template: str, context: 'dict[str, str | int] | None' = None) -> None: ...
+    def message(self) -> str: ...
