8e14280e35f24135ebc099f24be50c73793a784e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:49 +0000

    Add package pretix-sofort/1.3.6/pretix-sofort-1.3.6.tar.gz

---
+++ packages/pretix-sofort/pretix-sofort-1.3.6.tar.gz/pretix-sofort-1.3.6/pretix_sofort/__init__.py
@@ -0,0 +1,24 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy
+
+__version__ = '1.3.6'
+
+
+class PluginApp(AppConfig):
+    name = 'pretix_sofort'
+    verbose_name = 'Sofort payment'
+
+    class PretixPluginMeta:
+        name = 'SOFORT'
+        author = 'Raphael Michel'
+        category = 'PAYMENT'
+        description = gettext_lazy('Accept payments through Sofort, a payment method offered by Klarna.')
+        visible = True
+        picture = "pretix_sofort/logo.png"
+        version = __version__
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = 'pretix_sofort.PluginApp'
