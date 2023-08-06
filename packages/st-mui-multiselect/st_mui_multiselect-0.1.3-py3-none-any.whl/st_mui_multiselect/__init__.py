acef7cf8784ff35d319e1dc4ee17d62e99798bde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:39 +0000

    Add package st-mui-multiselect/0.1.3/st_mui_multiselect-0.1.3-py3-none-any.whl

---
+++ packages/st-mui-multiselect/st_mui_multiselect-0.1.3-py3-none-any.whl/st_mui_multiselect/__init__.py
@@ -0,0 +1,40 @@
+import os
+
+import streamlit.components.v1 as components
+
+_SERVED = True if os.environ.get("ST_CG_SERVE", "0") == "1" else False
+
+if _SERVED:
+    # Development setup. From frontend run `npm run serve`
+    _component_func = components.declare_component(
+        "st_mui_multiselect", url="http://localhost:3001"
+    )
+else:
+    # Normal setup. After editing frontend code remember to `npm run build`
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    _component_func = components.declare_component("st_mui_multiselect", path=build_dir)
+
+
+def st_mui_multiselect(options, size=4, key=None):
+    """Create a new instance of "st_mui_multiselect".
+
+    Parameters
+    ----------
+    options: Iterable[str]
+        The options to display in the multiselect
+    size: int
+        Set the size of the multiselect tool
+    key: str or None
+        An optional key that uniquely identifies this component. If this is
+        None, and the component's arguments are changed, the component will
+        be re-mounted in the Streamlit frontend and lose its current state.
+
+    Returns
+    -------
+    List[str]
+        The options that the user has selected
+    """
+    component_value = _component_func(options=options, size=size, key=key, default=[])
+
+    return component_value
---
+++ packages/st-mui-multiselect/st_mui_multiselect-0.1.3-py3-none-any.whl/st_mui_multiselect/frontend/src/StMuiMultiselect.tsx
@@ -0,0 +1,69 @@
+import {
+  Streamlit,
+  StreamlitComponentBase,
+  withStreamlitConnection,
+} from "streamlit-component-lib"
+import React, { ReactNode } from "react"
+import Select from '@material-ui/core/Select';
+
+interface State {
+  selectedOptions: string[]
+}
+
+class StMuiMultiselect extends StreamlitComponentBase<State> {
+  public state = { selectedOptions: [] }
+
+  /** Method for rendering the component */
+  public render = (): ReactNode => {
+    const options: string[] = this.props.args["options"]
+
+    const { theme } = this.props
+    const style: React.CSSProperties = {}
+
+    if (theme) {
+      if (theme.base === "dark") {
+        style.color = "#FAFAFA"
+      }
+    }
+
+    return (
+      <Select
+        labelId="demo-mutiple-checkbox-label"
+        id="demo-mutiple-checkbox"
+        style={style}
+        multiple
+        native
+        value={this.state.selectedOptions}
+        onChange={this.onChange}
+        inputProps={{
+          id: 'select-multiple-native',
+          size: this.props.args["size"]
+        }}
+        disabled={this.props.disabled}
+      >
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </Select>
+    )
+  }
+
+  /** Called when user selects items. Update JS State as well as streamlit component state  */
+  private onChange = (event: any): void => {
+    const { options } = event.target as HTMLSelectElement;
+    const value: string[] = [];
+    for (let i = 0, l = options.length; i < l; i += 1) {
+      if (options[i].selected) {
+        value.push(options[i].value);
+      }
+    }
+    this.setState(
+        prevState => ({ selectedOptions: value }),
+        () => Streamlit.setComponentValue(this.state.selectedOptions)
+    )
+  }
+}
+
+export default withStreamlitConnection(StMuiMultiselect)
