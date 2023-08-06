304a6229aa5cfeefd4c8a6b32c8fa12df1e8a649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:42 +0000

    Add package pywapor/3.1.1/pywapor-3.1.1.tar.gz

---
+++ packages/pywapor/pywapor-3.1.1.tar.gz/pywapor-3.1.1/pywapor/enhancers/__init__.py
@@ -0,0 +1,5 @@
+from pywapor.enhancers import gap_fill, apply_enhancers, dem, lulc, pressure, temperature, other
+
+__all__ = ['gap_fill', 'apply_enhancers', 'dem', 'lulc', 'pressure', 'temperature', 'other']
+
+__version__ = '0.1'
