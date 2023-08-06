12084bf07c6f99504592dcee315eb6b1f94232a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:20 +0000

    Add package simple-test-packet/0.2.1/simple_test_packet-0.2.1.tar.gz

---
+++ packages/simple-test-packet/simple_test_packet-0.2.1.tar.gz/simple_test_packet-0.2.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['simple_test_packet']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fastapi>=0.85.0,<0.86.0', 'uvicorn>=0.18.3,<0.19.0']
+
+setup_kwargs = {
+    'name': 'simple-test-packet',
+    'version': '0.2.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Dima Abramovich',
+    'author_email': 'abramovich_d@rocketdata.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/rofar91/simple-test-packet',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
