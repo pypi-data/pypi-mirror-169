c5e64282b690cc4657dc2fe5330b2bcc3e1d2035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:33 +0000

    Add package sklearndf/2.2.dev0/sklearndf-2.2.dev0-py3-none-any.whl

---
+++ packages/sklearndf/sklearndf-2.2.dev0-py3-none-any.whl/sklearndf/pipeline/_pipeline.py
@@ -0,0 +1,42 @@
+"""
+Core implementation of :mod:`sklearndf.pipeline`
+"""
+
+import logging
+
+from sklearn.pipeline import FeatureUnion, Pipeline
+
+from pytools.api import AllTracker
+
+from .wrapper import FeatureUnionWrapperDF, PipelineWrapperDF
+
+log = logging.getLogger(__name__)
+
+__all__ = ["PipelineDF", "FeatureUnionDF"]
+
+
+#
+# Ensure all symbols introduced below are included in __all__
+#
+
+__tracker = AllTracker(globals())
+
+
+#
+# Class definitions
+#
+
+
+class PipelineDF(PipelineWrapperDF, native=Pipeline):
+    """Stub for DF wrapper of class ``Pipeline``"""
+
+
+class FeatureUnionDF(FeatureUnionWrapperDF, native=FeatureUnion):
+    """Stub for DF wrapper of class ``FeatureUnion``"""
+
+
+#
+# Validate __all__
+#
+
+__tracker.validate()
