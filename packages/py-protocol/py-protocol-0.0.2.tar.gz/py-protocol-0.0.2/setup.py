bef3d147011e8c37875fd9d8d2211120597f6b0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:21 +0000

    Add package py-protocol/0.0.2/py-protocol-0.0.2.tar.gz

---
+++ packages/py-protocol/py-protocol-0.0.2.tar.gz/py-protocol-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='py-protocol',
+    version='0.0.2',
+    description= 'Support for network protocol senders',
+    author='Phuc Bao',
+    packages=['src/protocol'],
+    
+)
---
+++ packages/py-protocol/py-protocol-0.0.2.tar.gz/py-protocol-0.0.2/src/protocol/__init__.py
@@ -0,0 +1,21 @@
+from .http import HTTPCode
+from .http import HTTPRequest
+from .http import HTTPResponse
+from .http import HTTPVer
+from .http import HEADER
+from .http import HEADERSNAME
+from .http import payload
+
+from .http import NoneVariableException
+from .http import FoundKeyNameException
+
+from .http import T_ENV
+from .http import T_FileReader
+from .http import T_Header
+from .http import T_NUM_SERIES
+from .http import T_Payload
+from .http import T_RequestValue
+from .http import T_ResponseValue
+from .http import T_STATUS
+from .http import T_URL
+from .http import T_Version
