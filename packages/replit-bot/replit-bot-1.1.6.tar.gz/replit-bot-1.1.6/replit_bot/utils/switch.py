3df8b8c02b4de072e03317b56aff5258bad84114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:09 +0000

    Add package replit-bot/1.1.6/replit-bot-1.1.6.tar.gz

---
+++ packages/replit-bot/replit-bot-1.1.6.tar.gz/replit-bot-1.1.6/replit_bot/utils/switch.py
@@ -0,0 +1,10 @@
+from typing import Any
+
+
+class Switch:
+
+    def __init__(self, name: Any):
+        self.n = name
+
+    def case(self, current: Any) -> bool:
+        return self.n == current
