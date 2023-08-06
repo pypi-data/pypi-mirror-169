e525298f8d216e2e8e8412b08cbb624291ef2e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:38 +0000

    Add package mtbase/3.0.202209260639/mtbase-3.0.202209260639-py3-none-any.whl

---
+++ packages/mtbase/mtbase-3.0.202209260639-py3-none-any.whl/mt/np/__init__.py
@@ -0,0 +1,18 @@
+"""Alias module. Instead of 'import numpy as np' you can do 'from mt import np'."""
+
+
+from numpy import *
+from numpy import __version__
+
+import numpy as _np
+
+from .ndarray import *
+from .sparse import *
+from .matrix import *
+from .image import *
+
+ndarray = _np.ndarray
+import numpy.linalg as linag
+import numpy.fft as fft
+import numpy.matlib as matlib
+import numpy.random as random
