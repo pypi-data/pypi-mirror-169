3f1634320dcbbd5e7ef4c3d39bb9a008672b621d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:45 +0000

    Add package datasetsync/0.0.1/datasetsync-0.0.1.tar.gz

---
+++ packages/datasetsync/datasetsync-0.0.1.tar.gz/datasetsync-0.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Download dataset'
+LONG_DESCRIPTION = 'A package that allows to Download dataset from layerx'
+
+# Setting up
+setup(
+    name="datasetsync",
+    version=VERSION,
+    author="layerx",
+    author_email="<annotation.manager.dev@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=['python', 'dataset', 'datasetsync', 'ai', 'annotation', 'layerx', 'machine learning'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
