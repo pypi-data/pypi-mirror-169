9aca9b784864163b0362e8bc7952fe62104a6d68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:19:05 +0000

    Add package mati/2.0.0/mati-2.0.0.tar.gz

---
+++ packages/mati/mati-2.0.0.tar.gz/mati-2.0.0/mati/types/__init__.py
@@ -0,0 +1,25 @@
+__all__ = [
+    'SerializableEnum',
+    'PageType',
+    'ValidationInputType',
+    'ValidationType',
+    'VerificationDocumentStep',
+    'VerificationDocument',
+    'LivenessMedia',
+    'Liveness',
+    'DocumentScore',
+    'UserValidationFile',
+]
+
+from .enums import (
+    DocumentScore,
+    Liveness,
+    LivenessMedia,
+    PageType,
+    SerializableEnum,
+    UserValidationFile,
+    ValidationInputType,
+    ValidationType,
+    VerificationDocument,
+    VerificationDocumentStep,
+)
