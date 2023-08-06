8f613624bff19074c2c01d318b69cedd11c54355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:44 +0000

    Add package lyrebird/2.9.2/lyrebird-2.9.2.tar.gz

---
+++ packages/lyrebird/lyrebird-2.9.2.tar.gz/lyrebird-2.9.2/lyrebird/base_server.py
@@ -0,0 +1,39 @@
+"""
+Base threading server class
+"""
+
+from threading import Thread
+
+
+class ThreadServer:
+
+    def __init__(self):
+        self.server_thread = None
+        self.running = False
+        self.name = None
+
+    def start(self, *args, **kwargs):
+        if self.running:
+            return
+        self.running = True
+        self.server_thread = Thread(target=self.run, name=self.name, args=args, kwargs=kwargs)
+        self.server_thread.start()
+
+    def stop(self):
+        self.running = False
+        # TODO terminate self.server_thread
+
+    def run(self):
+        """
+        Server main function
+        """
+        pass
+
+
+class StaticServer:
+
+    def start(self, *args, **kwargs):
+        pass
+
+    def stop(self):
+        pass
---
+++ packages/lyrebird/lyrebird-2.9.2.tar.gz/lyrebird-2.9.2/setup.py
@@ -0,0 +1,51 @@
+import runpy
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = runpy.run_path(
+    os.path.join(here, "lyrebird", "version.py")
+)["VERSION"]
+
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(os.path.join(here, 'install_requires.txt'), encoding='utf-8') as f:
+    install_requires_str = f.read()
+    install_requires = install_requires_str.split()
+
+setup(
+    name='lyrebird',
+    version=VERSION,
+    packages=['lyrebird'],
+    url='https://github.com/meituan/lyrebird',
+    author='HBQA',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    entry_points={
+        'console_scripts': [
+            'lyrebird = lyrebird.manager:main'
+        ]
+    },
+    install_requires=install_requires,
+    extras_require={
+        'dev': [
+            "autopep8",
+            "pylint",
+            "pytest",
+            "pytest-cov"
+        ]
+    }
+)
