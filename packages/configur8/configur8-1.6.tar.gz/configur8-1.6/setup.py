2864f84f140b4cb5245abc0fb3bbe052b6a4045f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:17 +0000

    Add package configur8/1.6/configur8-1.6.tar.gz

---
+++ packages/configur8/configur8-1.6.tar.gz/configur8-1.6/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['configur8']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['email-validator>=1,<2']
+
+setup_kwargs = {
+    'name': 'configur8',
+    'version': '1.6',
+    'description': 'Type-safe configuration and validation library',
+    'long_description': 'None',
+    'author': 'Nick Joyce',
+    'author_email': 'nick@stratuscode.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
