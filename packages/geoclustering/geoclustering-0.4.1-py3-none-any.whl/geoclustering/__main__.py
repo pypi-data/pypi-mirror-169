0c2cb1bf21996e600a00931506655692e5e44208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:24 +0000

    Add package geoclustering/0.4.1/geoclustering-0.4.1-py3-none-any.whl

---
+++ packages/geoclustering/geoclustering-0.4.1-py3-none-any.whl/geoclustering/__main__.py
@@ -0,0 +1,96 @@
+from pathlib import Path
+import click
+import webbrowser
+
+import geoclustering.clustering as clustering
+import geoclustering.encoding as encoding
+import geoclustering.io as io
+
+
+@click.command(
+    help="Tool to cluster geolocations. A cluster is created when a certain number of points (defined with --size) each are within a given distance (defined with --distance) of at least one other point in the cluster. Input is supplied as a csv file. At a minimum, each row needs to have a 'lat' and a 'lon' column. Other rows are reflected to the output."
+)
+@click.option(
+    "--distance",
+    "-d",
+    type=click.FLOAT,
+    required=True,
+    help="(in km) Max. distance between two points in a cluster.",
+)
+@click.option(
+    "--size",
+    "-s",
+    type=click.INT,
+    required=True,
+    help="Min. number of points in a cluster.",
+)
+@click.option(
+    "--output",
+    "-o",
+    type=click.Path(exists=False),
+    default="output",
+    help="Output directory for results. Default: ./output",
+)
+@click.option(
+    "--algorithm",
+    "-a",
+    type=click.Choice(
+        ["dbscan", "optics"],
+        case_sensitive=False,
+    ),
+    default="dbscan",
+    help="Clustering algorithm to be used. `optics` produces tighter clusters but is slower. Default: dbscan",
+)
+@click.option(
+    "--open",
+    "_open",
+    is_flag=True,
+    help="Open the generated visualization in the default browser automatically.",
+)
+@click.option("--debug", is_flag=True, help="Print debug output.")
+@click.argument("filename", type=click.Path(exists=True))
+def main(distance, size, output, filename, algorithm, _open, debug):
+    def print_debug(s):
+        if debug:
+            click.secho(s, fg="bright_black")
+
+    df = io.read_csv_file(filename)
+    print_debug(f"Read {len(df)} valid coordinates from {Path(filename).absolute()}")
+
+    clusters = clustering.cluster_locations(
+        df=df, algorithm=algorithm, radius_km=distance, min_cluster_size=size
+    )
+
+    if not bool(clusters):
+        click.secho("Did not find clusters matching input parameters.", fg="yellow")
+        return
+
+    print_debug(f"Found {len(clusters)} valid clusters using {algorithm}")
+
+    encoded = encoding.encode_clusters(clusters)
+    io.write_output_file(output, "result.txt", encoded["string"])
+    io.write_output_file(output, "result.json", encoded["json"])
+    io.write_output_file(output, "result.geojson", encoded["geojson"])
+    io.write_output_file(output, "result.csv", encoded["csv"])
+
+    vis = io.write_visualization(output, "result.html", encoded["geojson"])
+    if vis is None:
+        print_debug("Skipped generating visualization: kepler is not installed.")
+
+    click.echo(f"Output files saved to {Path(output).absolute()}")
+
+    if _open:
+        if vis:
+            webbrowser.open_new_tab("file://" + str(vis.absolute()))
+            print_debug("Opened visualization in default browser.")
+        else:
+            click.secho(
+                "Can't open kepler.gl: package not installed. Please re-install geoclustering with `pip install geoclustering[full]`.",
+                fg="yellow",
+            )
+
+    click.secho("Clustering completed.", fg="green")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/geoclustering/geoclustering-0.4.1-py3-none-any.whl/geoclustering/encoding.py
@@ -0,0 +1,119 @@
+import json
+import numpy as np
+import geojson
+import csv
+import io  # not io.py
+
+
+class NpEncoder(json.JSONEncoder):
+    """JSONEncoder with support for numpy's numerical types."""
+
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        return super(NpEncoder, self).default(obj)
+
+
+class StringEncoder:
+    """Encodes clustering result as a string."""
+
+    def __init__(self):
+        self.state = []
+
+    def visitor(self, cluster_id, cluster):
+        self.state.append("Cluster {}".format(cluster_id))
+
+        for record in cluster:
+            s = []
+            for key, val in record.items():
+                s.append("{} {}".format(key, val))
+            self.state.append(", ".join(s))
+
+        # separate clusters by an empty line.
+        self.state.append("")
+
+    def get(self):
+        return "\n".join(self.state)
+
+
+class JSONEncoder:
+    """Encodes clustering result as a JSON array."""
+
+    def __init__(self):
+        self.state = []
+
+    def visitor(self, cluster_id, cluster):
+        cluster_data = {"cluster_id": cluster_id, "points": []}
+
+        for record in cluster:
+            cluster_data["points"].append(record)
+        self.state.append(cluster_data)
+
+    def get(self):
+        return json.dumps(self.state, cls=NpEncoder)
+
+
+class GeoJSONEncoder:
+    def __init__(self):
+        self.state = []
+
+    def visitor(self, cluster_id, cluster):
+        for record in cluster:
+            props = {
+                **record,
+                "cluster_id": cluster_id,
+            }
+
+            lon = float(props.pop("lon"))
+            lat = float(props.pop("lat"))
+
+            point = geojson.Point((lon, lat))
+            self.state.append(geojson.Feature(geometry=point, properties=props))
+
+    def get(self):
+        return json.dumps(geojson.FeatureCollection(self.state), cls=NpEncoder)
+
+
+class CSVEncoder:
+    """Encodes clustering result as a CSV"""
+
+    def __init__(self):
+        self.state = io.StringIO()
+        self.writer = False
+
+    def visitor(self, cluster_id, cluster):
+        if not self.writer:
+            self.writer = csv.DictWriter(
+                self.state,
+                fieldnames=["cluster_id"] + list(cluster[0].keys()),
+                quoting=csv.QUOTE_NONNUMERIC,
+                lineterminator="\n",
+            )
+            self.writer.writeheader()
+
+        for record in cluster:
+            self.writer.writerow({**record, "cluster_id": cluster_id})
+
+    def get(self):
+        return self.state.getvalue()
+
+
+def encode_clusters(clusters):
+    json_encoder = JSONEncoder()
+    geojson_encoder = GeoJSONEncoder()
+    string_encoder = StringEncoder()
+    csv_encoder = CSVEncoder()
+
+    encoders = [json_encoder, geojson_encoder, string_encoder, csv_encoder]
+    for cluster_id, cluster in clusters.items():
+        for encoder in encoders:
+            encoder.visitor(cluster_id, cluster)
+
+    return {
+        "json": json_encoder.get(),
+        "geojson": geojson_encoder.get(),
+        "string": string_encoder.get(),
+        "csv": csv_encoder.get(),
+    }
