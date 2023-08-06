6e8c1c712c58dff76b95b9f91b31e694cefe3214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:02 +0000

    Add package indonlp/0.3.3/indoNLP-0.3.3-py3-none-any.whl

---
+++ packages/indonlp/indoNLP-0.3.3-py3-none-any.whl/indoNLP/__init__.py
@@ -0,0 +1,9 @@
+""" Indonesian NLP library written in python """
+
+import indoNLP.dataset
+import indoNLP.preprocessing
+
+__all__ = ["dataset", "preprocessing"]
+__version__ = "0.3.3"
+__author__ = "Wahyu Setianto"
+__source__ = "https://github.com/Hyuto/indo-nlp/"
