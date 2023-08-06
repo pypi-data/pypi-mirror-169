d0de455a8ac1d8987e59fd807e9270863086d51f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:52 +0000

    Add package dagmc-geometry-slice-plotter/0.1.2/dagmc_geometry_slice_plotter-0.1.2-py3-none-any.whl

---
+++ packages/dagmc-geometry-slice-plotter/dagmc_geometry_slice_plotter-0.1.2-py3-none-any.whl/dagmc_geometry_slice_plotter/__init__.py
@@ -0,0 +1,14 @@
+from importlib.metadata import version  # , PackageNotFoundError
+
+# try:
+__version__ = version("dagmc_geometry_slice_plotter")
+# except PackageNotFoundError:
+#     from setuptools_scm import get_version
+
+#     __version__ = get_version(root="..", relative_to=__file__)
+
+__all__ = ["__version__"]
+
+from .utils import plot_slice_of_dagmc_file
+from .utils import plot_slice_of_trimesh_object
+from .utils import plot_slice_of_dagmc_geometry
