db5cb8c1b4d2a690951f75436763ed01056ce59a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:29 +0000

    Add package xinshengming-shuzi-mima-vol1/2022.9.29.0/xinshengming_shuzi_mima_vol1-2022.9.29.0-py3-none-any.whl

---
+++ packages/xinshengming-shuzi-mima-vol1/xinshengming_shuzi_mima_vol1-2022.9.29.0-py3-none-any.whl/XinshengmingShuziMimaVol1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xinshengming-shuzi-mima-vol1
+https://github.com/apachecn/xinshengming-shuzi-mima-vol1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.9.29.0"
