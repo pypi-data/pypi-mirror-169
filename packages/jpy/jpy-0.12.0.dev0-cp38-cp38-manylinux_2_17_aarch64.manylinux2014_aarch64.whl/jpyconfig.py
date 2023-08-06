8922bee9e178ef45db40cd5a64a72e04dc659827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:37 +0000

    Add package jpy/0.12.0.dev0/jpy-0.12.0.dev0-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/jpy/jpy-0.12.0.dev0-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/jpyconfig.py
@@ -0,0 +1,8 @@
+# Created by 'jpyutil.py' tool on 2022-09-27 17:35:52.952006
+# This file is read by the 'jpyutil' module in order to load and configure the JVM from Python
+java_home = '/usr/lib/jvm/temurin-8-jdk-arm64'
+jvm_dll = '/usr/lib/jvm/temurin-8-jdk-arm64/jre/lib/aarch64/server/libjvm.so'
+jvm_maxmem = None
+jvm_classpath = []
+jvm_properties = {}
+jvm_options = []
