34da6665e6823b5b3dac56f3810c988dd541f2f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:59 +0000

    Add package azdsdr/1.220929.6/azdsdr-1.220929.6.tar.gz

---
+++ packages/azdsdr/azdsdr-1.220929.6.tar.gz/azdsdr-1.220929.6/README.md
@@ -0,0 +1,3 @@
+# AZDSDR - Data Scientist's Data Reader
+
+This package includes data reader for DS to access data in a easy way. 
---
+++ packages/azdsdr/azdsdr-1.220929.6.tar.gz/azdsdr-1.220929.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='azdsdr',
+    version='1.220929.6',
+    license='Apache License',
+    author="Andrew Zhu",
+    author_email='xhinker@hotmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/xhinker/azdsdr',
+    keywords='DS Data Reader',
+    install_requires=[
+        'pandas'
+        ,'pyodbc'
+    ],
+)
