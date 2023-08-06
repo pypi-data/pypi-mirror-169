1e61bdc68acc0cc3cb5a5520b974ed3bf36bcc9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:43 +0000

    Add package elphmod/0.16/elphmod-0.16.tar.gz

---
+++ packages/elphmod/elphmod-0.16.tar.gz/elphmod-0.16/elphmod/__init__.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2017-2022 elphmod Developers
+# This program is free software under the terms of the GNU GPLv3 or later.
+
+"""Python modules for electron-phonon models."""
+
+__version__ = '0.16'
+
+from . import MPI
+from . import bravais
+from . import diagrams
+from . import dispersion
+from . import dos
+from . import el
+from . import elel
+from . import eliashberg
+from . import elph
+from . import misc
+from . import occupations
+from . import ph
+from . import plot
