332b03bc8114f6f8931f46936f7ec9a3f81ced42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:00 +0000

    Add package laygo2/0.2.1/laygo2-0.2.1.tar.gz

---
+++ packages/laygo2/laygo2-0.2.1.tar.gz/laygo2-0.2.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+'''
+Pypi update instruction:
+1) update the version number in setup.py.
+2) run 'python setup.py sdist bdist_wheel' to build whl.
+3) run 'python -m twine upload dist/*'.
+4) type username and password.
+
+'''
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='laygo2',
+    version='0.2.1',
+    author='Jaeduk Han',
+    description='LAYout with Gridded Object 2 - A Python Package for Scripted IC Layout Generation Flow',
+    long_description=readme,
+    url='https://github.com/niftylab/laygo2', # project address
+    license=license,
+    packages=find_packages(exclude=('test', 'docs')),
+    python_requires='>=3.0',
+)
