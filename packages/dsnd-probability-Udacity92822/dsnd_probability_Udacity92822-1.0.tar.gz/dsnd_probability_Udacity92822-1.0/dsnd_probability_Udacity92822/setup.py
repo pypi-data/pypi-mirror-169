71d0163f8c573298f96e270652f55a4163c80b0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:49 +0000

    Add package dsnd-probability-Udacity92822/1.0/dsnd_probability_Udacity92822-1.0.tar.gz

---
+++ packages/dsnd-probability-Udacity92822/dsnd_probability_Udacity92822-1.0.tar.gz/dsnd_probability_Udacity92822-1.0/dsnd_probability_Udacity92822/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 28 14:47:20 2022
+
+@author: jtrej
+"""
+
+setup (name = 'dsnd_probability_UdacityPractice',
+       version = '1.0',
+       description = 'Gaussian and Binomail distributions',
+       packages = ['dsn_probability_UdacityPractice'],
+       author = 'J A Trejo'
+       author_email = 'jtrejo@gmail.com',
+     zip_safe = False)
---
+++ packages/dsnd-probability-Udacity92822/dsnd_probability_Udacity92822-1.0.tar.gz/dsnd_probability_Udacity92822-1.0/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 28 14:47:20 2022
+
+@author: jtrej
+"""
+
+from setuptools import setup
+
+setup (name = 'dsnd_probability_Udacity92822',
+       version = '1.0',
+       description = 'Gaussian and Binomail distributions',
+       packages = ['dsnd_probability_Udacity92822'],
+       author = 'J A Trejo',
+       author_email = 'jtrejo@gmail.com',
+     zip_safe = False)
