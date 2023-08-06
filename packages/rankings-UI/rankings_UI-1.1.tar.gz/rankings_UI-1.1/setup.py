10445d93cfb2c6d7c4bd333f96bfe80bd1e225c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:18 +0000

    Add package rankings-UI/1.1/rankings_UI-1.1.tar.gz

---
+++ packages/rankings-UI/rankings_UI-1.1.tar.gz/rankings_UI-1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='rankings_UI',
+    version='1.1',
+    license='MIT',
+    author="Haoqiang Kang",
+    author_email='haoqik@cs.washington.edu',
+    packages=find_packages(),
+    url='https://github.com/mk322/Rankings-UI',
+    description="This is the first version of the package of Rankings UI, which helps visualize the rankings and ratings for comparison.",
+    keywords=['GUI'],
+    install_requires=[
+          'toml>=0.10.2',
+          'pandas>=1.3.4',
+          'numpy>=1.22.2']
+    )
