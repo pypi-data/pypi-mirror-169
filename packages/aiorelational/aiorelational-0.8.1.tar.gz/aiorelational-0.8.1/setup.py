7b281acc4b5e9d577e67a59f1a1bfae1cad01e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:40 +0000

    Add package aiorelational/0.8.1/aiorelational-0.8.1.tar.gz

---
+++ packages/aiorelational/aiorelational-0.8.1.tar.gz/aiorelational-0.8.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aiorelational']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'aiorelational',
+    'version': '0.8.1',
+    'description': 'Relational operators for working with Async iterators',
+    'long_description': 'Async relational iterators/generators for manipulating data streams\n',
+    'author': 'Willem Thiart',
+    'author_email': 'himself@willemthiart.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/willemt/aiorelational',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
