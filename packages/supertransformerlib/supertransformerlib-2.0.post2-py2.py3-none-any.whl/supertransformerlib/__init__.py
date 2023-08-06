20c8464b59eab0681176b61851d829c0d7c44a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:32 +0000

    Add package supertransformerlib/2.0.post2/supertransformerlib-2.0.post2-py2.py3-none-any.whl

---
+++ packages/supertransformerlib/supertransformerlib-2.0.post2-py2.py3-none-any.whl/supertransformerlib/__init__.py
@@ -0,0 +1,14 @@
+"""
+A library for fast, smart transformers.
+
+
+"""
+
+__version__ = "2.0r2"
+
+
+from . import Glimpses # noqa
+from . import Attention # noqa
+from . import Adaptive # noqa
+from . import Core # noqa
+from . import Loss # noqa
