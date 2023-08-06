942918606ff38cf6a724c5b095df8edcacf457eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:00 +0000

    Add package dao-analyzer/1.2.1.dev2/dao_analyzer-1.2.1.dev2-py3-none-any.whl

---
+++ packages/dao-analyzer/dao_analyzer-1.2.1.dev2-py3-none-any.whl/dao_analyzer/web/apps/daohaus/data_access/daos/metric/srcs.py
@@ -0,0 +1,20 @@
+"""
+   Descp: This file stores the various sources as an enum
+
+   Created on: 13-dec-2021
+
+   Copyright 2021 David Davó
+        <david@ddavo.me>
+"""
+
+from pathlib import Path
+from typing import List
+
+CACHE_PATH: Path = Path('datawarehouse') / 'daohaus'
+MOLOCHES: Path = CACHE_PATH / 'moloches.arr'
+PROPOSALS: Path = CACHE_PATH / 'proposals.arr'
+MEMBERS: Path = CACHE_PATH / 'members.arr'
+RAGE_QUITS: Path = CACHE_PATH / 'rageQuits.arr'
+VOTES: Path = CACHE_PATH / 'votes.arr'
+TOKEN_BALANCES: Path = CACHE_PATH / 'tokenBalances.arr'
+ALL_FILES: List[str] = [MOLOCHES, PROPOSALS, MEMBERS, RAGE_QUITS, VOTES, TOKEN_BALANCES]
