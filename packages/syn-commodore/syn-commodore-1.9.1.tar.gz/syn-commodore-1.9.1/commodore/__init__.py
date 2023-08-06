94ecb98d8323fa8e70d5d189f4f8c289b8b6f78b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:54 +0000

    Add package syn-commodore/1.9.1/syn-commodore-1.9.1.tar.gz

---
+++ packages/syn-commodore/syn-commodore-1.9.1.tar.gz/syn-commodore-1.9.1/commodore/__init__.py
@@ -0,0 +1,12 @@
+"""
+Commodore. Build dynamic inventories and compile catalogs with Kapitan
+"""
+from pathlib import Path as P
+from importlib_metadata import version
+
+__url__ = "https://github.com/projectsyn/commodore/"
+__git_version__ = 'v1.9.1'
+__version__ = version("syn-commodore")
+
+# provide Commodore installation dir as variable that can be imported
+__install_dir__ = P(__file__).parent
