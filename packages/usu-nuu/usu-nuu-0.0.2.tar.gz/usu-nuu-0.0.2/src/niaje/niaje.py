0ca7c52133c2bae895e51ecbf3fea9a8f26132fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:57 +0000

    Add package usu-nuu/0.0.2/usu-nuu-0.0.2.tar.gz

---
+++ packages/usu-nuu/usu-nuu-0.0.2.tar.gz/usu-nuu-0.0.2/src/niaje/niaje.py
@@ -0,0 +1,10 @@
+def niajeWasee():
+    print("Wasee Niaje")
+
+"""
+This method takes in a list and applies 
+list comprehension to it
+"""
+def loopArray(array):
+    items = [x for x in array]
+    return items
