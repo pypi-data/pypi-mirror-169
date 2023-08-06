c2e71c1a3dd85fb00267e87b1da03e4a89378c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:51 +0000

    Add package django-nice-form-fields/0.8.3/django-nice-form-fields-0.8.3.tar.gz

---
+++ packages/django-nice-form-fields/django-nice-form-fields-0.8.3.tar.gz/django-nice-form-fields-0.8.3/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(name='django-nice-form-fields',
+      version='0.8.3',
+      description='Django custom imagefield that dynamically renders the loaded image.',
+      author='Artur Karpovich',
+      author_email='onelifeitsme@gmail.com',
+      url='https://github.com/onelifeitsme/django-nice-form-fields',
+      packages=['django-nice-form-fields', 'django-nice-form-fields.templates', 'django-nice-form-fields.templates.django_dynamic_imagefield'],
+      include_package_data=True,
+      package_data={
+        'django-nice-form-fields' : ['django-nice-form-fields/*.html']
+    },
+      install_requires=['django', 'pillow'],
+     )
