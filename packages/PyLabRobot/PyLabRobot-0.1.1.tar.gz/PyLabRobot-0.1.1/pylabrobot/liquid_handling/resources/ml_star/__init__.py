accc0f0406d4f9cf81697422c5e83108a42ed413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:53 +0000

    Add package PyLabRobot/0.1.1/PyLabRobot-0.1.1.tar.gz

---
+++ packages/PyLabRobot/PyLabRobot-0.1.1.tar.gz/PyLabRobot-0.1.1/pylabrobot/liquid_handling/resources/ml_star/__init__.py
@@ -0,0 +1,4 @@
+from .plate_carriers import *
+from .tip_carriers import *
+from .tipracks import *
+from .tip_types import *
---
+++ packages/PyLabRobot/PyLabRobot-0.1.1.tar.gz/PyLabRobot-0.1.1/setup.py
@@ -0,0 +1,72 @@
+from setuptools import setup, find_packages
+
+from pylabrobot.__version__ import __version__
+
+long_description = open('README.md', encoding='utf-8').read()
+
+extras_docs = [
+    'sphinx_book_theme',
+    'myst_nb',
+    'sphinx_copybutton',
+]
+
+extras_fw = [
+    'pyusb'
+]
+
+extras_http = [
+    'requests',
+]
+
+extras_websockets = [
+    'websockets'
+]
+
+extras_simulation = extras_websockets
+
+extras_venus = [
+    'pyhamilton'
+]
+
+extras_opentrons = [
+    'opentrons-python-api',
+    'opentrons-shared-data'
+]
+
+extras_testing = [
+    'pytest',
+    'pytest-timeout',
+    'requests',
+    'pylint'
+] + extras_simulation + extras_opentrons
+
+extras_server = [
+    'flask',
+    'wtforms',
+    'wtforms_json'
+]
+
+extras_all = extras_docs + extras_simulation + extras_http + extras_websockets + extras_testing + \
+              extras_venus + extras_server + extras_fw + extras_opentrons
+
+setup(
+    name='PyLabRobot',
+    version=__version__,
+    packages=find_packages(exclude="tools"),
+    description='A hardware agnostic platform for liquid handling',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[],
+    url='https://github.com/pylabrobot/pylabrobot.git',
+    extras_require={
+        'testing': extras_testing,
+        'docs': extras_docs,
+        'simulation': extras_simulation,
+        'http': extras_http,
+        'websockets': extras_websockets,
+        'venus': extras_venus,
+        'opentrons': extras_opentrons,
+        'server': extras_server,
+        'all': extras_all
+    }
+)
