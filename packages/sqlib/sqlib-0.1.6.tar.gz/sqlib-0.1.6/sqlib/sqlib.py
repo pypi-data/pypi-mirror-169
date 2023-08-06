94247d55ca75f8f1f938413ff59de80f4c84c6a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:04 +0000

    Add package sqlib/0.1.6/sqlib-0.1.6.tar.gz

---
+++ packages/sqlib/sqlib-0.1.6.tar.gz/sqlib-0.1.6/sqlib/sqlib.py
@@ -0,0 +1,23 @@
+from sqlib.modules.connection import *
+from sqlib.modules.create import Create
+from sqlib.modules.select import *
+
+
+class Sqlib(Connection, Select, Create):
+    def __init__(self, *, host: str, user: str, password: str, database: str, settings: dict = {}):
+        if "port" in settings:
+            self.host = f"{host}:{settings['port']}"
+        super().__init__(host, user, password, database)
+        self.host = host
+        self.user = user
+        self.password = password
+        self.settings = settings
+
+
+    # def select(self, table: str, columns: list, where: dict = None, limit: int = None):
+    #     return super().select(table, columns, where, limit)
+
+    # def create(self, cursor, table, columns, value):
+    #     super().create(cursor, "table_name", ["name", "age", "email"], ["'martin'", "20", "'qwerty'"])
+    def __str__(self) -> str:
+        return f"Host: {self.host}, User: {self.user}, Password: {self.password}, Settings: {self.settings}"
