0c2a67789b8a354f77d78768a34cd161d837a071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:07 +0000

    Add package dask-cuda/22.10.0a220928/dask_cuda-22.10.0a220928-py3-none-any.whl

---
+++ packages/dask-cuda/dask_cuda-22.10.0a220928-py3-none-any.whl/dask_cuda/__init__.py
@@ -0,0 +1,31 @@
+import sys
+
+if sys.platform != "linux":
+    raise ImportError("Only Linux is supported by Dask-CUDA at this time")
+
+
+import dask
+import dask.dataframe.core
+import dask.dataframe.shuffle
+
+from ._version import get_versions
+from .cuda_worker import CUDAWorker
+from .explicit_comms.dataframe.shuffle import get_rearrange_by_column_wrapper
+from .local_cuda_cluster import LocalCUDACluster
+from .proxify_device_objects import proxify_decorator, unproxify_decorator
+
+__version__ = get_versions()["version"]
+del get_versions
+
+
+# Monkey patching Dask to make use of explicit-comms when `DASK_EXPLICIT_COMMS=True`
+dask.dataframe.shuffle.rearrange_by_column = get_rearrange_by_column_wrapper(
+    dask.dataframe.shuffle.rearrange_by_column
+)
+
+
+# Monkey patching Dask to make use of proxify and unproxify in compatibility mode
+dask.dataframe.shuffle.shuffle_group = proxify_decorator(
+    dask.dataframe.shuffle.shuffle_group
+)
+dask.dataframe.core._concat = unproxify_decorator(dask.dataframe.core._concat)
