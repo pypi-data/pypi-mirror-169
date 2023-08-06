4ed3ab627316b1fb41d4b5001368c049744f013e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:03 +0000

    Add package file-light/0.4/file_light-0.4.tar.gz

---
+++ packages/file-light/file_light-0.4.tar.gz/file_light-0.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='file_light',
+      version='0.4',
+      description='Модуль для более быстрой работы с файлами',
+      packages=['file_light'],
+      author_email='kassas20@mail.ru',
+      zip_safe=False)
