81ca0b43f9eb8cc61dcb497c6623cf4b59e24a39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:42 +0000

    Add package mloptm/1.0.8/mloptm-1.0.8-py3-none-any.whl

---
+++ packages/mloptm/mloptm-1.0.8-py3-none-any.whl/mloptm/__init__.py
@@ -0,0 +1,15 @@
+from mloptm.methods import (
+    Golden,
+    BiSection,
+    Newton,
+    Secant
+)
+
+from mloptm.grads import (
+    SteepestDescent,
+    NewtonND
+)
+
+
+__version__ = "1.0.8"
+
