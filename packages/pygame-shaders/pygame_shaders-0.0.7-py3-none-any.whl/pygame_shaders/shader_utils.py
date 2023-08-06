1e0672d15be5223eefc182f8dfdea95a585c7110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:50 +0000

    Add package pygame-shaders/0.0.7/pygame_shaders-0.0.7-py3-none-any.whl

---
+++ packages/pygame-shaders/pygame_shaders-0.0.7-py3-none-any.whl/pygame_shaders/shader_utils.py
@@ -0,0 +1,14 @@
+from OpenGL.GL import *
+from OpenGL.GL.shaders import compileProgram,compileShader
+
+def create_shader(vertex_filepath, fragment_filepath):
+    with open(vertex_filepath,'r') as f:
+        vertex_src = f.readlines()
+
+    with open(fragment_filepath,'r') as f:
+        fragment_src = f.readlines()
+    
+    shader = compileProgram(compileShader(vertex_src, GL_VERTEX_SHADER),
+                            compileShader(fragment_src, GL_FRAGMENT_SHADER))
+    
+    return shader
