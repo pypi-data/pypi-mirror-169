75065e20e007605df6cfb5507a639b60c8a19adc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:52 +0000

    Add package Indrajeet/0.2/Indrajeet-0.2.tar.gz

---
+++ packages/Indrajeet/Indrajeet-0.2.tar.gz/Indrajeet-0.2/README.md
@@ -0,0 +1,5 @@
+# My private package. Don't share,copy,use or change without permission
+
+[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
+
+# licence - https://www.eulatemplate.com/live.php?token=bGWPixXa1XA3VMj5H7IToOGlUadEIaJD
---
+++ packages/Indrajeet/Indrajeet-0.2.tar.gz/Indrajeet-0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name='Indrajeet',
+    version='0.2',
+    author="Indrajeet Singh Yadav",
+    author_email="inderjityadav93@gmail.com",
+    description="My Personal Package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="Proprietary",
+    url="https://github.com/javatechy/dokr",
+    packages=setuptools.find_packages(),
+    # py_modules=['my_modules'],
+    install_requires=[
+        'numpy>=1.19.0',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'License :: Other/Proprietary License',
+        "Operating System :: OS Independent",
+    ],
+ )
