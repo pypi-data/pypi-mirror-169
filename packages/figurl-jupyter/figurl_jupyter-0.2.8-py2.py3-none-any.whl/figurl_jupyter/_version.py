20c795a39a829bf9dc02e3fad0d224360877fb21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:41 +0000

    Add package figurl-jupyter/0.2.8/figurl_jupyter-0.2.8-py2.py3-none-any.whl

---
+++ packages/figurl-jupyter/figurl_jupyter-0.2.8-py2.py3-none-any.whl/figurl_jupyter/_version.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Jeremy Magland.
+# Distributed under the terms of the Modified BSD License.
+
+# version_info = (0, 2, 8, 'dev')
+version_info = (0, 2, 8)
+__version__ = ".".join(map(str, version_info))
