25dc116a2498ee2d52bff7898df75dcd372f9fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:28 +0000

    Add package hotweb-cli/0.0.129/hotweb_cli-0.0.129.tar.gz

---
+++ packages/hotweb-cli/hotweb_cli-0.0.129.tar.gz/hotweb_cli-0.0.129/hotweb_cli/migrations/migration.py
@@ -0,0 +1,14 @@
+# migrations skeleton
+def up(queryInterface,DataTypes):
+    pass
+
+def down(queryInterface,DataTypes):
+    pass
+
+"""
+TO HANDLE MIGRATIONS
+<---------------------------------------------->
+import the up function from the migration file and pass the required two args then call
+the cunction ------------------->
+--->
+"""
---
+++ packages/hotweb-cli/hotweb_cli-0.0.129.tar.gz/hotweb_cli-0.0.129/hotweb_cli/models/testd.py
@@ -0,0 +1,7 @@
+
+from hotweb.models.models import Models
+model = Models()
+model.init({'jfjfj': 'kfk'})
+def testd():
+    {'jfjfj': 'kfk'}
+    
