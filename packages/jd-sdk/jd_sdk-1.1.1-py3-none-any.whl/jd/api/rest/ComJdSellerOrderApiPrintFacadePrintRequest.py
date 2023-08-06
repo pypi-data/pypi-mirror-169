03cbd9b7be1bde31af2d6733c60fb0c405d98ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:54 +0000

    Add package jd-sdk/1.1.1/jd_sdk-1.1.1-py3-none-any.whl

---
+++ packages/jd-sdk/jd_sdk-1.1.1-py3-none-any.whl/jd/api/rest/ComJdSellerOrderApiPrintFacadePrintRequest.py
@@ -0,0 +1,21 @@
+from jd.api.base import RestApi
+
+class ComJdSellerOrderApiPrintFacadePrintRequest(RestApi):
+		def __init__(self,domain,port=80):
+			"""
+			"""
+			RestApi.__init__(self,domain, port)
+			self.language = None
+			self.orderId = None
+			self.format = None
+			self.type = None
+			self.needSku = None
+
+		def getapiname(self):
+			return 'jingdong.com.jd.seller.order.api.PrintFacade.print'
+
+			
+
+
+
+
