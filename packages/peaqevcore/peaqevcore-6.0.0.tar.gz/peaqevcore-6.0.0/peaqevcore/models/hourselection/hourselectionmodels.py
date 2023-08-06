24c5a3cdb24bbbf0b342e3c90c9e923d87ba5d5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:23 +0000

    Add package peaqevcore/6.0.0/peaqevcore-6.0.0.tar.gz

---
+++ packages/peaqevcore/peaqevcore-6.0.0.tar.gz/peaqevcore-6.0.0/peaqevcore/models/hourselection/hourselectionmodels.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass, field
+from typing import List, Dict
+from .hourobject import HourObject
+
+@dataclass(frozen=False)
+class HoursModel:
+    non_hours: List[int] = field(default_factory=lambda : [])
+    caution_hours: List[int] = field(default_factory=lambda : [])
+    dynamic_caution_hours: Dict[int, float] = field(default_factory=lambda : {})
+    hours_today: HourObject = field(default_factory=lambda : HourObject([],[],dict()))
+    hours_tomorrow: HourObject = field(default_factory=lambda : HourObject([],[],dict()))
+
+
+@dataclass(frozen=False)
+class HourSelectionOptions:
+    cautionhour_type: float = 0
+    absolute_top_price: float = 0
+    min_price: float = 0
+    allow_top_up: bool = False
+    conserve_top_up: bool = False
+
+
+@dataclass(frozen=False)
+class HourSelectionModel:
+    prices_today: List[float] = field(default_factory=lambda : [])
+    prices_tomorrow: List[float] = field(default_factory=lambda : [])
+    hours: HoursModel = HoursModel()
+    options: HourSelectionOptions = HourSelectionOptions
---
+++ packages/peaqevcore/peaqevcore-6.0.0.tar.gz/peaqevcore-6.0.0/peaqevcore/services/locale/countries/default.py
@@ -0,0 +1,11 @@
+from ..querytypes.const import (
+    QUERYTYPE_BASICMAX
+)
+
+from ..locale_model import Locale_Type
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class Default(Locale_Type):
+    observed_peak = QUERYTYPE_BASICMAX
+    charged_peak = QUERYTYPE_BASICMAX
