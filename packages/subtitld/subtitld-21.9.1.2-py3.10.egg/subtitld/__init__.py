8b0e5d7ffb459b2e1de16b8972a0b411c7d4785c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:29 +0000

    Add package subtitld/21.9.1.2/subtitld-21.9.1.2-py3.10.egg

---
+++ packages/subtitld/subtitld-21.9.1.2-py3.10.egg/subtitld/__init__.py
@@ -0,0 +1,19 @@
+#######################################################################
+#
+# Subtitld
+#
+#######################################################################
+
+__version__ = '21.09.01.02'
+
+__appname__ = 'Subtitld'
+__domain__ = 'subtitld.org'
+__desktopid__ = 'org.jonata.Subtitld'
+# __appid__ = ''
+
+__author__ = 'Jonatã Bolzan Loss'
+__email__ = 'subtitld@jonata.org'
+__website__ = 'https://subtitld.org'
+__bugreport__ = 'https://gitlab.com/jonata/subtitld/issues'
+
+__ispypi__ = False
