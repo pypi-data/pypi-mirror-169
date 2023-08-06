4dd67776ae47cba5df995f629bd97aa6f29ecfef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:41 +0000

    Add package gaodeng-lingxing-xiulian-famen/2022.9.29.0/gaodeng_lingxing_xiulian_famen-2022.9.29.0-py3-none-any.whl

---
+++ packages/gaodeng-lingxing-xiulian-famen/gaodeng_lingxing_xiulian_famen-2022.9.29.0-py3-none-any.whl/GaodengLingxingXiulianFamen/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""gaodeng-lingxing-xiulian-famen
+https://github.com/apachecn/gaodeng-lingxing-xiulian-famen"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.9.29.0"
