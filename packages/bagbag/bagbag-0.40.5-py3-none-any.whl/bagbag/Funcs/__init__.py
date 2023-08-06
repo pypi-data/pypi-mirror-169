553b5d5a1117498e97a4da40e89a5df5d1569728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:50 +0000

    Add package bagbag/0.40.5/bagbag-0.40.5-py3-none-any.whl

---
+++ packages/bagbag/bagbag-0.40.5-py3-none-any.whl/bagbag/Funcs/__init__.py
@@ -0,0 +1,11 @@
+from .Wget import Wget
+from .IPAddressConvert import Int2IP, IP2Int
+from .ResizeImage import ResizeImage
+from .GetPublicIP import GetPublicIP
+from .Ping import Ping
+from .CutChinese import CutChinese
+import time
+
+def Block():
+    while True:
+        time.sleep(1)
