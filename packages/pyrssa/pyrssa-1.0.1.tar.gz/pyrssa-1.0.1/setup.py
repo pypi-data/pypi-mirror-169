91a6dac83bde9bb6a35c3dd0b3da042f34778439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:12 +0000

    Add package pyrssa/1.0.1/pyrssa-1.0.1.tar.gz

---
+++ packages/pyrssa/pyrssa-1.0.1.tar.gz/pyrssa-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pyrssa',
+    packages=['pyrssa'],
+    version='1.0.1',
+    description='Rssa for Python',
+    author='Fleyderer',
+    author_email='fleyderer@gmail.com',
+    url="https://github.com/Fleyderer/pyrssa",
+    license="Apache 2.0",
+    install_requires=['rpy2', 'pandas', 'numpy', 'matplotlib']
+)
