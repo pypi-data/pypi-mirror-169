806a55314ec2bfcb26e4419a85de5f25a8cd1136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:18 +0000

    Add package deephy/0.5.2/deephy-0.5.2.tar.gz

---
+++ packages/deephy/deephy-0.5.2.tar.gz/deephy-0.5.2/deephy/__init__.py
@@ -0,0 +1,19 @@
+from deephy.deephy import (
+    ImageFile,
+    Neuron,
+    Layer,
+    Model,
+    Test,
+    Stopwatch,
+    import_torch_dataset,
+)
+
+__all__ = [
+    "ImageFile",
+    "Neuron",
+    "Layer",
+    "Model",
+    "Test",
+    "Stopwatch",
+    "import_torch_dataset",
+]
