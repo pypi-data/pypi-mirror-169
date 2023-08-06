9bf947c4289ab6e0e6e263e6e5ec6c2564827015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:57 +0000

    Add package bluescan/0.8.6/bluescan-0.8.6-py3-none-any.whl

---
+++ packages/bluescan/bluescan-0.8.6-py3-none-any.whl/bluescan/helper.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+# import pickle
+import subprocess
+
+from serial.tools.list_ports import comports
+
+from pyclui import Logger, blue, green, yellow, red
+
+from . import LOG_LEVEL
+
+# from . import LE_DEVS_SCAN_RESULT_CACHE
+
+
+logger = Logger(__name__, LOG_LEVEL)
+
+
+# def determine_addr_type(self):
+#     """For user not provide the remote LE address type."""
+#     from .le_scan import LeScanner
+#     try:
+#         with open(LE_DEVS_SCAN_RESULT_CACHE, 'rb') as le_devs_scan_result_cache:
+#             le_devs_scan_result = pickle.load(le_devs_scan_result_cache)
+#             for dev_info in le_devs_scan_result.devices_info:
+#                 if self.result.addr == dev_info.addr:
+#                     logger.debug("determine_addr_type, {} {}".format(self.result.addr, dev_info.addr_type))
+#                     return dev_info.addr_type
+#     except FileNotFoundError as e:
+#         logger.debug("No {} found".format(LE_DEVS_SCAN_RESULT_CACHE))
+            
+#     le_devs_scan_result = LeScanner(self.iface).scan_devs()
+#     for dev_info in le_devs_scan_result.devices_info:
+#         if self.result.addr == dev_info.addr:
+#             return dev_info.addr_type
+
+#     raise RuntimeError("Couldn't determine the LE address type. Please provide it explicitly.")
+
+
+def find_rfkill_devid(dev='hci0') -> int:
+    exitcode, output = subprocess.getstatusoutput('rfkill -rno ID,DEVICE')
+
+    for line in output.splitlines():
+        id_dev = line.split()
+        if len(id_dev) == 2 and id_dev[1] == dev:
+            return int(id_dev[0])
+        else:
+            continue
+    
+    raise RuntimeError("Can't find the ID of %s in rfkill" % blue(dev))
+
+
+def get_microbit_devpaths() -> list:
+    """Get serial device path of all connected micro:bits."""
+    # microbit_infos = []
+    dev_paths = []
+    for info in comports():
+        dev_path, desc, hwid = info
+        # logger.debug("info.device: {}".format(info.device))
+        # logger.debug("info.name: {}".format(info.name))
+        # logger.debug("info.description: {}".format(info.description))
+        # logger.debug("info.hwid: {}".format(info.hwid))
+
+        # # USB specific data
+        # logger.debug("info.vid:".format(info.vid))
+        # logger.debug("info.pid:".format(info.pid))
+        # logger.debug("info.serial_number:".format(info.serial_number))
+        # logger.debug("info.location:".format(info.location))
+        # logger.debug("info.manufacturer:".format(info.manufacturer))
+        # logger.debug("info.product:".format(info.product))
+        # logger.debug("info.interface:".format(info.interface))
+
+
+        # logger.debug(port)
+        # logger.debug(desc)
+        # logger.debug(hwid)
+    
+        if desc == "DAPLink CMSIS-DAP - mbed Serial Port":
+            dev_paths.append(dev_path)
+            # microbit_infos.append({
+            #     'path': dev_path,
+            #     'desc': desc,
+            #     'hwid': hwid
+            # })
+
+    logger.debug("get_microbit_devpaths, dev_paths: {}".format(dev_paths))
+    return dev_paths
+
+
+def __test():
+    #valid_bdaddr('11:22:33:44:55:66')
+    try:
+        print(find_rfkill_devid('hci0'))
+    except RuntimeError as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    __test()
---
+++ packages/bluescan/bluescan-0.8.6-py3-none-any.whl/bluescan/plugin.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python3
+
+import subprocess
+from subprocess import CalledProcessError
+from typing import Union
+from abc import ABC, abstractmethod
+from importlib.metadata import metadata, version
+
+import pkg_resources
+from pyclui import Logger
+from docopt import DocoptExit
+
+from .ui import LOG_LEVEL
+
+
+logger = Logger(__name__, LOG_LEVEL)
+
+
+class BluescanPluginError(Exception):
+    pass
+
+
+class BluescanPluginInstallError(BluescanPluginError):
+    pass
+
+
+class BluescanPluginOptionError(BluescanPluginError):
+    pass
+
+
+class BluescanPluginRuntimeError(BluescanPluginError):
+    pass
+
+
+class BluescanPluginPrepareError(BluescanPluginError):
+    pass
+
+
+class BluescanPluginCleanError(BluescanPluginError):
+    pass
+
+
+class BluescanPluginHelpException(Exception):
+    """给 plugin 传递 -h, --help 时，plugin 应该抛出该异常，表示自己只打印了帮助，并没有
+    执行核心内容"""
+    pass
+
+
+class BluescanPlugin(ABC):
+    MAGIC_CLASSIFIER = 'Framework :: Bluescan :: Plugin'
+    
+    @abstractmethod
+    def __init__(self, argv: Union[list[str], None] = None) -> None:
+        """
+        如果只是创建了 plugin 实例，那么 clean() 不需要调用
+        """
+        pass
+
+    @abstractmethod
+    def prepare(self):
+        """
+        只要调用了 prepare()，那么 clean() 就必须被调用。
+        
+        如果一个 plugin 的某些初始化代码，会影响到其他 plugin 的创建，那么应该把它们写
+        在 prepare() 中，而不是 __init__() 中。"""
+        pass
+    
+    @abstractmethod
+    def run(self):
+        pass
+    
+    @abstractmethod
+    def clean(self):
+        pass
+    
+    @abstractmethod
+    def print_result(self):
+        pass
+
+
+def list_plugins():
+    for dist in pkg_resources.working_set:
+        pkg_name = dist.key
+        try:
+            if BluescanPlugin.MAGIC_CLASSIFIER in str(metadata(pkg_name)):
+                logger.debug("BluescanPlugin.MAGIC_CLASSIFIER: {}".format(BluescanPlugin.MAGIC_CLASSIFIER))
+                logger.debug("str(metadata(pkg_name): {}".format(str(metadata(pkg_name))))
+                print(pkg_name.replace('-', '_'), version(pkg_name))
+        except:
+            pass
+
+        # if 'meta' in str(metadata(pkg_name)):
+        #     version(pkg_name)
+
+        #     metadata(pkg_name)
+        #     print(pkg_name)
+
+        # try:
+        #     dist.get_metadata('METADATA'))
+        # except FileNotFoundError:
+        #     pass
+        
+        # print(distri.key)
+
+
+def install_plugin(whl_path: str):
+    # ps = subprocess.Popen(('pip', 'install', '--force-reinstalls', whl_path, '2>2&1'), stdout=subprocess.PIPE, shell=True)
+    # subprocess.check_output('grep -v "pip as the \'root\'"'.format(whl_path), shell=True)
+    
+    # 下面安装 plugin 的方法无法检测出，pip 的安装错误。因为在 check_output 中携带管道时，
+    # 当管道成功时不会包抛出异常。
+    # subprocess.check_output('pip install --force-reinstall --no-deps {} 2>&1 | grep -v "pip as the \'root\'"'.format(whl_path), shell=True)
+    
+    # --force-reinstall
+    #     该参数让 pip 在安装 package 时，无视 package 版本，强制重新安装它。这样做可以方
+    #     便开发者在不更改版本号的情况下，调试未发布的 plugin。
+    # --no-deps
+    #     该参数配合 --force-reinstall 使用，即在强制重装的过程中，不重新安装依赖包。这样
+    #     可以避免在重装过程中遇到 apt 安装的 python package 无法被 pip 卸载的问题。
+    try:
+        subprocess.check_output('pip install --force-reinstall --no-deps {}'.format(whl_path), shell=True)
+        logger.info("Installed {}".format(whl_path))
+    except CalledProcessError:
+        raise BluescanPluginInstallError(whl_path)
+
+
+def uninstall_plugin(name: str):
+    subprocess.check_output('pip uninstall -y {}  2>&1 | grep -v "pip as the \'root\'"'.format(name), shell=True)
+    logger.info("Uninstalled {}".format(name))
+
+
+def run_plugin(name: str, opts: str):
+    """
+    Parameters
+        name
+            plugin 的名字。
+            
+        opts
+            传给 plugin 的所有命令行选项，是一个单独的字符串。该字符串不包括 plugin 的名字本身。
+            就好像 plugin 单独运行时，少了 sys.argv[0] 的 sys.argv 一样。
+    """
+    if ' ' in name or ';' in name or '(' in name or ')' in name or '{' in name or \
+       '}' in name or ':' in name or '\'' in name or '"' in name or '.' in name or \
+       len(name) > 34:
+        raise ValueError("malicious name")
+    
+    # cmd = 'python -m {} {}'.format(name, opts)
+    # logger.debug("{}: {}".format(blue("run_plugin()"), cmd))
+    # try:
+        # subprocess.run(cmd, shell=True, check=True)
+    # except CalledProcessError as e:
+        # logger.error("{}".format(e))
+        
+    exec("import {}".format(name))
+    plugin__all__ = eval("{}.__all__".format(name))
+    for i in plugin__all__:
+        if issubclass(i, BluescanPlugin):
+            PluginSubclass = i
+            logger.debug("PluginSubclass: {}".format(i))
+    
+    try:
+        logger.debug("opts: {}".format(opts))
+        plugin = PluginSubclass(argv=opts)
+    except DocoptExit as e:
+        # 当不给 plugin 提供任何参数时，解析 plugin 参数的 docopt() 会抛出携带帮助信息的
+        # DocoptExit 异常。
+        logger.debug("{}".format(type(e)))
+        help_msg = str(e)
+        print(help_msg)
+    except BluescanPluginHelpException as e:
+        # print help doc of the plugin
+        # logger.debug("BluescanPluginHelpException, {}".format(type(e)))
+        print(e)
+    except Exception as e:
+        logger.error("{}: {}".format(e.__class__.__name__, e))
+        return
+        
+    try:
+        plugin.prepare()
+        plugin.run()
+    except Exception as e:
+        logger.error("{}: {}".format(e.__class__.__name__, e))
+    finally:
+        plugin.print_result()
+        plugin.clean()
