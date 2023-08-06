a26c6c6120db93a9c55e3ccfda52be02a1c0b47d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:04 +0000

    Add package home-frugal/0.0.0/home_frugal-0.0.0.tar.gz

---
+++ packages/home-frugal/home_frugal-0.0.0.tar.gz/home_frugal-0.0.0/setup.py
@@ -0,0 +1,15 @@
+# This package is empty, just a placeholder to prevent dependency
+# confusion attacks.
+
+from setuptools import setup
+
+setup(
+    name='home_frugal',
+    version='0.0.0',
+    description='Placeholder to prevent dependency confusion',
+    url='https://github.com/Workiva/',
+    author='Matthew Belisle',
+    author_email='matthew.belisle@workiva.com',
+    license='MIT',
+    packages=('home_frugal',),
+)
