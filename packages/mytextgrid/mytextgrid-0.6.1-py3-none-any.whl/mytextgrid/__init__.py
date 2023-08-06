fc732680c9a4b551db7ac17a1fdd8de51ae68dad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:24 +0000

    Add package mytextgrid/0.6.1/mytextgrid-0.6.1-py3-none-any.whl

---
+++ packages/mytextgrid/mytextgrid-0.6.1-py3-none-any.whl/mytextgrid/__init__.py
@@ -0,0 +1,6 @@
+"""A tiny python project for creating, reading, writing and querying Praat annotation files
+(TextGrid).
+"""
+from mytextgrid.core.textgrid import create_textgrid
+from mytextgrid.io.textgrid_in import read_from_file
+from mytextgrid.core.textgrid import TextGrid
