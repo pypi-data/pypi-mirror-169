663f8a1163686781e489765457f4a72f157014e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:43 +0000

    Add package tezos-etl/1.4.6/tezos-etl-1.4.6.tar.gz

---
+++ packages/tezos-etl/tezos-etl-1.4.6.tar.gz/tezos-etl-1.4.6/tezosetl/enums/operation_kinds.py
@@ -0,0 +1,40 @@
+class OperationKind:
+    activate_account = 'activate_account'
+    ballot = 'ballot'
+    delegation = 'delegation'
+    double_baking_evidence = 'double_baking_evidence'
+    double_endorsement_evidence = 'double_endorsement_evidence'
+    double_preendorsement_evidence = 'double_preendorsement_evidence'
+    endorsement = 'endorsement'
+    preendorsement = 'preendorsement'
+    endorsement_with_slot = 'endorsement_with_slot'
+    origination = 'origination'
+    proposals = 'proposals'
+    register_global_constant = 'register_global_constant'
+    reveal = 'reveal'
+    seed_nonce_revelation = 'seed_nonce_revelation'
+    set_deposits_limit = 'set_deposits_limit'
+    transaction = 'transaction'
+    increase_paid_storage = 'increase_paid_storage'
+    vdf_revelation = 'vdf_revelation'
+
+    ALL = [
+        activate_account,
+        ballot,
+        delegation,
+        double_baking_evidence,
+        double_endorsement_evidence,
+        double_preendorsement_evidence,
+        endorsement,
+        preendorsement,
+        endorsement_with_slot,
+        origination,
+        proposals,
+        register_global_constant,
+        reveal,
+        seed_nonce_revelation,
+        set_deposits_limit,
+        transaction,
+        increase_paid_storage,
+        vdf_revelation,
+    ]
