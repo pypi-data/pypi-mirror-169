aea162540d490323c4c95609732ec4d9d197f667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:27 +0000

    Add package sinai/0.0.27/sinai-0.0.27-py2.py3-none-any.whl

---
+++ packages/sinai/sinai-0.0.27-py2.py3-none-any.whl/sinai/rules/base.py
@@ -0,0 +1,18 @@
+"""A Rule takes data from sources, makes metrics and puts them into stores."""
+from __future__ import annotations
+
+from sinai.types import Evaluation, MonitorInstance, SourceClasses, StoreClasses
+
+
+class Rule:
+    """The sources and stores are defined in the rule, instantiated by the monitor."""
+
+    sources: SourceClasses = []
+    stores: StoreClasses = []
+
+    def __init__(self, monitor: MonitorInstance):
+        self.monitor = monitor
+
+    def evaluate(self) -> Evaluation:
+        """The evalutation function is called by the monitor, which stores any returned metrics."""
+        return None
