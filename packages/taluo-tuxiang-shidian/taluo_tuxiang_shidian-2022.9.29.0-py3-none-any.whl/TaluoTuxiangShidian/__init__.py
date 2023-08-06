264e735485e7dbdb8efd45ca0180dd439d0d50a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:32 +0000

    Add package taluo-tuxiang-shidian/2022.9.29.0/taluo_tuxiang_shidian-2022.9.29.0-py3-none-any.whl

---
+++ packages/taluo-tuxiang-shidian/taluo_tuxiang_shidian-2022.9.29.0-py3-none-any.whl/TaluoTuxiangShidian/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""taluo-tuxiang-shidian
+https://github.com/apachecn/taluo-tuxiang-shidian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.9.29.0"
