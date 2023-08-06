70adf25b785cdfb3df139bdd17473ae6e4ec41de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:03 +0000

    Add package PyOptionPane/0.15/PyOptionPane-0.15.tar.gz

---
+++ packages/PyOptionPane/PyOptionPane-0.15.tar.gz/PyOptionPane-0.15/PyOptionPane/__init__.py
@@ -0,0 +1,6 @@
+from PyOptionPane import TextBox
+from PyOptionPane import StringInput
+from PyOptionPane import IntInput
+from PyOptionPane import Message
+from PyOptionPane import Dropdown
+from PyOptionPane import YesOrNo
