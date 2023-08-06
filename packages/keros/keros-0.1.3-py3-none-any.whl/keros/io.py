9a66c9d86767baf002a6e767bc857446ebb3e45f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:32 +0000

    Add package keros/0.1.3/keros-0.1.3-py3-none-any.whl

---
+++ packages/keros/keros-0.1.3-py3-none-any.whl/keros/io.py
@@ -0,0 +1,34 @@
+import glob
+from .dataset import dataset
+
+class Io:
+
+    def read(path, batch_size=10):
+        """
+        Info:
+            Lê um conjunto de imagens de um diretório.
+        Params:
+            path (string): Caminho do diretório.
+            batch_size (int): Tamanho do lote de imagens.
+        return:
+            Objeto dataset.
+        """
+        batchs = {}
+        files = glob.glob(path)
+
+        if len(files) < batch_size:
+            raise Exception("O tamanho do lote é maior que o número de imagens no diretório.")
+
+        count = 0
+        for batch in range(0, len(files), batch_size):
+            batchs[str(count)] = files[batch:batch+batch_size]
+
+            count += 1
+
+        return dataset(batchs)
+
+        
+
+        
+
+
