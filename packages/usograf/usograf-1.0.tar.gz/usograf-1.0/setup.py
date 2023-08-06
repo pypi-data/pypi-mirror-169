813a76cdaa6fa4399d289c716bc8e5422e4ba398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:42 +0000

    Add package usograf/1.0/usograf-1.0.tar.gz

---
+++ packages/usograf/usograf-1.0.tar.gz/usograf-1.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from setuptools import setup
+
+
+# In[2]:
+
+
+setup(
+    name="usograf",
+    version="1.0",
+    description="Funciones para Graficos del curso para FISI UNMSM",
+    author="Jorge Guerra Guerra",
+    author_email="jguerra91@gmail.com",
+    url="https://dina.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador=12581",
+    packages=['usograf'],
+    scripts=[]
+)
+
+
+# In[ ]:
+
+
+
+
