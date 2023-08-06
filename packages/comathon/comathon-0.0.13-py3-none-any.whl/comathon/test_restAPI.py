8d1db9896cf496c670cac6682cdc4753bf425b39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:23 +0000

    Add package comathon/0.0.13/comathon-0.0.13-py3-none-any.whl

---
+++ packages/comathon/comathon-0.0.13-py3-none-any.whl/comathon/test_restAPI.py
@@ -0,0 +1,36 @@
+import requests
+import comathon as cmt
+import pyupbit
+
+
+print("Hello World")
+
+print("Get Bot List")
+
+
+pyupbit.get_tickers()
+
+print("code end")
+
+# cmt.get_tickers()
+
+
+# url = "http://121.137.95.97:8889/BotList"
+# response = requests.get(url)
+# print(response.json())
+
+# print("Call Bot 001")
+
+# url = "http://121.137.95.97:8889/BotWithinUserList?botid=BOT001"
+# response = requests.get(url)
+# print(response.json())
+
+
+# print("Call Bot 002")
+
+# url = "http://121.137.95.97:8889/BotWithinUserList?botid=BOT002"
+# response = requests.get(url)
+# print(response.json())
+
+# print("End")
+
