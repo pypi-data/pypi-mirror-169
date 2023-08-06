90f7d5bb03d975ab2b61f0970084d1ced449d258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:21 +0000

    Add package code-var/0.1.1/code_var-0.1.1.tar.gz

---
+++ packages/code-var/code_var-0.1.1.tar.gz/code_var-0.1.1/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+    ['code_var']
+
+package_data = \
+    {'': ['*']}
+
+install_requires = \
+    [
+        "bs4==0.0.1",
+        "requests==2.28.1",
+        "typer==0.6.1"
+    ]
+
+entry_points = \
+    {'console_scripts': ['var = code_var.cli:run']}
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup_kwargs = {
+    'name': 'code_var',
+    'version': '0.1.1',
+    'description': '"code-var is tools for varname in shell"',
+    "long_description": long_description,
+    "long_description_content_type": "text/markdown",
+    'author': 'LiPinCao',
+    'maintainer': None,
+    'maintainer_email': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6.2,<4.0.0',
+}
+
+setup(**setup_kwargs)
