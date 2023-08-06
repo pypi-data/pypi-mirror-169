fc46f7e531349a5478a1246252b95450e879d54f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:10 +0000

    Add package taskqtool/0.0.24/taskqtool-0.0.24-py3-none-any.whl

---
+++ packages/taskqtool/taskqtool-0.0.24-py3-none-any.whl/taskqtool/cli/task.py
@@ -0,0 +1,39 @@
+import json
+import time
+from argparse import ArgumentParser
+from pathlib import Path
+from pprint import pprint
+
+from ..api import addTask
+import uuid
+
+
+class CLICommand:
+    """提交队列
+    """
+
+    @staticmethod
+    def add_arguments(parser: ArgumentParser):
+        add = parser.add_argument
+        add("--scriptPath", default="./sub.sh", help="队列服务运行路径")
+        add("--shell", default="bash", help="队列脚本shell[bash,sh,python,...]")
+
+    @staticmethod
+    def run(args, parser):
+        scriptPath = Path(args.scriptPath).absolute()
+        shell = args.shell
+        submitTime = time.time()
+        pid = uuid.uuid1(clock_seq=int(submitTime))
+        running = False
+        res = addTask([{
+            "scriptPath": scriptPath.__str__(),
+            "shell": shell,
+            "submitTime": submitTime,
+            "pid": pid.int,
+            "running": running
+        }])
+        pprint(res)
+
+
+if __name__ == '__main__':
+    pass
