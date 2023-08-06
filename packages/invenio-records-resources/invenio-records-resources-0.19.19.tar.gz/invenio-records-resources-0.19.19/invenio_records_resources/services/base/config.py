3676633956cac3656bbbfa47dbe87a62b203f476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:24 +0000

    Add package invenio-records-resources/0.19.19/invenio-records-resources-0.19.19.tar.gz

---
+++ packages/invenio-records-resources/invenio-records-resources-0.19.19.tar.gz/invenio-records-resources-0.19.19/invenio_records_resources/services/base/config.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020-2022 CERN.
+# Copyright (C) 2020 Northwestern University.
+#
+# Invenio-Records-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Service API."""
+
+from invenio_records_permissions.policies import BasePermissionPolicy
+
+from .results import ServiceItemResult, ServiceListResult
+
+
+class ServiceConfig:
+    """Service Configuration."""
+
+    # Common configuration for all Services
+    service_id = None
+    permission_policy_cls = BasePermissionPolicy
+    result_item_cls = ServiceItemResult
+    result_list_cls = ServiceListResult
---
+++ packages/invenio-records-resources/invenio-records-resources-0.19.19.tar.gz/invenio-records-resources-0.19.19/tests/resources/test_resource_preference.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+# Copyright (C) 2020 Northwestern University.
+#
+# Invenio-Records-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Test preference."""
+
+from unittest import mock
+
+from invenio_records_resources.resources.records import resource
+
+
+def test_get_es_preference(app, client, headers, monkeypatch):
+    monkeypatch.setattr(resource, "es_preference", mock.MagicMock(return_value="test"))
+
+    h = {**headers, "User-Agent": "Chrome"}
+    environ = {"REMOTE_ADDR": "1.2.3.4"}
+    r = client.get("/mocks", headers=h, environ_overrides=environ)
+
+    assert resource.es_preference.called
