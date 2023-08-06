ca8eb8597d9f40e672cd268a1bad82bb83433389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:16 +0000

    Add package chae-probability/1.2/chae_probability-1.2.tar.gz

---
+++ packages/chae-probability/chae_probability-1.2.tar.gz/chae_probability-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='chae_probability',
+      version='1.2',
+      description='Gaussian and Binomisl distributions',
+      packages=['chae_probability'],
+      author='Cesar Acuna',
+      author_email='acuna.cesar.h@gmail.com',
+      zip_safe=False)
