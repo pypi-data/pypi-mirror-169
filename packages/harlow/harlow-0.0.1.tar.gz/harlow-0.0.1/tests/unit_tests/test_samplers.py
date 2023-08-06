8fda56798a0ad83fc7ee5c385d87d7784574d471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:30 +0000

    Add package harlow/0.0.1/harlow-0.0.1.tar.gz

---
+++ packages/harlow/harlow-0.0.1.tar.gz/harlow-0.0.1/tests/unit_tests/test_samplers.py
@@ -0,0 +1,6 @@
+"""
+Sampler unit tests, mostly to ensure that all samplers:
+
+    * Correctly inherit from the `Sampler` abstract class
+    * Can be initialized with the same set of keyword arguments
+"""
