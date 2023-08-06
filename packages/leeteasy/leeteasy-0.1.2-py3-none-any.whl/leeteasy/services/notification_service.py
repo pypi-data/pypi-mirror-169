d1d680a5fffbc9fce78a70b88e4be467ec6cf772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:50 +0000

    Add package leeteasy/0.1.2/leeteasy-0.1.2-py3-none-any.whl

---
+++ packages/leeteasy/leeteasy-0.1.2-py3-none-any.whl/leeteasy/services/notification_service.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+from notifypy import Notify
+
+from leeteasy.services.request_handler import RequestHandler
+from leeteasy.services.request_parser import RequestParser
+
+
+class Notifier:
+    """Handles notification related functionalities."""
+
+    target_difficulty = ['Easy']
+    app_name = 'LeetEasy'
+    challenge = None
+
+    @classmethod
+    def prepare_notification(cls):
+        """Prepares notification msg and triggers notification."""
+        challenge_info = RequestHandler.get_challenge_info()
+        cls.challenge = RequestParser.parse(challenge_info)
+        if cls.challenge.difficulty in cls.target_difficulty:
+            return '{0}\nLink: {1}'.format(
+                cls.challenge.title,
+                cls.challenge.problem_link,
+            )
+
+    @classmethod
+    def notify(cls):
+        notification = Notify()
+        notification.message = cls.prepare_notification()
+        notification.title = f'{cls.app_name} - {cls.challenge.difficulty} ' \
+                             f'Problem Alert \U0001F514'
+        resource_path = Path(__file__).parent.parent / 'assets/leetcoin.png'
+        notification.icon = resource_path
+        notification.send()
