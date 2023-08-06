2b67cc7682124c447079c40aa445bd4be6a0dc1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:47 +0000

    Add package pytiers/2.1.15/pytiers-2.1.15.tar.gz

---
+++ packages/pytiers/pytiers-2.1.15.tar.gz/pytiers-2.1.15/pytiers/by.py
@@ -0,0 +1,7 @@
+"""Methods for referenceing Point objects: by.TIME, by.INDEX, by.POINT\nEssentially integers.\nUsage example:\n pytiers.get_point(1, by.INDEX)\n returns the point in the Tier object with the #1 index."""
+
+TIME = 0
+INDEX = 1
+POINT = 2
+
+by_methods = [0, 1, 2]
