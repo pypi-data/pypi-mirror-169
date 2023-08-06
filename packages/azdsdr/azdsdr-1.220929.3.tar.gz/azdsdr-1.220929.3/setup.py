93ecdcd7bbc3d691c7d79916294649e924ba6e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:00 +0000

    Add package azdsdr/1.220929.3/azdsdr-1.220929.3.tar.gz

---
+++ packages/azdsdr/azdsdr-1.220929.3.tar.gz/azdsdr-1.220929.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='azdsdr',
+    version='1.220929.3',
+    license='Apache License',
+    author="Andrew Zhu",
+    author_email='xhinker@hotmail.com',
+    packages=find_packages('src/azdsdr'),
+    package_dir={'': 'src/azdsdr'},
+    url='https://github.com/xhinker/azdsdr',
+    keywords='DS Data Reader',
+    install_requires=[
+          'pandas'
+          ,'pyodbc'
+      ],
+)
