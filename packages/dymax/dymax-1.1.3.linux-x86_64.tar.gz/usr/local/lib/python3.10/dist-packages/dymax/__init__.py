d5d837219b43a7a7b361f3be799ddc4456f74712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:08 +0000

    Add package dymax/1.1.3/dymax-1.1.3.linux-x86_64.tar.gz

---
+++ packages/dymax/dymax-1.1.3.linux-x86_64.tar.gz/usr/local/lib/python3.10/dist-packages/dymax/__init__.py
@@ -0,0 +1,7 @@
+'''dymax converts (Lon, Lat) to (X,Y) Dymaxion Fuller Projection'''
+__version__ = '1.1.3'
+
+from .constants import *
+from .convert import *
+from .examples import *
+from .io import *
