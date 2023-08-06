116e675c785aca2286cada1d5721caa38aeb8f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:01 +0000

    Add package prymbr/0.0.9.2/prymbr-0.0.9.2.tar.gz

---
+++ packages/prymbr/prymbr-0.0.9.2.tar.gz/prymbr-0.0.9.2/src/prymbr/__init__.py
@@ -0,0 +1,9 @@
+from types import NoneType
+
+from .prymbr import isprime
+from .prymbr import decomp
+from .prymbr import pgcd, gcd
+from .prymbr import ppcm, lcm
+from .prymbr import frac
+from .prymbr import simp
+from .prymbr import deci
