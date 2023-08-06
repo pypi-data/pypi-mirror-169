97e40b8c82a0995eae67ad525e6b03c4e01f0139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:19 +0000

    Add package dbtjumpstart/0.1.4/dbtjumpstart-0.1.4-py3-none-any.whl

---
+++ packages/dbtjumpstart/dbtjumpstart-0.1.4-py3-none-any.whl/dbtjumpstart/setup.py
@@ -0,0 +1,31 @@
+# from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name='dbtjumpstart',
+    packages=['dbtjumpstart'],
+    version='0.1.4',
+    license='MIT',
+    description='A package to jumpstart a dbt project',
+    author='Hari Prasad SA',
+    author_email='sahariprasad@outlook.com',
+    url='https://github.com/sahariprasad/dbt-jumpstart',
+    download_url='https://github.com/sahariprasad/dbt-jumpstart/archive/refs/tags/v0.1.tar.gz',
+    keywords=['dbt', 'jumpstart'],
+    install_requires=[
+        'pandas',
+        'openpyxl',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
