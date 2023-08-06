b599634957cc9e3eda7c4579b591cb7b880621ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:21 +0000

    Add package pgrwpy/0.1.1/pgrwpy-0.1.1-py3-none-any.whl

---
+++ packages/pgrwpy/pgrwpy-0.1.1-py3-none-any.whl/pgrwpy/constants/url.py
@@ -0,0 +1,8 @@
+class URL(object):
+    SANDBOX_BASE_URL = 'https://devtopup.pgecom.com/api/'
+    PRODUCTION_BASE_URL = 'https://pgecom.com/api/'
+    RESOLVE = 'https://help.pgecom.com/en/'
+    MONCASH = "moncash/token"
+    TOKEN = "token"
+    ORDER = "order/"
+    
