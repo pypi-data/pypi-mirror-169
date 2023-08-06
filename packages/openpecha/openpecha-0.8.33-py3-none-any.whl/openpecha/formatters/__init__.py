ce7696b2f3a0912fbc7d997abc71810748eadfaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:32 +0000

    Add package openpecha/0.8.33/openpecha-0.8.33-py3-none-any.whl

---
+++ packages/openpecha/openpecha-0.8.33-py3-none-any.whl/openpecha/formatters/__init__.py
@@ -0,0 +1,6 @@
+from .empty import EmptyEbook
+from .formatter import BaseFormatter
+from .google_vision import GoogleVisionFormatter
+from .hfml import HFMLFormatter
+from .tsadra import TsadraFormatter
+from .pedurma import PedurmaFormatter
