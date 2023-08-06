96c873582a0512557355c37e2a81d55b770cbe76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:13 +0000

    Add package wdata-sdk/0.0.0/wdata-sdk-0.0.0.tar.gz

---
+++ packages/wdata-sdk/wdata-sdk-0.0.0.tar.gz/wdata-sdk-0.0.0/setup.py
@@ -0,0 +1,15 @@
+# This package is empty, just a placeholder to prevent dependency
+# confusion attacks.
+
+from setuptools import setup
+
+setup(
+    name='wdata-sdk',
+    version='0.0.0',
+    description='Placeholder to prevent dependency confusion',
+    url='https://github.com/Workiva/',
+    author='Matthew Belisle',
+    author_email='matthew.belisle@workiva.com',
+    license='MIT',
+    packages=('wdata-sdk',),
+)
