ddb0ef6fb85e572a1b44da7c3c192e8631e9553c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:52 +0000

    Add package CircuitoPograma/1.0.0/CircuitoPograma-1.0.0.tar.gz

---
+++ packages/CircuitoPograma/CircuitoPograma-1.0.0.tar.gz/CircuitoPograma-1.0.0/README.md
@@ -0,0 +1,4 @@
+#Este proyecto se baso en este ejercicio que aparece y pudimos hacer que cada persona la cual tenga este mismo ejercicio pero diferente numeros pueda allar, el resultado.
+[![Whats-App-Image-2022-09-24-at-12-20-36-AM.jpg](https://i.postimg.cc/43KxX4ZS/Whats-App-Image-2022-09-24-at-12-20-36-AM.jpg)](https://postimg.cc/QB33b3FQ)
+
+
---
+++ packages/CircuitoPograma/CircuitoPograma-1.0.0.tar.gz/CircuitoPograma-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+readme = open("./README.md", "r")
+
+
+setup(
+    name='CircuitoPograma',
+    packages=['CircuitoPograma'],  # this must be the same as the name above
+    version='1.0.0',
+    description='Esta es la descripcion de mi paquete',
+    long_description=readme.read(),
+    long_description_content_type='text/markdown',
+    author='Cristian Rivera, Marcos David, Keren Pajon',
+    author_email='',
+    # use the URL to the github repo
+    url='https://github.com/CristianR746/CircuitoPrograma.git',
+    download_url='https://github.com/CristianR746/CircuitoPrograma.git',
+    keywords=['testing', 'logging', 'example'],
+    classifiers=[ ],
+    license='MIT',
+    include_package_data=True
+)
+
+
+
