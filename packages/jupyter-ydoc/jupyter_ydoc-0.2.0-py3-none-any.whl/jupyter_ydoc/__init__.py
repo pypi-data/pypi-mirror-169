3d1e7a4d17b22850c2da0e6f04059ac1f1c3af1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:33 +0000

    Add package jupyter-ydoc/0.2.0/jupyter_ydoc-0.2.0-py3-none-any.whl

---
+++ packages/jupyter-ydoc/jupyter_ydoc-0.2.0-py3-none-any.whl/jupyter_ydoc/__init__.py
@@ -0,0 +1,7 @@
+import pkg_resources
+
+from .ydoc import YFile, YNotebook  # noqa
+
+ydocs = {ep.name: ep.load() for ep in pkg_resources.iter_entry_points(group="jupyter_ydoc")}
+
+__version__ = "0.2.0"
