8126489b62498391ab2b81417bd472fd9e2e53b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:42 +0000

    Add package hts-forecast/0.0.1/hts_forecast-0.0.1.tar.gz

---
+++ packages/hts-forecast/hts_forecast-0.0.1.tar.gz/hts_forecast-0.0.1/README.md
@@ -0,0 +1,19 @@
+# Auto Hierarchical Time Series Forecasting
+
+## Installation
+Few modules used in this package are not supported by python >= 3.9.* . So, we recommend you to create an virtual environment with python=3.8.* and run requirements.txt file 
+
+Step 1 :
+Create virtual environment using conda
+```bash
+conda create -n auto_hts python=3.8
+```
+Step 2 :
+```bash
+pip install -r requirements.txt
+```
+Step 3 :
+```bash
+pip install auto_hts_forecast
+```
+## Usage
---
+++ packages/hts-forecast/hts_forecast-0.0.1.tar.gz/hts_forecast-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name = "hts_forecast",
+    version = "0.0.1",
+    author = "Ishita Roy,  Rama Chaitanya Karanam",
+    author_email = "ramachaitanya0@gmail.com",
+    description = 'AUTO HIERARCHIAL TIME SERIES FORECASTING',
+    long_description = README,
+    long_description_content_type = 'text/markdown',
+    # packages = find_packages(),
+    license="MIT",
+    py_modules=["hts_forecast"],
+    package_dir={"":"src"},
+    install_requires=["pystan==2.19.1.1","scikit_learn==1.0.2","scikit-hts==0.5.12","arch==5.2.0","pmdarima==1.8.5","fbprophet==0.7.1"],
+    url = "https://github.com/ramachaitanya0/auto_hts_package",
+
+    classifiers = [ 
+        "Programming Language :: Python :: 3 ",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ] 
+       
+    
+)
