d6326b7f406030a7b12c86b2f865f909651f2dd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:18 +0000

    Add package tuote-taluo-xuexi-shouce/2022.9.29.0/tuote-taluo-xuexi-shouce-2022.9.29.0.tar.gz

---
+++ packages/tuote-taluo-xuexi-shouce/tuote-taluo-xuexi-shouce-2022.9.29.0.tar.gz/tuote-taluo-xuexi-shouce-2022.9.29.0/README.md
@@ -0,0 +1,27 @@
+# 托特塔罗学习手册
+
+## 下载
+
+### Docker
+
+```
+docker pull apachecn0/tuote-taluo-xuexi-shouce
+docker run -tid -p <port>:80 apachecn0/tuote-taluo-xuexi-shouce
+# 访问 http://localhost:{port} 查看文档
+```
+
+### PYPI
+
+```
+pip install tuote-taluo-xuexi-shouce
+tuote-taluo-xuexi-shouce <port>
+# 访问 http://localhost:{port} 查看文档
+```
+
+### NPM
+
+```
+npm install -g tuote-taluo-xuexi-shouce
+tuote-taluo-xuexi-shouce <port>
+# 访问 http://localhost:{port} 查看文档
+```
---
+++ packages/tuote-taluo-xuexi-shouce/tuote-taluo-xuexi-shouce-2022.9.29.0.tar.gz/tuote-taluo-xuexi-shouce-2022.9.29.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import TuoteTaluoXuexiShouce
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('TuoteTaluoXuexiShouce'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="tuote-taluo-xuexi-shouce",
+    version=TuoteTaluoXuexiShouce.__version__,
+    url="https://github.com/apachecn/tuote-taluo-xuexi-shouce",
+    author=TuoteTaluoXuexiShouce.__author__,
+    author_email=TuoteTaluoXuexiShouce.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="托特塔罗学习手册",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "tuote-taluo-xuexi-shouce=TuoteTaluoXuexiShouce.__main__:main",
+            "TuoteTaluoXuexiShouce=TuoteTaluoXuexiShouce.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
