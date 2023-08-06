eacdd517ca27d81bd93c106fc34a8813d0e7bbb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:15 +0000

    Add package Antonina1/1.0.1/Antonina1-1.0.1-py3-none-any.whl

---
+++ packages/Antonina1/Antonina1-1.0.1-py3-none-any.whl/Antonina/stpop.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 29 10:23:19 2022
+
+@author: Jinqk
+"""
+
+_version_ = '1.0.1'
+import PySimpleGUI as sg
+def stpop():
+    title = '安冬妮娜小助手'
+    sg.popup_ok('又要上班了吗？',title =title,keep_on_top = True,grab_anywhere = True,image = '.\\Antonina2.png')
+    
+
+    
