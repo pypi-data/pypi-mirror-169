c5e5e59bbb2d5411c859398cd05f12fcd9aa6ada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:28 +0000

    Add package bonesinger/0.2.4/bonesinger-0.2.4-py3.10.egg

---
+++ packages/bonesinger/bonesinger-0.2.4-py3.10.egg/bonesinger/util.py
@@ -0,0 +1,26 @@
+
+def strong_key_format(line, keys):
+    for key, value in keys.items():
+        line = line.replace("{{" + key + "}}", value)
+    return line
+
+
+def merge_dicts(*dict_args):
+    result = {}
+    for dictionary in dict_args:
+        result.update(dictionary)
+    return result
+
+
+def merge_dicts_and_lists(*dict_args):
+    """ Deep merge dicts common way, but extend lists if it has some keys. """
+    result = {}
+    for dictionary in dict_args:
+        for key, value in dictionary.items():
+            if key in result and isinstance(result[key], dict):
+                result[key] = merge_dicts(result[key], value)
+            elif key in result and isinstance(result[key], list):
+                result[key] = result[key] + value
+            else:
+                result[key] = value
+    return result
