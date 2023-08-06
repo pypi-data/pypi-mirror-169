b986c1f7389f3f64e650428437cb45bdb8392627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:18 +0000

    Add package feature-store-bundle/2.8.1/feature_store_bundle-2.8.1-py3-none-any.whl

---
+++ packages/feature-store-bundle/feature_store_bundle-2.8.1-py3-none-any.whl/featurestorebundle/notebook/decorator/tests/feature_test.py
@@ -0,0 +1,68 @@
+import os
+from pyspark.sql import types as t
+from daipecore.decorator.DecoratedDecorator import DecoratedDecorator
+from daipecore.decorator.notebook_function import notebook_function
+from featurestorebundle.entity.Entity import Entity
+from featurestorebundle.feature.Feature import Feature
+from featurestorebundle.notebook.decorator.feature import feature
+
+os.environ["APP_ENV"] = "test"
+
+
+class FakeSchema:
+    json = {
+        "fields": [
+            {"name": "client_id", "type": "long", "metadata": {}},
+            {"name": "timestamp", "type": "timestamp", "metadata": {}},
+            {"name": "my_sample_feature", "type": "long", "metadata": {}},
+        ],
+    }
+
+    @staticmethod
+    def jsonValue():  # noqa # pylint: disable=invalid-name
+        return FakeSchema.json
+
+
+class FakeResult:
+
+    columns = ["client_id", "timestamp", "my_sample_feature"]
+
+    def __init__(self, value):
+        self.value = value
+        self.schema = FakeSchema
+
+    def select(self, *args, **kwargs):  # noqa # pylint: disable=unused-argument
+        return self
+
+    def fillna(self, *args, **kwargs):  # noqa # pylint: disable=unused-argument
+        return self
+
+
+entity = Entity(
+    name="client_test",
+    id_column="client_id",
+    id_column_type=t.StringType(),
+    time_column="timestamp",
+    time_column_type=t.TimestampType(),
+)
+
+
+@DecoratedDecorator
+class client_feature(feature):  # noqa # pylint: disable=invalid-name
+    def __init__(self, *args, category=None):
+        super().__init__(*args, entity=entity, category=category)
+
+
+expected_value = FakeResult("not_a_real_dataframe")
+
+try:
+
+    @notebook_function()
+    @client_feature(Feature("my_sample_feature", "my_sample_description", 0), category="test")
+    def my_sample_feature():
+        return expected_value
+
+    assert expected_value == my_sample_feature.result
+
+except ModuleNotFoundError as e:
+    assert str(e) == "No module named 'IPython'"
