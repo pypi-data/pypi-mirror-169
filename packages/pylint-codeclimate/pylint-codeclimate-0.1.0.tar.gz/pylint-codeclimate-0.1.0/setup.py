75956e13f520188546306a77f29d72e03c49f3c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:53 +0000

    Add package pylint-codeclimate/0.1.0/pylint-codeclimate-0.1.0.tar.gz

---
+++ packages/pylint-codeclimate/pylint-codeclimate-0.1.0.tar.gz/pylint-codeclimate-0.1.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+
+from setuptools import setup
+
+#with open("requirements.txt", "r", encoding="UTF-8") as fd:
+#    requirements = fd.read().splitlines()
+
+setup(
+#    install_requires=requirements,
+    entry_points={
+    },
+)
