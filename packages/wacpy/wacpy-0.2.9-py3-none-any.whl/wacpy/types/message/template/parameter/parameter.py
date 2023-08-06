73c177f4f63b728c4081744a05f49e7f0e4c6c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:52 +0000

    Add package wacpy/0.2.9/wacpy-0.2.9-py3-none-any.whl

---
+++ packages/wacpy/wacpy-0.2.9-py3-none-any.whl/wacpy/types/message/template/parameter/parameter.py
@@ -0,0 +1,34 @@
+from dataclasses_json import config, dataclass_json
+from dataclasses import dataclass, field
+from typing import Optional
+
+
+@dataclass_json
+@dataclass
+class Parameter:
+    type: str
+    """
+    Required.
+
+    Describes the parameter type.
+
+    Supported Options:
+        • text
+        • currency
+        • date_time
+        • image
+        • document
+        • video
+        
+    For text-based templates, the only supported parameter types are text, currency, date_time.
+    """
+
+    text: Optional[str] = field(default=None, metadata=config(exclude=lambda f: f is None))
+    """
+    Required when type=text.
+
+    The message's text.
+
+    For the header component, the character limit is 60 characters.
+    For the body component, the character limit is 1024 characters.
+    """
