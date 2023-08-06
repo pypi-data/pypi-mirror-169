d04a7108e9ed811abfb734380f3ae14948149624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:09 +0000

    Add package rankings-UI/1.4/rankings_UI-1.4-py3-none-any.whl

---
+++ packages/rankings-UI/rankings_UI-1.4-py3-none-any.whl/rankings_UI/__init__.py
@@ -0,0 +1,5 @@
+from rankings_UI.Proposal import Proposals
+from rankings_UI.Ranking import Rankings
+from rankings_UI.Review import Reviews
+from rankings_UI.Reviewer import Reviewers
+from rankings_UI.GUI import GUI
