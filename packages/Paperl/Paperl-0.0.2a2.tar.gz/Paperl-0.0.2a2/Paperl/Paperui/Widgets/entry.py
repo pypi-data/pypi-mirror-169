946bd018f445b0bcff7d579935fc755e8989cb9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:04 +0000

    Add package Paperl/0.0.2a2/Paperl-0.0.2a2.tar.gz

---
+++ packages/Paperl/Paperl-0.0.2a2.tar.gz/Paperl-0.0.2a2/Paperl/Paperui/Widgets/entry.py
@@ -0,0 +1,19 @@
+from Paperl.Paperui.Widgets.widget import Widget
+
+
+class Entry(Widget):
+    def __init__(self, parent: Widget, text: str = ""):
+        self.build(parent.Me, text)
+
+    def build(self, parent: Widget, text: str = ""):
+        from tkinter.ttk import Entry
+        from tkinter import StringVar
+        self.Var = StringVar()
+        self.Var.set(text)
+        self.Me = Entry(parent, textvariable=text)
+
+    def setText(self, text: str):
+        self.Var.set(text)
+
+    def getText(self):
+        return self.Var.get()
