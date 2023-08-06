3f373ec2c6116d4c66d5f798dffa98e36ca0c102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:26 +0000

    Add package codex/0.13.1a5/codex-0.13.1a5-py3-none-any.whl

---
+++ packages/codex/codex-0.13.1a5-py3-none-any.whl/codex/static_root/assets/placeholder-loading.db5009c0.164e6da5a5f3.js
@@ -0,0 +1,1 @@
+import{n as r}from"./main.88f99a16.js";import{az as a}from"./button.ebd6f435.js";const o={name:"PlaceholderLoading",props:{size:{type:Number,default:128}}};var l=function(){var e=this,n=e._self._c;return n(a,{attrs:{indeterminate:"",size:e.size,color:"#cc7b19","aria-label":"loading"}})},s=[],t=r(o,l,s,!1,null,null,null,null);const m=t.exports;export{m as P};
