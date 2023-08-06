fe9f2ca059b0e47ab444f840f2521da639a3d327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:05 +0000

    Add package mengzi-zero-shot/0.2.1/mengzi_zero_shot-0.2.1-py3-none-any.whl

---
+++ packages/mengzi-zero-shot/mengzi_zero_shot-0.2.1-py3-none-any.whl/mengzi_zs/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding=utf-8 -*-
+"""
+# library: mengzi-zero-shot
+# author: huajingyun
+# license: Apache License 2.0
+# email: huajingyun@chuagnxin.com
+# github: https://github.com/Langboat/Mengzi
+# description: NLU & NLG (zero-shot) depend on mengzi-t5-base-mt pretrained model
+# website: www.langboat.com
+"""
+
+from .inference import MengziZeroShot
+
+__all__ = ['MengziZeroShot']
+__version__ = '0.1.0'
---
+++ packages/mengzi-zero-shot/mengzi_zero_shot-0.2.1-py3-none-any.whl/mengzi_zs/inference.py
@@ -0,0 +1,44 @@
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+from collections import Counter
+from .prompts import create_input_with_prompt
+
+
+class MengziZeroShot(object):
+    def __init__(self):
+        pass
+
+    # def prompt_map(self, task_type, input_string, label_list=None):
+    def load(self, model_name="Langboat/mengzi-t5-base-mt"):
+        # load model
+        self.model_name = model_name
+        self.tokenizer = T5Tokenizer.from_pretrained(self.model_name)
+        self.model = T5ForConditionalGeneration.from_pretrained(
+            self.model_name)
+
+    def token_decode(self, s):
+        return self.tokenizer.decode(s, skip_special_tokens=True)
+
+    def pick_most_common(self, x: list) -> str:
+        return Counter(x).most_common(1)[0][0]
+
+    def inference(self, task_type, input_string, input_string2=None, entity1=None, entity2=None):
+        # make input text
+        input_text = create_input_with_prompt(
+            task_type, input_string, input_string2, entity1, entity2)
+        # tokenize
+        encodings = self.tokenizer(
+            input_text, max_length=512, padding=True, return_tensors="pt")
+        # print('encodings', encodings)
+
+        # model inference
+        outputs = self.model.generate(
+            encodings['input_ids'], attention_mask=encodings['attention_mask'], max_length=512, num_beams=1)
+        # print('outputs', outputs)
+
+        # decode model output
+        dec_out = list(map(self.token_decode, outputs))
+
+        # print("dec_out: ", dec_out)
+        # return result to web
+
+        return self.pick_most_common(dec_out)
