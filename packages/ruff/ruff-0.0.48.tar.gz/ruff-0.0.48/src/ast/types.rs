d6499a861d28cbe38e1ddc34526588c7c313e702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:02 +0000

    Add package ruff/0.0.48/ruff-0.0.48.tar.gz

---
+++ packages/ruff/ruff-0.0.48.tar.gz/ruff-0.0.48/src/ast/types.rs
@@ -0,0 +1,71 @@
+use std::collections::BTreeMap;
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+use rustpython_parser::ast::Location;
+
+fn id() -> usize {
+    static COUNTER: AtomicUsize = AtomicUsize::new(1);
+    COUNTER.fetch_add(1, Ordering::Relaxed)
+}
+
+#[derive(Clone, Debug, Default)]
+pub struct FunctionScope {
+    pub uses_locals: bool,
+}
+
+#[derive(Clone, Debug)]
+pub enum ScopeKind {
+    Class,
+    Function(FunctionScope),
+    Generator,
+    Module,
+}
+
+#[derive(Clone, Debug)]
+pub struct Scope {
+    pub id: usize,
+    pub kind: ScopeKind,
+    pub import_starred: bool,
+    pub values: BTreeMap<String, Binding>,
+}
+
+impl Scope {
+    pub fn new(kind: ScopeKind) -> Self {
+        Scope {
+            id: id(),
+            kind,
+            import_starred: false,
+            values: BTreeMap::new(),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub enum BindingKind {
+    Annotation,
+    Argument,
+    Assignment,
+    Binding,
+    LoopVar,
+    Builtin,
+    ClassDefinition,
+    Definition,
+    Export(Vec<String>),
+    FutureImportation,
+    Importation(String),
+    StarImportation,
+    SubmoduleImportation(String),
+}
+
+#[derive(Clone, Debug)]
+pub struct Binding {
+    pub kind: BindingKind,
+    pub location: Location,
+    /// Tuple of (scope index, location) indicating the scope and location at which the binding was
+    /// last used.
+    pub used: Option<(usize, Location)>,
+}
+
+pub trait CheckLocator {
+    fn locate_check(&self, default: Location) -> Location;
+}
