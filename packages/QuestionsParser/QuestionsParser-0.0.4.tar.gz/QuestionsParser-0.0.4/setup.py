7007ef0edc10c07d19bb1c43e819f718adb77ec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:36 +0000

    Add package QuestionsParser/0.0.4/QuestionsParser-0.0.4.tar.gz

---
+++ packages/QuestionsParser/QuestionsParser-0.0.4.tar.gz/QuestionsParser-0.0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+if __name__ == '__main__':
+    setup(
+        name='QuestionsParser',
+        packages=find_packages(),
+        version='0.0.4',
+        description='A Python library for parsing questions from pdf files',
+        author='Andrew Yaroshevych',
+        install_requires=[
+            'regex==2022.9.13',
+            'cloudscraper==1.2.64',
+            'beautifulsoup4==4.11.1',
+            'pypdf2==2.11.0',
+            'fpdf==1.7.2',
+            'python-docx==0.8.11'
+        ],
+    )
