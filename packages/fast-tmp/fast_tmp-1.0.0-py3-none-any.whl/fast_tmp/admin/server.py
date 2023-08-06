9acbc10f9219e63c30e91d44bb2b44ffada867e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:34 +0000

    Add package fast-tmp/1.0.0/fast_tmp-1.0.0-py3-none-any.whl

---
+++ packages/fast-tmp/fast_tmp-1.0.0-py3-none-any.whl/fast_tmp/admin/server.py
@@ -0,0 +1,119 @@
+import os
+from datetime import timedelta
+from typing import Optional
+
+from fastapi import Depends, FastAPI, Form, Request
+from fastapi.templating import Jinja2Templates
+from starlette import status
+from starlette.responses import RedirectResponse
+from tortoise.exceptions import BaseORMException
+
+from fast_tmp.admin.site import GroupAdmin, UserAdmin
+from fast_tmp.conf import settings
+from fast_tmp.models import User
+from fast_tmp.responses import BaseRes, FastTmpError
+from fast_tmp.site import model_list, register_model_site
+from fast_tmp.utils.token import create_access_token
+
+from ..jinja_extension.tags import register_tags
+from .depends import get_user
+from .endpoint import router
+from .exception_handlers import fasttmp_exception_handler, tortoise_exception_handler
+
+base_path = os.path.dirname(__file__)
+templates = Jinja2Templates(directory=base_path + "/templates")
+register_tags(templates)
+admin = FastAPI(title="fast-tmp")
+register_model_site({"Auth": [UserAdmin(), GroupAdmin()]})
+admin.include_router(router)
+
+admin.exception_handler(FastTmpError)(fasttmp_exception_handler)
+admin.exception_handler(BaseORMException)(tortoise_exception_handler)
+
+
+@admin.post("/", name="index", dependencies=[Depends(get_user)])
+@admin.get("/", name="index", dependencies=[Depends(get_user)])
+async def index(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "title": admin.title},
+    )
+
+
+@admin.post("/login", name="login")
+async def login(
+    request: Request,
+    username: Optional[str] = Form(None),
+    password: Optional[str] = Form(None),
+):
+    context = {
+        "request": request,
+        "errinfo": "",
+        "username_err": False,
+        "password_err": False,
+    }
+    if not username:
+        context["username_err"] = True
+        return templates.TemplateResponse("login.html", context)
+    if not password:
+        context["password_err"] = True
+        return templates.TemplateResponse("login.html", context)
+    user = await User.filter(username=username).first()
+    if not user or not user.check_password(password) or not user.is_active:
+        context["errinfo"] = "username or password error!"
+        return templates.TemplateResponse("login.html", context)
+
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username, "id": user.pk}, expires_delta=access_token_expires
+    )
+    res = RedirectResponse(
+        request.url_for("admin:index"),
+        status_code=status.HTTP_302_FOUND,
+    )
+    res.set_cookie("access_token", access_token, expires=settings.ACCESS_TOKEN_EXPIRE_MINUTES * 60)
+    return res
+
+
+@admin.get("/login", name="login")
+def login_html(request: Request):
+    context = {
+        "request": request,
+        "errinfo": "",
+        "username_err": False,
+        "password_err": False,
+    }
+    return templates.TemplateResponse("login.html", context)
+
+
+@admin.get("/logout", name="logout")
+def logout(request: Request):
+    context = {
+        "request": request,
+        "errinfo": "",
+        "username_err": False,
+        "password_err": False,
+    }
+    res = templates.TemplateResponse("login.html", context)
+    res.delete_cookie("access_token")
+    return res
+
+
+@admin.get("/site", dependencies=[Depends(get_user)])
+def get_site(request: Request):
+    pages = []
+    for name, ml in model_list.items():  # todo add home page
+        pages.append(  # todo 增加权限控制，确认对应的页面
+            {
+                "label": name,
+                "children": [
+                    {
+                        "label": model.name(),
+                        "url": model.name(),
+                        "schemaApi": model.name() + "/schema",
+                    }
+                    for model in ml
+                ],
+            }
+        )
+    return BaseRes(data={"pages": pages})
---
+++ packages/fast-tmp/fast_tmp-1.0.0-py3-none-any.whl/fast_tmp/conf/__init__.py
@@ -0,0 +1,85 @@
+import importlib
+import os
+import sys
+from typing import Any, Dict, List, Optional, Union
+
+from pydantic import AnyHttpUrl, BaseSettings, validator
+
+from fast_tmp.utils.db import init_model
+
+FASTAPI_VARIABLE = "FASTAPI_SETTINGS_MODULE"
+
+
+class Settings(BaseSettings):
+    SECRET_KEY: str = ""
+    ALGORITHM: str = "HS256"
+    AUTH_USER_MODEL_NAME: str = "User"
+    AUTH_APP_NAME: str = "fast_tmp"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    FASTAPI_SETTINGS_MODULE: str
+    DEBUG: bool = True
+    LOGIN_URL: str = "/api-token-auth"
+    STATIC_ROOT = "static"
+    STATIC_PATH = "static"
+    LOCAL_FILE: bool = True
+
+    @validator("DEBUG", pre=True, allow_reuse=True)
+    def get_debug(cls, v: str) -> bool:
+        if isinstance(v, str):
+            if v != "True":
+                return False
+        return True
+
+    @validator("BACKEND_CORS_ORIGINS", pre=True, allow_reuse=True)
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
+
+    DB_TYPE: str = "mysql"
+    TORTOISE_ORM: Optional[Dict[str, Any]] = None
+    # 额外的配置信息
+    EXTRA_SETTINGS: Dict[str, Any] = {}
+
+    # cas相关配置
+    CAS_SERVER_URL: str = "/cas/"
+
+    class Config:
+        case_sensitive = True
+        env_file = ".env"
+
+    def __init__(self):
+        super(Settings, self).__init__()
+        try:
+            workdir = os.getcwd()  # 把工作路径加入到代码执行里面
+            for path in sys.path:
+                if path == workdir:
+                    break
+            else:
+                sys.path.append(workdir)
+            mod = importlib.import_module(self.FASTAPI_SETTINGS_MODULE)
+        except Exception as e:
+            raise ImportError(f"导入settings报错:{e}")
+
+        for setting in dir(mod):
+            if setting.isupper():
+                setting_value = getattr(mod, setting)
+                if hasattr(self, setting):
+                    setattr(self, setting, setting_value)
+                else:
+                    self.EXTRA_SETTINGS[setting] = setting_value
+
+    def _init_model(self):
+        if not getattr(self, "TORTOISE_ORM"):
+            import warnings
+
+            warnings.warn("TORTOISE_ORM为空")
+        else:
+            init_model(self)
+
+
+settings = Settings()
+settings._init_model()
