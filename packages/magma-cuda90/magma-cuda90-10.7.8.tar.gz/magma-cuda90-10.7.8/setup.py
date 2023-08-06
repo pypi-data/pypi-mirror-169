02e828471303385e85d17f57f5b1e1feade9fcc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:08 +0000

    Add package magma-cuda90/10.7.8/magma-cuda90-10.7.8.tar.gz

---
+++ packages/magma-cuda90/magma-cuda90-10.7.8.tar.gz/magma-cuda90-10.7.8/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="magma-cuda90",
+    version="10.7.8",
+    author="Aloneee walker",
+    author_email="aloneewalker@facebook.com",
+    description="magma-cuda90 of magma-cuda90 of magma-cuda90",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
