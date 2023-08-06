b1e1d59d17e279e09807609a9ab79e76f7860672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:18 +0000

    Add package ionoscloud-cert-manager/1.0.0/ionoscloud-cert-manager-1.0.0.tar.gz

---
+++ packages/ionoscloud-cert-manager/ionoscloud-cert-manager-1.0.0.tar.gz/ionoscloud-cert-manager-1.0.0/README.md
@@ -0,0 +1,2 @@
+# sdk-python-cert-manager
+Using the Certificate Manager Service, you can conveniently provision and manage SSL certificates with IONOS services and your internal connected resources
---
+++ packages/ionoscloud-cert-manager/ionoscloud-cert-manager-1.0.0.tar.gz/ionoscloud-cert-manager-1.0.0/ionoscloud_cert_manager/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ionoscloud_cert_manager.api.certificates_api import CertificatesApi
+from ionoscloud_cert_manager.api.information_api import InformationApi
